8dee09be2296c5d61d84e24cf332584de3f7e3b4
salman-codewar <salmannaseem131@gmail.com>
2021-10-28 21:49:27 +0500

    Failed test fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 it("Dummy test ", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>,
+        div
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
