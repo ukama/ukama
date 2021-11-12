cea53a1aa649d5e7b0c8a204e589e1264e4cf82e
salman-codewar <salmannaseem131@gmail.com>
2021-11-12 13:07:53 +0500

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,62 +21,6 @@ class ApiMethods {
         }
         return res;
     };
-    putData = async (path: string, params?: any, headers?: any, body?: any) => {
-        const res = await axios.put(path, body, {
-            params,
-            headers: {
-                ...headers,
-                Accept: "*/*",
-            },
-        });
-        return res;
-    };
-    postData = async (
-        path: string,
-        params?: any,
-        headers?: any,
-        body?: any
-    ) => {
-        let res;
-        try {
-            res = await axios.post(path, body, {
-                params,
-                headers: {
-                    ...headers,
-                    Accept: "*/*",
-                },
-            });
-        } catch (error: any) {
-            res = error.response.data.message;
-            logger.error(error);
-        }
-        return res;
-    };
-    patchData = async (
-        path: string,
-        params?: any,
-        headers?: any,
-        body?: any
-    ) => {
-        const res = await axios.patch(path, body, {
-            params,
-            headers: {
-                ...headers,
-                Accept: "*/*",
-            },
-        });
-        return res;
-    };
-    deleteData = async (path: string, params?: any, headers?: any) => {
-        const res = await axios.delete(path, {
-            params,
-            headers: {
-                ...headers,
-                Accept: "*/*",
-            },
-        });
-        return res;
-    };
 }
 
 export default new ApiMethods();
