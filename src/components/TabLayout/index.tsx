8668e4279eb01654f1123164d5eea4ac9a3c00d7
salman-codewar <salmannaseem131@gmail.com>
2021-11-19 16:04:51 +0500

    Extra wrapper removed

--- src/components/TabLayout/index.tsx
+++ src/components/TabLayout/index.tsx
@@ -8,33 +8,32 @@ type TabLayoutProps = {
 
 const TabLayoutHeader = ({ tab, tabs, onTabChange }: TabLayoutProps) => {
     return (
-        <Box sx={{ width: "100%", typography: "body1" }}>
-            <Box
-                sx={{
-                    borderBottom: 2,
-                    borderColor: "rgba(196, 196, 196, 0.1)",
-                }}
+        <Box
+            sx={{
+                width: "100%",
+                borderBottom: 2,
+                borderColor: "rgba(196, 196, 196, 0.1)",
+            }}
+        >
+            <Tabs
+                value={tab}
+                onChange={(_, b) => onTabChange(b)}
+                aria-label="wrapped label tabs example"
             >
-                <Tabs
-                    value={tab}
-                    onChange={(_, b) => onTabChange(b)}
-                    aria-label="wrapped label tabs example"
-                >
-                    {tabs.map(({ id, label, value }: SelectItemType) => (
-                        <Tab
-                            key={id}
-                            value={value}
-                            label={label}
-                            wrapped
-                            sx={{
-                                p: "0px 36px",
-                                fontSize: "15px",
-                                fontWeight: 500,
-                            }}
-                        />
-                    ))}
-                </Tabs>
-            </Box>
+                {tabs.map(({ id, label, value }: SelectItemType) => (
+                    <Tab
+                        key={id}
+                        value={value}
+                        label={label}
+                        wrapped
+                        sx={{
+                            p: "0px 36px",
+                            fontSize: "15px",
+                            fontWeight: 500,
+                        }}
+                    />
+                ))}
+            </Tabs>
         </Box>
     );
 };
