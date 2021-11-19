b400c3257215f089fceeaa78d133a81f2e5c648c
Salman Naseem <83802574+salman-codewar@users.noreply.github.com>
2021-11-19 17:02:05 +0500

    TabLayoutHeader Component added and Consumed in Billing page (#72)
    * TabLayoutHeader Component added
    
    * Extra wrapper removed

---
+++ src/components/TabLayout/index.tsx
@@ -0,0 +1,41 @@
+import { Box, Tabs, Tab } from "@mui/material";
+import { SelectItemType } from "../../types";
+type TabLayoutProps = {
+    tab: string;
+    tabs: SelectItemType[];
+    onTabChange: Function;
+};
+
+const TabLayoutHeader = ({ tab, tabs, onTabChange }: TabLayoutProps) => {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                borderBottom: 2,
+                borderColor: "rgba(196, 196, 196, 0.1)",
+            }}
+        >
+            <Tabs
+                value={tab}
+                onChange={(_, b) => onTabChange(b)}
+                aria-label="wrapped label tabs example"
+            >
+                {tabs.map(({ id, label, value }: SelectItemType) => (
+                    <Tab
+                        key={id}
+                        value={value}
+                        label={label}
+                        wrapped
+                        sx={{
+                            p: "0px 36px",
+                            fontSize: "15px",
+                            fontWeight: 500,
+                        }}
+                    />
+                ))}
+            </Tabs>
+        </Box>
+    );
+};
+
+export default TabLayoutHeader;
