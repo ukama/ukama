49c572583ea912857c52b310e69a75f71f131261
Salman Naseem <83802574+salman-codewar@users.noreply.github.com>
2021-11-23 18:29:45 +0500

    Apollo client setup + codegen added (#75)

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,30 @@ import { theme } from "./theme";
 import Router from "./router/Router";
 import { isLoginAtom } from "./recoil";
 import { useRecoilValue } from "recoil";
+import client from "./api/ApolloClient";
 import { routes } from "./router/config";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
+import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
     const isLogin = useRecoilValue(isLoginAtom);
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <BrowserRouter>
-                {isLogin ? (
-                    <Layout>
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <BrowserRouter>
+                    {isLogin ? (
+                        <Layout>
+                            <Router routes={routes} />
+                        </Layout>
+                    ) : (
                         <Router routes={routes} />
-                    </Layout>
-                ) : (
-                    <Router routes={routes} />
-                )}
-            </BrowserRouter>
-        </ThemeProvider>
+                    )}
+                </BrowserRouter>
+            </ThemeProvider>
+        </ApolloProvider>
     );
 };
 
