f7ce9e7c732b4f9721d8d23031cf899ab92edffe
Kashif Ali <kashif@ukama.com>
2021-11-28 22:13:45 -0800

    Refactor the network creation script to be able to call from C program. Issue#92

--- distro/scripts/setup_space_network.sh
+++ distro/scripts/setup_space_network.sh
@@ -7,7 +7,14 @@
 # base parameters
 UKAMA_OS=../../
 DEF_BRIDGE="ukama_bridge"
-MIN_ARGS=2
+MIN_ARGS=4
+
+CMD="NaN"
+DEV="NaN"
+PID="NaN"
+IFACE="NaN"
+SPACE="NaN"
+BRIDGE="NaN"
 
 usage() {
     echo 'Usage: setup_space_network.sh interface space_name'
@@ -16,32 +23,87 @@ usage() {
 
 msg_usage() {
     echo "Usage:"
-    echo "      setup_space_network.sh [args]"
+    echo "      setup_space_network.sh --add [bridge | cspace] [args]"
     echo ""
-    echo "Args:"
-    echo "   pid         # PID of the cspace process"
-    echo "   interface   # Interface to use to connect with Internet"
-    echo "   space_name  # name of the space"
-    echo "   bridge_name # name to the master bridge (optional)"
-    exit 99
+    echo "Options:"
+    echo "   Dev=bridge:          # args when dev is 'bridge'"
+    echo "       interface        # Interface to use to connect with Internet"
+    echo "       bridge_name      # name to the master bridge"
+    echo "   Dev=cspace:          # args when dev is 'cspace'"
+    echo "       pid              # PID of the cspace process"
+    echo "       space_name       # name of space"
+    echo "       bridge_name      # name to valid master bridge"
+    exit 100
 }
 
 valid_args() {
 
-    # Test valid PID
-    ps -p ${PID} > /dev/null
-    if [ "$?" -eq "1" ]
-    then
-	exit 1
+    # --add bridge iface bridge_name
+    if [ "${DEV}" -eq "cspace" ]; then
+	# Test valid PID
+	ps -p ${PID} > /dev/null
+	if [ "$?" -eq "1" ]
+	then
+	    exit 101
+	fi
     fi
-      
-    # Test valid interface
-    ip -o a show | cut -d ' ' -f 2,7 | \
-	awk '{print $1}' | grep ${IFACE} > /dev/null
-    if [ "$?" -eq "1" ]
-    then
-	exit 2
+
+    # --add cspace pid space_name bridge_name
+    if [ "${DEV}" -eq "bridge" ]; then
+	# Test valid interface
+	ip -o a show | cut -d ' ' -f 2,7 | \
+	    awk '{print $1}' | grep ${IFACE} > /dev/null
+	if [ "$?" -eq "1" ]
+	then
+	    exit 102
+	fi
     fi
+}
+
+add_bridge() {
+
+    IF=$1
+    BR=$2
+
+    # Setup bridge
+    ip link add ${BR} type bridge
+
+    # Setup host machine to allow NATing.
+    iptables -t nat -A POSTROUTING -o ${BR} -j MASQUERADE
+    iptables -t nat -A POSTROUTING -o ${IF} -j MASQUERADE
+}
+
+add_cspace() {
+
+    ID=$1
+    SP=$2
+    BR=$3
+
+    NS=net_${SP}
+
+    # Setup paired veth for each cspace on the host
+    ip link   add dev veth1_${SP} type veth peer name veth2_${SP}
+
+    # Bring up the host iface
+    ip link   set dev veth1_${SP} up
+    ip tuntap add tap_${SP} mode tap
+    ip link   set dev tapm_${SP} up
+
+    # setup named network namespace and attach to cspace PID
+    ip netns add ${NS}
+    ip netns attach ${NS} ${ID}
+
+    # Move the veth2 into network namespace
+    ip link set veth2_${SP} netns ${NS}
+
+    # Enable loopback interface on the new namespace
+    ip netns exec ${NS} ip link set dev lo up
+
+    # Setup the veth2 on the cspace
+    ip netns ${NS} addr add 10.0.0.2/24 dev veth2_${SP}
+    ip netns ${NS} link set dev veth2_${SP} up
+    ip netns ${NS} route add default via 10.0.0.1
+
 }
 
 # Script main
@@ -51,68 +113,40 @@ then
     msg_usage
 fi
 
-PID=$1
-IFACE=$2
-SPACE=$3
-BRIDGE=$4
-NET_NS=ns_${SPACE}
+if [ "$#" -gt 0 ]; then
 
-if [ -z "${BRIDGE}"]
-then
-    BRIDGE=${DEF_BRIDGE}
+    case $1 in
+	-a|--add)
+	    DEV=$2
+
+	    if [ "${DEV}" -eq "bridge" ]; then
+		IFACE=$3
+		BRIDGE=$4
+	    elif [ "${DEV}" -eq "cspace" ]; then
+		PID=$3
+		SPACE=$4
+		BRIDGE=$5
+	    else
+		exit 100
+	    fi
+	    shift
+	    ;;
+	*)
+	    exit 100
+    esac
 fi
 
 # Test PID and IFACE are valid
 valid_args
 
-# Basic flow is:
-#
-#  setup paired veth device (veth1_* for host and veth2_* for cspace)
-#  setup bridge and connect the host veth to it as master
-#  add network namespace and attach it to the cspace PID
-#  move the veth2_* to the network namespace
-#  bring up loopback in the network namespace
-#  setup host machine to allow NATing
-#  finally, setup veth2_* in the network namespace and setup routing
-#
-
-# Setup paired veth for each cspace on the host.
-ip link   add dev veth1_${SPACE} type veth peer name veth2_${SPACE}
-
-# Bring up the host iface
-ip link   set dev veth1_${SPACE} up
-ip tuntap add tap_${SPACE} mode tap
-ip link   set dev tapm_${SPACE} up
-
-# Setup bridge
-ip link   add $(BRIDGE} type bridge
-
-# Attach ifaces to the bridge
-ip link set tap_${SPACE}   master ${BRIDGE}
-ip link set veth1_${SPACE} master ${BRIDGE}
-
-# Give address to the bridge
-ip addr add 10.0.0.1/24 dev ${BRIDGE}
-
-# setup named network namespace and attach to cspace PID
-ip netns add ${NET_NS}
-ip netns attach ${NET_NS} ${PID}
-
-# Move the veth2 into network namespace
-ip link set veth2_${SPACE} netns ${NET_NS}
-
-# Enable loopback interface on the new namespace
-ip netns exec ${NET_NS} ip link set dev lo up
-
-# Setup host machine to allow NATing.
-iptables -t nat -A POSTROUTING -o ${BRIDGE} -j MASQUERADE
-iptables -t nat -A POSTROUTING -o ${IFACE}  -j MASQUERADE
-
-# Setup the veth2 on the cspace.
-ip netns ${NET_NS} addr add 10.0.0.2/24 dev veth2_${CSPACE}
-ip netns ${NET_NS} link set dev veth2_${CSPACE} up
-ip netns ${NET_NS} route add default via 10.0.0.1
-
-echo "Done"
+if [ "$CMD" -eq "add" ]; then
+    if [ "${DEV}" -eq "bridge" ]; then
+	add_bridge $IFACE $BRIDGE
+    elif [ "${DEV}" -eq "cspace" ]; then
+	add_cspace $PID $SPACE $BRIDGE
+    else
+	exit 100
+    fi
+fi
 
 exit 0
