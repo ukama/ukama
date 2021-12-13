41d34989a43789c38485712fbab3e3a861da6de8
salman-codewar <salmannaseem131@gmail.com>
2021-12-13 21:15:32 +0500

    Update Authentication.ts

--- src/common/Authentication.ts
+++ src/common/Authentication.ts
@@ -5,15 +5,12 @@ import {
 import { ResolverData } from "type-graphql/dist/interfaces/ResolverData";
 import { Context } from "./types";
 import { Service } from "typedi";
-import { HTTP401Error, Messages } from "../errors";
 
 @Service()
 export class Authentication implements MiddlewareInterface<Context> {
     async use({ context }: ResolverData<Context>, next: NextFn): Promise<void> {
-        if (!context.req.headers.cookie)
-            throw new HTTP401Error(Messages.ERR_REQUIRED_HEADER_NOT_FOUND);
-
-        context.cookie = context.req.headers.cookie;
+        if (context.req.headers.cookie)
+            context.cookie = context.req.headers.cookie;
         return next();
     }
 }
