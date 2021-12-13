995473be335526246a9bee0827abe755542b538d
salman-codewar <salmannaseem131@gmail.com>
2021-12-13 22:12:42 +0500

    Cookie header remove

--- src/api/ApolloClient/index.tsx
+++ src/api/ApolloClient/index.tsx
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
     uri: process.env.REACT_APP_API,
     cache: new InMemoryCache(),
-    credentials: "include",
+    // credentials: "include",
     headers: {
         "Content-Type": "application/json",
     },
