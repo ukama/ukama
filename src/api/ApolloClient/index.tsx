c0b401454f453d52feb691592496f0fea580e5dd
salman-codewar <salmannaseem131@gmail.com>
2021-12-13 19:00:16 +0500

    Content type header added

--- src/api/ApolloClient/index.tsx
+++ src/api/ApolloClient/index.tsx
@@ -4,6 +4,9 @@ const client = new ApolloClient({
     uri: process.env.REACT_APP_API,
     cache: new InMemoryCache(),
     credentials: "include",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export default client;
