13a46eabd5af106f2d222a3d8966f1aee64e6167
salman-codewar <salmannaseem131@gmail.com>
2021-12-16 20:45:02 +0500

    Headers updated

--- src/common/Authentication.ts
+++ src/common/Authentication.ts
@@ -5,15 +5,14 @@ import {
 import { ResolverData } from "type-graphql/dist/interfaces/ResolverData";
 import { Context } from "./types";
 import { Service } from "typedi";
-// import { HTTP401Error, Messages } from "../errors";
+import { HTTP401Error, Messages } from "../errors";
 
 @Service()
 export class Authentication implements MiddlewareInterface<Context> {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async use({ context }: ResolverData<Context>, next: NextFn): Promise<void> {
-        // if (!context.req.headers.cookie)
-        //     throw new HTTP401Error(Messages.ERR_REQUIRED_HEADER_NOT_FOUND);
-        // context.cookie = context.req.headers.cookie;
+        if (!context.req.headers.cookie)
+            throw new HTTP401Error(Messages.ERR_REQUIRED_HEADER_NOT_FOUND);
+        context.cookie = context.req.headers.cookie;
         return next();
     }
 }
