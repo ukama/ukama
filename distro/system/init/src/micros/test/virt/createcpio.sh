a07e102d70f5dfe2602b89ea933860a5e3605c2d
Kashif Ali <kashif@ukama.com>
2021-12-17 02:58:38 -0800

    Updated test script to create cpio image. Issue #102

--- distro/system/init/src/micros/test/virt/createcpio.sh
+++ distro/system/init/src/micros/test/virt/createcpio.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
+
 # Only meant for test purpose. This temporary test file will be replaced by ukamaOS virtual test suit which
 # will mock devices.   
 
 #Run script with sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
 #Cleaning files
 echo "Cleaning microfs...!"
-sudo rm -rf ./microfs/init ./microfs/bin/rc.init ./microfs/usr/bin/usysinit
+sudo rm -rf ./microfs/preInit ./microfs/init ./microfs/sbin/sysInit ./microfs/sbin/init
 sync
 
 #Copying file
 echo "Copying init files...!"
-sudo cp -v ../../init/target/x86_64-unknown-linux-musl/release/init ./microfs/init
-sudo cp -v ../../rcinit/target/x86_64-unknown-linux-musl/release/rcinit ./microfs/bin/rc.init
-sudo cp -v ../../sysinit/config/microInit.toml ./microfs/etc/microInit.toml
-sudo cp -v ../../sysinit/target/x86_64-unknown-linux-musl/release/sysinit ./microfs/usr/bin/usysinit
+sudo cp -v ../../../../init ./microfs/sbin/init
+sudo cp -v ../../preInit/target/x86_64-unknown-linux-musl/release/preInit ./microfs/preInit
+sudo cp -v ../../sysInit/target/x86_64-unknown-linux-musl/release/sysInit ./microfs/sbin/sysInit
 sync
 
+# create missing dir
+sudo mkdir ./microfs/tmp ./microfs/var/log ./microfs/mnt ./microfs/sys
 
 #Creating archive
 cd ./microfs/
@@ -33,5 +35,4 @@ sudo find . | cpio --quiet -H newc -o | gzip -9 -n > ../microfs.img;
 sync
 
 #Help to start qemu
-echo "Use command: sudo qemu-system-x86_64 -m 512M -kernel ./virt-kernel  -initrd ./microfs.img -append \"console=ttyAMA0 console=tty0 console=ttyS0 rdinit=/init\" -serial stdio"
-
+echo "Use command: sudo qemu-system-x86_64 -m 512M -kernel ./virt-kernel  -initrd ./microfs.img -append \"console=ttyAMA0 console=tty0 console=ttyS0 rdinit=/preInit\" -serial stdio"
