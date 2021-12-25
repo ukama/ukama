20e7ceb552aeb306bd64077460d644f19c88cc21
Kashif Ali <kashif@ukama.com>
2021-12-24 22:47:30 -0800

    Script to build ukamaOS image (#107)

---
+++ distro/scripts/buildOS.sh
@@ -0,0 +1,460 @@
+#!/bin/sh
+# Copyright (c) 2021-present, Ukama Inc.
+# All rights reserved.
+
+# Script to generate ukamaOS bootable image with minimal rootfs
+
+# Build busybox
+# Build init (and microinits)
+# Build lxce.d
+# Build dhcpcd
+# Build sysctl
+# Copy all lib dependencies
+# create cpio arch
+
+# Base parameters
+UKAMA_OS=`realpath ../../.`
+SYS_ROOT=${UKAMA_OS}/distro/system
+SCRIPTS_ROOT=${UKAMA_OS}/distro/scripts
+BB_ROOT=${SYS_ROOT}/busybox
+INIT_ROOT=${SYS_ROOT}/init
+LXCE_ROOT=${SYS_ROOT}/lxce
+MICRO_INIT_ROOT=${INIT_ROOT}/src/micros
+
+# Release related parameters
+PREINIT_REL=${MICRO_INIT_ROOT}/preInit/target/x86_64-unknown-linux-musl/release
+SYSINIT_REL=${MICRO_INIT_ROOT}/sysInit/target/x86_64-unknown-linux-musl/release
+
+# Build config parameters
+BB_CONFIG=ukama_minimal_defconfig
+
+# command line arguments
+MIN_ARGS=2
+DEF_ROOTFS=_ukama_os_rootfs/
+
+# For os-release
+OS_NAME="ukamaOS"
+OS_ID="ukama"
+OS_VERSION_ID="0.0.1"
+OS_PRETTY_NAME="UkamaOS V0.0.1"
+OS_HOME_URL="http://www.ukama.com/"
+
+#Various network related parameters
+HOSTNAME="localhost"
+
+LIGHT_RED='\e[31m'
+NO_COLOR='\e[0m'
+
+# default target is local machine (gcc)
+DEF_TARGET="local"
+TARGET=${DEF_TARGET}
+
+# default rootfs location is ${DEF_ROOTFS}
+ROOTFS=`realpath ${DEF_ROOTFS}`
+
+log_info() {
+    echo "Info: $1"
+}
+
+log_error() {
+    echo "${LIGHT_RED}Error:${NO_COLOR} $1"
+}
+
+copy_file() {
+    SRC=$1
+    DST=$2
+
+    if [ -d "${DST}" ]; then
+	if [ -f "${SRC}" ]; then
+	    cp $SRC $DST
+	else
+	    log_error "${SRC} not found. Exiting"
+	    exit 1
+	fi
+    else
+	log_error "${DST} not found. Exiting"
+	exit 1
+    fi
+}
+
+usage() {
+    echo 'Usage: buildOS.sh -t <build_target> -p <path_for_rootfs>'
+    exit
+}
+
+msg_usage() {
+    echo "Usage:"
+    echo "      buildOS.sh [options]"
+    echo ""
+    echo "Options:"
+    echo "     -t target      # Target is local(default), cnode, anode, etc."
+    echo "     -p string      # Path for minimal rootfs, e.g. _ukama_os_rootfs"
+    echo "     -h             # Display this help message."
+    exit
+}
+
+#
+# copy_all_libs
+#
+copy_all_libs() {
+
+    ARGS=$1
+
+    log_info "Copying lib for: ${ARGS}"
+    
+    for BIN in ${ARGS}
+    do
+	for lib in $(ldd ${BIN} | cut -d '>' -f2 | awk '{print $1}')
+	do
+	    if [ -f "${lib}" ]; then
+		cp --parents "${lib}" ${ROOTFS}
+		cp "${lib}" ${ROOTFS}/lib
+	    fi
+	done
+    done
+}
+
+#
+# Build system init
+#
+build_init() {
+    CWD=`pwd`
+
+    # setup proper compiler option
+    if [ "${TARGET}" != "local" ]
+    then
+	XGCC_PATH=${UKAMA_OS}/distro/tools/musl-cross-make/output/bin
+    else
+	XGCC_PATH=`which gcc | awk 'BEGIN{FS=OFS="/"}{NF--; print}'`
+    fi
+
+    # build and copy init micros
+    cd ${MICRO_INIT_ROOT}
+    make clean; make
+    copy_file ${PREINIT_REL}/preInit ${ROOTFS}/boot/
+    copy_file ${SYSINIT_REL}/sysInit ${ROOTFS}/boot/
+
+    # now build main init
+    cd ${INIT_ROOT}
+    make clean; make XGCCPATH=${XGCC_PATH}/
+    copy_file ${INIT_ROOT}/init $ROOTFS/boot
+
+    # Go back and clean up
+    cd ${MICRO_INIT_ROOT}; make clean
+    cd ${INIT_ROOT}; make clean
+    cd ${CWD}
+
+    log_info "init successfully build" 
+}
+
+#
+# Build lxce.d
+#
+build_lxce() {
+   CWD=`pwd`
+
+   # setup proper compiler option
+   if [ "${TARGET}" != "local" ]
+   then
+       XGCC_PATH=${UKAMA_OS}/distro/tools/musl-cross-make/output/bin
+   else
+       XGCC_PATH=`which gcc | awk 'BEGIN{FS=OFS="/"}{NF--; print}'`
+   fi
+
+   cd ${LXCE_ROOT}
+   make clean; make XGCCPATH=${XGCC_PATH}/
+   copy_file ${LXCE_ROOT}/lxce.d $ROOTFS/sbin
+
+   # Go back and clean up
+   cd ${LXCE_ROOT}; make clean
+   cd ${CWD}
+
+   log_info "lxce.d successfully build"
+}
+
+#
+# Build busybox using the Ukama minimal configuration
+#
+build_busybox() {
+    CWD=`pwd`
+    cd ${BB_ROOT}
+
+    # set the config file for BB build
+    BB_CONFIG=ukama_minimal_defconfig
+    #Execute make and copy conent of _ukamafs to rootfs
+
+    mkdir -p ${BB_ROOT}/${BB_ROOTFS}
+
+    # setup proper compiler option
+    if [ "${TARGET}" != "local" ]
+    then
+	XGCC_PATH=${UKAMA_OS}/distro/tools/musl-cross-make/output/bin
+    else
+	XGCC_PATH=`which gcc | awk 'BEGIN{FS=OFS="/"}{NF--; print}'`
+    fi
+
+    make XGCCPATH=${XGCC_PATH}/ BBCONFIG=${BB_CONFIG} \
+	 ROOTFSPATH=${BB_ROOT}/${BB_ROOTFS}
+
+    if [ $? -ne 0 ]
+    then
+       log_error "Busybox compliation failed"
+       exit 1
+    fi
+
+    cd ${CWD}
+    cp -rf ${BB_ROOT}/${BB_ROOTFS}/* $ROOTFS
+
+    # Go back and clean up
+    cd ${BB_ROOT}
+    make clean XGCCPATH=${XGCC_PATH}/
+    cd ${CWD}
+
+    log_info "Busybox successfully build"
+}
+
+#
+# Build the usr directory structure
+#
+build_usr_dirs() {
+
+    DIRS="bin"
+    DIRS="lib $DIRS"
+    DIRS="local/bin   ${DIRS}"
+    DIRS="local/lib   ${DIRS}"
+    DIRS="local/share ${DIRS}"
+    DIRS="sbin        ${DIRS}"
+    DIRS="share/misc  ${DIRS}"
+
+    cd ./usr
+    mkdir -p ${DIRS}
+    cd ../
+}
+
+#
+# Build the etc directory structure
+#
+build_etc_dirs() {
+
+    DIRS="network/if-down.d"
+    DIRS="network/if-post-down.d ${DIRS}"
+    DIRS="network/if-post-up.d   ${DIRS}"
+    DIRS="network/if-pre-down.d  ${DIRS}"
+    DIRS="network/if-pre-up.d    ${DIRS}"
+    DIRS="network/if-up.d        ${DIRS}"
+
+    cd ./etc
+    mkdir -p ${DIRS}
+    cd ../
+}
+
+#
+# Build rootfs directory structure
+#
+build_rootfs_dirs() {
+
+    DIRS="boot"
+    DIRS="bin  ${DIRS}"
+    DIRS="sbin ${DIRS}"
+    DIRS="etc  ${DIRS}"
+    DIRS="home ${DIRS}"
+    DIRS="lib  ${DIRS}"
+    DIRS="mnt  ${DIRS}"
+    DIRS="tmp  ${DIRS}"
+    DIRS="sys  ${DIRS}"
+    DIRS="usr  ${DIRS}"
+    DIRS="var  ${DIRS}"
+    DIRS="dev  ${DIRS}"
+    DIRS="conf ${DIRS}"
+    DIRS="mnt  ${DIRS}"
+    DIRS="var/log ${DIRS}"
+
+    cd ${ROOTFS}
+    mkdir -p ${DIRS}
+    build_etc_dirs
+    build_usr_dirs
+
+    cd ../
+
+    log_info "Building rootfs directory structure at: ${ROOTFS}"
+}
+
+#
+# setup /etc content
+#
+setup_etc() {
+
+    cd ${ROOTFS}/etc/
+
+    printf "${OS_VERSION_ID}\n" > ./ukama-release
+    printf "${HOSTNAME}\n"   > ./hostname
+
+    printf "127.0.0.1  ${HOSTNAME} ${HOSTNAME}.localdomain\n" > ./hosts
+    printf "::1        ${HOSTNAME} ${HOSTNAME}.localdomain\n" >> ./hosts
+
+    printf "Welcome to ${OS_NAME}\n" > ./issue
+
+    cat << EOF > ./motd
+The programs included with the ${OS_NAME} Linux system are free software;
+the exact distribution terms for each program are described in the
+individual files in /usr/share/doc/*/copyright.
+${OS_NAME} Linux comes with ABSOLUTELY NO WARRANTY, to the extent
+permitted by applicable law.
+EOF
+
+    cat <<EOF > ./network/if-up.d/dad
+#!/bin/sh
+
+# Block ifup until DAD completion
+# Copyright (c) 2016-2018 Kaarle Ritvanen
+
+has_flag() {
+	ip address show dev $IFACE | grep -q " $1 "
+}
+
+while has_flag tentative && ! has_flag dadfailed; do
+	sleep 0.2
+done
+EOF
+
+    cat <<EOF > ./os-release 
+NAME=${OS_NAME}
+ID=${OS_ID}
+VERSION_ID=${OS_VERSION_ID}
+PRETTY_NAME=${OS_PRETTY_NAME}
+HOME_URL=${OS_HOME_URL}
+EOF
+
+    printf "root:x:0:0:root:/root:/bin/sh\nservice:x:1:1:service:/var/www/html:/usr/sbin/nologin \n" > ./passwd
+    printf "ukama:x:1000:1000:Linux User,,,:/home/ukama:/bin/sh \n" >> ./passwd
+
+    printf "root:mTteXHTdcIbEc:17743::::::\n" > ./shadow
+    printf "ukama:mTteXHTdcIbEc:18585:0:99999:7:::\n" >> ./shadow
+
+    printf "root:x:0:root\nservice:x:1:service\n" > ./group
+    printf "ukama:x:1000:\n" >> ./group
+
+    printf "/bin/sh\n" > ./shells
+
+    cat <<EOF > ./securetty
+console
+tty1
+tty2
+tty3
+tty4
+tty5
+tty6
+tty7
+tty8
+tty9
+tty10
+tty11
+EOF
+
+    cp ../../files/protocols ./protocols
+    cp ../../files/services  ./services
+
+    cd ../../
+}
+
+#
+# Script main
+#
+WD=`pwd`
+while [ "$#" -gt 0 ]; do
+    case $1 in
+	-p|--path)
+	    if [ -z "$2" ]
+	    then
+		log_info "Missing rootfs parameter for -p"
+		log_info "Setting to default: ${DEF_ROOTFS}"
+		ROOTFS=${DEF_ROOTFS}
+	    else
+                ROOTFS=$2
+                log_info "ukamaOS RootFS Path is: ${ROOTFS}"
+                shift # Remove path from processing
+	    fi
+            shift
+            ;;
+        -h|--help)
+            echo "Help message"
+            msg_usage
+            shift
+            ;;
+	-t|--target)
+	    if [ -z "$2" ]
+	    then
+		log_info "Missing target parameter for -t"
+		log_info "Setting to default: ${DEF_TARGET}"
+		TARGET=${DEF_TARGET}
+	    else
+		TARGET=$2
+		log_info "Target is: ${TARGET}"
+		shift
+	    fi
+	    shift
+	    ;;
+        *)
+            log_error "Invalid args: ${1}"
+            msg_usage
+            shift # Remove generic argument from processing
+            ;;
+    esac
+done
+
+if [ -z ${ROOTFS} ]
+then
+    log_info "-p not defined. Setting to default: ${DEF_ROOTFS}"
+    ROOTFS=${DEF_ROOTFS}
+fi
+
+#setup rootfs location
+if [ -d "${ROOTFS}" ]
+then
+    log_error "Please remove existing copy of ${ROOTFS}"
+    exit
+fi
+
+mkdir -p ${ROOTFS}
+BB_ROOTFS=${ROOTFS}
+
+log_info "Setting up rootfs directory structure"
+build_rootfs_dirs
+
+log_info "Building busy box with Ukama minimal configuration"
+build_busybox
+
+log_info "Building system init, micros and lxce.d"
+build_init
+build_lxce
+
+log_info "Setting up /etc contents under rootfs"
+setup_etc
+
+log_info "Copying all lib for executables"
+EXEC="${ROOTFS}/sbin/busybox"
+EXEC="${ROOTFS}/boot/preInit ${EXEC}"
+EXEC="${ROOTFS}/boot/sysInit ${EXEC}"
+EXEC="${ROOTFS}/boot/init    ${EXEC}"
+EXEC="${ROOTFS}/sbin/lxce.d  ${EXEC}"
+copy_all_libs ${EXEC}
+
+# Change ownership and create archieve
+log_info "Changing ownership and creating cpio archive"
+cd ${ROOTFS}
+sudo chown -R root:root *
+find . | cpio  --quiet -H newc -o | gzip -9 -n > ${WD}/ukamaOS.img
+cd ${WD}
+
+TOTAL_ROOTFS_SIZE=`du -chs ${ROOTFS} | awk '{print $1}' | uniq`
+IMAGE_SIZE=`du -kh ${WD}/ukamaOS.img | cut -f1`
+IMAGE_LOC=`realpath ${WD}/ukamaOS.img`
+
+log_info "All done. Have fun!"
+log_info "------------------"
+log_info "  Rootfs loc:   ${ROOTFS}"
+log_info "  Rootfs size:  ${TOTAL_ROOTFS_SIZE}"
+log_info "  ukamaOS loc:  ${IMAGE_LOC}"
+log_info "  ukamaOS size: ${IMAGE_SIZE}"
+
+exit
