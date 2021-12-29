6ea14f5fdfb32e9801efb0f603cf54d7fae4ac32
salman-codewar <salmannaseem131@gmail.com>
2021-12-29 16:46:43 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -34,13 +34,21 @@ const initializeApp = async () => {
     server.installSubscriptionHandlers(httpServer);
 
     app.get("/playground", (req, res) => {
-        const playgroundRenderPageOptions: PlaygroundRenderPageOptions = {
-            endpoint: "/graphql",
-        };
+        if (hasSession(req.headers.cookie || "")) {
+            const playgroundRenderPageOptions: PlaygroundRenderPageOptions = {
+                endpoint: "/graphql",
+            };
 
-        const playground = renderPlaygroundPage(playgroundRenderPageOptions);
-        res.write(playground);
-        res.end();
+            const playground = renderPlaygroundPage(
+                playgroundRenderPageOptions
+            );
+            res.write(playground);
+            res.end();
+        } else {
+            res.cookie("redirect", "https://bff.dev.ukama.com/playground");
+            res.redirect("https://auth.dev.ukama.com/auth/login");
+            res.end();
+        }
     });
 
     app.get("/ping", (req, res) => {
