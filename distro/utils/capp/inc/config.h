58fef9a51cf0f045a15c0b57ad44b296b7ddaf29
Kashif Ali <kashif@ukama.com>
2022-01-10 01:03:01 -0800

    Initial implementation of capp utility. Handle config file. (#100)

---
+++ distro/utils/capp/inc/config.h
@@ -0,0 +1,90 @@
+/*
+ * config.h
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Various table defs */
+#define TABLE_BUILD         "build"
+#define TABLE_CAPP          "capp"
+#define TABLE_BUILD_FROM    "build.from"
+#define TABLE_BUILD_COMPILE "build.compile"
+#define TABLE_BUILD_ROOTFS  "build.rootfs"
+#define TABLE_BUILD_CONF    "build.conf"
+#define TABLE_CAPP_EXEC     "capp.exec"
+#define TABLE_CAPP_OUTPUT   "capp.output"
+
+/* Keys for various table */
+#define KEY_ROOTFS    "rootfs"
+#define KEY_CONTAINED "contained"
+#define KEY_VERSION   "version"
+#define KEY_STATIC    "static"
+#define KEY_SOURCE    "source"
+#define KEY_CMD       "cmd"
+#define KEY_BIN_FROM  "bin_from"
+#define KEY_BIN_TO    "bin_to"
+#define KEY_MKDIR     "mkdir"
+#define KEY_FROM      "from"
+#define KEY_TO        "to"
+#define KEY_FORMAT    "format"
+#define KEY_EXEC      "exec"
+#define KEY_PATH      "path"
+#define KEY_ARGS      "args"
+#define KEY_ENVS      "envs"
+#define KEY_NAME      "name"
+
+#define DATUM_INT       0x01
+#define DATUM_STRING    0x02
+#define DATUM_MANDATORY 0x04
+
+#define BUILD_ONLY 0x01
+#define CAPP_ONLY  0x02
+
+#define MAX_ERR_BUFFER 256
+
+#define TRUE  1
+#define FALSE 0
+
+typedef struct build_config_t {
+  
+  /* from */
+  char *rootfs;
+  char *contained;
+
+  /* build */
+  char *version;
+  int  staticFlag;
+  char *source;
+  char *cmd;
+  char *binFrom;
+  char *binTo;
+
+  /* rootfs */
+  char *mkdir;
+
+  /* conf */
+  char *from;
+  char *to;
+}BuildConfig;
+
+typedef struct capp_config_t {
+
+  char *name;   /* Name of the binary file */
+  char *path;   /* Absolute path to the bin */
+  char *args;   /* Runtime arguments, if any */
+  char *envs;   /* Environment variables, if any */
+  char *format; /* Output format */
+}CappConfig;
+
+typedef struct config_t {
+
+  CappConfig  *capp;
+  BuildConfig *build;
+} Config;
+
+/* Function headers */
+int read_config_file(Config *config, char *fileName);
+void clear_config(Config *config, int flag);
+
+#endif /* CONFIG_H */
---
+++ distro/utils/capp/src/main.c
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2021-present, Ukama Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the XXX-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/*
+ * capp - utility to create capp from source code.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+
+#include "config.h"
+#include "log.h"
+
+#define VERSION       "0.0.1"
+#define DEF_LOG_LEVEL "TRACE"
+
+enum {
+  CAPP_CMD_NONE=0,
+  CAPP_CMD_VERIFY,
+  CAPP_CMD_CREATE,
+};
+
+/*
+ * usage -- 
+ *
+ */
+void usage() {
+
+  printf("Usage: [options] \n");
+  printf("Options:\n");
+  printf("--h, --help                         Help menu.\n");
+  printf("--c, --create                       Create capp\n");
+  printf("--v, --verify                       Verify config file\n");
+  printf("--C, --config                       Config file name\n");
+  printf("--l, --level <ERROR | DEBUG | INFO> Logging levels\n");
+  printf("--V, --version                      Version.\n");
+}
+
+/* Set the verbosity level for logs. */
+void set_log_level(char *slevel) {
+
+  int ilevel = LOG_TRACE;
+
+  if (!strcmp(slevel, "DEBUG")) {
+    ilevel = LOG_DEBUG;
+  } else if (!strcmp(slevel, "INFO")) {
+    ilevel = LOG_INFO;
+  } else if (!strcmp(slevel, "ERROR")) {
+    ilevel = LOG_ERROR;
+  }
+
+  log_set_level(ilevel);
+}
+
+int main (int argc, char *argv[]) {
+
+  int cmd=CAPP_CMD_NONE, ret=FALSE;
+  char *configFile=NULL;
+  char *debug=DEF_LOG_LEVEL;
+  Config *config=NULL;
+  
+  /* Prase command line args. */
+  while (TRUE) {
+
+    int opt = 0;
+    int opdidx = 0;
+
+    static struct option long_options[] = {
+      { "create",    no_argument,       0, 'c'},
+      { "verify",    no_argument,       0, 'v'},
+      { "config",      required_argument, 0, 'C'},
+      { "level",     required_argument, 0, 'l'},
+      { "help",      no_argument,       0, 'h'},
+      { "version",   no_argument,       0, 'V'},
+      { 0,           0,                 0,  0}
+    };
+
+    opt = getopt_long(argc, argv, "C:cvhV:", long_options, &opdidx);
+    if (opt == -1) {
+      break;
+    }
+
+    switch (opt) {
+    case 'h':
+      usage();
+      exit(0);
+      break;
+
+    case 'c':
+      cmd = CAPP_CMD_CREATE;
+      break;
+
+    case 'v':
+      cmd = CAPP_CMD_VERIFY;
+      break;
+
+    case 'C':
+      configFile = optarg;
+      break;
+      
+    case 'l':
+      debug = optarg;
+      set_log_level(debug);
+      break;
+
+    case 'V':
+      fprintf(stdout, "capp - Version: %s\n", VERSION);
+      exit(0);
+
+    default:
+      usage();
+      exit(0);
+    }
+  } /* while */
+
+  if (argc == 1 || cmd == CAPP_CMD_NONE) {
+    fprintf(stderr, "Missing required parameters.\n");
+    usage();
+    exit(1);
+  }
+
+  config = (Config *)calloc(1, sizeof(Config));
+  if (!config) {
+    log_error("Memory allocation error of size: %d Exiting", sizeof(Config));
+    exit(1);
+  }
+  
+  ret = read_config_file(config, configFile);
+  if (!ret) {
+    log_error("%s parsing error. Exiting.", configFile);
+    clear_config(config, BUILD_ONLY & CAPP_ONLY);
+    exit(1);
+  }
+
+  return 0;
+}
