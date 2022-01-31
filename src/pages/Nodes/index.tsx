dc3854bcd61f9939116c5bbdabd0ba3dbd75d6ac
brackleycassinga <pcassinga@gmail.com>
2022-01-31 10:55:49 +0200

    Get list of nodes byOrg added to NodePage  header #125

--- src/pages/Nodes/index.tsx
+++ src/pages/Nodes/index.tsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
 import { Box, Grid } from "@mui/material";
-import { NodeDetails, NodeStatus } from "../../components";
-import { NodeDetailsStub, NodesData } from "../../constants/stubData";
+import { NodeDetails, NodeStatus, LoadingWrapper } from "../../components";
+import { NodeDetailsStub } from "../../constants/stubData";
+import { useGetNodesByOrgQuery } from "../../generated";
+import { useRecoilValue } from "recoil";
+import { isSkeltonLoading, organizationId } from "../../recoil";
 
 const Nodes = () => {
     const [selectedNodeIndex, setSelectedNodeIndex] = useState(0);
+    const orgId = useRecoilValue(organizationId);
+    const isSkeltonLoad = useRecoilValue(isSkeltonLoading);
+    const { data: nodesRes, loading: nodesLoading } = useGetNodesByOrgQuery({
+        variables: { orgId: orgId || "" },
+    });
 
     return (
         <Box sx={{ height: "calc(100vh - 8vh)" }}>
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container spacing={3}>
                     <Grid xs={12} item>
-                        <NodeStatus
-                            nodes={NodesData}
-                            selectedNodeIndex={selectedNodeIndex}
-                            setSelectedNodeIndex={setSelectedNodeIndex}
-                        />
+                        <LoadingWrapper
+                            width="100%"
+                            height="30px"
+                            isLoading={isSkeltonLoad || nodesLoading}
+                        >
+                            <NodeStatus
+                                nodes={nodesRes?.getNodesByOrg?.nodes}
+                                selectedNodeIndex={selectedNodeIndex}
+                                setSelectedNodeIndex={setSelectedNodeIndex}
+                            />
+                        </LoadingWrapper>
                     </Grid>
                     <Grid xs={12} item container spacing={3}>
                         <NodeDetails detailsList={NodeDetailsStub} />
