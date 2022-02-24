d0de5fce355ae51bc393088a1ef7e74d702c8ded
vthakur7f <vthakur7@hotmail.com>
2022-02-23 22:17:07 -0800

    Timer implementation.

---
+++ distro/platform/sys/inc/usys_timer.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2020-present, Ukama.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef USYS_TIMER_H
+#define USYS_TIMER_H
+
+#include "usys_types.h"
+
+/**
+ * @fn bool sp_start_sys_timer(uint32_t, void(*)())
+ * @brief
+ *
+ * @param  resolution
+ * @param  tick_handler
+ * @return On Success true
+ *         On Failure false
+ */
+bool sp_start_sys_timer(uint32_t resolution, void (*tick_handler)());
+
+
+#endif /* USYS_TIMER_H */
