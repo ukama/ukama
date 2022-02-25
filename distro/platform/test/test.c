b70274e7875d972c253438f5ea3e5929666ef3d3
vthakur7f <vthakur7@hotmail.com>
2022-02-24 23:31:07 -0800

    Added a testcase for file operations.

--- distro/platform/test/test.c
+++ distro/platform/test/test.c
@@ -11,6 +11,7 @@
 
 #include "unity.h"
 #include "usys_api.h"
+#include "usys_string.h"
 
 void setUp(void) {
     // set stuff up here
@@ -30,5 +31,25 @@ void test_usys_fopen_should_return_null(void) {
 
 void test_usys_fopen_create_new_file(void) {
     TEST_ASSERT_NOT_EQUAL(NULL, usys_fopen("./test/data/newfile.txt", "w+"));
+}
+
+void test_usys_foperations(void) {
+    char data[] = "abcdef";
+    char rdata[10];
+    FILE* fp = usys_fopen("./test/data/newfile.txt", "w+");
+    TEST_ASSERT_MESSAGE(fp!= NULL, "failed to open file.");
+    if (fp != NULL) {
+        int wbytes = usys_fwrite(data, sizeof(char), sizeof(data), fp);
+        TEST_ASSERT_MESSAGE(wbytes == sizeof(data), "Bytes Written not matching");
+
+        int seek = usys_fseek(fp, 0, SEEK_SET);
+        TEST_ASSERT_MESSAGE(seek == 0, "Setting file pointer to beginning of file failed.");
+
+        int rbytes = usys_fread(&rdata, sizeof(char), sizeof(data), fp);
+        TEST_ASSERT_MESSAGE(rbytes == sizeof(data), "Bytes read not matching");
+
+        TEST_ASSERT_MESSAGE(usys_memcmp(data, rdata, wbytes) == 0, "Read data not matching to written data");
+    }
+    TEST_ASSERT_MESSAGE(0 == usys_fclose(fp), "Failed to close file.");
 }
 
