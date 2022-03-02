0d2dce1c2afbfaa66974849ee6de5ad0d949ae72
Denis Palnitsky <palnitsky@gmail.com>
2022-03-02 14:09:09 +0100

    APIGateway. Handle auth cookie in Debug mode (#129)

--- cloud/api-gateway/pkg/rest/auth_debug.go
+++ cloud/api-gateway/pkg/rest/auth_debug.go
@@ -1,9 +1,10 @@
 package rest
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"net/http"
 )
 
 type DebugAuthMiddleware struct {
@@ -12,14 +13,23 @@ type DebugAuthMiddleware struct {
 func (r *DebugAuthMiddleware) IsAuthenticated(c *gin.Context) {
 	authHeader := c.Request.Header.Get("authorization")
 	token := c.Request.Header.Get("token")
-
 	if len(token) > 0 || len(authHeader) > 0 {
-		logrus.Info("authorization header: ", authHeader)
+		logrus.Info("cookie header: ", authHeader)
 		logrus.Info("token header: ", authHeader)
 		logrus.Info("Bypassing authentication because we are in a debug mode")
 		c.Set(USER_ID_KEY, "11111111-1111-1111-1111-111111111111")
 		return
 	}
+
+	_, err := c.Request.Cookie("ukama_session")
+	if err == nil {
+
+		return
+	}
+	if err != nil {
+		logrus.Error("Cookie not found. Error: ", err)
+	}
+
 	c.AbortWithStatus(http.StatusUnauthorized)
 }
 
