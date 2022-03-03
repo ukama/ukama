49c07a0cf23478c3372f26cb375ad7536ba4977b
salman-codewar <salmannaseem131@gmail.com>
2022-03-03 11:33:15 +0500

    Network metrics added

---
+++ src/modules/node/resolver/getMetricsERAB.resolver.ts
@@ -0,0 +1,21 @@
+import { Service } from "typedi";
+import { MetricDto } from "../types";
+import { NodeService } from "../service";
+import { getHeaders } from "../../../common";
+import { Authentication } from "../../../common/Authentication";
+import { Context, MetricsInputDTO } from "../../../common/types";
+import { Resolver, Query, UseMiddleware, Arg, Ctx } from "type-graphql";
+
+@Service()
+@Resolver()
+export class GetMetricsERABResolver {
+    constructor(private readonly nodeService: NodeService) {}
+    @Query(() => [MetricDto])
+    @UseMiddleware(Authentication)
+    async getMetricsERAB(
+        @Ctx() ctx: Context,
+        @Arg("data") data: MetricsInputDTO
+    ): Promise<MetricDto[] | null> {
+        return this.nodeService.getMetricsErab(data, getHeaders(ctx));
+    }
+}
--- src/modules/node/service.ts
+++ src/modules/node/service.ts
@@ -300,4 +300,43 @@ export class NodeService implements INodeService {
         if (checkError(res)) throw new Error(res.message);
         return NodeMapper.dtoToMetricDto(res.data?.result);
     };
+    getMetricsRrc = async (
+        data: MetricsInputDTO,
+        header: HeaderType
+    ): Promise<MetricDto[]> => {
+        const res = await catchAsyncIOMethod({
+            type: API_METHOD_TYPE.GET,
+            headers: header,
+            path: `${SERVER.ORG}/${data.orgId}/nodes/${data.nodeId}/metrics/rrc`,
+            params: { from: data.from, to: data.to, step: data.step },
+        });
+        if (checkError(res)) throw new Error(res.message);
+        return NodeMapper.dtoToMetricDto(res.data?.result);
+    };
+    getMetricsErab = async (
+        data: MetricsInputDTO,
+        header: HeaderType
+    ): Promise<MetricDto[]> => {
+        const res = await catchAsyncIOMethod({
+            type: API_METHOD_TYPE.GET,
+            headers: header,
+            path: `${SERVER.ORG}/${data.orgId}/nodes/${data.nodeId}/metrics/erab`,
+            params: { from: data.from, to: data.to, step: data.step },
+        });
+        if (checkError(res)) throw new Error(res.message);
+        return NodeMapper.dtoToMetricDto(res.data?.result);
+    };
+    getMetricsRlc = async (
+        data: MetricsInputDTO,
+        header: HeaderType
+    ): Promise<MetricDto[]> => {
+        const res = await catchAsyncIOMethod({
+            type: API_METHOD_TYPE.GET,
+            headers: header,
+            path: `${SERVER.ORG}/${data.orgId}/nodes/${data.nodeId}/metrics/rlc`,
+            params: { from: data.from, to: data.to, step: data.step },
+        });
+        if (checkError(res)) throw new Error(res.message);
+        return NodeMapper.dtoToMetricDto(res.data?.result);
+    };
 }
