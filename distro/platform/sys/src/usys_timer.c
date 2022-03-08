414ebf981710ba9b27d336110921764786a9336e
vthakur7f <vthakur7@hotmail.com>
2022-03-07 20:32:10 -0800

    Updated Ukama header.

--- distro/platform/sys/src/usys_timer.c
+++ distro/platform/sys/src/usys_timer.c
@@ -1,8 +1,8 @@
 /**
- * Copyright (c) 2020-present, Ukama.
+ * Copyright (c) 2021-present, Ukama Inc.
  * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
+ * This source code is licensed under the XXX-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
