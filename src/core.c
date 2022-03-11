9d71217f44df2065106859d694c2a78819e57003
vthakur7f <vthakur7@hotmail.com>
2022-03-11 13:57:09 -0800

    Noded new dir structure.

---
+++ src/core.c
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2021-present, Ukama Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the XXX-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include "usys_types.h"
+
+int main() {
+  return 0;
+}
---
+++ src/ledger/ledger.c
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2021-present, Ukama Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the XXX-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+
+
+
