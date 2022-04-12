a98d1197220799d87214e9a983c73c684f9ab307
Kashif Ali <kashif@ukama.com>
2022-04-12 16:07:00 -0700

    Initial commit for service router

---
+++ transport/router/src/jserdes.c
@@ -0,0 +1,187 @@
+/**
+ * Copyright (c) 2022-present, Ukama Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the XXX-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <jansson.h>
+
+#include "jserdes.h"
+#include "log.h"
+
+static void log_json(json_t *json);
+static int add_key_value_to_pattern(Pattern **pattern, const char *key,
+				    json_t *jValue);
+
+/*
+ * deserialize_post_route_request --
+ *
+ *  {
+ *      "pattern": {
+ *		"key1": "value1",
+ *		"key2": "value2"
+ *	},
+ *
+ *	"forward": {
+ *		"ip": "192.168.0.1",
+ *		"port": "8080"
+ *	}
+ *  }
+ */
+int deserialize_post_route_request(Service **service, json_t *json) {
+
+  json_t *jPattern=NULL, *jForward=NULL;
+  json_t *value, *jIP, *jPort;
+  void *iter;
+  const char *key;
+
+  if (json == NULL) return FALSE;
+
+  jPattern = json_object_get(json, JSON_PATTERN);
+  jForward = json_object_get(json, JSON_FORWARD);
+
+  if (jPattern == NULL || jForward == NULL) {
+    log_error("Missing mandatory %s or %s from recvd json request",
+	      JSON_PATTERN, JSON_FORWARD);
+    log_json(json);
+    return FALSE;
+  }
+
+  /* forward: ip and port */
+  jIP   = json_object_get(jForward, JSON_IP);
+  jPort = json_object_get(jForward, JSON_PORT);
+
+  if (jIP == NULL || jPort == NULL) {
+    log_error("Missing %s or %s from recvd json request", JSON_IP, JSON_PORT);
+    log_json(json);
+    return FALSE;
+  }
+
+  *service = (Service *)calloc(1, sizeof(Service));
+  if (*service == NULL) {
+    log_error("Error allocating memory of size: %lu", sizeof(Service));
+    return FALSE;
+  }
+
+  uuid_clear((*service)->uuid);
+  (*service)->pattern = (Pattern *)calloc(1, sizeof(Pattern));
+  (*service)->forward = (Forward *)calloc(1, sizeof(Forward));
+
+  if ((*service)->pattern == NULL || (*service)->forward == NULL) {
+    log_error("Error allocating memory of size: %ls or %lu",
+	      sizeof(Pattern), sizeof(Forward));
+    goto failure;
+  }
+
+  /* Iterate to get all key-value pairs for pattern json object */
+  iter = json_object_iter(jPattern);
+  while (iter) {
+
+    key   = json_object_iter_key(iter);
+    value = json_object_iter_value(iter);
+
+    add_key_value_to_pattern(&((*service)->pattern), key, value);
+
+    /* iterate to next one */
+    iter = json_object_iter_next(jPattern, iter);
+  }
+
+  (*service)->forward->ip   = strdup(json_string_value(jIP));
+  (*service)->forward->port = strdup(json_string_value(jPort));
+
+  return TRUE;
+
+ failure:
+  if (*service) {
+    if ((*service)->pattern) free((*service)->pattern);
+    if ((*service)->forward) free((*service)->forward);
+    free(*service);
+    *service=NULL;
+  }
+  return FALSE;
+}
+
+/*
+ * serialize_post_route_response --
+ *
+ */
+int serialize_post_route_response(json_t **json, int respCode, uuid_t uuid,
+				  char *errStr) {
+
+  char idStr[36+1];
+
+  *json = json_object();
+  if (*json == NULL) {
+    return FALSE;
+  }
+
+  switch (respCode) {
+  case UUID:
+    if (uuid_is_null(uuid)) return FALSE;
+
+    uuid_unparse(uuid, &idStr[0]);
+    json_object_set_new(*json, JSON_UUID, json_string(idStr));
+    break;
+
+  case ERROR:
+    if (errStr==NULL) return FALSE;
+
+    json_object_set_new(*json, JSON_ERROR, json_string(errStr));
+    break;
+
+  default:
+    break;
+  }
+
+  return TRUE;
+}
+
+/*
+ * add_key_value_to_pattern --
+ *
+ */
+static int add_key_value_to_pattern(Pattern **pattern, const char *key,
+				    json_t *jValue) {
+
+  Pattern *ptr=NULL;
+
+  if (jValue == NULL) return FALSE;
+
+  if (*pattern == NULL) {
+    ptr = *pattern;
+  } else {
+    for (ptr=*pattern; ptr->next; ptr=ptr->next);
+  }
+
+  ptr = (Pattern *)calloc(1, sizeof(Pattern));
+  if (ptr == NULL) {
+    log_error("Error allocating memory of size: %lu", sizeof(Pattern));
+    return FALSE;
+  }
+
+  ptr->key   = strdup(key);
+  ptr->value = strdup(json_string_value(jValue));
+  ptr->next  = NULL;
+
+  return TRUE;
+}
+
+/*
+ * log_json --
+ *
+ */
+static void log_json(json_t *json) {
+
+  char *str = NULL;
+
+  str = json_dumps(json, 0);
+  if (str) {
+    log_debug("json str: %s", str);
+    free(str);
+  }
+}
---
+++ transport/router/src/network.c
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2021-present, Ukama Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the XXX-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/*
+ * Network related stuff based on ulfius framework.
+ */
+
+#include <ulfius.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "router.h"
+#include "callback.h"
+#include "log.h"
+
+/*
+ * init_sockaddr --
+ *
+ */
+static int init_sockaddr(struct sockaddr_in *name, char *hostName, char *port) {
+
+  struct hostent *hostInfo=NULL;
+
+  if (hostName==NULL || port==NULL) return FALSE;
+
+  hostInfo = gethostbyname(hostName);
+  if (hostInfo == NULL) {
+    log_error("Unable to resolve hostName: %s", hostName);
+    return FALSE;
+  }
+  
+  name->sin_family = AF_INET;
+  name->sin_port   = htons(atoi(port));
+  name->sin_addr   = *(struct in_addr *)hostInfo->h_addr;
+
+  return TRUE;
+}
+
+/* 
+ * init_frameworks -- initializa ulfius framework and register various
+ *                    callbacks.
+ *
+ */
+static int init_frameworks(Config *config, struct _u_instance *webInst) {
+  
+  struct sockaddr_in sockAddr;
+#if 0
+  if (init_sockaddr(&sockAddr, config->hostName, config->port)) {
+    log_error("Unable to init sockaddr for hostName %s port: %s",
+	      config->hostName, config->port);
+    return FALSE;
+  }
+#endif
+
+  if (ulfius_init_instance(webInst, atoi(config->port), NULL, NULL)
+      != U_OK) {
+    log_error("Error initializing instance for hostName: %s port %s",
+	      config->hostName, config->port);
+    return FALSE;
+  }
+
+  /* Set few params. */
+  webInst->max_post_body_size = 1024;
+
+  return TRUE;
+}
+
+/*
+ * setup_endpoints --
+ *
+ */
+static void setup_endpoints(Router *router, struct _u_instance *instance) {
+
+  /* GET, POST /route */
+  ulfius_add_endpoint_by_val(instance, "GET", EP_ROUTE, NULL, 0,
+                             &callback_get_route, router);
+  ulfius_add_endpoint_by_val(instance, "POST", EP_ROUTE, NULL, 0,
+                             &callback_post_route, router);
+  /* GET /stats */
+  ulfius_add_endpoint_by_val(instance, "GET", EP_STATS, NULL, 0,
+                             &callback_get_stats, router);
+
+  /* default endpoint. */
+  ulfius_set_default_endpoint(instance, &callback_default, router);
+}
+
+/* 
+ * start_framework --
+ *
+ */
+int start_framework(struct _u_instance *instance) {
+
+  int ret;
+  
+  /* open HTTP connection. */
+  ret = ulfius_start_framework(instance);
+
+  if (ret != U_OK) {
+    log_error("Error starting the webservice.");
+    
+    /* clean up. */
+    ulfius_stop_framework(instance); /* don't think need this. XXX */
+    ulfius_clean_instance(instance);
+    
+    return FALSE;
+  }
+
+  log_debug("Webservice succesfully started");
+  
+  return TRUE;
+}
+
+/*
+ * start_web_service --
+ *
+ */
+int start_web_service(Router *router, struct _u_instance *webInst) {
+
+  if (router == NULL)               return FALSE;
+  if (router->config == NULL)       return FALSE;
+  if (router->config->hostName == NULL ||
+      router->config->port == NULL) return FALSE;
+
+  /* Initialize the webservices framework. */
+  if (init_frameworks(router->config, webInst) != TRUE) {
+    log_error("Error initializing webservice framework");
+    return FALSE;
+  }
+
+  /* setup endpoints and methods callback. */
+  setup_endpoints(router, webInst);
+
+  /* open connection for webservices */
+  if (!start_framework(webInst)) {
+    log_error("Failed to start webservices for hostName:%s port:%s",
+	      router->config->hostName, router->config->port);
+    return FALSE;
+  }
+
+  log_debug("Webservice on hostName:%s port:%s started.",
+	    router->config->hostName, router->config->port);
+  return TRUE;
+}
