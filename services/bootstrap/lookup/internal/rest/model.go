d8e61dc2898f93b4a8607f430c390464b61824f5
vthakur7f <vthakur7@hotmail.com>
2022-04-28 00:18:52 -0700

    Making sure looking_for and looking_to values matches for request to  lookup service.

--- services/bootstrap/lookup/internal/rest/model.go
+++ services/bootstrap/lookup/internal/rest/model.go
@@ -2,13 +2,13 @@ package rest
 
 type ReqAddNode struct {
 	NodeID    string `query:"node" validate:"required"`
-	LookingTo string `query:"looking_to" validate:"required"`
+	LookingTo string `query:"looking_to" validate:"eq=add_node,required"`
 	OrgName   string `query:"org" validate:"required"`
 }
 
 type ReqGetNode struct {
 	NodeID     string `query:"node" validate:"required"`
-	LookingFor string `query:"looking_for" validate:"required"`
+	LookingFor string `query:"looking_for" validate:"eq=org_credentials,required"`
 	OrgName    string `query:"org"`
 }
 
@@ -21,7 +21,7 @@ type RespGetNode struct {
 
 type ReqAddOrg struct {
 	OrgName     string `query:"org" validate:"required"`
-	LookingTo   string `query:"looking_to" validate:"required"`
+	LookingTo   string `query:"looking_to" validate:"eq=add_org,required"`
 	Certificate string `json:"certificate" binding:"required,base64"`
 	Ip          string `json:"ip" binding:"required,ip"`
 }
