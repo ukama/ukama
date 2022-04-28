05d185fa2d2bb0d2fb19287fd86b2f2fda928d00
vthakur7f <vthakur7@hotmail.com>
2022-04-28 00:17:56 -0700

    Making sure looking_for and looking_to values matches for request.

--- services/bootstrap/bootstrap/pkg/server/api.go
+++ services/bootstrap/bootstrap/pkg/server/api.go
@@ -2,7 +2,7 @@ package server
 
 type BootstrapRequest struct {
 	Nodeid     string `query:"node" validate:"required"`
-	LookingFor string `query:"looking_for" validate:"required"`
+	LookingFor string `query:"looking_for" validate:"eq=validation,required"`
 }
 
 type BootstrapResponse struct {
