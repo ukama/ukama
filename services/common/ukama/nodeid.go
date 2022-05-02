bf5e3c75c79e347057aaa3e253e8ebef42d8ae87
vthakur7f <vthakur7@hotmail.com>
2022-05-02 15:48:53 -0700

    Updated common for module id's

--- services/common/ukama/nodeid.go
+++ services/common/ukama/nodeid.go
@@ -22,18 +22,28 @@ const (
 
 const (
 	NODE_ID_TYPE_HOMENODE  = "HOMENODE"
+	NODE_ID_TYPE_TOWERNODE = "TOWERNODE"
 	NODE_ID_TYPE_COMPNODE  = "COMPNODE"
 	NODE_ID_TYPE_AMPNODE   = "AMPNODE"
 	NODE_ID_TYPE_UNDEFINED = "UNDEFINED"
 )
 
 const (
+	MODULE_ID_TYPE_COMP      = "COMV1"
+	MODULE_ID_TYPE_TRX       = "TRX"
+	MODULE_ID_TYPE_CTRL      = "CTRL"
+	MODULE_ID_TYPE_FE        = "FE"
+	MODULE_ID_TYPE_UNDEFINED = "UNDEFINED"
+)
+
+const (
 	node_id_type_component_home      = "hnode"
-	node_id_type_component_tower     = "comv1"
+	node_id_type_component_tower     = "tnode"
 	node_id_type_component_amplifier = "anode"
 )
 
 type NodeID string
+type ModuleID string
 
 func (n NodeID) String() string {
 	return string(n)
@@ -74,10 +84,28 @@ func GetNodeCodeForUnits(ntype string) string {
 	switch ntype {
 	case NODE_ID_TYPE_HOMENODE, "HomeNode", "homenode":
 		code = "HNODE"
-	case NODE_ID_TYPE_COMPNODE, "CompNode", "compnode":
-		code = "COMv1"
+	case NODE_ID_TYPE_TOWERNODE, "TowerNode", "tnode", "TNODE", "TNode":
+		code = "TNODE"
 	case NODE_ID_TYPE_AMPNODE, "AmpNode", "ampnode":
 		code = "ANODE"
+	default:
+		code = "XXXXX"
+	}
+	return code
+}
+
+/* Get HW Code */
+func GetModuleCodeForUnits(mtype string) string {
+	var code string
+	switch mtype {
+	case MODULE_ID_TYPE_COMP, "ComV1", "com", "COM":
+		code = "COM"
+	case MODULE_ID_TYPE_TRX, "Trx", "trx":
+		code = "TRX"
+	case MODULE_ID_TYPE_CTRL, "Ctrl", "ctrl":
+		code = "CTRL"
+	case MODULE_ID_TYPE_FE, "Fe", "fe":
+		code = "RFFE"
 	default:
 		code = "XXXXX"
 	}
@@ -105,9 +133,56 @@ func NewVirtualNodeId(ntype string) NodeID {
 	return NodeID(lid)
 }
 
+/* Generate new module id for virtual module */
+func NewVirtualModuleId(mtype string) ModuleID {
+	t := time.Now()
+	year, week := t.ISOWeek()
+	yearstr := strconv.Itoa(year)
+	yearcode := yearstr[len(yearstr)-2:]
+	weekstr := fmt.Sprintf("%02d", week)
+	code := GetModuleCodeForUnits(mtype)
+
+	/*2+1+6+1+5+1+2+1+4*/
+	/* UK-SA2154-HNODE-A1-XXXX*/
+	uuid := OEMCODE + DELIMITER + MFGCODE + yearcode + weekstr + DELIMITER + code + DELIMITER + HWVERSION + DELIMITER + getRandCode(t)
+
+	log.Infof("UUID: New ModuleID for %s is %s and length is %d", mtype, uuid, len(uuid))
+
+	/* RFC 1123 lowercase id and tags*/
+	lid := strings.ToLower(uuid)
+
+	return ModuleID(lid)
+}
+
+func NewVirtualComId() ModuleID {
+	return NewVirtualModuleId(MODULE_ID_TYPE_COMP)
+}
+
+func NewVirtualTRXId() ModuleID {
+	return NewVirtualModuleId(MODULE_ID_TYPE_TRX)
+}
+
+func NewVirtualCtrlId() ModuleID {
+	return NewVirtualModuleId(MODULE_ID_TYPE_CTRL)
+}
+
+func NewVirtualFEId() ModuleID {
+	return NewVirtualModuleId(MODULE_ID_TYPE_FE)
+}
+
 // Generate new node id for home node
 func NewVirtualHomeNodeId() NodeID {
 	return NewVirtualNodeId(NODE_ID_TYPE_HOMENODE)
+}
+
+// Generate new node id for amplifier node
+func NewVirtualAmplifierNodeId() NodeID {
+	return NewVirtualNodeId(NODE_ID_TYPE_AMPNODE)
+}
+
+// Generate new node id for tower node
+func NewVirtualTowerNodeId() NodeID {
+	return NewVirtualNodeId(NODE_ID_TYPE_TOWERNODE)
 }
 
 func ValidateNodeId(id string) (NodeID, error) {
