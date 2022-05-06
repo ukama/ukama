6259f753bc80d39e6c372c412551bcaf288d667f
vthakur7f <vthakur7@hotmail.com>
2022-05-06 16:45:43 -0700

    Added VNode Database.

---
+++ testing/network/internal/db/model.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type VNodeStatus string
+
+const (
+	VnodeOn  VNodeStatus = "PowerOn"
+	VnodeOff VNodeStatus = "PowerOff"
+)
+
+type VNode struct {
+	gorm.Model
+	NodeID string `gorm:"unique;type:string;size:23;expression:lower(node_id);size:32;not null" json:"nodeID" `
+	Status string `gorm:"size:32;not null" json:"status"`
+}
+
+func (s VNodeStatus) String() string {
+	return string(s)
+}
+
+func VnodeState(s string) (*VNodeStatus, error) {
+	vNodeStatus := map[VNodeStatus]struct{}{
+		VnodeOn:  {},
+		VnodeOff: {},
+	}
+
+	status := VNodeStatus(s)
+
+	_, ok := vNodeStatus[status]
+	if !ok {
+		return nil, fmt.Errorf("%s is invalid virtual node status", s)
+	}
+
+	return &status, nil
+
+}
---
+++ testing/network/internal/db/vnode_repo.go
@@ -0,0 +1,74 @@
+package db
+
+import (
+	"github.com/ukama/ukama/services/common/sql"
+	"gorm.io/gorm/clause"
+)
+
+type VNodeRepo interface {
+	Upsert(nodeId string, status string) error
+	PowerOn(nodeId string) error
+	PowerOff(nodeId *string) error
+	GetInfo(nodeId string) (*VNode, error)
+	List() (*[]VNode, error)
+}
+
+type vNodeRepo struct {
+	Db sql.Db
+}
+
+func NewVnodeRepo(db sql.Db) *vNodeRepo {
+	return &vNodeRepo{
+		Db: db,
+	}
+}
+
+/* Upsert is used when we know the node id */
+func (r *vNodeRepo) Upsert(nodeId string, status string) error {
+	vNode := &VNode{
+		NodeID: nodeId,
+		Status: status,
+	}
+
+	d := r.Db.GetGormDb().Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "node_id"}},
+		UpdateAll: true,
+	}).Create(vNode)
+	return d.Error
+}
+
+/* PowerOn */
+func (r *vNodeRepo) PowerOn(nodeId string) error {
+	return Upsert(nodeId, db.VnodeOn.String())
+}
+
+/* PowerOn */
+func (r *vNodeRepo) PowerOff(nodeId string) error {
+	return Upsert(nodeId, db.VnodeOff.String())
+}
+
+/* Get VirtNode info */
+func (r *vNodeRepo) GetInfo(nodeId string) (*VNode, error) {
+	var vNode VNode
+	result := r.Db.GetGormDb().Preload(clause.Associations).First(&VNode, "node_id = ?", nodeId)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &vNode, nil
+}
+
+/* List all Modules */
+func (r *vNodeRepo) List() (*[]VNode, error) {
+	var vNodes []VNode
+
+	result := r.Db.GetGormDb().Preload(clause.Associations).Find(&vNodes)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return nil, nil
+	} else {
+		return &vNodes, nil
+	}
+}
