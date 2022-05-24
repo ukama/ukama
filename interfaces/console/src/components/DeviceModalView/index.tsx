d2b1dac8c65639e7826e97c386be39574d471391
salman-codewar <salmannaseem131@gmail.com>
2022-05-24 18:25:32 +0500

    Code smell fixes

--- interfaces/console/src/components/DeviceModalView/index.tsx
+++ interfaces/console/src/components/DeviceModalView/index.tsx
@@ -1,29 +1,10 @@
 import { Box } from "@mui/material";
 import { Node_Type } from "../../generated";
 import { NODE_IMAGES } from "../../constants";
-// import * as THREE from "three";
-// import { Suspense } from "react";
-// import { DDSLoader } from "three-stdlib";
-// import { Canvas } from "@react-three/fiber";
-// import { OrbitControls, useFBX } from "@react-three/drei";
 
 interface IDeviceModalView {
     nodeType: Node_Type | undefined;
 }
-
-// THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
-
-// const Scene = () => {
-//     const fbx = useFBX("node.fbx");
-//     return (
-//         <primitive
-//             position={[0, -2, 0]}
-//             scale={8}
-//             object={fbx}
-//             rotation={[Math.PI / 1, 0, 0]}
-//         />
-//     );
-// };
 
 const DeviceModalView = ({ nodeType = Node_Type.Home }: IDeviceModalView) => {
     return (
@@ -37,22 +18,6 @@ const DeviceModalView = ({ nodeType = Node_Type.Home }: IDeviceModalView) => {
                 marginTop: 6,
             }}
         >
-            {/* <Canvas>
-                <pointLight
-                    color="white"
-                    intensity={1}
-                    position={[10, 10, 10]}
-                />
-                <pointLight
-                    color="white"
-                    intensity={0.5}
-                    position={[10, -10, -10]}
-                />
-                <Suspense fallback={null}>
-                    <Scene />
-                    <OrbitControls minDistance={1.5} maxDistance={10} />
-                </Suspense>
-            </Canvas> */}
             <img
                 src={NODE_IMAGES[nodeType]}
                 alt="node-img"
