e8b6844ba702bab02f5c176863e799cf9d6f28a0
Brackley cassinga <pcassinga@gmail.com>
2022-06-13 17:47:53 +0200

    Update getUser test
    Signed-off-by: Brackley cassinga <pcassinga@gmail.com>

--- services/cloud/users/pkg/kratos_client_test.go
+++ services/cloud/users/pkg/kratos_client_test.go
@@ -2,20 +2,24 @@ package pkg
 
 import (
     "log"
+    "testing"
 )
 type KratosClient interface{
-	GetAccountName(networkOwnerId string )(string ,error) 
+	GetAccountName(networkOwnerId string) (string, error)
    
 }
 type kratosClient struct{
-
+   
 }
-func (k *kratosClient) GetAccountName(networkOwnerId string )(string ,error) {
+func (k *kratosClient) GetAccountName(networkOwnerId string) (string, error){
+ 
+}
+
+func Test_GetAccounName(t *testing.T)  {
     c:= kratosClient {}
     usr, err := c.GetAccountName("a32485e4-d842-45da-bf3e-798889c68ad0")
    if usr !="Ukama Dev Team"{
     log.Fatal(err)
    }
-  return usr, err
 }
 
