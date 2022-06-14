92d9d048940d81fe3536a61b07ce7055cb00db47
Brackley cassinga <pcassinga@gmail.com>
2022-06-14 15:44:54 +0200

    Add user object to extarct usr from json objt

--- services/cloud/users/pkg/kratos_client_test.go
+++ services/cloud/users/pkg/kratos_client_test.go
@@ -3,23 +3,49 @@ package pkg
 import (
     "log"
     "testing"
+    "io/ioutil"
+	"net/http"
+    "fmt"
 )
-type KratosClient interface{
-	GetAccountName(networkOwnerId string) (string, error)
+
+type kratosClient struct {
    
 }
-type kratosClient struct{
-   
+
+  
+type UserOject struct {
+    Name string
+    Email  string
 }
-func (k *kratosClient) GetAccountName(networkOwnerId string) (string, error){
- 
+func (i kratosClient) GetAccountName(networkOwnerId string) (string, error) {
+
+resp, err := http.Get(`https://kratos-admin.dev.ukama.com/admin/identities/`+networkOwnerId)
+	if err != nil {
+	   log.Fatal(err)
+      
+	}
+bytes ,errRead :=ioutil.ReadAll(resp.Body)
+
+if errRead!=nil{
+	log.Fatal(errRead)
+}
+fmt.Println(string(bytes.schema_url))
+
+return string(bytes),errRead
+
 }
 
 func Test_GetAccounName(t *testing.T)  {
+   
     c:= kratosClient {}
     usr, err := c.GetAccountName("a32485e4-d842-45da-bf3e-798889c68ad0")
+  
+    if err !=nil{
+        log.Fatal(err)
+    }
+   
    if usr !="Ukama Dev Team"{
-    log.Fatal(err)
+    log.Fatal("Failed to find the user")
    }
 }
 
