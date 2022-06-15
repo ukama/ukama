dd23846a7e949cbb89a36a8a18c22611b2d10afc
Brackley cassinga <pcassinga@gmail.com>
2022-06-15 09:38:43 +0200

    GetUserName test cleam up

--- services/cloud/users/pkg/kratos_client.go
+++ services/cloud/users/pkg/kratos_client.go
@@ -1,33 +1,43 @@
 package pkg
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type KratosClient interface {
-	GetAccountName(networkOwnerId string) (string, error)
-}
-
 type kratosClient struct {
-
+   
 }
 
-
+type Response struct {
+	Traits         struct {
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	} `json:"traits"`
+}
 func (i kratosClient) GetAccountName(networkOwnerId string) (string, error) {
-	resp, err := http.Get(`https://kratos-admin.dev.ukama.com/admin/identities/`+networkOwnerId)
+
+resp, err := http.Get(`https://kratos-admin.dev.ukama.com/admin/identities/`+networkOwnerId)
 	if err != nil {
 	   log.Fatal(err)
+      
 	}
-bytes ,errRead :=ioutil.ReadAll(resp.Body)
-if errRead!=nil{
-	log.Fatal(errRead)
-}
-return string(bytes),errRead
-}
+    
+ defer resp.Body.Close()
 
+dataByte,erroBytes:=ioutil.ReadAll(resp.Body)
 
+var result Response
+
+    if err := json.Unmarshal(dataByte, &result); err != nil {  
+        fmt.Println("Can not unmarshal JSON")
+    }
+   
+return result.Traits.Name,erroBytes
+}
 
 
 
