47fb071ab2df8ca10121818ed97b0e2a9f1de582
salman-codewar <salmannaseem131@gmail.com>
2022-07-01 16:40:32 +0500

    Static session added

--- services/bff/src/common/index.ts
+++ services/bff/src/common/index.ts
@@ -33,5 +33,11 @@ export const parseCookie = (ctx: Context): ParsedCookie => {
             Cookie: `ukama_session=${cookieObj["ukama_session"]}`,
         };
     }
-    return { header: header, orgId: cookieObj["id"] };
+    // return { header: header, orgId: cookieObj["id"] };
+    return {
+        header: {
+            Cookie: "ukama_session=MTY1NjY3NTU4N3xyRjkzLUZGTEYzaGVkamJVVmJXanNrbjQ5YzlTZllad292OEt6SDVNLUsxNUF3Nks4c1VISFpYMDRTWExFazRXRHFEU1U4ZnNkdF9YbEVSd21SS2Q2b0kyUEUwajY1elRnMjd5VTRfYnZwQVRBN1o2SWw4dWoyTEtpbWFpQ1JiN1JKenBuTE9yMEE9PXwwKhXp-1c_swFCbwvGTJ4uptxaY1o16v23j6_z29LFDA==",
+        },
+        orgId: "a32485e4-d842-45da-bf3e-798889c68ad0",
+    };
 };
