f5d785b8569b29521b11971c43332a2f0157bb24
salman-codewar <salmannaseem131@gmail.com>
2022-08-16 20:39:49 +0500

    Bug Fix

--- interfaces/console/src/layout/Sidebar/index.tsx
+++ interfaces/console/src/layout/Sidebar/index.tsx
@@ -1,7 +1,6 @@
 import {
     Box,
     List,
-    Chip,
     Paper,
     Stack,
     Drawer,
@@ -11,7 +10,7 @@ import {
     ListItemIcon,
     ListItemText,
 } from "@mui/material";
-import React, { useEffect } from "react";
+import React from "react";
 import { colors } from "../../theme";
 import { useRecoilValue } from "recoil";
 import { makeStyles } from "@mui/styles";
@@ -20,7 +19,6 @@ import { MenuItemType } from "../../types";
 import { useHistory } from "react-router-dom";
 import { LoadingWrapper } from "../../components";
 import { DRAWER_WIDTH, SIDEBAR_MENU1 } from "../../constants";
-import { useGetNodesByOrgLazyQuery } from "../../generated";
 
 const Logo = React.lazy(() =>
     import("../../assets/svg").then(module => ({
@@ -68,13 +66,7 @@ const Sidebar = (
     const classes = useStyles();
     const history = useHistory();
     const _isDarkMod = useRecoilValue(isDarkmode);
-    const [getNodesByOrg, { data: nodesRes, loading: nodesLoading }] =
-        useGetNodesByOrgLazyQuery({
-            fetchPolicy: "cache-and-network",
-        });
-    useEffect(() => {
-        getNodesByOrg();
-    }, []);
+
     const MenuList = (list: any) => (
         <List sx={{ padding: "8px 20px" }}>
             {list.map(({ id, title, Icon, route }: MenuItemType) => (
@@ -122,23 +114,6 @@ const Sidebar = (
                             >
                                 {title}
                             </Typography>
-                            {title == "Nodes" &&
-                                nodesRes?.getNodesByOrg.totalNodes && (
-                                    <LoadingWrapper
-                                        isLoading={nodesLoading}
-                                        width={"120px"}
-                                        height={"20px"}
-                                    >
-                                        <Chip
-                                            label={
-                                                nodesRes?.getNodesByOrg
-                                                    .totalNodes
-                                            }
-                                            size="small"
-                                            color="primary"
-                                        />
-                                    </LoadingWrapper>
-                                )}
                         </Stack>
                     </ListItemText>
                 </ListItem>
