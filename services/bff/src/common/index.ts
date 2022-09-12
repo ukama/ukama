3f65d8864ac56a90b92cd024c1e33324e88cf229
Brackley cassinga <pcassinga@gmail.com>
2022-09-12 16:49:50 +0200

    Remove hard coded cookie from bff

--- services/bff/src/common/index.ts
+++ services/bff/src/common/index.ts
@@ -33,11 +33,6 @@ export const parseCookie = (ctx: Context): ParsedCookie => {
             Cookie: `ukama_session=${cookieObj["ukama_session"]}`,
         };
     }
-    // return { header: header, orgId: cookieObj["id"] };
-    return {
-        header: {
-            Cookie: "ukama_session=MTY2MjczODQ3MnxiNVJaTEo2YzRvNi1GdzdHVVVZSzVPOW54WHhuRVpnNlhfbEJOeG9nMmVGM05GWGh5bkRrV0hGTHIzZ29iWHBLcDNTcHVFVVFOeHo3bm9RMU8wcm5fMk5reDJMWUJKbDNJTDdJb0VKZm9oa21pdkNpQ3dOUXE0X0s2NUpRM1hQYmlvVDVJcVN3eVE9PXysbUDw8bXvLodtEHkPZwZJRi3tVEv33y37B6zWHHTx7Q==",
-        },
-        orgId: "d89d860d-fa7d-4523-b460-f767fd48c623",
-    };
+
+    return { header: header, orgId: cookieObj["id"] };
 };
