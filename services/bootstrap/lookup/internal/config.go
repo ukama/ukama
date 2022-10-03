e8c4260cd1a5020e9b334396718e6e74e295427b
Kashif Ali <kashif@ukama.com>
2022-10-03 16:27:41 -0700

    Revert "New arch (#320)"
    This reverts commit 3dfbcb855b51c885e9b8c70e40369cdaaf737631.

---
+++ services/bootstrap/lookup/internal/config.go
@@ -0,0 +1,50 @@
+package internal
+
+import (
+	"github.com/ukama/ukama/services/common/config"
+	"github.com/ukama/ukama/services/common/rest"
+)
+
+type Config struct {
+	config.BaseConfig `mapstructure:",squash"`
+	Server            rest.HttpConfig
+	Metrics           config.Metrics
+	ApiIf             config.ServiceApiIf
+	ServiceRouter     string
+	DB                config.Database
+}
+
+var ServiceConf *Config
+
+// NewConfig creates new config with default values. Those values will be overridden by Viper
+func NewConfig() *Config {
+	return &Config{
+
+		Server:        rest.DefaultHTTPConfig(),
+		ServiceRouter: "http://localhost:8091",
+		ApiIf: config.ServiceApiIf{
+			Name: ServiceName,
+			P: []config.Route{
+				{
+					"ping": ServiceName, "path": "/ping",
+				},
+				{
+					"node": "*", "looking_for": "org_credentials", "path": "/orgs/node",
+				},
+				{
+					"node": "*", "looking_to": "add_node", "org": "*", "path": "/orgs/node",
+				},
+				{
+					"org": "*", "looking_to": "add_org", "path": "/orgs",
+				},
+			},
+
+			F: config.Forward{
+				Ip:   "localhost",
+				Port: 8080,
+			},
+		},
+
+		DB: config.DefaultDatabaseName(ServiceName),
+	}
+}
--- systems/init/api-gateway/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/ukama/ukama/systems/init/api-gateway
-
-go 1.14
-
-require google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
