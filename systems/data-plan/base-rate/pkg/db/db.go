cb9b6a6e3ebd613232fa75253e7f8a46b20d2330
Brackley cassinga <pcassinga@gmail.com>
2022-10-25 15:35:12 +0200

    Check for empty request to query rate bya country or provider

--- systems/data-plan/base-rate/pkg/db/db.go
+++ systems/data-plan/base-rate/pkg/db/db.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/ukama/ukama/systems/data-plan/base-rate/pkg/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -20,8 +19,8 @@ func Init(url string) Handler {
 		log.Fatalln(err)
 	}
 	fmt.Println("Connecte to db")
-	db.AutoMigrate(&models.Rate{})
-	db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&models.Rate{})
+	// db.AutoMigrate(&models.Rate{})
+	// db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&models.Rate{})
 
 	return Handler{db}
 }
--- systems/data-plan/base-rate/pkg/services/rates.go
+++ systems/data-plan/base-rate/pkg/services/rates.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/sirupsen/logrus"
 	pb "github.com/ukama/ukama/systems/data-plan/base-rate/pb"
@@ -16,51 +15,67 @@ type Server struct {
 }
 
 func (s *Server) GetRates(ctx context.Context, req *pb.RatesRequest) (*pb.RatesResponse, error) {
-	logrus.Infof("Get all rates  %v", req.GetCountry())
+	logrus.Infof("Get all rates %v", req.GetCountry())
 
 	var rate_list *pb.RatesResponse = &pb.RatesResponse{}
+	if !isRequestEmpty(req.GetCountry(), *req.Provider) {
+		if result := s.H.DB.Where("Country = ? AND Network = ?", req.Country, req.Provider).Find(&rate_list.Rates); result.Error != nil {
+			logrus.Error(result.Error)
+		}
 
-	if result := s.H.DB.Find(&rate_list.Rates); result.Error != nil {
-		fmt.Println(result.Error)
+	} else if !isRequestEmpty(req.GetCountry()) {
+
+		if result := s.H.DB.Where("Country = ? ", req.Country).Find(&rate_list.Rates); result.Error != nil {
+			logrus.Error(result.Error)
+		}
+
+	} else {
+		if result := s.H.DB.Find(&rate_list.Rates); result.Error != nil {
+			logrus.Error(result.Error)
+		}
 	}
-
-	rates := pb.Rate{}
-	rate_list.Rates = append(rate_list.Rates, &rates)
 
 	return rate_list, nil
 
 }
 
 func (s *Server) GetRate(ctx context.Context, req *pb.RateRequest) (*pb.RateResponse, error) {
-	logrus.Infof("Get rate  %v", req.GetRateId())
+	logrus.Infof("Get rate by Id : %v", req.GetRateId())
 
 	var rate models.Rate
-
-	if result := s.H.DB.First(&rate, req.RateId); result.Error != nil {
-		fmt.Println(result.Error)
+	if !isRequestEmpty(req.GetRateId()) {
+		if result := s.H.DB.First(&rate, req.RateId); result.Error != nil {
+			logrus.Error(result.Error)
+		}
 	}
 
 	data := &pb.Rate{
-		Country:           rate.Country,
-		CountryOnCronus:   rate.Country_on_cronus,
-		Network:           rate.Network,
-		NetworkIdOnCronus: rate.Network_id_on_cronus,
-		Vpmn:              rate.Vpmn,
-		Imsi:              rate.Imsi,
-		SmsMo:             rate.Sms_mo,
-		SmsMt:             rate.Sms_mt,
-		Data:              rate.Data,
-		X2G:               rate.X2g,
-		X3G:               rate.X3g,
-		Lte:               rate.Lte,
-		LteM:              rate.Lte_m,
-		Apn:               rate.Apn,
-		CreatedAt:         rate.Created_at,
-		EffectiveAt:       rate.Effective_at,
-		EndAt:             rate.End_at,
+		Country:     rate.Country,
+		Network:     rate.Network,
+		Vpmn:        rate.Vpmn,
+		Imsi:        rate.Imsi,
+		SmsMo:       rate.Sms_mo,
+		SmsMt:       rate.Sms_mt,
+		Data:        rate.Data,
+		X2G:         rate.X2g,
+		X3G:         rate.X3g,
+		Lte:         rate.Lte,
+		LteM:        rate.Lte_m,
+		Apn:         rate.Apn,
+		CreatedAt:   rate.Created_at,
+		EffectiveAt: rate.Effective_at,
+		EndAt:       rate.End_at,
 	}
 	return &pb.RateResponse{
 		Rate: data,
 	}, nil
 
+}
+func isRequestEmpty(ss ...string) bool {
+	for _, s := range ss {
+		if s == "" {
+			return true
+		}
+	}
+	return false
 }
