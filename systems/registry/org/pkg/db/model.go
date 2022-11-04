79f19525a272cb3fc32148c1a8fd8ed9809079f6
Elvis Attro <attroelvis@gmail.com>
2022-11-04 00:49:47 +0000

    registry/orgs: update orgs models.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/registry/org/pkg/db/model.go
+++ systems/registry/org/pkg/db/model.go
@@ -1,13 +1,33 @@
 package db
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Org struct {
 	gorm.Model
+	ID          uint      `gorm:"primaryKey"`
 	Name        string    `gorm:"uniqueIndex"`
 	Owner       uuid.UUID `gorm:"type:uuid"`
 	Certificate string
+	Members     []*User `gorm:"many2many:org_members;"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
+
+type User struct {
+	ID   uint      `gorm:"primaryKey"`
+	Uuid uuid.UUID `gorm:"uniqueIndex:uuid_unique,where:deleted_at is null;not null;type:uuid"`
+}
+
+type OrgUser struct {
+	OrgID     int `gorm:"primaryKey"`
+	UserID    int `gorm:"primaryKey"`
+	CreatedAt time.Time
+	DeletedAt gorm.DeletedAt
+	// Role (owner, admin, vendor)
 }
