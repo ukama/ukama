a28161b884394903b9f6312b5fc197844c82e877
Elvis Attro <attroelvis@gmail.com>
2022-11-07 01:25:15 +0000

    registry/orgs: update orgs protos & gen files.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/registry/org/pb/gen/org.validator.pb.go
+++ systems/registry/org/pb/gen/org.validator.pb.go
@@ -7,9 +7,9 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "github.com/golang/protobuf/proto"
+	_ "google.golang.org/protobuf/types/known/wrapperspb"
 	_ "github.com/mwitkow/go-proto-validators"
 	_ "google.golang.org/protobuf/types/known/timestamppb"
-	_ "google.golang.org/protobuf/types/known/wrapperspb"
 	regexp "regexp"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
@@ -61,7 +61,13 @@ func (this *GetByOwnerRequest) Validate() error {
 	}
 	return nil
 }
+
+var _regex_GetByOwnerResponse_Owner = regexp.MustCompile(`^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$`)
+
 func (this *GetByOwnerResponse) Validate() error {
+	if !_regex_GetByOwnerResponse_Owner.MatchString(this.Owner) {
+		return github_com_mwitkow_go_proto_validators.FieldError("Owner", fmt.Errorf(`value '%v' must be a string conforming to regex "^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$"`, this.Owner))
+	}
 	for _, item := range this.Orgs {
 		if item != nil {
 			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(item); err != nil {
