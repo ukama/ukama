febc7d8366fc54689d32318e639edde3fafe464f
Elvis Attro <attroelvis@gmail.com>
2022-11-08 14:22:22 +0000

    registry/orgs: add a remote users service client provider.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

---
+++ systems/registry/org/pkg/providers/user_client.go
@@ -0,0 +1,45 @@
+package provider
+
+import (
+	"context"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	pb "github.com/ukama/ukama/systems/registry/users/pb/gen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// UserClientProvider  creates a Users client.
+type UserClientProvider interface {
+	GetClient() (pb.UserServiceClient, error)
+}
+
+type userClientProvider struct {
+	userService pb.UserServiceClient
+	userHost    string
+}
+
+func NewUserClientProvider(hssHost string) UserClientProvider {
+	return &userClientProvider{userHost: hssHost}
+}
+
+func (u *userClientProvider) GetClient() (pb.UserServiceClient, error) {
+	if u.userService == nil {
+		var conn *grpc.ClientConn
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		log.Infoln("Connecting to user service ", u.userHost)
+
+		conn, err := grpc.DialContext(ctx, u.userHost, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("Failed to connect to user service %s. Error: %v", u.userHost, err)
+		}
+
+		u.userService = pb.NewUserServiceClient(conn)
+	}
+
+	return u.userService, nil
+}
