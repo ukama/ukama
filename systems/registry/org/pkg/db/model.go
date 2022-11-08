0ac462c52b648eb35a85547d36c34afabf7f8222
Elvis Attro <attroelvis@gmail.com>
2022-11-08 13:52:41 +0000

    registry/orgs: update orgs models.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/registry/org/pkg/db/model.go
+++ systems/registry/org/pkg/db/model.go
@@ -12,7 +12,7 @@ type Org struct {
 	Name        string    `gorm:"uniqueIndex"`
 	Owner       uuid.UUID `gorm:"type:uuid"`
 	Certificate string
-	Members     []*User `gorm:"many2many:org_members;"`
+	Members     []*User `gorm:"many2many:org_users"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	Deactivated bool
@@ -20,15 +20,18 @@ type Org struct {
 }
 
 type User struct {
-	ID   uint      `gorm:"primaryKey"`
-	Uuid uuid.UUID `gorm:"uniqueIndex:uuid_unique;not null;type:uuid"`
+	ID          uint      `gorm:"primaryKey"`
+	Uuid        uuid.UUID `gorm:"uniqueIndex:uuid_unique,where:deleted_at is null;not null;type:uuid"`
+	Deactivated bool
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
 
 type OrgUser struct {
-	OrgID       uint `gorm:"primaryKey"`
-	UserID      uint `gorm:"primaryKey"`
-	CreatedAt   time.Time
-	DeletedAt   gorm.DeletedAt
+	OrgID       uint      `gorm:"primaryKey"`
+	UserID      uint      `gorm:"primaryKey"`
+	Uuid        uuid.UUID `gorm:"not null;type:uuid"`
 	Deactivated bool
+	CreatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 	// Role (owner, admin, vendor)
 }
