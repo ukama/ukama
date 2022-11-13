f7f47f11906253037f7f43c78edad942656f68c8
Elvis Attro <attroelvis@gmail.com>
2022-11-13 11:30:19 +0000

    registry/users: update user service server & config.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/registry/users/cmd/server/main.go
+++ systems/registry/users/cmd/server/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"os"
 
+	"github.com/num30/config"
 	"github.com/ukama/ukama/systems/common/metrics"
 	"github.com/ukama/ukama/systems/registry/users/pkg/server"
+	"gopkg.in/yaml.v2"
 
 	"github.com/ukama/ukama/systems/registry/users/pkg"
 
@@ -12,16 +14,21 @@ import (
 
 	"github.com/ukama/ukama/systems/registry/users/pkg/db"
 
+	provider "github.com/ukama/ukama/systems/registry/users/pkg/providers"
+
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	ccmd "github.com/ukama/ukama/systems/common/cmd"
-	"github.com/ukama/ukama/systems/common/config"
+
+	uconf "github.com/ukama/ukama/systems/common/config"
+
 	ugrpc "github.com/ukama/ukama/systems/common/grpc"
 	"github.com/ukama/ukama/systems/common/sql"
 	"github.com/ukama/ukama/systems/registry/users/pb/gen"
 	"google.golang.org/grpc"
 )
 
-var serviceConfig *pkg.Config
+var svcConf *pkg.Config
 
 func main() {
 	ccmd.ProcessVersionArgument(pkg.ServiceName, os.Args, version.Version)
@@ -31,22 +38,43 @@ func main() {
 
 	usersDb := initDb()
 
-	metrics.StartMetricsServer(serviceConfig.Metrics)
+	metrics.StartMetricsServer(svcConf.Metrics)
 
 	runGrpcServer(usersDb)
 }
 
 // initConfig reads in config file, ENV variables, and flags if set.
 func initConfig() {
-	serviceConfig = pkg.NewConfig()
-	config.LoadConfig(pkg.ServiceName, serviceConfig)
-	pkg.IsDebugMode = serviceConfig.DebugMode
+	svcConf = &pkg.Config{
+		DB: &uconf.Database{
+			DbName: pkg.ServiceName,
+		},
+		Grpc: &uconf.Grpc{
+			Port: 9090,
+		},
+		Metrics: &uconf.Metrics{
+			Port: 10250,
+		},
+	}
+
+	err := config.NewConfReader(pkg.ServiceName).Read(svcConf)
+	if err != nil {
+		log.Fatalf("Error reading config file. Error: %v", err)
+	} else if svcConf.DebugMode {
+		// output config in debug mode
+		b, err := yaml.Marshal(svcConf)
+		if err != nil {
+			logrus.Infof("Config:\n%s", string(b))
+		}
+	}
+
+	pkg.IsDebugMode = svcConf.DebugMode
 }
 
 func initDb() sql.Db {
 	log.Infof("Initializing Database")
 
-	d := sql.NewDb(serviceConfig.DB, serviceConfig.DebugMode)
+	d := sql.NewDb(svcConf.DB, svcConf.DebugMode)
 
 	err := d.Init(&db.User{})
 	if err != nil {
@@ -57,8 +85,11 @@ func initDb() sql.Db {
 }
 
 func runGrpcServer(gormdb sql.Db) {
-	userService := server.NewUserService(db.NewUserRepo(gormdb))
-	grpcServer := ugrpc.NewGrpcServer(*serviceConfig.Grpc, func(s *grpc.Server) {
+	userService := server.NewUserService(db.NewUserRepo(gormdb),
+
+		provider.NewOrgClientProvider(svcConf.OrgHost),
+	)
+	grpcServer := ugrpc.NewGrpcServer(*svcConf.Grpc, func(s *grpc.Server) {
 		gen.RegisterUserServiceServer(s, userService)
 	})
 
