6a0fa1bd50f9f277cf52b145de45ffc1b3e9c4fb
Elvis Attro <attroelvis@gmail.com>
2022-11-20 10:14:40 +0000

    registry/net: add unit test for net service GetSite not found.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/registry/network/pkg/server/network_test.go
+++ systems/registry/network/pkg/server/network_test.go
@@ -43,6 +43,7 @@ func TestNetworkServer_AddNetwork(t *testing.T) {
 
 		// Assert
 		assert.NoError(t, err)
+		assert.NotNil(t, res)
 		assert.Equal(t, orgName, res.Org)
 		assert.Equal(t, netName, res.Network.Name)
 		netRepo.AssertExpectations(t)
@@ -68,6 +69,7 @@ func TestNetworkServer_Get(t *testing.T) {
 			NetworkID: netID})
 
 		assert.NoError(t, err)
+		assert.NotNil(t, netResp)
 		assert.Equal(t, uint64(netID), netResp.GetNetwork().GetId())
 		assert.Equal(t, netName, netResp.Network.Name)
 		netRepo.AssertExpectations(t)
@@ -94,6 +96,7 @@ func TestNetworkServer_GetByName(t *testing.T) {
 			Name: netName, OrgName: orgName})
 
 		assert.NoError(t, err)
+		assert.NotNil(t, netResp)
 		assert.Equal(t, uint64(netID), netResp.GetNetwork().GetId())
 		assert.Equal(t, netName, netResp.Network.Name)
 		netRepo.AssertExpectations(t)
@@ -129,6 +132,7 @@ func TestNetworkServer_GetByOrg(t *testing.T) {
 			&pb.GetByOrgRequest{OrgName: orgName})
 
 		assert.NoError(t, err)
+		assert.NotNil(t, netResp)
 		assert.Equal(t, uint64(netID), netResp.GetNetworks()[0].GetId())
 		assert.Equal(t, orgName, netResp.Org)
 		netRepo.AssertExpectations(t)
@@ -164,10 +168,11 @@ func TestNetworkServer_Delete(t *testing.T) {
 		netRepo.On("Delete", orgName, netName).Return(gorm.ErrRecordNotFound).Once()
 
 		s := NewNetworkServer(netRepo, nil, nil, nil)
-		_, err := s.Delete(context.TODO(), &pb.DeleteRequest{
+		netResp, err := s.Delete(context.TODO(), &pb.DeleteRequest{
 			Name: netName, OrgName: orgName})
 
 		assert.Error(t, err)
+		assert.Nil(t, netResp)
 		netRepo.AssertExpectations(t)
 	})
 }
@@ -207,6 +212,7 @@ func TestNetworkServer_AddSite(t *testing.T) {
 
 		// Assert
 		assert.NoError(t, err)
+		assert.NotNil(t, res)
 		assert.Equal(t, siteName, res.Site.Name)
 		assert.Equal(t, uint64(netID), res.Site.NetworkID)
 		netRepo.AssertExpectations(t)
@@ -232,8 +238,26 @@ func TestNetworkServer_GetSite(t *testing.T) {
 			SiteID: siteID})
 
 		assert.NoError(t, err)
+		assert.NotNil(t, netResp)
 		assert.Equal(t, uint64(siteID), netResp.GetSite().GetId())
 		assert.Equal(t, siteName, netResp.GetSite().GetName())
+		siteRepo.AssertExpectations(t)
+	})
+
+	t.Run("Site not found", func(t *testing.T) {
+		const siteID = 1
+		const siteName = "site-A"
+
+		siteRepo := &mocks.SiteRepo{}
+
+		siteRepo.On("Get", uint(siteID)).Return(nil, gorm.ErrRecordNotFound).Once()
+
+		s := NewNetworkServer(nil, nil, siteRepo, nil)
+		netResp, err := s.GetSite(context.TODO(), &pb.GetSiteRequest{
+			SiteID: siteID})
+
+		assert.Error(t, err)
+		assert.Nil(t, netResp)
 		siteRepo.AssertExpectations(t)
 	})
 }
