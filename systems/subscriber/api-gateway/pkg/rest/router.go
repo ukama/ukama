f868afca60ea3ee2828c158c2c4b79b625fb43d5
vthakur7f <vthakur7@hotmail.com>
2022-12-14 12:54:40 -0800

    Updated all routes.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/subscriber/api-gateway/pkg/rest/router.go
+++ systems/subscriber/api-gateway/pkg/rest/router.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ukama/ukama/systems/common/rest"
 	"github.com/ukama/ukama/systems/subscriber/api-gateway/cmd/version"
 	"github.com/ukama/ukama/systems/subscriber/api-gateway/pkg"
+	"github.com/ukama/ukama/systems/subscriber/api-gateway/pkg/client"
 	"github.com/wI2L/fizz/openapi"
 )
 
@@ -86,18 +87,28 @@ func (r *Router) init() {
 
 	r.f = rest.NewFizzRouter(r.config.serverConf, pkg.SystemName, version.Version, r.config.debugMode)
 	v1 := r.f.Group("/v1", "subscriber system ", "subscriber system version v1")
+	/* These two API will be available based on RBAC */
+	v1.GET("/susbscribers/networks/:networkid", formatDoc("List all subscibers for a Network", ""), tonic.Handler(r.getAllSubscribers, http.StatusOK))
+	v1.GET("/sims/networks/:networkid", formatDoc("List all sims for a Network", ""), tonic.Handler(r.getAllSims, http.StatusOK))
 
 	pool := v1.Group("/simpool", "SIM Pool", "SIM store for Org")
 	pool.GET("/stats", formatDoc("Get SIM Pool stats", ""), tonic.Handler(r.getSimPoolStats, http.StatusOK))
 	pool.PUT("/sim", formatDoc("Add new SIM to SIM pool", ""), tonic.Handler(r.addSimsToSimPool, http.StatusCreated))
 	pool.PUT("/sim/upload", formatDoc("Upload CSV file to add new sim to SIM Pool", ""), tonic.Handler(r.uploadSimsToSimPool, http.StatusCreated))
-	pool.DELETE("/sim", formatDoc("Remove SIM from SIM Pool", ""), tonic.Handler(r.deleteSimPool, http.StatusOK))
+	pool.DELETE("/sim", formatDoc("Remove SIM from SIM Pool", ""), tonic.Handler(r.deleteSimFromSimPool, http.StatusOK))
 
 	subscriber := v1.Group(subs, "Subscriber", "Orgs Subscriber database")
 	subscriber.GET("", formatDoc("Get System credential for Org", ""), tonic.Handler(r.getSubscriber, http.StatusOK))
+	subscriber.PUT("", formatDoc("Add a new subsciber", ""), tonic.Handler(r.putSubscriber, http.StatusOK))
+	subscriber.DELETE("", formatDoc("Delete a subsciber", ""), tonic.Handler(r.deleteSubscriber, http.StatusOK))
 
 	sim := subscriber.Group("/sim", "SIM", "Orgs SIM data base")
-	sim.GET("/:sim", formatDoc("Get Orgs credential for Node", ""), tonic.Handler(r.getSim, http.StatusOK))
+	sim.GET("/:sim", formatDoc("Get a SIM of the subscriber by SIM Id", ""), tonic.Handler(r.getSim, http.StatusOK))
+	sim.POST("/:sim", formatDoc("Allocate  anew sim to subsciber", ""), tonic.Handler(r.allocateSim, http.StatusOK))
+	sim.POST("/:sim/package", formatDoc("Add a new package to the subscriber's sim", ""), tonic.Handler(r.postPackageToSim, http.StatusOK))
+	sim.DELETE("/:sim/package", formatDoc("Delete a package from subscriber's sim", ""), tonic.Handler(r.deletePackageForSim, http.StatusOK))
+	sim.DELETE("/:sim", formatDoc("Delete the SIM for the subscriber", ""), tonic.Handler(r.deleteSim, http.StatusOK))
+	sim.PATCH("/:sim", formatDoc("Update the SIM state to active/inactive for the subscriber's sim", ""), tonic.Handler(r.patchSim, http.StatusOK))
 
 }
 
@@ -106,6 +117,14 @@ func formatDoc(summary string, description string) []fizz.OperationOption {
 		info.Summary = summary
 		info.Description = description
 	}}
+}
+
+func (r *Router) getAllSubscribers(c *gin.Context) error {
+	return nil
+}
+
+func (r *Router) getAllSims(c *gin.Context) error {
+	return nil
 }
 
 func (r *Router) getSimPoolStats(c *gin.Context) error {
@@ -120,7 +139,7 @@ func (r *Router) uploadSimsToSimPool(c *gin.Context) error {
 	return nil
 }
 
-func (r *Router) deleteSimPool(c *gin.Context) error {
+func (r *Router) deleteSimFromSimPool(c *gin.Context) error {
 	return nil
 }
 
@@ -128,6 +147,34 @@ func (r *Router) getSubscriber(c *gin.Context) error {
 	return nil
 }
 
+func (r *Router) putSubscriber(c *gin.Context) error {
+	return nil
+}
+
+func (r *Router) deleteSubscriber(c *gin.Context) error {
+	return nil
+}
+
 func (r *Router) getSim(c *gin.Context) error {
+	return nil
+}
+
+func (r *Router) allocateSim(c *gin.Context) error {
+	return nil
+}
+
+func (r *Router) postPackageToSim(c *gin.Context) error {
+	return nil
+}
+
+func (r *Router) deletePackageForSim(c *gin.Context) error {
+	return nil
+}
+
+func (r *Router) deleteSim(c *gin.Context) error {
+	return nil
+}
+
+func (r *Router) patchSim(c *gin.Context) error {
 	return nil
 }
