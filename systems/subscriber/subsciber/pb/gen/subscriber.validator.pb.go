d7bdcc555b2a701aeeb9a6e27110d966bcbf4b6d
Brackley cassinga <pcassinga@gmail.com>
2022-12-21 15:28:15 +0200

    proto updated

--- systems/subscriber/subsciber/pb/gen/subscriber.validator.pb.go
+++ systems/subscriber/subsciber/pb/gen/subscriber.validator.pb.go
@@ -56,6 +56,7 @@ func (this *GetSubscriberResponse) Validate() error {
 
 var _regex_AddSubscriberRequest_Email = regexp.MustCompile(`^$|^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 var _regex_AddSubscriberRequest_PhoneNumber = regexp.MustCompile(`^$|^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$`)
+var _regex_AddSubscriberRequest_NetworkID = regexp.MustCompile(`^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$`)
 
 func (this *AddSubscriberRequest) Validate() error {
 	if this.FirstName == "" {
@@ -75,6 +76,12 @@ func (this *AddSubscriberRequest) Validate() error {
 	}
 	if !_regex_AddSubscriberRequest_PhoneNumber.MatchString(this.PhoneNumber) {
 		return github_com_mwitkow_go_proto_validators.FieldError("PhoneNumber", fmt.Errorf(`must be a phone number format`))
+	}
+	if !_regex_AddSubscriberRequest_NetworkID.MatchString(this.NetworkID) {
+		return github_com_mwitkow_go_proto_validators.FieldError("NetworkID", fmt.Errorf(`value '%v' must be a string conforming to regex "^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$"`, this.NetworkID))
+	}
+	if this.NetworkID == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("NetworkID", fmt.Errorf(`value '%v' must not be an empty string`, this.NetworkID))
 	}
 	if this.ProofOfIdentification == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("ProofOfIdentification", fmt.Errorf(`value '%v' must not be an empty string`, this.ProofOfIdentification))
