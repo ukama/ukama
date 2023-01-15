b494df29c41735086a888ba5d520b23706cfe005
Elvis Attro <attroelvis@gmail.com>
2023-01-15 13:37:27 +0000

    subscriber/sim-manager: add OrgID to sim model.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/subscriber/sim-manager/pkg/db/model.go
+++ systems/subscriber/sim-manager/pkg/db/model.go
@@ -13,6 +13,7 @@ type Sim struct {
 	ID                 uuid.UUID `gorm:"primaryKey;type:uuid"`
 	SubscriberID       uuid.UUID `gorm:"not null;type:uuid"`
 	NetworkID          uuid.UUID `gorm:"not null;type:uuid"`
+	OrgID              uuid.UUID `gorm:"not null;type:uuid"`
 	Package            Package
 	Iccid              string `gorm:"index:idx_iccid,unique"`
 	Msisdn             string
--- systems/subscriber/sim-manager/pkg/db/sim_repo_test.go
+++ systems/subscriber/sim-manager/pkg/db/sim_repo_test.go
@@ -358,7 +358,7 @@ func TestSimRepo_Add(t *testing.T) {
 
 		mock.ExpectExec(regexp.QuoteMeta(`INSERT`)).
 			WithArgs(sim.ID, sim.SubscriberID, sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(),
-				sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(),
+				sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(),
 				sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
 			WillReturnResult(sqlmock.NewResult(1, 1))
 
