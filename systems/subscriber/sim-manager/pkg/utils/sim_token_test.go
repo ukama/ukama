1a053b94a423ddcf29f9d50130c5a19364c0dd22
Elvis Attro <attroelvis@gmail.com>
2023-01-16 11:04:04 +0000

    subscriber/sim-amanger: add support for sim token.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

---
+++ systems/subscriber/sim-manager/pkg/utils/sim_token_test.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const textToEncode = "{ 'iccid': '8910300000003540855' }"
+const testKey = "the-key-has-to-be-32-bytes-long!"
+
+func Test_encrypt(t *testing.T) {
+
+	r, err := encrypt(textToEncode, testKey)
+	fmt.Println(r)
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, "encrypt failed")
+	}
+	res, err := decrypt(r, testKey)
+	assert.NoError(t, err)
+	fmt.Println(res)
+	assert.Equal(t, textToEncode, res)
+}
+
+func Test_encryptErrors(t *testing.T) {
+
+	tests := []struct {
+		key  string
+		text string
+	}{
+		{key: "short testKey", text: textToEncode},
+		{key: "", text: textToEncode},
+	}
+	for _, tt := range tests {
+		_, err := encrypt(tt.text, tt.key)
+		assert.Error(t, err)
+	}
+}
+
+func Test_decryptErrors(t *testing.T) {
+	encrText, err := encrypt(textToEncode, testKey)
+	if err != nil {
+		assert.FailNow(t, "encrypt failed", err)
+	}
+
+	tests := []struct {
+		key  string
+		text string
+	}{
+		{key: "short key", text: encrText},
+		{key: "", text: textToEncode},
+		{key: "", text: "Not encoded text"},
+	}
+	for _, tt := range tests {
+		_, err := decrypt(tt.text, tt.key)
+		assert.Error(t, err)
+	}
+}
