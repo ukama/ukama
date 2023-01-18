4c8b35455af9141518d365131fa1e37336942dc9
Elvis Attro <attroelvis@gmail.com>
2023-01-18 12:29:01 +0000

    subscriber/sim-manager: update test agent adapter.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/subscriber/sim-manager/pkg/clients/adapters/test_agent_adapter.go
+++ systems/subscriber/sim-manager/pkg/clients/adapters/test_agent_adapter.go
@@ -5,8 +5,6 @@ import (
 
 	pb "github.com/ukama/ukama/systems/subscriber/test-agent/pb/gen"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type TestAgentAdapter struct {
@@ -15,21 +13,22 @@ type TestAgentAdapter struct {
 	client pb.TestAgentServiceClient
 }
 
-func (t *TestAgentAdapter) ActivateSim(ctx context.Context, simID string) error {
-	_, err := t.client.ActivateSim(ctx, &pb.ActivateSimRequest{SimID: simID})
+func (t *TestAgentAdapter) ActivateSim(ctx context.Context, iccid string) error {
+	_, err := t.client.ActivateSim(ctx, &pb.ActivateSimRequest{Iccid: iccid})
 
 	return err
 }
 
-func (t *TestAgentAdapter) DeactivateSim(ctx context.Context, simID string) error {
-	_, err := t.client.DeactivateSim(ctx, &pb.DeactivateSimRequest{SimID: simID})
+func (t *TestAgentAdapter) DeactivateSim(ctx context.Context, iccid string) error {
+	_, err := t.client.DeactivateSim(ctx, &pb.DeactivateSimRequest{Iccid: iccid})
 
 	return err
 }
 
-func (t *TestAgentAdapter) TerminateSim(ctx context.Context, simID string) error {
-	// Add final implementation here for impelement remote call
-	return status.Errorf(codes.Unimplemented, "must implentent TerminateSim on both adapter and remove server")
+func (t *TestAgentAdapter) TerminateSim(ctx context.Context, iccid string) error {
+	_, err := t.client.TerminateSim(ctx, &pb.TerminateSimRequest{Iccid: iccid})
+
+	return err
 }
 
 func (t *TestAgentAdapter) Close() {
