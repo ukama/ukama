c7ed90833e055e63dfce9f6e7b1f1a8bcf2199d2
Brackley cassinga <pcassinga@gmail.com>
2023-02-08 20:31:11 +0200

    RestClient added to common pkg (#404)

---
+++ systems/common/rest/restClient.go
@@ -0,0 +1,28 @@
+package rest
+
+import (
+	"net/url"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/sirupsen/logrus"
+)
+
+type RestClient struct {
+	C   *resty.Client
+	URL *url.URL
+}
+
+func NewRestClient(path string, debug bool) (*RestClient, error) {
+	url, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+	c := resty.New()
+	c.SetDebug(debug)
+	rc := &RestClient{
+		C:   c,
+		URL: url,
+	}
+	logrus.Tracef("Client created %+v for %s ", rc, rc.URL.String())
+	return rc, nil
+}
