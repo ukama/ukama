5d8f4e2dcda2ea55e9eed02599022c9dc55f2040
Brackley cassinga <pcassinga@gmail.com>
2023-02-09 18:47:28 +0200

    server test updated

--- systems/subscriber/registry/pkg/server/subscriber_test.go
+++ systems/subscriber/registry/pkg/server/subscriber_test.go
@@ -1,122 +1,52 @@
-package server_test
+package server
 
 import (
 	"context"
-	"fmt"
 	"testing"
-	"time"
 
-	"github.com/sirupsen/logrus"
 	mbmocks "github.com/ukama/ukama/systems/common/mocks"
-	"google.golang.org/protobuf/types/known/timestamppb"
-
-	uuid "github.com/ukama/ukama/systems/common/uuid"
-
-	mocks "github.com/ukama/ukama/systems/subscriber/registry/mocks"
+	"github.com/ukama/ukama/systems/subscriber/registry/mocks"
 	pb "github.com/ukama/ukama/systems/subscriber/registry/pb/gen"
-	"github.com/ukama/ukama/systems/subscriber/registry/pkg/db"
-	"github.com/ukama/ukama/systems/subscriber/registry/pkg/server"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-func TestSubscriberServer_Add(t *testing.T) {
-	subRepo := &mocks.SubscriberRepo{}
-	netRepo := &mocks.Network{}
-	simRepo := &mocks.SimManagerClientProvider{}
+func TestAdd(t *testing.T) {
+	// Test case 1: Add subscriber successfully
+	t.Run("Add subscriber successfully", func(t *testing.T) {
+		subscriberRepo := &mocks.SubscriberRepo{}
+		msgBus := &mbmocks.MsgBusServiceClient{}
+		simManagerService := &mocks.SimManagerClientProvider{}
+		network := &mocks.NetworkInfoClient{}
 
-	msgbusClient := &mbmocks.MsgBusServiceClient{}
-	netID := uuid.NewV4()
-	orgID := uuid.NewV4()
-	mockDate := &timestamppb.Timestamp{
-		Seconds: time.Now().Unix(),
-		Nanos:   0,
-	}
+		subscriberRepo.On("Add", mock.AnythingOfType("*db.Subscriber")).Return(nil)
+		network.On("ValidateNetwork", mock.Anything, mock.Anything).Return(nil)
 
-	sub := &db.Subscriber{
-		SubscriberID:          uuid.NewV4(),
-		FirstName:             "john",
-		LastName:              "Doe",
-		Email:                 "john@gmail.com",
-		NetworkID:             netID,
-		OrgID:                 orgID,
-		PhoneNumber:           "0791240041",
-		Gender:                "male",
-		IdSerial:              "00000",
-		DOB:                   mockDate.AsTime(),
-		ProofOfIdentification: "passwport",
-		Address:               "kigali",
-		CreatedAt:             time.Now(),
-		UpdatedAt:             time.Now(),
-		DeletedAt:             nil,
-	}
+		s := NewSubscriberServer(subscriberRepo, msgBus, simManagerService, network)
 
-	psub := &pb.AddSubscriberRequest{FirstName: "john", LastName: "Doe", Email: "john@gmail.com", NetworkID: netID.String(), OrgID: orgID.String(), PhoneNumber: "0791240041", Gender: "male", IdSerial: "00000", DateOfBirth: mockDate, ProofOfIdentification: "passwport", Address: "kigali"}
+		req := &pb.AddSubscriberRequest{
+			OrgID:               "7e82c8b1-a746-4f2c-a80e-f4d14d863ea3",
+			FirstName:           "John",
+			LastName:            "Doe",
+			NetworkID:           "9e82c8b1-a746-4f2c-a80e-f4d14d863ea3",
+			Email:               "johndoe@example.com",
+			PhoneNumber:         "1234567890",
+			Gender:              "Male",
+			Address:             "1 Main St",
+			ProofOfIdentification: "Passport",
+			IdSerial: "123456789",
+		}
 
-	subRepo.On("Add", sub).Return(nil).Once()
-	msgbusClient.On("PublishRequest", mock.Anything, psub).Return(nil).Once()
+		resp, err := s.Add(context.Background(), req)
 
-	s := server.NewSubscriberServer(subRepo, msgbusClient, nil, netRepo)
-	simRepo.On("GetClient").Return(nil, nil)
-
-	 netRepo.On("ValidateNetwork", netID.String(), orgID.String()).Return(nil).Once()
-	_, err := s.Add(context.TODO(), psub)
-	assert.NoError(t, err)
-	subRepo.AssertExpectations(t)
-}
-func TestSubscriberServer_Update(t *testing.T) {
-	subRepo := &mocks.SubscriberRepo{}
-	msgbusClient := &mbmocks.MsgBusServiceClient{}
-	sub := &db.Subscriber{
-		SubscriberID: uuid.NewV4(),
-		FirstName:    "john",
-		LastName:     "Doe",
-		NetworkID:    uuid.NewV4(),
-		Email:        "john@gmail.com",
-		PhoneNumber:  "0791240041",
-		Gender:       "male",
-	}
-
-	psub := &pb.UpdateSubscriberRequest{Email: "john@gmail.com", PhoneNumber: "0791240041"}
-
-	subRepo.On("GetByNetwork", sub.NetworkID).Return(sub, nil).Once()
-	subRepo.On("Update", sub).Return(nil).Once()
-	msgbusClient.On("PublishRequest", mock.Anything, psub).Return(nil).Once()
-
-	s := server.NewSubscriberServer(subRepo, msgbusClient, nil, nil)
-
-	_, err := s.Update(context.TODO(), psub)
-
-	assert.NoError(t, err)
-	subRepo.AssertExpectations(t)
-}
-
-func TestSubscriberServer_Get(t *testing.T) {
-	subRepo := &mocks.SubscriberRepo{}
-	msgbusClient := &mbmocks.MsgBusServiceClient{}
-	subscriberUUID, err := uuid.FromString("dbe9a556-a626-11ed-afa1-0242ac120002")
-	if err != nil {
-		logrus.Errorf("Invalid format uuid %s", err.Error())
-	}
-	sub := &db.Subscriber{
-		SubscriberID: subscriberUUID,
-		FirstName:    "john",
-		LastName:     "Doe",
-		NetworkID:    uuid.NewV4(),
-		Email:        "john@gmail.com",
-		PhoneNumber:  "0791240041",
-		Gender:       "male",
-	}
-	subRepo.On("Get", sub.SubscriberID).Return(sub, nil).Once()
-
-	s := server.NewSubscriberServer(subRepo, msgbusClient, nil, nil)
-	fmt.Println("BRACKLEY",s)
-
-	resp, err := s.Get(context.TODO(), &pb.GetSubscriberRequest{SubscriberID: subscriberUUID.String()})
-	fmt.Println("VANESSA",resp)
-
-	assert.NoError(t, err)
-	assert.Equal(t, sub.SubscriberID, resp.GetSubscriber())
-	subRepo.AssertExpectations(t)
-}
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		assert.Equal(t, "7e82c8b1-a746-4f2c-a80e-f4d14d863ea3", resp.Subscriber.OrgID)
+		assert.Equal(t, "John", resp.Subscriber.FirstName)
+		assert.Equal(t, "Doe", resp.Subscriber.LastName)
+		assert.Equal(t, "9e82c8b1-a746-4f2c-a80e-f4d14d863ea3", resp.Subscriber.NetworkID)
+		assert.Equal(t, "johndoe@example.com", resp.Subscriber.Email)
+		assert.Equal(t, "1234567890", resp.Subscriber.PhoneNumber)
+		assert.Equal(t, "Male", resp.Subscriber.Gender)
+	})}
