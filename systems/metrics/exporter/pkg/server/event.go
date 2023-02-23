22c415719944ef2a703eeb5edaa3fae4d6756b7e
vthakur7f <vthakur7@hotmail.com>
2023-02-22 17:06:19 -0800

    No dynamic labels.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/metrics/exporter/pkg/server/event.go
+++ systems/metrics/exporter/pkg/server/event.go
@@ -13,7 +13,7 @@ import (
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-var customLabels = []string{"test"}
+var customLabelsSimUsage = []string{"session", "start", "end"}
 
 type ExporterEventServer struct {
 	mc *collector.MetricsCollector
@@ -78,17 +78,17 @@ func AddSimUsage(key string, msg *pb.SimUsage, s *ExporterEventServer) error {
 	m, err := s.mc.GetMetric(n)
 	if err == nil {
 		/* Update value */
-		return m.SetMetric(float64(msg.BytesUsed), SetUpDynamicLabelsForSimUsage(msg))
+		return m.SetMetric(float64(msg.BytesUsed), nil)
 
 	} else {
 		l := SetUpStaticLabelsForSimUsage(msg)
 
-		m, err := collector.SetUpMetric(key, s.mc, l, n, []string{"session", "start", "end"})
+		m, err := collector.SetUpMetric(key, s.mc, l, n, nil)
 		if err != nil {
 			return err
 		}
 
-		m.SetMetric(float64(msg.BytesUsed), SetUpDynamicLabelsForSimUsage(msg))
+		m.SetMetric(float64(msg.BytesUsed), nil)
 
 	}
 	return nil
@@ -101,17 +101,17 @@ func AddSimUsageDuration(key string, msg *pb.SimUsage, s *ExporterEventServer) e
 	m, err := s.mc.GetMetric(n)
 	if err == nil {
 		/* Update value */
-		return m.SetMetric(float64(msg.EndTime-msg.StartTime), SetUpDynamicLabelsForSimUsageDuration(msg))
+		return m.SetMetric(float64(msg.EndTime-msg.StartTime), nil)
 
 	} else {
 		l := SetUpStaticLabelsForSimUsageDuration(msg)
 
-		m, err := collector.SetUpMetric(key, s.mc, l, n, []string{"session", "start", "end"})
+		m, err := collector.SetUpMetric(key, s.mc, l, n, nil)
 		if err != nil {
 			return err
 		}
 
-		m.SetMetric(float64(msg.EndTime-msg.StartTime), SetUpDynamicLabelsForSimUsageDuration(msg))
+		m.SetMetric(float64(msg.EndTime-msg.StartTime), nil)
 
 	}
 	return nil
@@ -152,3 +152,23 @@ func SetUpDynamicLabelsForSimUsageDuration(msg *pb.SimUsage) map[string]string {
 	labels["end"] = strconv.FormatInt(msg.EndTime, 10)
 	return labels
 }
+
+
+/*
+
+sum_over_time(sim_usage_b20c61f11c5a4559bfffcd00f746697d_sum[5m])
+
+- increase(sim_usage_b20c61f11c5a4559bfffcd00f746697d_sum[10m])
+{instance="localhost:10251", job="ukama-org", name="usage", network="9fd07299-2826-4f8b-aea9-69da56440bec", org="75ec112a-8745-49f9-ab64-1a37edade794", sim="b20c61f1-1c5a-4559-bfff-cd00f746697d", sim_type="test_simple", subscriber="c214f255-0ed6-4aa1-93e7-e333658c7318", system="dev"}
+	194.87179487179486
+- sum(sim_usage_b20c61f11c5a4559bfffcd00f746697d_sum)
+{} 360
+
+- sum_over_time(sim_usage_b20c61f11c5a4559bfffcd00f746697d_sum[2m])
+{instance="localhost:10251", job="ukama-org", name="usage", network="9fd07299-2826-4f8b-aea9-69da56440bec", org="75ec112a-8745-49f9-ab64-1a37edade794", sim="b20c61f1-1c5a-4559-bfff-cd00f746697d", sim_type="test_simple", subscriber="c214f255-0ed6-4aa1-93e7-e333658c7318", system="dev"} 2880
+
+sum_over_time(sim_usage_b20c61f11c5a4559bfffcd00f746697d_sum[15s])
+{instance="localhost:10251", job="ukama-org", name="usage", network="9fd07299-2826-4f8b-aea9-69da56440bec", org="75ec112a-8745-49f9-ab64-1a37edade794", sim="b20c61f1-1c5a-4559-bfff-cd00f746697d", sim_type="test_simple", subscriber="c214f255-0ed6-4aa1-93e7-e333658c7318", system="dev"}
+	420
+
+	
