ed7bd3042b357d1f8a71b7f810c96002ca1018ed
salman-codewar <salmannaseem131@gmail.com>
2023-03-07 22:09:07 +0500

    Subscriber API GW example values added
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- systems/subscriber/api-gateway/pkg/rest/api.go
+++ systems/subscriber/api-gateway/pkg/rest/api.go
@@ -3,7 +3,6 @@ package rest
 import (
 	"time"
 
-	uuid "github.com/ukama/ukama/systems/common/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -47,32 +46,32 @@ type Package struct {
 }
 
 type Subscriber struct {
-	SubscriberId          uuid.UUID `json:"subscriber_id" validate:"required"`
-	FirstName             string    `json:"first_name" validate:"required"`
-	LastName              string    `json:"last_name" validate:"required"`
-	Email                 string    `json:"email" validate:"email,required"`
-	Phone                 string    `json:"phone" validate:"required"`
-	Dob                   time.Time `json:"dob" validate:"required"`
-	ProofOfIdentification string    `json:"proof_of_Identification" validate:"required"`
-	IdSerial              string    `json:"id_serial" validate:"required"`
-	Address               string    `json:"address" validate:"required"`
-	SimList               []SIM     `json:"sims" validate:"required"`
+	SubscriberId          string `json:"subscriber_id" validate:"required"`
+	FirstName             string `json:"first_name" validate:"required"`
+	LastName              string `json:"last_name" validate:"required"`
+	Email                 string `json:"email" validate:"email,required"`
+	Phone                 string `json:"phone" validate:"required"`
+	Dob                   string `json:"dob" validate:"required"`
+	ProofOfIdentification string `json:"proof_of_Identification" validate:"required"`
+	IdSerial              string `json:"id_serial" validate:"required"`
+	Address               string `json:"address" validate:"required"`
+	SimList               []SIM  `json:"sims" validate:"required"`
 }
 
 type SimByIccidReq struct {
-	Iccid string `path:"iccid" validate:"required"`
+	Iccid string `example:"8910300000003540855" path:"iccid" validate:"required"`
 }
 
 type SimPoolStatByTypeReq struct {
-	SimType string `form:"sim_type" json:"sim_type" path:"sim_type" binding:"required" validate:"required"`
+	SimType string `example:"test" form:"sim_type" json:"sim_type" path:"sim_type" binding:"required" validate:"required"`
 }
 
 type SimPoolRemoveSimReq struct {
-	Id []uint64 `form:"id" json:"id" path:"sim_id" binding:"required" validate:"required"`
+	Id []uint64 `example:"[1]" form:"id" json:"id" path:"sim_id" binding:"required" validate:"required"`
 }
 
 type SimPoolUploadSimReq struct {
-	SimType string `form:"sim_type" json:"sim_type" binding:"required" validate:"required"`
+	SimType string `example:"test" form:"sim_type" json:"sim_type" binding:"required" validate:"required"`
 }
 
 type SimPoolAddSimReq struct {
@@ -80,83 +79,79 @@ type SimPoolAddSimReq struct {
 }
 
 type SubscriberAddReq struct {
-	FirstName             string `json:"first_name" validate:"required"`
-	LastName              string `json:"last_name" validate:"required"`
-	Email                 string `json:"email" validate:"required"`
-	Phone                 string `json:"phone" validate:"required"`
-	Dob                   string `json:"dob" validate:"required"`
-	ProofOfIdentification string `json:"proof_of_Identification" validate:"required"`
-	IdSerial              string `json:"id_serial" validate:"required"`
-	Address               string `json:"address" validate:"required"`
-	NetworkId             string `json:"network_id" validate:"required"`
-	Gender                string `json:"gender" validate:"required"`
-	OrgId                 string `json:"org_id" validate:"required"`
+	FirstName             string `example:"John" json:"first_name" validate:"required"`
+	LastName              string `example:"Doe" json:"last_name" validate:"required"`
+	Email                 string `example:"john@example.com" json:"email" validate:"required"`
+	Phone                 string `example:"4151231234" json:"phone" validate:"required"`
+	Dob                   string `example:"Mon, 02 Jan 2006 15:04:05 MST" json:"dob" validate:"required"`
+	ProofOfIdentification string `example:"passport" json:"proof_of_Identification" validate:"required"`
+	IdSerial              string `example:"123456789" json:"id_serial" validate:"required"`
+	Address               string `example:"Mr John Smith. 132, My Street, Kingston, New York 12401" json:"address" validate:"required"`
+	NetworkId             string `example:"NetworkUUID" json:"network_id" validate:"required"`
+	Gender                string `example:"male" json:"gender" validate:"required"`
+	OrgId                 string `example:"OrgUUID" json:"org_id" validate:"required"`
 }
 
 type SubscriberGetReq struct {
-	SubscriberId string `form:"subscriber_id" json:"subscriber_id" path:"subscriber_id" binding:"required" validate:"required"`
+	SubscriberId string `example:"SubscriberUUID" form:"subscriber_id" json:"subscriber_id" path:"subscriber_id" binding:"required" validate:"required"`
 }
 
 type SubscriberDeleteReq struct {
-	SubscriberId string `form:"subscriber_id" json:"subscriber_id" path:"subscriber_id" binding:"required" validate:"required"`
+	SubscriberId string `example:"SubscriberUUID" form:"subscriber_id" json:"subscriber_id" path:"subscriber_id" binding:"required" validate:"required"`
 }
 
 type SubscriberByNetworkReq struct {
-	NetworkId string `form:"network_id" json:"network_id" path:"network_id" binding:"required" validate:"required"`
+	NetworkId string `example:"NetworkUUID" form:"network_id" json:"network_id" path:"network_id" binding:"required" validate:"required"`
 }
 
 type SubscriberUpdateReq struct {
-	SubscriberId          string `path:"subscriber_id" validate:"required"`
-	Email                 string `json:"email"`
-	Phone                 string `json:"phone"`
-	Address               string `json:"address"`
-	ProofOfIdentification string `json:"proof_of_Identification"`
-	IdSerial              string `json:"id_serial"`
+	SubscriberId          string `example:"SubscriberUUID" path:"subscriber_id" validate:"required"`
+	Email                 string `example:"test@example.com" json:"email"`
+	Phone                 string `example:"4151231234" json:"phone"`
+	Address               string `example:"Mr John Smith. 132, My Street, Kingston, New York 12401" json:"address"`
+	ProofOfIdentification string `example:"passport" json:"proof_of_Identification"`
+	IdSerial              string `example:"123456789" json:"id_serial"`
 }
 
 type SimListReq struct {
-	NetworkId uuid.UUID `form:"network_id" json:"network_id" path:"network_id" binding:"required" validate:"required"`
-}
-
-type SimListResp struct {
-	Subscribers []Subscriber `json:"subscribers"`
+	NetworkId string `example:"NetworkUUID" form:"network_id" json:"network_id" path:"network_id" binding:"required" validate:"required"`
 }
 
 type AllocateSimReq struct {
-	SubscriberId string `json:"subscriber_id" validate:"required"`
-	SimToken     string `json:"sim_token"`
-	PackageId    string `json:"package_id" validate:"required"`
-	NetworkId    string `json:"network_id" validate:"required"`
-	SimType      string `json:"sim_type" validate:"required"`
+	SubscriberId string `example:"SubscriberUUID" json:"subscriber_id" validate:"required"`
+	SimToken     string `example:"pj/9A5Hk8VkkZxOJyu0+9fWs7J6HCOjhmD5jEsIvOfZqmFFFMyStgC3Va4l1b6I5+2ibKOsJjR9KGug=" json:"sim_token"`
+	PackageId    string `example:"PackageUUID" json:"package_id" validate:"required"`
+	NetworkId    string `example:"NetworkUUID" json:"network_id" validate:"required"`
+	SimType      string `example:"test" json:"sim_type" validate:"required"`
 }
 
 type SetActivePackageForSimReq struct {
-	SimId     string `path:"sim_id" validate:"required"`
-	PackageId string `path:"package_id" validate:"required"`
+	SimId     string `example:"SimUUID" path:"sim_id" validate:"required"`
+	PackageId string `example:"PackageUUID" path:"package_id" validate:"required"`
 }
 type SimReq struct {
-	SimId string `form:"sim_id" json:"sim_id" path:"sim_id" binding:"required" validate:"required"`
+	SimId string `example:"SimUUID" form:"sim_id" json:"sim_id" path:"sim_id" binding:"required" validate:"required"`
 }
 
 type SimByNetworkReq struct {
-	NetworkId string `form:"network_id" json:"network_id" path:"network_id" binding:"required" validate:"required"`
+	NetworkId string `example:"NetworkUUID" form:"network_id" json:"network_id" path:"network_id" binding:"required" validate:"required"`
 }
 
 type ActivateDeactivateSimReq struct {
-	SimId  string `path:"sim_id" validate:"required"`
-	Status string `json:"status" binding:"required" validate:"required"`
+	SimId  string `example:"SimUUID" path:"sim_id" validate:"required"`
+	Status string `example:"active" json:"status" binding:"required" validate:"required"`
 }
 
 type GetSimsBySubReq struct {
-	SubscriberId string `form:"subscriber_id" json:"subscriber_id" path:"subscriber_id" binding:"required" validate:"required"`
+	SubscriberId string `example:"SubscriberUUID" form:"subscriber_id" json:"subscriber_id" path:"subscriber_id" binding:"required" validate:"required"`
 }
 type AddPkgToSimReq struct {
-	SimId     string                 `json:"sim_id" validate:"required"`
-	PackageId string                 `json:"package_id" validate:"required"`
-	StartDate *timestamppb.Timestamp `json:"start_date" validate:"required"`
+	SimId     string                 `example:"SimUUID" json:"sim_id" validate:"required"`
+	PackageId string                 `example:"PackageUUID" json:"package_id" validate:"required"`
+	StartDate *timestamppb.Timestamp `example:"" json:"start_date" validate:"required"`
 }
 
 type RemovePkgFromSimReq struct {
-	SimId     string `form:"sim_id" json:"sim_id" path:"sim_id" binding:"required" validate:"required"`
-	PackageId string `form:"package_id" json:"package_id" path:"package_id" binding:"required" validate:"required"`
+	SimId     string `example:"SimUUID" form:"sim_id" json:"sim_id" path:"sim_id" binding:"required" validate:"required"`
+	PackageId string `example:"PackageUUID" form:"package_id" json:"package_id" path:"package_id" binding:"required" validate:"required"`
 }
