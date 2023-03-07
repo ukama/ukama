fe4ad8567ad4a8cec97d59a234c3be5ece60a7a8
Brackley cassinga <pcassinga@gmail.com>
2023-03-07 12:49:24 +0200

    conflict resolve with bas-branch

--- systems/registry/org/pb/gen/mocks/OrgServiceServer.go
+++ systems/registry/org/pb/gen/mocks/OrgServiceServer.go
@@ -175,29 +175,6 @@ func (_m *OrgServiceServer) GetMembers(_a0 context.Context, _a1 *gen.GetMembersR
 	return r0, r1
 }
 
-// GetRunningOrg provides a mock function with given fields: _a0, _a1
-func (_m *OrgServiceServer) GetRunningOrg(_a0 context.Context, _a1 *gen.GetRunningOrgRequest) (*gen.GetRunningOrgResponse, error) {
-	ret := _m.Called(_a0, _a1)
-
-	var r0 *gen.GetRunningOrgResponse
-	if rf, ok := ret.Get(0).(func(context.Context, *gen.GetRunningOrgRequest) *gen.GetRunningOrgResponse); ok {
-		r0 = rf(_a0, _a1)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*gen.GetRunningOrgResponse)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context, *gen.GetRunningOrgRequest) error); ok {
-		r1 = rf(_a0, _a1)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // RegisterUser provides a mock function with given fields: _a0, _a1
 func (_m *OrgServiceServer) RegisterUser(_a0 context.Context, _a1 *gen.RegisterUserRequest) (*gen.MemberResponse, error) {
 	ret := _m.Called(_a0, _a1)
