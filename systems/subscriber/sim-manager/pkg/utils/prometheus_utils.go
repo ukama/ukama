c554108955359a6e1e1d9740d745402862685dad
Brackley cassinga <pcassinga@gmail.com>
2023-03-09 18:07:50 +0200

    prmoteus util bug fix

--- systems/subscriber/sim-manager/pkg/utils/prometheus_utils.go
+++ systems/subscriber/sim-manager/pkg/utils/prometheus_utils.go
@@ -26,7 +26,6 @@ type Metrics struct {
 	Labels    prometheus.Labels
 }
 
-
 func MetricTypeFromString(s string) MetricType {
 	switch s {
 	case "gauge":
@@ -117,67 +116,64 @@ func (m *Metrics) SetMetric(value float64, labels prometheus.Labels) error {
 	return nil
 }
 
+func PushMetrics(metrics []pkg.SimMetrics) {
+	labelDimensions := make([]string, 0, len(metrics[0].Labels))
+	for key := range metrics[0].Labels {
+		labelDimensions = append(labelDimensions, key)
+	}
 
-func PushMetrics(metricJob string, metrics []pkg.SimMetrics) {
-    labelDimensions := make([]string, 0, len(metrics[0].Labels))
-    for key := range metrics[0].Labels {
-        labelDimensions = append(labelDimensions, key)
-    }
+	// Check if the metrics already exist and create them if they don't
+	for _, metric := range metrics {
+		if _, ok := metricCollectors[metric.Name]; !ok {
+			// Metric does not exist, create a new one
+			newMetric := NewMetrics(metric.Name, metric.Type)
+			if err := newMetric.InitializeMetric(metric.Name, pkg.MetricConfig{
+				Name:   metric.Name,
+				Type:   metric.Type,
+				Labels: metric.Labels,
+			}, labelDimensions); err != nil {
+				log.Errorf("Failed to initialize metric %s: %v", metric.Name, err)
+				continue
+			}
+			metricCollectors[metric.Name] = []*Metrics{newMetric}
+		}
 
-    // Check if the metrics already exist and create them if they don't
-    for _, metric := range metrics {
-        if _, ok := metricCollectors[metric.Name]; !ok {
-            // Metric does not exist, create a new one
-            newMetric := NewMetrics(metric.Name, metric.Type)
-            if err := newMetric.InitializeMetric(metric.Name,pkg.MetricConfig{
-                Name:   metric.Name,
-                Type:   metric.Type,
-                Labels: metric.Labels,
-            }, labelDimensions); err != nil {
-                log.Errorf("Failed to initialize metric %s: %v", metric.Name, err)
-                continue
-            }
-            metricCollectors[metric.Name] = []*Metrics{newMetric}
-        }
+		// Set the metric value
+		for _, m := range metricCollectors[metric.Name] {
+			if err := m.SetMetric(metric.Value, metric.Labels); err != nil {
+				log.Errorf("Failed to set metric %s value: %v", metric.Name, err)
+				continue
+			}
+		}
+	}
 
-        // Set the metric value
-        for _, m := range metricCollectors[metric.Name] {
-            if err := m.SetMetric(metric.Value, metric.Labels); err != nil {
-                log.Errorf("Failed to set metric %s value: %v", metric.Name, err)
-                continue
-            }
-        }
-    }
-
-    // Push all metrics to the Pushgateway
-    pusher := push.New("http://localhost:9091", pkg.SystemName)
-    for _, metrics := range metricCollectors {
-        for _, m := range metrics {
-            pusher.Collector(m.collector)
-        }
-    }
-    if err := pusher.Push(); err != nil {
-        fmt.Println("Could not push metrics to Pushgateway:", err)
-    }
+	// Push all metrics to the Pushgateway
+	pusher := push.New("http://localhost:9091", pkg.SystemName)
+	for _, metrics := range metricCollectors {
+		for _, m := range metrics {
+			pusher.Collector(m.collector)
+		}
+	}
+	if err := pusher.Push(); err != nil {
+		fmt.Println("Could not push metrics to Pushgateway:", err)
+	}
 }
-
 
 func CollectAndPushSimMetrics(configMetrics []pkg.SimMetrics, selectedMetric string, Value float64, Labels map[string]string) error {
-    var selectedMetrics []pkg.SimMetrics
-    for i, metric := range configMetrics {
-        if metric.Name == selectedMetric {
-            metric.Value = Value
-            for k, v := range Labels {
-                metric.Labels[k] = v
-            }
-            selectedMetrics = append(selectedMetrics, metric)
-            configMetrics[i] = metric
-        }
-    }
-    if len(selectedMetrics) == 0 {
-        return fmt.Errorf("metric %q not found", selectedMetric)
-    }
-    PushMetrics(pkg.SystemName, selectedMetrics)
-    return nil
+	var selectedMetrics []pkg.SimMetrics
+	for i, metric := range configMetrics {
+		if metric.Name == selectedMetric {
+			metric.Value = Value
+			for k, v := range Labels {
+				metric.Labels[k] = v
+			}
+			selectedMetrics = append(selectedMetrics, metric)
+			configMetrics[i] = metric
+		}
+	}
+	if len(selectedMetrics) == 0 {
+		return fmt.Errorf("metric %q not found", selectedMetric)
+	}
+	PushMetrics(selectedMetrics)
+	return nil
 }
-
