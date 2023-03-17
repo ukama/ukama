5b3c55e9524506b743286c6a7179c1c247627e72
salman-codewar <salmannaseem131@gmail.com>
2023-03-18 02:10:13 +0500

    Registry-Network api's hooked
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- systems/bff/src/modules/network/service.ts
+++ systems/bff/src/modules/network/service.ts
@@ -1,17 +1,25 @@
 import { Service } from "typedi";
-import { NetworkDto } from "./types";
-import { checkError } from "../../errors";
-import { INetworkService } from "./interface";
 import { catchAsyncIOMethod } from "../../common";
-import { SERVER } from "../../constants/endpoints";
-import { API_METHOD_TYPE } from "../../constants";
 import { ParsedCookie } from "../../common/types";
 import setupLogger from "../../config/setupLogger";
+import { API_METHOD_TYPE } from "../../constants";
+import { SERVER } from "../../constants/endpoints";
+import { checkError } from "../../errors";
+import { INetworkService } from "./interface";
 import NetworkMapper from "./mapper";
+import {
+    NetworkDto,
+    NetworksResDto,
+    NetworkStatusDto,
+    SiteDto,
+    SitesResDto,
+} from "./types";
 const logger = setupLogger("service");
 @Service()
 export class NetworkService implements INetworkService {
-    getNetworkStatus = async (cookie: ParsedCookie): Promise<NetworkDto> => {
+    getNetworkStatus = async (
+        cookie: ParsedCookie,
+    ): Promise<NetworkStatusDto> => {
         const resLiveNodes = await catchAsyncIOMethod({
             type: API_METHOD_TYPE.GET,
             path: `${SERVER.ORG}/${cookie.orgId}/metrics/live-nodes`,
@@ -35,5 +43,62 @@ export class NetworkService implements INetworkService {
             resTotalNodes.nodes.length,
             resLiveNodes.data.result[0],
         );
+    };
+
+    getNetworks = async (cookie: ParsedCookie): Promise<NetworksResDto> => {
+        const res = await catchAsyncIOMethod({
+            type: API_METHOD_TYPE.GET,
+            path: `${SERVER.REGISTRY_NETWORKS_API_URL}`,
+            params: {
+                org: cookie.orgId,
+            },
+            headers: cookie.header,
+        });
+
+        if (checkError(res)) throw new Error(res.message);
+        return NetworkMapper.dtoToNetworksDto(res);
+    };
+
+    getNetwork = async (
+        networkId: string,
+        cookie: ParsedCookie,
+    ): Promise<NetworkDto> => {
+        const res = await catchAsyncIOMethod({
+            type: API_METHOD_TYPE.GET,
+            path: `${SERVER.REGISTRY_NETWORKS_API_URL}/${networkId}`,
+            headers: cookie.header,
+        });
+
+        if (checkError(res)) throw new Error(res.message);
+        return NetworkMapper.dtoToNetworkDto(res);
+    };
+
+    getSites = async (
+        networkId: string,
+        cookie: ParsedCookie,
+    ): Promise<SitesResDto> => {
+        const res = await catchAsyncIOMethod({
+            type: API_METHOD_TYPE.GET,
+            path: `${SERVER.REGISTRY_NETWORKS_API_URL}/${networkId}/sites`,
+            headers: cookie.header,
+        });
+
+        if (checkError(res)) throw new Error(res.message);
+        return NetworkMapper.dtoToSitesDto(res);
+    };
+
+    getSite = async (
+        siteId: string,
+        networkId: string,
+        cookie: ParsedCookie,
+    ): Promise<SiteDto> => {
+        const res = await catchAsyncIOMethod({
+            type: API_METHOD_TYPE.GET,
+            path: `${SERVER.REGISTRY_NETWORKS_API_URL}/${networkId}/sites/${siteId}`,
+            headers: cookie.header,
+        });
+
+        if (checkError(res)) throw new Error(res.message);
+        return NetworkMapper.dtoToSiteDto(res);
     };
 }
