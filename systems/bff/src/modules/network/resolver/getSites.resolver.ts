50f09015c0dc26f3cb7d05742bc7f117a46ffc03
salman-codewar <salmannaseem131@gmail.com>
2023-03-18 02:10:13 +0500

    Registry-Network api's hooked
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- systems/bff/src/modules/network/resolver/getSites.resolver.ts
+++ systems/bff/src/modules/network/resolver/getSites.resolver.ts
@@ -1,19 +1,22 @@
-import { Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
 import { Service } from "typedi";
 import { parseCookie } from "../../../common";
 import { Authentication } from "../../../common/Authentication";
 import { Context } from "../../../common/types";
 import { NetworkService } from "../service";
-import { NetworkStatusDto } from "../types";
+import { SitesResDto } from "../types";
 
 @Service()
 @Resolver()
-export class GetNetworkStatusResolver {
+export class GetSitesResolver {
     constructor(private readonly networkService: NetworkService) {}
 
-    @Query(() => NetworkStatusDto)
+    @Query(() => SitesResDto)
     @UseMiddleware(Authentication)
-    async getNetworkStatus(@Ctx() ctx: Context): Promise<NetworkStatusDto> {
-        return this.networkService.getNetworkStatus(parseCookie(ctx));
+    async getSites(
+        @Arg("networkId") networkId: string,
+        @Ctx() ctx: Context,
+    ): Promise<SitesResDto> {
+        return this.networkService.getSites(networkId, parseCookie(ctx));
     }
 }
