15b202aacf0634fcdffc400960d966e467be1726
Brackley cassinga <pcassinga@gmail.com>
2023-03-20 12:13:00 +0200

    Bff system node api updates ->Api gateway node endpoint added (#449)
    * BFF system added in docker compose
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Node api gateway req updated
    
    * minor fixes on addNode resolver and types
    
    * node api-gateway added and bff node service api updated
    
    * req url updated in getNode call
    
    * docker.yml file reverted
    
    ---------
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    Co-authored-by: salman-codewar <salmannaseem131@gmail.com>

--- systems/bff/src/constants/endpoints.ts
+++ systems/bff/src/constants/endpoints.ts
@@ -2,6 +2,7 @@ import { API_URL } from "../constants/index";
 
 export const SERVER = {
     REGISTRY_USERS_API_URL: `${API_URL}/v1/users`,
+    REGISTRY_NODE_API_URL: `${API_URL}/v1/nodes`,
     REGISTRY_ORGS_API_URL: `${API_URL}/v1/orgs`,
     REGISTRY_NETWORKS_API_URL: `${API_URL}/v1/networks`,
     GET_CONNECTED_USERS: `${API_URL}/user/get_conneted_users`,
