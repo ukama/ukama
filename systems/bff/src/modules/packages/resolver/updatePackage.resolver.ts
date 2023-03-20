1408f6b7e3e76ad2f10f9d94c3dadd07886ba292
Salman Naseem <83802574+salman-codewar@users.noreply.github.com>
2023-03-20 18:35:18 +0500

    Frontend system > Data-plan - Packages endpoint integrated (#457)
    * Data-plan - Package endpoints hooked with bff
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Minor DTO issue fixes
    
    * PackageDto type bug fixed
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    ---------
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

---
+++ systems/bff/src/modules/packages/resolver/updatePackage.resolver.ts
@@ -0,0 +1,27 @@
+import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
+import { Service } from "typedi";
+import { parseCookie } from "../../../common";
+import { Authentication } from "../../../common/Authentication";
+import { Context } from "../../../common/types";
+import { PackageService } from "../service";
+import { PackageDto, UpdatePackageInputDto } from "../types";
+
+@Service()
+@Resolver()
+export class UpdatePackageResolver {
+    constructor(private readonly packageService: PackageService) {}
+
+    @Mutation(() => PackageDto)
+    @UseMiddleware(Authentication)
+    async updatePackage(
+        @Arg("packageId") packageId: string,
+        @Arg("data") data: UpdatePackageInputDto,
+        @Ctx() ctx: Context,
+    ): Promise<PackageDto> {
+        return this.packageService.updatePackage(
+            packageId,
+            data,
+            parseCookie(ctx),
+        );
+    }
+}
