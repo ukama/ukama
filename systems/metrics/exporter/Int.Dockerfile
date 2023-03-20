1fd1b9b3832c06a535aa510e3d7c7ba443c8255e
vthakur7f <vthakur7@hotmail.com>
2023-03-20 10:53:02 -0700

    Removed exporter files from this branch.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/metrics/exporter/Int.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM 003664043471.dkr.ecr.us-east-1.amazonaws.com/e2e-base-image:latest 
-
-ENV PROJECT_NAME=exporter
-COPY bin/integration /usr/bin/integration
--- systems/metrics/exporter/pkg/global.go
+++
@@ -1,7 +0,0 @@
-package pkg
-
-const ServiceName = "exporter"
-const SystemName = "metrics"
-const InstanceId = SystemName + "-" + ServiceName + "-debug"
-
-var IsDebugMode bool = true
