43bfd347e1c67479bc1f707d0e9d5561537d89ec
vthakur7f <vthakur7@hotmail.com>
2023-04-07 14:23:39 -0700

    Updated api-gateway for baserate changes.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

---
+++ systems/data-plan/api-gateway/pkg/client/baserate.go
@@ -0,0 +1,85 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	pb "github.com/ukama/ukama/systems/data-plan/base-rate/pb/gen"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type BaseRateClient struct {
+	conn    *grpc.ClientConn
+	timeout time.Duration
+	host    string
+	client  pb.BaseRatesServiceClient
+}
+
+func NewBaseRateClient(baserateHost string, timeout time.Duration) *BaseRateClient {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, baserateHost, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logrus.Fatalf("did not connect: %v", err)
+	}
+	client := pb.NewBaseRatesServiceClient(conn)
+
+	return &BaseRateClient{
+		conn:    conn,
+		client:  client,
+		timeout: timeout,
+		host:    baserateHost,
+	}
+}
+
+func NewBaseRateClientFromClient(client pb.BaseRatesServiceClient) *BaseRateClient {
+	return &BaseRateClient{
+		host:    "localhost",
+		timeout: 1 * time.Second,
+		conn:    nil,
+		client:  client,
+	}
+}
+
+func (b *BaseRateClient) Close() {
+	b.conn.Close()
+}
+
+func (b *BaseRateClient) GetBaseRateById(req *pb.GetBaseRatesByIdRequest) (*pb.GetBaseRatesByIdResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), b.timeout)
+	defer cancel()
+
+	return b.client.GetBaseRatesById(ctx, req)
+}
+
+func (b *BaseRateClient) GetBaseRatesByCountry(req *pb.GetBaseRatesByCountryRequest) (*pb.GetBaseRatesResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), b.timeout)
+	defer cancel()
+
+	return b.client.GetBaseRatesByCountry(ctx, req)
+}
+
+func (b *BaseRateClient) GetBaseRatesHistoryByCountry(req *pb.GetBaseRatesByCountryRequest) (*pb.GetBaseRatesResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), b.timeout)
+	defer cancel()
+
+	return b.client.GetBaseRatesHistoryByCountry(ctx, req)
+}
+
+func (b *BaseRateClient) GetBaseRatesForPeriod(req *pb.GetBaseRatesByPeriodRequest) (*pb.GetBaseRatesResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), b.timeout)
+	defer cancel()
+
+	return b.client.GetBaseRatesForPeriod(ctx, req)
+}
+
+func (b *BaseRateClient) UploadBaseRates(req *pb.UploadBaseRatesRequest) (*pb.UploadBaseRatesResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), b.timeout)
+	defer cancel()
+
+	return b.client.UploadBaseRates(ctx, req)
+}
--- systems/data-plan/api-gateway/pkg/client/dataPlan.go
+++ systems/data-plan/api-gateway/pkg/client/dataPlan.go
@@ -5,23 +5,20 @@ import (
 	"time"
 
 	"github.com/sirupsen/logrus"
-	pbBaseRate "github.com/ukama/ukama/systems/data-plan/base-rate/pb/gen"
 	pb "github.com/ukama/ukama/systems/data-plan/package/pb/gen"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type DataPlan struct {
-	conn           *grpc.ClientConn
-	baseRateConn   *grpc.ClientConn
-	timeout        time.Duration
-	packageClient  pb.PackagesServiceClient
-	host           string
-	baseRateClient pbBaseRate.BaseRatesServiceClient
+type PackageClient struct {
+	conn          *grpc.ClientConn
+	timeout       time.Duration
+	packageClient pb.PackagesServiceClient
+	host          string
 }
 
-func NewDataPlan(packageHost, baseRateHost string, timeout time.Duration) *DataPlan {
+func NewPackageClient(packageHost string, timeout time.Duration) *PackageClient {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
@@ -31,80 +28,56 @@ func NewDataPlan(packageHost, baseRateHost string, timeout time.Duration) *DataP
 	}
 	client := pb.NewPackagesServiceClient(packageConn)
 
-	baseRateConn, err := grpc.DialContext(ctx, baseRateHost, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		logrus.Fatalf("did not connect: %v", err)
-	}
-	baseRateClient := pbBaseRate.NewBaseRatesServiceClient(baseRateConn)
-
-	return &DataPlan{
-		conn:           packageConn,
-		packageClient:  client,
-		timeout:        timeout,
-		host:           packageHost,
-		baseRateClient: baseRateClient,
+	return &PackageClient{
+		conn:          packageConn,
+		packageClient: client,
+		timeout:       timeout,
+		host:          packageHost,
 	}
 }
 
-func NewPackageFromClient(client pb.PackagesServiceClient, baseRateClient pbBaseRate.BaseRatesServiceClient) *DataPlan {
-	return &DataPlan{
-		host:           "localhost",
-		timeout:        1 * time.Second,
-		conn:           nil,
-		packageClient:  client,
-		baseRateClient: baseRateClient,
+func NewPackageFromClient(client pb.PackagesServiceClient) *PackageClient {
+	return &PackageClient{
+		host:          "localhost",
+		timeout:       1 * time.Second,
+		conn:          nil,
+		packageClient: client,
 	}
 }
 
-func (r *DataPlan) Close() {
+func (r *PackageClient) Close() {
 	r.conn.Close()
-	r.baseRateConn.Close()
 }
 
-func (d *DataPlan) AddPackage(req *pb.AddPackageRequest) (*pb.AddPackageResponse, error) {
+func (d *PackageClient) AddPackage(req *pb.AddPackageRequest) (*pb.AddPackageResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
 	defer cancel()
 
 	return d.packageClient.Add(ctx, req)
 }
 
-func (d *DataPlan) DeletePackage(id string) (*pb.DeletePackageResponse, error) {
+func (d *PackageClient) DeletePackage(id string) (*pb.DeletePackageResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
 	defer cancel()
 
 	return d.packageClient.Delete(ctx, &pb.DeletePackageRequest{Uuid: id})
 }
 
-func (d *DataPlan) UpdatePackage(req *pb.UpdatePackageRequest) (*pb.UpdatePackageResponse, error) {
+func (d *PackageClient) UpdatePackage(req *pb.UpdatePackageRequest) (*pb.UpdatePackageResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
 	defer cancel()
 
 	return d.packageClient.Update(ctx, req)
 }
-func (d *DataPlan) GetPackage(id string) (*pb.GetPackageResponse, error) {
+
+func (d *PackageClient) GetPackage(id string) (*pb.GetPackageResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
 	defer cancel()
 
 	return d.packageClient.Get(ctx, &pb.GetPackageRequest{Uuid: id})
 }
 
-func (d *DataPlan) UploadBaseRates(req *pbBaseRate.UploadBaseRatesRequest) (*pbBaseRate.UploadBaseRatesResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
-	defer cancel()
-
-	return d.baseRateClient.UploadBaseRates(ctx, req)
-}
-func (d *DataPlan) GetBaseRates(req *pbBaseRate.GetBaseRatesRequest) (*pbBaseRate.GetBaseRatesResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
-	defer cancel()
-	return d.baseRateClient.GetBaseRates(ctx, req)
-}
-func (d *DataPlan) GetBaseRate(id string) (*pbBaseRate.GetBaseRateResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
-	defer cancel()
-	return d.baseRateClient.GetBaseRate(ctx, &pbBaseRate.GetBaseRateRequest{Uuid: id})
-}
-func (d *DataPlan) GetPackageByOrg(orgId string) (*pb.GetByOrgPackageResponse, error) {
+func (d *PackageClient) GetPackageByOrg(orgId string) (*pb.GetByOrgPackageResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), d.timeout)
 	defer cancel()
 	return d.packageClient.GetByOrg(ctx, &pb.GetByOrgPackageRequest{OrgId: orgId})
