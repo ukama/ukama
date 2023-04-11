3700d1533b10d8cbdee1454b89fcfd1c0175ab12
Salman Naseem <83802574+salman-codewar@users.noreply.github.com>
2023-04-11 19:15:15 +0500

    Auth system middleware integration (#491)
    * Swagger ui updates, GetUserInfo auth request added
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Auth client provider added in common
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * auth apigw
    
    * GetSessionFromCookies common method added, Auth API GW common method used
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Swagger ui auth app redirect added for setting browser cookies in DataPlan, Subscriber, Registry
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Auth api gw refactoring
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Added security schema in Fizz common initialization
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Update registy system router
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Auth middlewre integration in subscriber, registry and data plan
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Auth api gw workflow added
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Auth api gw build issue fixed
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Auth system api gw fixes
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    * Auth api gw bug fixes
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>
    
    ---------
    
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- systems/registry/api-gateway/pkg/rest/router.go
+++ systems/registry/api-gateway/pkg/rest/router.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/ukama/ukama/systems/common/providers"
 	"github.com/ukama/ukama/systems/common/rest"
 	"github.com/wI2L/fizz/openapi"
 
@@ -22,15 +23,16 @@ import (
 	userspb "github.com/ukama/ukama/systems/registry/users/pb/gen"
 )
 
-var REDIRECT_URI = "http://localhost:4455/?redirect=localhost:8080/swagger/#/"
+var REDIRECT_URI = "https://registry.ukama.com/swagger/#/"
 
 const USER_ID_KEY = "UserId"
 const ORG_URL_PARAMETER = "org"
 
 type Router struct {
-	f       *fizz.Fizz
-	clients *Clients
-	config  *RouterConfig
+	f              *fizz.Fizz
+	clients        *Clients
+	config         *RouterConfig
+	authRestClient *providers.AuthRestClient
 }
 
 type RouterConfig struct {
@@ -74,8 +76,9 @@ func NewClientsSet(endpoints *pkg.GrpcEndpoints) *Clients {
 
 func NewRouter(clients *Clients, config *RouterConfig, authfunc func(*gin.Context, string) error) *Router {
 	r := &Router{
-		clients: clients,
-		config:  config,
+		clients:        clients,
+		config:         config,
+		authRestClient: authRestClient,
 	}
 
 	if !config.debugMode {
