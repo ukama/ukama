c48fd66e4f9b6a8e8ecb45767c47dbf347f7dc79
salman-codewar <salmannaseem131@gmail.com>
2023-04-18 20:54:32 +0500

    Config changes reverted
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- systems/subscriber/api-gateway/pkg/config.go
+++ systems/subscriber/api-gateway/pkg/config.go
@@ -41,7 +41,7 @@ func NewConfig() *Config {
 
 		Services: GrpcEndpoints{
 			Timeout:    3 * time.Second,
-			SimPool:    "localhost:9090",
+			SimPool:    "sim:9090",
 			Registry:   "registry:9090",
 			SimManager: "simmanager:9090",
 		},
