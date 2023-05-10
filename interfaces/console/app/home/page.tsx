99aa6527f0b9e41b78d3e6bb28a8b6e6024f32a9
salman-codewar <salmannaseem131@gmail.com>
2023-05-11 02:49:26 +0500

    Nextjs app version downgraded to 12
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- interfaces/console/app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Home page</h1>
-    </div>
-  );
-}
--- interfaces/console/ui/components/index.tsx
+++
@@ -1,2 +0,0 @@
-// Desc: This file is used to export all the components in the components folder
-export {};
