451fe878cea03065a8714d4479c3bcce4d8762a2
Kashif Ali <kashif@ukama.com>
2023-05-17 16:34:19 -0700

    Update to check the exit status of all cmd in the scripts
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- testing/node/mk_local_vnode.sh
+++ testing/node/mk_local_vnode.sh
@@ -4,28 +4,50 @@
 
 # Script to create ukama's virtual node locally 
 
-set -x
+RUN() {
+
+	# Execute the command
+	"$@"
+
+	# Check the exit status of the command
+	if [[ $? -eq 0 ]]; then
+		echo -e "\033[32mCommand '$@' Ok\033[0m"
+	else
+		echo -e "\033[31mCommand '$@' Failed\033[0m"
+		exit 1
+	fi
+}
+
+if [[ -z "${VNODE_METADATA}" ]]; then
+	echo "VNODE_METADATA environment variable is not set"
+	exit 1
+fi
+
+if [[ -z "${VNODE_ID}" ]]; then
+	echo "VNODE_ID environment variable is not set"
+	exit 1
+fi
 
 # This will create the ukamaOS image and container within which the virtual
 # node will be incubated
-make clean; make container
+RUN make clean; make container
 
 # Kill an existing running registry
-sudo docker rm --force registry
+RUN sudo docker rm --force registry
 
 # Created local registry at port 5000
-sudo docker run -d -p 5000:5000 --name registry registry:latest
+RUN sudo docker run -d -p 5000:5000 --name registry registry:latest
 
 # start the virtual node incubator via podman
-podman run --network host --privileged  -it \
-	   -e VNODE_METADATA="$VNODE_METADATA" \
-	   -e VNODE_ID="$VNODE_ID" \
-	   -e VNODE_RUN_TARGET="local" \
-	   -e REPO_SERVER_URL="testing" \
-	   -e REPO_NAME="virtualnode" \
-	   localhost/testing/virtualnode:74ba00fc1-dirty
+RUN podman run --network host --privileged  -it \
+	-e VNODE_METADATA="$VNODE_METADATA" \
+	-e VNODE_ID="$VNODE_ID" \
+	-e VNODE_RUN_TARGET="local" \
+	-e REPO_SERVER_URL="testing" \
+	-e REPO_NAME="virtualnode" \
+	localhost/testing/virtualnode:74ba00fc1-dirty
 
 # Info on the newly created image.
-buildah info ${REPO_SERVER_URL}/${REPO_NAME}:${VNODE_ID}
+RUN buildah info ${REPO_SERVER_URL}/${REPO_NAME}:${VNODE_ID}
 
 echo "Done"
