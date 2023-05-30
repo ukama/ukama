b3c712101a5fdb55766ac592d04bae02bb8887db
salman-codewar <salmannaseem131@gmail.com>
2023-05-31 02:25:05 +0500

    bff build issues fixed
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- systems/bff/src/modules/user/interface.ts
+++ systems/bff/src/modules/user/interface.ts
@@ -1,48 +1,15 @@
-import {
-    BoolResponse,
-    MetricServiceValueRes,
-    ParsedCookie,
-} from "../../common/types";
+import { MetricServiceValueRes } from "../../common/types";
 import {
     ConnectedUserDto,
-    DeactivateResponse,
-    ESimQRCodeRes,
-    GetESimQRCodeInput,
     GetUserDto,
     GetUserResponseDto,
-    OrgUserSimDto,
-    UpdateUserInputDto,
-    UpdateUserServiceInput,
     UserAPIResDto,
-    UserInputDto,
     UserResDto,
     WhoamiAPIDto,
     WhoamiDto,
 } from "./types";
-
-export interface IUserService {
-    getConnectedUsers(cookie: ParsedCookie): Promise<ConnectedUserDto>;
-    updateUser(
-        userId: string,
-        req: UpdateUserInputDto,
-        cookie: ParsedCookie
-    ): Promise<UserResDto>;
-    deactivateUser(
-        uuid: string,
-        cookie: ParsedCookie
-    ): Promise<DeactivateResponse>;
-    getUser(userId: string, cookie: ParsedCookie): Promise<UserResDto>;
-    addUser(req: UserInputDto, cookie: ParsedCookie): Promise<UserResDto>;
-    deleteUser(userId: string, cookie: ParsedCookie): Promise<BoolResponse>;
-    getEsimQRCode(
-        data: GetESimQRCodeInput,
-        cookie: ParsedCookie
-    ): Promise<ESimQRCodeRes>;
-    updateUserRoaming(
-        data: UpdateUserServiceInput,
-        cookie: ParsedCookie
-    ): Promise<OrgUserSimDto>;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IUserService {}
 
 export interface IUserMapper {
     connectedUsersDtoToDto(res: MetricServiceValueRes[]): ConnectedUserDto;
