351a2e882210854da3885f615154e35570373fe6
salman-codewar <salmannaseem131@gmail.com>
2023-06-16 12:54:40 +0500

    console - gateway connection tested
    Signed-off-by: salman-codewar <salmannaseem131@gmail.com>

--- interfaces/ukama-console/generated/planning-tool/index.ts
+++ interfaces/ukama-console/generated/planning-tool/index.ts
@@ -15,8 +15,6 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
-  _Any: { input: any; output: any; }
-  _FieldSet: { input: any; output: any; }
 };
 
 export type AddDraftInput = {
@@ -83,7 +81,6 @@ export type MutationUpdateSiteArgs = {
 
 export type Query = {
   __typename?: 'Query';
-  _service: _Service;
   getDraft: Draft;
   getDrafts: Array<Draft>;
 };
@@ -123,11 +120,6 @@ export type UpdateSiteInput = {
   lng: Scalars['String']['input'];
   siteName: Scalars['String']['input'];
   solarUptime: Scalars['Float']['input'];
-};
-
-export type _Service = {
-  __typename?: '_Service';
-  sdl?: Maybe<Scalars['String']['output']>;
 };
 
 export type LocationFragment = { __typename?: 'Location', lat: string, lng: string, address: string };
