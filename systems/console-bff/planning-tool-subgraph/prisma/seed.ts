5a0cac475c8b3fd621899a7484da4668f50356e7
salman-codewar <salmannaseem131@gmail.com>
2023-06-19 20:41:06 +0500

    console bff > queries & mutations updates to support multi site flow

--- systems/console-bff/planning-tool-subgraph/prisma/seed.ts
+++ systems/console-bff/planning-tool-subgraph/prisma/seed.ts
@@ -5,25 +5,42 @@ const prisma = new PrismaClient();
 const draftData: Prisma.DraftCreateInput[] = [
   {
     name: "Draft 1",
-    lastSaved: 1686142941,
     userId: "16fe5842-05dd-11ee-8254-5eeae0fe08fe",
     createdAt: "2023-03-01T00:00:00.000Z",
     updatedAt: "2023-03-02T00:00:00.000Z",
-    site: {
-      create: {
-        name: "Site 1",
-        height: 100,
-        apOption: "ONE_TO_ONE",
-        solarUptime: 2,
-        isSetlite: false,
-        location: {
-          create: {
-            lat: "1001.123",
-            lng: "12421.213",
-            address: "Address 1",
+    sites: {
+      create: [
+        {
+          name: "Site 1",
+          height: 100,
+          apOption: "ONE_TO_ONE",
+          solarUptime: 95,
+          isSetlite: false,
+          location: {
+            create: {
+              lat: "1001.123",
+              lng: "12421.213",
+              lastSaved: 1686142941,
+              address: "Address 1",
+            },
           },
         },
-      },
+        {
+          name: "Site 2",
+          height: 90,
+          apOption: "ONE_TO_TWO",
+          solarUptime: 90,
+          isSetlite: true,
+          location: {
+            create: {
+              lat: "10012.123",
+              lng: "124221.213",
+              lastSaved: 1686133941,
+              address: "Address 1",
+            },
+          },
+        },
+      ],
     },
     events: {
       create: [
