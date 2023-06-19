039fbbf727039f66fd176019ac9c34b88270464e
salman-codewar <salmannaseem131@gmail.com>
2023-06-19 17:08:52 +0500

    bff planning tool data structure updated

--- systems/console-bff/planning-tool-subgraph/prisma/migrations/20230616112624_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Draft" DROP CONSTRAINT "Draft_siteId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Draft" ADD CONSTRAINT "Draft_siteId_fkey" FOREIGN KEY ("siteId") REFERENCES "Site"("id") ON DELETE CASCADE ON UPDATE CASCADE;
