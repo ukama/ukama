1b64bc0864f553d0b645782d4df192e2881dd22f
salman-codewar <salmannaseem131@gmail.com>
2023-06-22 02:20:51 +0500

    Power summary UI added and hooked with mock data

--- interfaces/ukama-console/ui/molecules/MapOverlayUI/index.tsx
+++ interfaces/ukama-console/ui/molecules/MapOverlayUI/index.tsx
@@ -5,11 +5,21 @@ import AddLocationIcon from '@mui/icons-material/AddLocation';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import PowerIcon from '@mui/icons-material/PowerSettingsNewOutlined';
 import RouteOutlinedIcon from '@mui/icons-material/RouteOutlined';
-import { Box, IconButton, Stack, Tooltip, Typography } from '@mui/material';
+import {
+  Box,
+  IconButton,
+  Stack,
+  Tab,
+  Tabs,
+  Tooltip,
+  Typography,
+} from '@mui/material';
 
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import DotIcon from '@mui/icons-material/FiberManualRecord';
 import { LatLngLiteral } from 'leaflet';
+import { useState } from 'react';
+import TabPanel from '../TabPanel';
 
 const LeftIconButtonStyle = {
   zIndex: 400,
@@ -152,20 +162,38 @@ const RightOverlayUI = ({
   </Box>
 );
 
-interface ISiteSummary {
-  title: string;
-  subtitle: number;
+interface IPlanningSummary {
+  subtitleOne: string;
+  subtitleTwo: string;
   siteSummary: any;
+  powerSummary: any;
 }
 
-const SiteSummary = ({ title, subtitle, siteSummary }: ISiteSummary) => (
-  <Stack spacing={1.2}>
-    <Stack direction={'row'} alignItems={'center'} spacing={0.5}>
-      <Typography variant="body2" sx={{ fontSize: 14, fontWeight: 600 }}>
-        {title}
-      </Typography>
-      <Typography variant="caption">{`(${subtitle})`}</Typography>
-    </Stack>
+const tabProps = (index: number) => ({
+  id: `sites-summary-tab-${index}`,
+  'aria-controls': `sites-summary-tabpanel-${index}`,
+});
+
+const PowerSummarySections = [
+  {
+    id: 'power-usage',
+    unit: 'W',
+    title: 'Power Usage',
+  },
+  {
+    id: 'solar-panels',
+    title: 'Solar Panels',
+    unit: '',
+  },
+  {
+    id: 'batteries',
+    title: 'Batteries',
+    unit: '',
+  },
+];
+
+const SiteSummary = ({ siteSummary }: any) => (
+  <>
     {siteSummary.length > 0 ? (
       siteSummary.map(({ id, name, status }: any) => (
         <Stack key={id} direction="row" spacing={1} alignItems={'center'}>
@@ -187,7 +215,110 @@ const SiteSummary = ({ title, subtitle, siteSummary }: ISiteSummary) => (
         No site added yet!
       </Typography>
     )}
+  </>
+);
+const PowerSummary = ({ powerSummary }: any) => (
+  <Stack direction={'column'} spacing={1}>
+    {PowerSummarySections.map(({ id, title, unit }) => {
+      const d: any = {
+        'power-usage': {
+          total: 0,
+          info: '',
+        },
+        'solar-panels': {
+          total: 0,
+          info: '',
+        },
+        batteries: {
+          total: 0,
+          info: '',
+        },
+      };
+
+      powerSummary.sites.forEach(
+        ({ id: _id, name, usage, panels, battries }: any, i: number) => {
+          const isLastItem = powerSummary.sites.length === i + 1;
+          d['power-usage'].total = d['power-usage'].total + usage;
+          d['solar-panels'].total = d['solar-panels'].total + panels;
+          d['batteries'].total = d['batteries'].total + battries;
+          d['power-usage'].info =
+            d['power-usage'].info +
+            `${name} (${usage})` +
+            (isLastItem ? '' : ' + ');
+          d['solar-panels'].info =
+            d['solar-panels'].info +
+            `${name} (${panels})` +
+            (isLastItem ? '' : ' + ');
+          d['batteries'].info =
+            d['batteries'].info +
+            `${name} (${battries})` +
+            (isLastItem ? '' : ' + ');
+        },
+      );
+
+      return (
+        <Stack
+          key={id}
+          direction="column"
+          spacing={0.6}
+          alignItems={'flex-start'}
+        >
+          <Stack
+            width={'100%'}
+            direction="row"
+            spacing={1}
+            justifyContent={'space-between'}
+          >
+            <Typography variant="body2" sx={{ fontSize: 14, fontWeight: 500 }}>
+              {title}
+            </Typography>
+            <Typography variant="body2" sx={{ fontSize: 14, fontWeight: 500 }}>
+              {d[id].total} {unit}
+            </Typography>
+          </Stack>
+          <Typography variant="body2" sx={{ fontSize: 14, fontWeight: 300 }}>
+            {d[id].info ? d[id].info : 'No data available!'}
+          </Typography>
+        </Stack>
+      );
+    })}
   </Stack>
 );
 
-export { LeftOverlayUI, RightOverlayUI, SiteSummary };
+const PlanningSummary = ({
+  subtitleOne,
+  subtitleTwo,
+  siteSummary,
+  powerSummary,
+}: IPlanningSummary) => {
+  const [value, setValue] = useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  return (
+    <Stack spacing={1.2}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', marginBottom: 0.8 }}>
+        <Tabs value={value} onChange={handleChange} aria-label="summary tabs">
+          <Tab
+            label={subtitleOne}
+            {...tabProps(0)}
+            sx={{ fontSize: '12px', fontWeight: 400, p: 0 }}
+          />
+          <Tab
+            label={subtitleTwo}
+            {...tabProps(1)}
+            sx={{ fontSize: '12px', fontWeight: 400, p: 0 }}
+          />
+        </Tabs>
+      </Box>
+      <TabPanel id={'sites-summary-tab'} value={value} index={0}>
+        <SiteSummary siteSummary={siteSummary} />
+      </TabPanel>
+      <TabPanel id={'sites-power-tab'} value={value} index={1}>
+        <PowerSummary powerSummary={powerSummary} />
+      </TabPanel>
+    </Stack>
+  );
+};
+
+export { LeftOverlayUI, PlanningSummary, RightOverlayUI };
