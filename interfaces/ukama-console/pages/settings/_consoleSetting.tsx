0e38a87e16377f7c574ee7768cae0e63ead1692d
salman-codewar <salmannaseem131@gmail.com>
2023-06-24 01:15:58 +0500

    network, alerts and console settings UI added

--- interfaces/ukama-console/pages/settings/_consoleSetting.tsx
+++ interfaces/ukama-console/pages/settings/_consoleSetting.tsx
@@ -1,15 +1,35 @@
+import { isDarkmode } from '@/app-recoil';
 import { colors } from '@/styles/theme';
 import LoadingWrapper from '@/ui/molecules/LoadingWrapper';
-import ConstructionSharpIcon from '@mui/icons-material/ConstructionSharp';
-import { Paper, Stack, Typography } from '@mui/material';
+import {
+  Card,
+  FormControlLabel,
+  Grid,
+  Paper,
+  Radio,
+  RadioGroup,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 
 export default function ConsoleSettings() {
+  const [_isDarkMod, _setIsDarkMod] = useRecoilState(isDarkmode);
+  const [apperanceMode, setAppearanceMode] = useState<string>(
+    _isDarkMod ? 'dark' : 'light',
+  );
+
+  const handleToggle = (e: any) => {
+    setAppearanceMode(e.target.value);
+    _setIsDarkMod(e.target.value === 'dark');
+  };
   return (
     <LoadingWrapper
       radius="small"
       width={'100%'}
       isLoading={false}
       cstyle={{
+        overflow: 'auto',
         backgroundColor: false ? colors.white : 'transparent',
       }}
     >
@@ -18,19 +38,43 @@ export default function ConsoleSettings() {
           py: 3,
           px: 4,
           width: '100%',
-          borderRadius: '5px',
+          borderRadius: '4px',
           height: 'calc(100vh - 200px)',
         }}
       >
-        <Stack
-          height={'100%'}
-          direction={'column'}
-          alignItems={'center'}
-          justifyContent={'center'}
-        >
-          <ConstructionSharpIcon />
-          <Typography variant="subtitle2">Console settings.</Typography>
-        </Stack>
+        <Grid container spacing={2}>
+          <Grid item container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h6">Appearance</Typography>
+            </Grid>
+            <Grid item container xs={12} md={8}>
+              <RadioGroup
+                onChange={handleToggle}
+                value={apperanceMode}
+                name="appearance-mode"
+                sx={{ width: { xs: '100%', md: '70%' } }}
+              >
+                {[
+                  { id: 1, label: 'Light', value: 'light' },
+                  { id: 2, label: 'Dark', value: 'dark' },
+                ].map(({ value, label }) => (
+                  <Card
+                    key={value}
+                    variant="outlined"
+                    sx={{ px: 3, py: 1, mb: 2, width: '100%' }}
+                  >
+                    <FormControlLabel
+                      value={value}
+                      label={label}
+                      control={<Radio />}
+                      sx={{ width: '100%', typography: 'body1' }}
+                    />
+                  </Card>
+                ))}
+              </RadioGroup>
+            </Grid>
+          </Grid>
+        </Grid>
       </Paper>
     </LoadingWrapper>
   );
--- interfaces/ukama-console/pages/settings/_networkSetting.tsx
+++ interfaces/ukama-console/pages/settings/_networkSetting.tsx
@@ -1,37 +1,177 @@
-import { colors } from '@/styles/theme';
-import LoadingWrapper from '@/ui/molecules/LoadingWrapper';
-import ConstructionSharpIcon from '@mui/icons-material/ConstructionSharp';
-import { Paper, Stack, Typography } from '@mui/material';
+import { ROAMING_SELECT } from '@/constants';
+import {
+  Divider,
+  FormControlLabel,
+  Grid,
+  MenuItem,
+  Paper,
+  Radio,
+  RadioGroup,
+  Switch,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
 
 export default function NetworkSettings() {
+  const [esim, setEsim] = useState('all');
+  const [networkSettings, setNetworkSettings] = useState<any>({
+    networkType: localStorage['networkType'] || 'private',
+    roamingOption: localStorage['roamingOption'] || false,
+  });
+
+  const handleTimezoneChange = (event: any) => {
+    setEsim(event.target.value);
+  };
   return (
-    <LoadingWrapper
-      radius="small"
-      width={'100%'}
-      isLoading={false}
-      cstyle={{
-        backgroundColor: false ? colors.white : 'transparent',
+    <Paper
+      sx={{
+        py: 3,
+        px: 4,
+        width: '100%',
+        borderRadius: '4px',
+        height: 'calc(100vh - 200px)',
       }}
     >
-      <Paper
-        sx={{
-          py: 3,
-          px: 4,
-          width: '100%',
-          borderRadius: '5px',
-          height: 'calc(100vh - 200px)',
-        }}
-      >
-        <Stack
-          height={'100%'}
-          direction={'column'}
-          alignItems={'center'}
-          justifyContent={'center'}
-        >
-          <ConstructionSharpIcon />
-          <Typography variant="subtitle2">Network settings.</Typography>
-        </Stack>
-      </Paper>
-    </LoadingWrapper>
+      <Grid container spacing={2}>
+        <Grid item container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Network Name</Typography>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Grid item xs={12} sm={10} md={8}>
+              <Typography
+                variant="body2"
+                sx={{
+                  mb: '18px',
+                  lineHeight: '19px',
+                }}
+              >
+                This is the name that shows up on xyz. You can edit this again
+                at any point.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={10} md={8}>
+              <TextField
+                fullWidth
+                id="name"
+                name="name"
+                disabled={true}
+                variant="standard"
+                value={'Network X'}
+                label={'NETWORK NAME'}
+                InputLabelProps={{ shrink: true }}
+                InputProps={{
+                  disableUnderline: true,
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Divider sx={{ width: '100%' }} />
+        <Grid item container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Network Type</Typography>
+          </Grid>
+          <Grid item container xs={12} md={8}>
+            <Grid item xs={12} sm={10} md={8}>
+              <Typography
+                variant="body2"
+                sx={{
+                  mb: '18px',
+                  lineHeight: '19px',
+                }}
+              >
+                Policy regarding network switching & explain how it’ll change
+                after hardware is actually shipped.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={10} md={8}>
+              <RadioGroup
+                aria-label="networkType"
+                defaultValue={networkSettings.networkType}
+                name="radio-buttons-group"
+                onChange={(event: any) => {
+                  setNetworkSettings({
+                    networkType: event.target.value,
+                  });
+                  localStorage.setItem('networkType', event.target.value);
+                }}
+              >
+                <FormControlLabel
+                  value="public"
+                  control={<Radio />}
+                  label="Public Network"
+                />
+                <FormControlLabel
+                  value="private"
+                  control={<Radio />}
+                  label="Private Network"
+                />
+              </RadioGroup>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Divider sx={{ width: '100%' }} />
+        <Grid item container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Roaming Options</Typography>
+          </Grid>
+          <Grid item container xs={12} md={8}>
+            <Grid item xs={12} sm={10} md={8}>
+              <Typography
+                variant="body2"
+                sx={{
+                  mb: '18px',
+                  lineHeight: '19px',
+                }}
+              >
+                Explanation of roaming & its rates. Your temporary eSIM has
+                roaming enabled by default, and cannot be disabled.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={10} md={8}>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={networkSettings.roamingOption}
+                    onChange={(e: any) => {
+                      setNetworkSettings({
+                        roamingOption: e.target.checked,
+                      });
+                      localStorage.setItem('roamingOption', e.target.checked);
+                    }}
+                  />
+                }
+                label="Enable roaming for all"
+                sx={{ typography: 'body1' }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={10} md={8}>
+              <TextField
+                select
+                id="eSims"
+                InputProps={{
+                  disabled: !networkSettings.roamingOption,
+                  disableUnderline: true,
+                }}
+                value={esim}
+                variant={'standard'}
+                sx={{ mt: '18px' }}
+                onChange={handleTimezoneChange}
+              >
+                {ROAMING_SELECT.map(({ value, text }: any) => (
+                  <MenuItem key={value} value={value}>
+                    <Typography variant="body2" sx={{ fontWeight: 500 }}>
+                      {text}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
