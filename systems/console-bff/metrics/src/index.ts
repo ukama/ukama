f67b7ae0e6adc3f25dd35b9d0560a75e4153d67a
salman-codewar <salmannaseem131@gmail.com>
2023-07-14 13:03:01 +0500

    Updated metrics server with typeGraphql, Apollo server, and express

--- systems/console-bff/metrics/src/index.ts
+++ systems/console-bff/metrics/src/index.ts
@@ -1,37 +1,80 @@
-import "dotenv/config";
-import * as fs from "fs";
-import { createPubSub, createSchema, createYoga } from "graphql-yoga";
-import { createServer } from "node:http";
-import * as path from "path";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServerPluginInlineTrace } from "@apollo/server/plugin/inlineTrace";
+import { buildSubgraphSchema, printSubgraphSchema } from "@apollo/subgraph";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import { GraphQLScalarType } from "graphql";
+import { DateTimeResolver } from "graphql-scalars";
+import gql from "graphql-tag";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { createServer } from "http";
+import "reflect-metadata";
+import * as tq from "type-graphql";
+import { WebSocketServer } from "ws";
 
+import { METRICS_PORT } from "../../common/configs";
 import { logger } from "../../common/logger";
-import { METRICS_PORT } from "./../../common/configs";
 import resolvers from "./resolvers";
 
-const typeDefs = fs.readFileSync(
-  path.join(process.cwd(), "metrics/schema.graphql"),
-  {
-    encoding: "utf-8",
-  }
-);
+const app = express();
+const httpServer = createServer(app);
 
-const pubSub = createPubSub({});
+const runServer = async () => {
+  const ts = await tq.buildSchema({
+    resolvers: resolvers,
+    scalarsMap: [{ type: GraphQLScalarType, scalar: DateTimeResolver }],
+    validate: { forbidUnknownValues: false },
+  });
 
-const yoga = createYoga({
-  schema: createSchema({
-    typeDefs,
-    resolvers,
-  }) as any,
-  logging: true,
-  context: {
-    pubSub,
-  },
-});
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql",
+  });
 
-const server = createServer(yoga);
+  const federatedSchema = buildSubgraphSchema({
+    typeDefs: gql(printSubgraphSchema(ts)),
+    resolvers: tq.createResolversMap(ts) as any,
+  });
 
-server.listen(METRICS_PORT, () => {
-  logger.info(
-    `🚀 Ukama Metrics service running at http://localhost:${METRICS_PORT}/graphql`
+  const serverCleanup = useServer({ schema: federatedSchema }, wsServer);
+
+  const server = new ApolloServer({
+    schema: federatedSchema,
+    csrfPrevention: false,
+    plugins: [
+      ApolloServerPluginInlineTrace({}),
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  await server.start();
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server)
   );
-});
+  httpServer.listen(METRICS_PORT, () => {
+    logger.info(
+      `🚀 Ukama Metrics service running at http://localhost:${METRICS_PORT}/graphql`
+    );
+    logger.info(
+      `🚀 Ukama Metrics subscription service running at ws://localhost:${METRICS_PORT}/graphql`
+    );
+  });
+};
+
+runServer();
