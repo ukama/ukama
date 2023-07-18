eb004e82711ceb75a761857bb7b812fa265d7f8c
vthakur7f <vthakur7@hotmail.com>
2023-07-18 15:59:27 -0700

    Added repo test.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

---
+++ systems/registry/member/pkg/db/member_repo_test.go
@@ -0,0 +1,237 @@
+package db
+
+import (
+	extsql "database/sql"
+	"regexp"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/ukama/ukama/systems/common/uuid"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/tj/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type UkamaDbMock struct {
+	GormDb *gorm.DB
+}
+
+func (u UkamaDbMock) Init(model ...interface{}) error {
+	panic("implement me: Init()")
+}
+
+func (u UkamaDbMock) Connect() error {
+	panic("implement me: Connect()")
+}
+
+func (u UkamaDbMock) GetGormDb() *gorm.DB {
+	return u.GormDb
+}
+
+func (u UkamaDbMock) InitDB() error {
+	return nil
+}
+
+func (u UkamaDbMock) ExecuteInTransaction(dbOperation func(tx *gorm.DB) *gorm.DB,
+	nestedFuncs ...func() error) error {
+	log.Fatal("implement me: ExecuteInTransaction()")
+	return nil
+}
+
+func (u UkamaDbMock) ExecuteInTransaction2(dbOperation func(tx *gorm.DB) *gorm.DB,
+	nestedFuncs ...func(tx *gorm.DB) error) error {
+	log.Fatal("implement me: ExecuteInTransaction2()")
+	return nil
+}
+
+func Test_AddMember(t *testing.T) {
+	t.Run("AddMember", func(t *testing.T) {
+		// Arrange
+		var db *extsql.DB
+
+		member := Member{
+			UserId: uuid.NewV4(),
+			Role:   Users,
+		}
+
+		db, mock, err := sqlmock.New() // mock sql.DB
+		assert.NoError(t, err)
+
+		mock.ExpectBegin()
+
+		mock.ExpectQuery(regexp.QuoteMeta(`INSERT`)).
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), member.UserId, false, member.Role).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+
+		mock.ExpectCommit()
+
+		dialector := postgres.New(postgres.Config{
+			DSN:                  "sqlmock_db_0",
+			DriverName:           "postgres",
+			Conn:                 db,
+			PreferSimpleProtocol: true,
+		})
+
+		gdb, err := gorm.Open(dialector, &gorm.Config{})
+		assert.NoError(t, err)
+
+		r := NewMemberRepo(&UkamaDbMock{
+			GormDb: gdb,
+		})
+
+		assert.NoError(t, err)
+
+		// Act
+		err = r.AddMember(&member)
+
+		// Assert
+		assert.NoError(t, err)
+
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
+	})
+}
+
+func Test_GetMember(t *testing.T) {
+	t.Run("MemberExist", func(t *testing.T) {
+		// Arrange
+		member := Member{
+			UserId: uuid.NewV4(),
+			Role:   Users,
+		}
+
+		var db *extsql.DB
+
+		db, mock, err := sqlmock.New() // mock sql.DB
+		assert.NoError(t, err)
+
+		rows := sqlmock.NewRows([]string{"id", "user_id", "role"}).
+			AddRow(1, member.UserId, member.Role)
+
+		mock.ExpectQuery(`^SELECT.*members.*`).
+			WithArgs(member.UserId).
+			WillReturnRows(rows)
+
+		dialector := postgres.New(postgres.Config{
+			DSN:                  "sqlmock_db_0",
+			DriverName:           "postgres",
+			Conn:                 db,
+			PreferSimpleProtocol: true,
+		})
+
+		gdb, err := gorm.Open(dialector, &gorm.Config{})
+		assert.NoError(t, err)
+
+		r := NewMemberRepo(&UkamaDbMock{
+			GormDb: gdb,
+		})
+
+		assert.NoError(t, err)
+
+		// Act
+		rm, err := r.GetMember(member.UserId)
+
+		// Assert
+		assert.NoError(t, err)
+
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
+		assert.NotNil(t, rm)
+	})
+}
+
+func Test_GetMembers(t *testing.T) {
+	t.Run("MembersOfAnOrg", func(t *testing.T) {
+		// Arrange
+
+		member := Member{
+			UserId: uuid.NewV4(),
+			Role:   Users,
+		}
+
+		var db *extsql.DB
+
+		db, mock, err := sqlmock.New() // mock sql.DB
+		assert.NoError(t, err)
+
+		rows := sqlmock.NewRows([]string{"id", "user_id", "role"}).
+			AddRow(1, member.UserId, member.Role)
+
+		mock.ExpectQuery(`^SELECT.*members.*`).
+			WillReturnRows(rows)
+
+		dialector := postgres.New(postgres.Config{
+			DSN:                  "sqlmock_db_0",
+			DriverName:           "postgres",
+			Conn:                 db,
+			PreferSimpleProtocol: true,
+		})
+
+		gdb, err := gorm.Open(dialector, &gorm.Config{})
+		assert.NoError(t, err)
+
+		r := NewMemberRepo(&UkamaDbMock{
+			GormDb: gdb,
+		})
+
+		assert.NoError(t, err)
+
+		// Act
+		members, err := r.GetMembers()
+
+		// Assert
+		assert.NoError(t, err)
+
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
+		assert.NotNil(t, members)
+	})
+}
+
+func Test_RemoveMember(t *testing.T) {
+	t.Run("RemoveMemberOfAnOrg", func(t *testing.T) {
+		// Arrange
+
+		member := Member{
+			UserId: uuid.NewV4(),
+			Role:   Users,
+		}
+
+		var db *extsql.DB
+
+		db, mock, err := sqlmock.New() // mock sql.DB
+		assert.NoError(t, err)
+		mock.ExpectBegin()
+		mock.ExpectExec(`^UPDATE.*members.*`).
+			WithArgs(sqlmock.AnyArg(), member.UserId).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+		dialector := postgres.New(postgres.Config{
+			DSN:                  "sqlmock_db_0",
+			DriverName:           "postgres",
+			Conn:                 db,
+			PreferSimpleProtocol: true,
+		})
+
+		gdb, err := gorm.Open(dialector, &gorm.Config{})
+		assert.NoError(t, err)
+
+		r := NewMemberRepo(&UkamaDbMock{
+			GormDb: gdb,
+		})
+
+		assert.NoError(t, err)
+
+		// Act
+		err = r.RemoveMember(member.UserId)
+
+		// Assert
+		assert.NoError(t, err)
+
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
+
+	})
+}
