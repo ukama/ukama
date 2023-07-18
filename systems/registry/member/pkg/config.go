c35719db3265869d3a1192ea8286f617f1d563ca
vthakur7f <vthakur7@hotmail.com>
2023-07-18 14:38:08 -0700

    added memeber service.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

---
+++ systems/registry/member/pkg/config.go
@@ -0,0 +1,62 @@
+package pkg
+
+import (
+	"time"
+
+	uconf "github.com/ukama/ukama/systems/common/config"
+	metric "github.com/ukama/ukama/systems/common/metrics"
+)
+
+const (
+	NumberOfMembers         = "number_of_members"
+	NumberOfActiveMembers   = "active_members"
+	NumberOfInactiveMembers = "inactive_members"
+	GaugeType               = "gauge"
+)
+
+type Config struct {
+	uconf.BaseConfig `mapstructure:",squash"`
+	DB               *uconf.Database  `default:"{}"`
+	Grpc             *uconf.Grpc      `default:"{}"`
+	Queue            *uconf.Queue     `default:"{}"`
+	Timeout          time.Duration    `default:"3s"`
+	MsgClient        *uconf.MsgClient `default:"{}"`
+	OrgHost          string           `default:"org:9090"`
+	OrgId            string
+	OrgName          string
+	Service          *uconf.Service
+	PushGateway      string `default:"http://localhost:9091"`
+}
+
+var MemberMetric = []metric.MetricConfig{
+	{
+		Name:   NumberOfMembers,
+		Type:   GaugeType,
+		Labels: map[string]string{"org": ""},
+		Value:  0,
+	},
+	{
+		Name:   NumberOfActiveMembers,
+		Type:   GaugeType,
+		Labels: map[string]string{"org": ""},
+		Value:  0,
+	},
+	{
+		Name:   NumberOfInactiveMembers,
+		Type:   GaugeType,
+		Labels: map[string]string{"org": ""},
+		Value:  0,
+	},
+}
+
+func NewConfig(name string) *Config {
+	return &Config{
+		DB: &uconf.Database{
+			DbName: name,
+		},
+		Service: uconf.LoadServiceHostConfig(name),
+		MsgClient: &uconf.MsgClient{
+			Timeout: 5 * time.Second,
+		},
+	}
+}
---
+++ systems/registry/member/pkg/db/member_repo.go
@@ -0,0 +1,107 @@
+package db
+
+import (
+	"github.com/ukama/ukama/systems/common/uuid"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+
+	"github.com/ukama/ukama/systems/common/sql"
+)
+
+type MemberRepo interface {
+
+	/* Members */
+	AddMember(member *Member) error
+	GetMember(userUUID uuid.UUID) (*Member, error)
+	GetMembers() ([]Member, error)
+	UpdateMember(member *Member) error
+	RemoveMember(userUUID uuid.UUID) error
+	GetMemberCount() (int64, int64, error)
+}
+
+type memberRepo struct {
+	Db sql.Db
+}
+
+func NewMemberRepo(db sql.Db) MemberRepo {
+	return &memberRepo{
+		Db: db,
+	}
+}
+
+func (r *memberRepo) AddMember(member *Member) error {
+	d := r.Db.GetGormDb().Create(member)
+
+	return d.Error
+}
+
+func (r *memberRepo) GetMember(userUUID uuid.UUID) (*Member, error) {
+	var member Member
+
+	result := r.Db.GetGormDb().
+		Where("user_id = ?", userUUID).First(&member)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &member, nil
+}
+
+func (r *memberRepo) GetMembers() ([]Member, error) {
+	var members []Member
+
+	result := r.Db.GetGormDb().Find(&members)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return members, nil
+}
+
+func (r *memberRepo) UpdateMember(member *Member) error {
+	d := r.Db.GetGormDb().Clauses(clause.Returning{}).
+		Where("user_id = ?", member.UserId).Updates(member)
+
+	if d.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+
+	return d.Error
+}
+
+func (r *memberRepo) RemoveMember(userUUID uuid.UUID) error {
+	var member Member
+
+	d := r.Db.GetGormDb().
+		Where("user_id = ?", userUUID).Delete(&member)
+
+	if d.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+
+	return d.Error
+}
+
+func (r *memberRepo) GetMemberCount() (int64, int64, error) {
+	var activeMemberCount int64
+	var deactiveMemberCount int64
+
+	result := r.Db.GetGormDb().Model(&Member{}).
+		Where("deactivated = ?", false).
+		Count(&activeMemberCount)
+
+	if result.Error != nil {
+		return 0, 0, result.Error
+	}
+
+	result = r.Db.GetGormDb().Model(&Member{}).
+		Where("deactivated = ?", true).
+		Count(&deactiveMemberCount)
+
+	if result.Error != nil {
+		return 0, 0, result.Error
+	}
+
+	return activeMemberCount, deactiveMemberCount, nil
+}
