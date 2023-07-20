07e0160676a4346128e2e62fec3ca4dfa545fca6
vthakur7f <vthakur7@hotmail.com>
2023-07-20 10:34:39 -0700

    Cleaned org.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/nucleus/org/pkg/server/org.go
+++ systems/nucleus/org/pkg/server/org.go
@@ -99,7 +99,6 @@ func (o *OrgService) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddRespon
 	}
 
 	_ = o.pushOrgCountMetric()
-	_ = o.pushOrgMemberCountMetric(org.Id)
 	_ = o.pushUserCountMetric()
 
 	return &pb.AddResponse{Org: dbOrgToPbOrg(org)}, nil
@@ -252,164 +251,10 @@ func (o *OrgService) RegisterUser(ctx context.Context, req *pb.RegisterUserReque
 		log.Errorf("Failed to publish message %+v with key %+v. Errors %s", req, route, err.Error())
 	}
 
-	_ = o.pushOrgMemberCountMetric(org.Id)
+	
 	_ = o.pushUserCountMetric()
 
 	return &pb.MemberResponse{Member: dbMemberToPbMember(member)}, nil
-}
-
-func (o *OrgService) AddMember(ctx context.Context, req *pb.AddMemberRequest) (*pb.MemberResponse, error) {
-	// Get the Organization
-	org, err := o.orgRepo.GetByName(req.GetOrgName())
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "org")
-	}
-
-	// Get the User
-	userUUID, err := uuid.FromString(req.GetUserUuid())
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument,
-			"invalid format of user uuid. Error %s", err.Error())
-	}
-
-	user, err := o.userRepo.Get(userUUID)
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "user")
-	}
-
-	log.Infof("Adding member")
-	member := &db.OrgUser{
-		OrgId:  org.Id,
-		UserId: user.Id,
-		Uuid:   userUUID,
-		Role:   pbRoleTypeToDb(req.GetRole()),
-	}
-
-	err = o.orgRepo.AddMember(member)
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "member")
-	}
-
-	route := o.baseRoutingKey.SetAction("add").SetObject("member").MustBuild()
-	err = o.msgbus.PublishRequest(route, req)
-	if err != nil {
-		log.Errorf("Failed to publish message %+v with key %+v. Errors %s", req, route, err.Error())
-	}
-
-	_ = o.pushOrgMemberCountMetric(org.Id)
-
-	return &pb.MemberResponse{Member: dbMemberToPbMember(member)}, nil
-}
-
-func (o *OrgService) GetMember(ctx context.Context, req *pb.MemberRequest) (*pb.MemberResponse, error) {
-	uuid, err := uuid.FromString(req.GetUserUuid())
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument,
-			"invalid format of user uuid. Error %s", err.Error())
-	}
-
-	// Get the Organization
-	org, err := o.orgRepo.GetByName(req.GetOrgName())
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "org")
-	}
-
-	member, err := o.orgRepo.GetMember(org.Id, uuid)
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "member")
-	}
-
-	return &pb.MemberResponse{Member: dbMemberToPbMember(member)}, nil
-}
-
-func (o *OrgService) GetMembers(ctx context.Context, req *pb.GetMembersRequest) (*pb.GetMembersResponse, error) {
-	org, err := o.orgRepo.GetByName(req.GetOrgName())
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "org")
-	}
-
-	members, err := o.orgRepo.GetMembers(org.Id)
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "orgs")
-	}
-
-	resp := &pb.GetMembersResponse{
-		Org:     org.Name,
-		Members: dbMembersToPbMembers(members),
-	}
-
-	return resp, nil
-}
-
-func (o *OrgService) UpdateMember(ctx context.Context, req *pb.UpdateMemberRequest) (*pb.MemberResponse, error) {
-	uuid, err := uuid.FromString(req.GetMember().GetUserUuid())
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument,
-			"invalid format of user uuid. Error %s", err.Error())
-	}
-
-	org, err := o.orgRepo.GetByName(req.GetMember().GetOrgName())
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "org")
-	}
-
-	member := &db.OrgUser{
-		OrgId:       org.Id,
-		Uuid:        uuid,
-		Deactivated: req.GetAttributes().IsDeactivated,
-	}
-
-	err = o.orgRepo.UpdateMember(member.OrgId, member)
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "member")
-	}
-
-	_ = o.pushOrgMemberCountMetric(org.Id)
-
-	return &pb.MemberResponse{Member: dbMemberToPbMember(member)}, nil
-}
-
-func (o *OrgService) RemoveMember(ctx context.Context, req *pb.MemberRequest) (*pb.MemberResponse, error) {
-	uuid, err := uuid.FromString(req.GetUserUuid())
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument,
-			"invalid format of user uuid. Error %s", err.Error())
-	}
-
-	org, err := o.orgRepo.GetByName(req.GetOrgName())
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "org")
-	}
-
-	member, err := o.orgRepo.GetMember(org.Id, uuid)
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "member")
-	}
-
-	if org.Owner == member.Uuid {
-		return nil, status.Errorf(codes.FailedPrecondition,
-			"cannot remove the current owner of the Organization")
-	}
-
-	if !member.Deactivated {
-		return nil, status.Errorf(codes.FailedPrecondition,
-			"member must be deactivated first")
-	}
-
-	err = o.orgRepo.RemoveMember(org.Id, uuid)
-	if err != nil {
-		return nil, grpc.SqlErrorToGrpc(err, "member")
-	}
-
-	route := o.baseRoutingKey.SetAction("remove").SetObject("member").MustBuild()
-	err = o.msgbus.PublishRequest(route, req)
-	if err != nil {
-		log.Errorf("Failed to publish message %+v with key %+v. Errors %s", req, route, err.Error())
-	}
-
-	_ = o.pushOrgMemberCountMetric(org.Id)
-
-	return &pb.MemberResponse{}, nil
 }
 
 func dbOrgToPbOrg(org *db.Org) *pb.Organization {
@@ -440,27 +285,6 @@ func dbUserToPbUser(user *db.User) *pb.User {
 	}
 }
 
-func dbMemberToPbMember(member *db.OrgUser) *pb.OrgUser {
-	return &pb.OrgUser{
-		OrgId:         member.OrgId.String(),
-		UserId:        uint64(member.UserId),
-		Uuid:          member.Uuid.String(),
-		Role:          pb.RoleType(member.Role),
-		IsDeactivated: member.Deactivated,
-		CreatedAt:     timestamppb.New(member.CreatedAt),
-	}
-}
-
-func dbMembersToPbMembers(members []db.OrgUser) []*pb.OrgUser {
-	res := []*pb.OrgUser{}
-
-	for _, m := range members {
-		res = append(res, dbMemberToPbMember(&m))
-	}
-
-	return res
-}
-
 func (o *OrgService) pushOrgCountMetric() error {
 	actOrg, inactOrg, err := o.orgRepo.GetOrgCount()
 	if err != nil {
@@ -477,31 +301,6 @@ func (o *OrgService) pushOrgCountMetric() error {
 	err = metric.CollectAndPushSimMetrics(o.pushgateway, pkg.OrgMetrics, pkg.NumberOfInactiveOrgs, float64(inactOrg), nil, pkg.SystemName+"-"+pkg.ServiceName)
 	if err != nil {
 		log.Errorf("Error while pushing inactive Org metric to pushgateway %s", err.Error())
-		return err
-	}
-	return nil
-}
-
-func (o *OrgService) pushOrgMemberCountMetric(orgId uuid.UUID) error {
-
-	actMemOrg, inactMemOrg, err := o.orgRepo.GetMemberCount(orgId)
-	if err != nil {
-		log.Errorf("failed to get member count for org %s.Error: %s", orgId.String(), err.Error())
-		return err
-	}
-
-	labels := make(map[string]string)
-	labels["org"] = orgId.String()
-
-	err = metric.CollectAndPushSimMetrics(o.pushgateway, pkg.OrgMetrics, pkg.NumberOfActiveMembersOfOrgs, float64(actMemOrg), labels, pkg.SystemName+"-"+pkg.ServiceName)
-	if err != nil {
-		log.Errorf("Error while pushing active members of Org metric to pushgateway %s", err.Error())
-		return err
-	}
-
-	err = metric.CollectAndPushSimMetrics(o.pushgateway, pkg.OrgMetrics, pkg.NumberOfInactiveMembersOfOrgs, float64(inactMemOrg), labels, pkg.SystemName+"-"+pkg.ServiceName)
-	if err != nil {
-		log.Errorf("Error while pushing inactive members Org metric to pushgateway %s", err.Error())
 		return err
 	}
 	return nil
@@ -535,31 +334,6 @@ func (o *OrgService) PushMetrics() error {
 
 	_ = o.pushUserCountMetric()
 
-	orgs, err := o.orgRepo.GetAll()
-	if err != nil {
-		log.Errorf("Error while reading orgs. Error %s", err.Error())
-		return err
-	}
-
-	for _, org := range orgs {
-		_ = o.pushOrgMemberCountMetric(org.Id)
-	}
-
 	return nil
 
-}
-
-func pbRoleTypeToDb(role pb.RoleType) db.RoleType {
-	switch role {
-	case pb.RoleType_ADMIN:
-		return db.Admin
-	case pb.RoleType_VENDOR:
-		return db.Vendor
-	case pb.RoleType_MEMBER:
-		return db.Member
-	case pb.RoleType_OWNER:
-		return db.Owner
-	default:
-		return db.Member
-	}
 }
