c26eb1a6ea6b9285022d70e61437a5945c774ebe
usama khan <usamakhan049@gmail.com>
2023-07-25 20:27:29 +0500

    subscriber subgraph

--- systems/console-bff/network/src/resolver/index.ts
+++ systems/console-bff/network/src/resolver/index.ts
@@ -1,20 +1,20 @@
 import { NonEmptyArray } from "type-graphql";
 
-import { AddMemberResolver } from "./addMember.resolver";
-import { AddOrgResolver } from "./addOrg.resolver";
-import { GetOrgResolver } from "./getOrg.resolver";
-import { GetOrgMembersResolver } from "./getOrgMembers.resolver";
-import { GetOrgsResolver } from "./getOrgs.resolver";
-import { RemoveMemberResolver } from "./removeMember.resolver";
-import { UpdateMemberResolver } from "./updateMember.resolver";
+import { AddNetworkResolver } from "./addNetwork.resolver";
+import { AddSiteResolver } from "./addSite.resolver";
+import { GetNetworkResolver } from "./getNetwork.resolver";
+import { GetNetworksResolver } from "./getNetworks.resolver";
+import { GetNetworkStatusResolver } from "./getNetworkStatus.resolver";
+import { GetSiteResolver } from "./getSite.resolver";
+import { GetSitesResolver } from "./getSites.resolver";
 
 
-const resolvers: NonEmptyArray<Function> = [AddMemberResolver,
-    AddOrgResolver,
-    GetOrgResolver,
-    GetOrgMembersResolver,
-    GetOrgsResolver,
-    RemoveMemberResolver,
-    UpdateMemberResolver];
+const resolvers: NonEmptyArray<Function> = [AddNetworkResolver,
+    AddSiteResolver,
+    GetNetworkResolver,
+    GetNetworksResolver,
+    GetNetworkStatusResolver,
+    GetSiteResolver,
+    GetSitesResolver];
 
 export default resolvers;
