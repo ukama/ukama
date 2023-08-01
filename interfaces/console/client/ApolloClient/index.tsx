792c1a57b40017a4f7d66564325bb05bb1d9d344
salman-codewar <salmannaseem131@gmail.com>
2023-08-01 18:12:18 +0500

    apollo client cleanup

--- interfaces/console/client/ApolloClient/index.tsx
+++ interfaces/console/client/ApolloClient/index.tsx
@@ -3,6 +3,14 @@ import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { createClient } from 'graphql-ws';
 
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_REACT_APP_API,
+  cache: new InMemoryCache(),
+  credentials: 'include',
+});
+
+export default client;
+
 const httpLink = new HttpLink({
   uri: 'http://localhost:4042/graphql',
 });
@@ -13,11 +21,6 @@ const wsLink = new GraphQLWsLink(
   }),
 );
 
-// The split function takes three parameters:
-//
-// * A function that's called for each operation to execute
-// * The Link to use for an operation if the function returns a "truthy" value
-// * The Link to use for an operation if the function returns a "falsy" value
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
@@ -29,68 +32,6 @@ const splitLink = split(
   wsLink,
   httpLink,
 );
-
-// const httpLink = new HttpLink({
-//   uri: process.env.NEXT_PUBLIC_REACT_APP_API,
-//   credentials: 'include',
-// });
-
-// const wsLink = new WebSocketLink({
-//   uri: process.env.NEXT_PUBLIC_REACT_APP_API_SOCKET || '',
-//   options: {
-//     reconnect: true,
-//     connectionParams: {
-//       credentials: 'include',
-//     },
-//   },
-// });
-
-// const splitLink = split(
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return (
-//       definition.kind === 'OperationDefinition' &&
-//       definition.operation === 'subscription'
-//     );
-//   },
-//   wsLink,
-//   httpLink,
-// );
-
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_REACT_APP_API,
-  cache: new InMemoryCache(),
-  credentials: 'include',
-});
-
-export default client;
-
-// const metricsHttpLink = new HttpLink({
-//   uri: process.env.NEXT_PUBLIC_REACT_METRICS_API,
-//   credentials: 'include',
-// });
-
-// const metricsWsLink = new WebSocketLink({
-//   uri: process.env.NEXT_PUBLIC_REACT_METRICS_API_SOCKET || '',
-//   options: {
-//     reconnect: true,
-//     connectionParams: {
-//       credentials: 'include',
-//     },
-//   },
-// });
-
-// const metricsSplitLink = split(
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return (
-//       definition.kind === 'OperationDefinition' &&
-//       definition.operation === 'subscription'
-//     );
-//   },
-//   metricsWsLink,
-//   metricsHttpLink,
-// );
 
 export const metricsClient = new ApolloClient({
   link: splitLink,
