7cba67234c191c891a5a9faf7d498af029c1c306
vthakur7f <vthakur7@hotmail.com>
2023-08-03 16:59:39 -0700

    updated copyLib.sh
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/services/initClient/copyLibs.sh
+++ systems/services/initClient/copyLibs.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 set -e
 TARGET_EXEC=$1
-echo "Copying libs for $TARGET_EXEC"
 rm -rf ./libs 
 mkdir ./libs
 
+# Logs
+ldd $TARGET_EXEC | awk 'NF == 4 { system("echo cp " $3 " ./libs") }'
+
+echo "Copying libs for $TARGET_EXEC"
+
 #Copying dependencies 
 ldd $TARGET_EXEC | awk 'NF == 4 { system("cp " $3 " ./libs") }'
+
+echo "Copied files"
+ls -ltr ./libs
+
 sleep 5;
