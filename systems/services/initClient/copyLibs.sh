9037213bd04f57fa0fd63664406b67858f6ff608
vthakur7f <vthakur7@hotmail.com>
2023-08-03 18:14:01 -0700

    Workaround for libmicrohttpd.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/services/initClient/copyLibs.sh
+++ systems/services/initClient/copyLibs.sh
@@ -12,7 +12,16 @@ echo "Copying libs for $TARGET_EXEC"
 #Copying dependencies 
 ldd $TARGET_EXEC | awk 'NF == 4 { system("cp " $3 " ./libs") }'
 
+
+if [ -d "/home/runner/work/ukama/ukama/nodes/ukamaOS/distro/vendor/build/lib" ]; then
+    echo "Workaround for microhttpd."
+    cp /home/runner/work/ukama/ukama/nodes/ukamaOS/distro/vendor/build/lib/libmicrohttpd.* ./libs 
+else 
+    echo "Nothing required"
+fi
+
+sleep 5;
+
 echo "Copied files"
 ls -ltr ./libs
 
-sleep 5;
