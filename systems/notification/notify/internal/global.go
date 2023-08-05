2620d037e7672b82e384d41cdb3ec37c9528786a
Elvis Donald Attro <attroelvis@gmail.com>
2023-08-05 10:55:13 +0000

    Notification system: Notify Service.  Fixes #543 (#550)
    * notification/notify: update repository>
    
        - update makefile
        - update go.sum
        - update proto package & go package
        - update system name
        - update project folder structure
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: update notify service.
    
        - DB models
        - DB repos
        - Protos
        - Rpcs
        - Configs
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: add notifications events publishing.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: update notify server units tests.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: add notifications events listening.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: add status field.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: comment old integ tests.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: fix gorm DB column name.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification: add system compose file.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: add CI/CD to notify service.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification: uncomment initClient compose service.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification/notify: update README.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    * notification: update README.
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>
    
    ---------
    
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/notification/notify/internal/global.go
+++ systems/notification/notify/internal/global.go
@@ -2,4 +2,8 @@ package internal
 
 const ServiceName = "notify"
 
+const SystemName = "notification"
+
 var IsDebugMode = false
+
+var InstanceId = "debug"
---
+++ systems/notification/notify/internal/server/event_test.go
@@ -0,0 +1,112 @@
+package server_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/ukama/ukama/systems/common/ukama"
+	"github.com/ukama/ukama/systems/notification/notify/internal/server"
+	"github.com/ukama/ukama/systems/notification/notify/mocks"
+	"google.golang.org/protobuf/types/known/anypb"
+
+	mbmocks "github.com/ukama/ukama/systems/common/mocks"
+	epb "github.com/ukama/ukama/systems/common/pb/gen/events"
+)
+
+func TestNotifyEventServer_HandleNotificationSentEvent(t *testing.T) {
+	msgbusClient := &mbmocks.MsgBusServiceClient{}
+	msgbusClient.On("PublishRequest", mock.Anything, mock.Anything).Return(nil).Once()
+	repo := mocks.NotificationRepo{}
+
+	node := ukama.NewVirtualHomeNodeId().String()
+	nt := NewTestDbNotification(node, "alert")
+
+	listenerRoutes := []string{"event.cloud.org.notification.sent",
+		"event.cloud.node.notification.sent"}
+
+	t.Run("NotificationEventSent", func(t *testing.T) {
+		routingKey := "event.cloud.node.notification.sent"
+
+		repo.On("Add", mock.Anything).Return(nil)
+
+		evt := &epb.Notification{
+			Id:          nt.Id.String(),
+			NodeId:      nt.NodeId,
+			NodeType:    nt.NodeType,
+			Severity:    nt.Severity.String(),
+			Type:        nt.Type.String(),
+			ServiceName: nt.ServiceName,
+			Status:      nt.Status,
+			EpochTime:   nt.Time,
+			Description: nt.Description,
+			Details:     nt.Details.String(),
+		}
+
+		anyE, err := anypb.New(evt)
+		assert.NoError(t, err)
+
+		msg := &epb.Event{
+			RoutingKey: routingKey,
+			Msg:        anyE,
+		}
+
+		s := server.NewNotifyEventServer(&repo, msgbusClient, listenerRoutes)
+
+		_, err = s.EventNotification(context.TODO(), msg)
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("InvalidNotificationEventSent", func(t *testing.T) {
+		routingKey := "event.cloud.node.notification.sent"
+
+		evt := &epb.Notification{
+			Id:          nt.Id.String(),
+			NodeId:      "foo",
+			NodeType:    nt.NodeType,
+			Severity:    nt.Severity.String(),
+			Type:        "bar",
+			ServiceName: nt.ServiceName,
+			Status:      nt.Status,
+			EpochTime:   nt.Time,
+			Description: nt.Description,
+			Details:     nt.Details.String(),
+		}
+
+		anyE, err := anypb.New(evt)
+		assert.NoError(t, err)
+
+		msg := &epb.Event{
+			RoutingKey: routingKey,
+			Msg:        anyE,
+		}
+
+		s := server.NewNotifyEventServer(&repo, msgbusClient, listenerRoutes)
+
+		_, err = s.EventNotification(context.TODO(), msg)
+
+		assert.Error(t, err)
+	})
+
+	t.Run("NotificationEventNotSent", func(t *testing.T) {
+		routingKey := "event.cloud.cdr.sim.usage"
+
+		evt := epb.SimUsage{}
+
+		anyE, err := anypb.New(&evt)
+		assert.NoError(t, err)
+
+		msg := &epb.Event{
+			RoutingKey: routingKey,
+			Msg:        anyE,
+		}
+
+		s := server.NewNotifyEventServer(&repo, msgbusClient, listenerRoutes)
+
+		_, err = s.EventNotification(context.TODO(), msg)
+
+		assert.NoError(t, err)
+	})
+}
