81d753b74ddf1ded4acd8ca71393fc7a5b67bf1c
salman-codewar <salmannaseem131@gmail.com>
2023-08-17 02:04:56 +0500

    parse query parmas and store in local storage, get networks against org

--- interfaces/console/pages/_main_app.tsx
+++ interfaces/console/pages/_main_app.tsx
@@ -7,9 +7,8 @@ import {
   snackbarMessage,
   user,
 } from '@/app-recoil';
-import { useWhoamiLazyQuery } from '@/generated';
+import { useGetNetworksLazyQuery } from '@/generated';
 import { MyAppProps, TCommonData, TSnackMessage, TUser } from '@/types';
-import { getTitleFromPath } from '@/utils';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
@@ -21,6 +20,7 @@ const Layout = dynamic(() => import('@/ui/layout'), {
 
 const MainApp = ({ Component, pageProps }: MyAppProps) => {
   const route = useRouter();
+
   const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
   const [_user, _setUser] = useRecoilState<TUser>(user);
   const [page, setPage] = useRecoilState(pageName);
@@ -32,15 +32,8 @@ const MainApp = ({ Component, pageProps }: MyAppProps) => {
   const [_commonData, setCommonData] = useRecoilState<TCommonData>(commonData);
   const resetData = useResetRecoilState(user);
   const resetPageName = useResetRecoilState(pageName);
-  const [getWhoami, { data, loading, error }] = useWhoamiLazyQuery({
-    fetchPolicy: 'cache-first',
-    onCompleted: (data) => {console.log(data)}
-  })
-  // const [getWhoami, { data, loading, error }] = useWhoamiLazyQuery({
-  //   fetchPolicy: 'cache-first',
-  //   onCompleted: (data) => {
-  //     if (data.whoami) {
-  //       if (!_user?.id)
+
+  //TODO: HOOK GET MEMBER QUERY & SET USER
   //         _setUser({
   //           id: data.whoami.id,
   //           name: data.whoami.name,
@@ -48,59 +41,59 @@ const MainApp = ({ Component, pageProps }: MyAppProps) => {
   //           role: data.whoami.role,
   //           isFirstVisit: data.whoami.isFirstVisit,
   //         });
-  //     }
-  //   },
-  //   onError: (error) => {
-  //     setSnackbarMessage({
-  //       id: 'whoami-msg',
-  //       message: error.message,
-  //       type: 'error' as AlertColor,
-  //       show: true,
-  //     });
-  //     // resetData();
-  //     // window.location.replace(`${process.env.NEXT_PUBLIC_REACT_AUTH_APP_URL}`);
-  //   },
-  // });
-  // const [
-  //   getNetworks,
-  //   { data: networksData, error: networksError, loading: networksLoading },
-  // ] = useGetNetworksLazyQuery({
-  //   fetchPolicy: 'cache-first',
-  //   onCompleted: (data) => {
-  //     if (data.getNetworks.networks.length === 1) {
-  //       setCommonData({
-  //         ..._commonData,
-  //         networkId: data.getNetworks.networks[0].id,
-  //         networkName: data.getNetworks.networks[0].name,
-  //       });
-  //     }
-  //   },
-  //   onError: (error) => {
-  //     setSnackbarMessage({
-  //       id: 'networks-msg',
-  //       message: error.message,
-  //       type: 'error' as AlertColor,
-  //       show: true,
-  //     });
-  //   },
-  // });
+
+  const [
+    getNetworks,
+    { data: networksData, error: networksError, loading: networksLoading },
+  ] = useGetNetworksLazyQuery({
+    fetchPolicy: 'cache-first',
+    onCompleted: (data) => {
+      if (data.getNetworks.networks.length === 1) {
+        setCommonData({
+          ..._commonData,
+          networkId: data.getNetworks.networks[0].id,
+          networkName: data.getNetworks.networks[0].name,
+        });
+      }
+    },
+    onError: (error) => {
+      setSnackbarMessage({
+        id: 'networks-msg',
+        message: error.message,
+        type: 'error',
+        show: true,
+      });
+    },
+  });
 
   useEffect(() => {
-    setSkeltonLoading(true);
-    getWhoami();
-    // getNetworks();
-  }, []);
+    const orgId = route.query['org-id'] as string;
+    const orgName = route.query['org-name'] as string;
+    const userId = route.query['uid'] as string;
 
-  useEffect(() => {
-    // if (
-    //   data?.whoami.id &&
-    //   networksData?.getNetworks?.networks &&
-    //   networksData?.getNetworks?.networks.length > 0
-    // ) {
-    //   setSkeltonLoading(false);
-    // }
+    if (orgId && orgName) {
+      setCommonData({
+        orgId,
+        orgName,
+        userId,
+        networkId: '',
+        networkName: '',
+      });
+      route.replace(route.pathname, undefined, { shallow: true });
+    } else {
+      // TODO: NO ORG FOUND AGAINST USER, Navigate to Org selection screen/ Invitation screen
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [route]);
+
+  useEffect(() => {
+    if (_commonData.userId) {
+    }
+    if (_commonData.orgId) {
+      setSkeltonLoading(true);
+      getNetworks();
+    }
+  }, [commonData]);
 
   // useEffect(() => {
   //   const { id, name, email } = _user;
@@ -130,18 +123,18 @@ const MainApp = ({ Component, pageProps }: MyAppProps) => {
   //   }
   // }, []);
 
-  useEffect(() => {
-    if (route.pathname) {
-      setIsFullScreen(
-        route.pathname === '/manage' ||
-          route.pathname === '/settings' ||
-          getTitleFromPath(route.pathname, route.query['id'] as string) ===
-            '404',
-      );
-      setPage(getTitleFromPath(route.pathname, route.query['id'] as string));
-      if (getTitleFromPath(route.pathname, '') === '404') route.replace('/404');
-    }
-  }, [route.pathname]);
+  // useEffect(() => {
+  //   if (route.pathname) {
+  //     setIsFullScreen(
+  //       route.pathname === '/manage' ||
+  //         route.pathname === '/settings' ||
+  //         getTitleFromPath(route.pathname, route.query['id'] as string) ===
+  //           '404',
+  //     );
+  //     setPage(getTitleFromPath(route.pathname, route.query['id'] as string));
+  //     if (getTitleFromPath(route.pathname, '') === '404') route.replace('/404');
+  //   }
+  // }, [route.pathname]);
 
   const handleGoToLogin = () => {
     setPage('Home');
@@ -151,13 +144,13 @@ const MainApp = ({ Component, pageProps }: MyAppProps) => {
 
   const handlePageChange = (page: string) => setPage(page);
   const handleNetworkChange = (id: string) => {
-    // setCommonData({
-    //   ..._commonData,
-    //   networkId: id,
-    //   networkName:
-    //     networksData?.getNetworks.networks.filter((n) => n.id === id)[0].name ??
-    //     '',
-    // });
+    setCommonData({
+      ..._commonData,
+      networkId: id,
+      networkName:
+        networksData?.getNetworks.networks.filter((n) => n.id === id)[0].name ??
+        '',
+    });
   };
 
   return (
@@ -170,8 +163,7 @@ const MainApp = ({ Component, pageProps }: MyAppProps) => {
       networkId={_commonData?.networkId}
       handlePageChange={handlePageChange}
       handleNetworkChange={handleNetworkChange}
-      // networks={networksData?.getNetworks.networks}
-      networks={[]}
+      networks={networksData?.getNetworks.networks}
     >
       <Component {...pageProps} />
     </Layout>
