ee5a925493af0fd917334c9981a1cfea032a72ca
Brackley cassinga <pcassinga@gmail.com>
2023-08-16 12:39:37 +0200

    rpcs added to invitation and nucleus/not  providers

--- systems/registry/invitation/pkg/server/invitation.go
+++ systems/registry/invitation/pkg/server/invitation.go
@@ -50,7 +50,6 @@ func (i *InvitationServer) Add(ctx context.Context, req *pb.AddInvitationRequest
 	if err != nil {
 		return nil, err
 	}
-
 	res, err := i.nucleusSystem.GetOrgByName(req.GetOrg())
 	if err != nil {
 		return nil, err
