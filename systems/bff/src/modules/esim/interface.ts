3ec86449ed4ecc7a23178e4b05b29f7467b712b7
salman-codewar <salmannaseem131@gmail.com>
2023-08-29 19:11:34 +0500

    old bff removed

--- systems/bff/src/modules/esim/interface.ts
+++
@@ -1,9 +0,0 @@
-import { EsimDto, EsimResponse } from "./types";
-
-export interface IEsimService {
-    getEsims(): Promise<EsimDto[]>;
-}
-
-export interface IEsimMapper {
-    dtoToDto(data: EsimResponse): EsimDto[];
-}
