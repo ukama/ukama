73f6a0466e5a10a74630093b73e2440382a50a46
Brackley cassinga <pcassinga@gmail.com>
2023-09-01 13:15:01 +0200

    configurator/controller srv added to systems

---
+++ systems/Configurator/controller/pkg/global.go
@@ -0,0 +1,7 @@
+package pkg
+
+const ServiceName = "controller"
+const SystemName = "configurator"
+
+var IsDebugMode bool = false
+var InstanceId = ServiceName + "-debug"
---
+++ systems/Configurator/controller/pkg/server/controller_test.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	mbmocks "github.com/ukama/ukama/systems/common/mocks"
+	"github.com/ukama/ukama/systems/common/uuid"
+	"github.com/ukama/ukama/systems/configurator/controller/mocks"
+	pb "github.com/ukama/ukama/systems/configurator/controller/pb/gen"
+	"github.com/ukama/ukama/systems/configurator/controller/pkg"
+)
+
+const testOrgName = "test-org"
+
+var orgId = uuid.NewV4()
+
+func TestControllerServer_RestartSite(t *testing.T) {
+	// Arrange
+	msgclientRepo := &mbmocks.MsgBusServiceClient{}
+
+	RegRepo := &mocks.RegistryProvider{}
+
+	
+netId:=uuid.NewV4()
+
+	
+	s := NewControllerServer(msgclientRepo, RegRepo,pkg.IsDebugMode, testOrgName)
+	
+	RegRepo.On("ValidateSite",mock.Anything,mock.Anything,mock.Anything).Return( nil).Once()
+	msgclientRepo.On("PublishRequest", mock.Anything, &pb.RestartSiteRequest{
+		SiteName:"pamoja",
+		NetworkId:netId.String(),
+	}).Return(nil).Once()
+	// Act
+	_, err := s.RestartSite(context.TODO(), &pb.RestartSiteRequest{
+		SiteName:"pamoja",
+		NetworkId:netId.String(),
+	})
+	// Assert
+	msgclientRepo.AssertExpectations(t)
+	assert.NoError(t, err)
+
+}
+func TestControllerServer_RestartNode(t *testing.T) {
+	// Arrange
+	msgclientRepo := &mbmocks.MsgBusServiceClient{}
+
+	RegRepo := &mocks.RegistryProvider{}
+
+	
+	nodeId:=uuid.NewV4()
+
+	
+	s := NewControllerServer(msgclientRepo, RegRepo,pkg.IsDebugMode, testOrgName)
+		RegRepo.On("ValidateNode",mock.Anything,mock.Anything).Return().Once()
+
+	msgclientRepo.On("PublishRequest", mock.Anything, &pb.RestartNodeRequest{
+		NodeId:nodeId.String(),
+	}).Return(nil).Once()
+	// Act
+	_, err := s.RestartNode(context.TODO(), &pb.RestartNodeRequest{
+		NodeId:nodeId.String(),
+	})
+	// Assert
+	msgclientRepo.AssertExpectations(t)
+	assert.NoError(t, err)
+
+}
+
