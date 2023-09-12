d28f08625d3b59f6c16e91bc4c7f81c33408f352
Elvis Attro <attroelvis@gmail.com>
2023-09-12 20:19:00 +0000

    api/api-gateway: update router client.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/api/api-gateway/pkg/client/client.go
+++ systems/api/api-gateway/pkg/client/client.go
@@ -1,13 +1,20 @@
 package client
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+
 	"github.com/ukama/ukama/systems/api/api-gateway/pkg"
 	"github.com/ukama/ukama/systems/api/api-gateway/pkg/db"
+	"github.com/ukama/ukama/systems/common/rest"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Client interface {
-	GetNetwork(string) (*NetworkInfo, *db.ResourceStatus, error)
-	CreateNetwork(string, string) (*NetworkInfo, *db.ResourceStatus, error)
+	GetNetwork(string) (*NetworkInfo, error)
+	CreateNetwork(string, string) (*NetworkInfo, error)
 }
 
 type clients struct {
@@ -23,26 +30,61 @@ func NewClientsSet(resRepo db.ResourceRepo, endpoints *pkg.HttpEndpoints) Client
 	return c
 }
 
-func (c *clients) GetNetwork(id string) (*NetworkInfo, *db.ResourceStatus, error) {
+func (c *clients) GetNetwork(id string) (*NetworkInfo, error) {
 	net, err := c.network.Get(id)
 	if err != nil {
-		return nil, nil, err
+		e := ErrorStatus{}
+
+		if errors.As(err, &e) {
+			return nil, rest.HttpError{
+				HttpCode: e.StatusCode,
+				Message:  err.Error(),
+			}
+		}
+
+		return nil, err
 	}
 
 	res, err := c.resRepo.Get(net.Id)
 	if err != nil {
-		return nil, nil, err
+		log.Errorf("inconsistent state. failed to get network resource status: %s",
+			err.Error())
+
+		return nil, rest.HttpError{
+			HttpCode: http.StatusUnprocessableEntity,
+			Message:  "inconsistent state. failed to get network resource status: " + err.Error(),
+		}
 	}
 
-	return net, &res.Status, nil
+	// TODO: IsComplete should be added to upstream resource in order to remove all this intermediary
+	// resource state management.
+	switch res.Status {
+	case db.ResourceStatusPending:
+		log.Warn("partial content. request is still ongoing")
+
+		return net, rest.HttpError{
+			HttpCode: http.StatusPartialContent,
+			Message:  "partial content. request is still ongoing",
+		}
+
+	case db.ResourceStatusFailed:
+		log.Warn("inconsistent state. request has failed")
+
+		return nil, rest.HttpError{
+			HttpCode: http.StatusUnprocessableEntity,
+			Message:  "inconsistent state. request has failed",
+		}
+	}
+
+	return net, nil
 }
 
-func (c *clients) CreateNetwork(orgName, NetworkName string) (*NetworkInfo, *db.ResourceStatus, error) {
+func (c *clients) CreateNetwork(orgName, NetworkName string) (*NetworkInfo, error) {
 	net, err := c.network.Add(AddNetworkRequest{
 		OrgName: orgName,
 		NetName: NetworkName})
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	res := &db.Resource{
@@ -52,8 +94,10 @@ func (c *clients) CreateNetwork(orgName, NetworkName string) (*NetworkInfo, *db.
 
 	err = c.resRepo.Add(res)
 	if err != nil {
-		return nil, nil, err
+		return nil,
+			fmt.Errorf("inconsistent state. failed to update network resource status: %w", err)
+
 	}
 
-	return net, &res.Status, nil
+	return net, nil
 }
