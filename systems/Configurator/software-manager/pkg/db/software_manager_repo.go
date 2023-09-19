116970361f6d4c425338c4806ab40c1346d3b3bb
Brackley cassinga <pcassinga@gmail.com>
2023-09-19 15:31:35 +0200

    software manager srv added to configurator

---
+++ systems/Configurator/software-manager/pkg/db/software_manager_repo.go
@@ -0,0 +1,66 @@
+package db
+
+import (
+	"github.com/ukama/ukama/systems/common/sql"
+	"github.com/ukama/ukama/systems/common/uuid"
+	"gorm.io/gorm"
+)
+
+type SoftwareManagerRepo interface {
+	CreateSoftware(Software *Software, nestedFunc func(string, string) error) error
+	ReadSoftware(id uuid.UUID) (*Software, error)
+	ListSoftwares() ([]*Software, error)
+	GetLatestSoftware() (*Software, error)
+	GetSoftwareByVersion(version string) (*Software, error)
+	GetSoftwareByAppName(name string) (*Software, error)
+}
+type softwareManagerRepo struct {
+	Db sql.Db
+}
+
+func NewSoftwareManagerRepo(db sql.Db) SoftwareManagerRepo {
+	return &softwareManagerRepo{
+		Db: db,
+	}
+}
+func (r *softwareManagerRepo) CreateSoftware(Software *Software, nestedFunc func(string, string) error) error {
+	err := r.Db.GetGormDb().Transaction(func(tx *gorm.DB) error {
+		if nestedFunc != nil {
+			nestErr := nestedFunc("", "")
+			if nestErr != nil {
+				return nestErr
+			}
+		}
+		if err := tx.Create(Software).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+func (r *softwareManagerRepo) ReadSoftware(id uuid.UUID) (*Software, error) {
+	var Software Software
+	err := r.Db.GetGormDb().Where("id = ?", id).First(&Software).Error
+	return &Software, err
+}
+func (r *softwareManagerRepo) ListSoftwares() ([]*Software, error) {
+	var Softwares []*Software
+	err := r.Db.GetGormDb().Find(&Softwares).Error
+	return Softwares, err
+}
+func (r *softwareManagerRepo) GetLatestSoftware() (*Software, error) {
+	var Software Software
+	err := r.Db.GetGormDb().Order("release_date desc").First(&Software).Error
+	return &Software, err
+}
+func (r *softwareManagerRepo) GetSoftwareByVersion(version string) (*Software, error) {
+	var Software Software
+	err := r.Db.GetGormDb().Where("version = ?", version).First(&Software).Error
+	return &Software, err
+}
+
+func (r *softwareManagerRepo) GetSoftwareByAppName(name string) (*Software, error) {
+	var Software Software
+	err := r.Db.GetGormDb().Where("name = ?", name).First(&Software).Error
+	return &Software, err
+}
