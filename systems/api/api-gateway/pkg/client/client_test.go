5a04cf353db5baab561b461ec47d31e2b9de259a
Elvis Attro <attroelvis@gmail.com>
2023-09-21 00:54:00 +0000

    api/api-gateway: update router client unit tests.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/api/api-gateway/pkg/client/client_test.go
+++ systems/api/api-gateway/pkg/client/client_test.go
@@ -18,7 +18,7 @@ func TestCient_GetNetwork(t *testing.T) {
 	netId := uuid.NewV4()
 	netName := "net-1"
 
-	c := client.NewClientsSet(netClient)
+	c := client.NewClientsSet(netClient, nil)
 
 	t.Run("NetworkFoundAndStatusCompleted", func(t *testing.T) {
 		netClient.On("Get", netId.String()).
@@ -96,7 +96,7 @@ func TestCient_AddNetwork(t *testing.T) {
 	countries := []string{"USA"}
 	paymentLinks := false
 
-	c := client.NewClientsSet(netClient)
+	c := client.NewClientsSet(netClient, nil)
 
 	t.Run("NetworkCreatedAndStatusUpdated", func(t *testing.T) {
 		netClient.On("Add", client.AddNetworkRequest{
@@ -135,5 +135,133 @@ func TestCient_AddNetwork(t *testing.T) {
 
 		assert.Contains(t, err.Error(), "error")
 		assert.Nil(t, netInfo)
+	})
+}
+
+func TestCient_GetSim(t *testing.T) {
+	simClient := &mocks.SimClient{}
+
+	simId := uuid.NewV4()
+	subscriberId := uuid.NewV4()
+
+	c := client.NewClientsSet(nil, simClient)
+
+	t.Run("SimFoundAndStatusCompleted", func(t *testing.T) {
+		simClient.On("Get", simId.String()).
+			Return(&client.SimInfo{
+				Id:           simId,
+				SubscriberId: subscriberId,
+				IsSynced:     true,
+			}, nil).Once()
+
+		simInfo, err := c.GetSim(simId.String())
+
+		assert.NoError(t, err)
+
+		assert.NotNil(t, simInfo)
+		assert.Equal(t, simInfo.Id, simId)
+		assert.Equal(t, simInfo.SubscriberId, subscriberId)
+	})
+
+	t.Run("SimFoundAndStatusPending", func(t *testing.T) {
+		simClient.On("Get", simId.String()).
+			Return(&client.SimInfo{
+				Id:           simId,
+				SubscriberId: subscriberId,
+				IsSynced:     false,
+			}, nil).Once()
+
+		simInfo, err := c.GetSim(simId.String())
+
+		assert.Error(t, err)
+		assert.IsType(t, err, rest.HttpError{})
+		assert.Contains(t, err.Error(), "partial")
+
+		assert.NotNil(t, simInfo)
+		assert.Equal(t, simInfo.Id, simId)
+		assert.Equal(t, simInfo.SubscriberId, subscriberId)
+	})
+
+	t.Run("SimNotFound", func(t *testing.T) {
+		simClient.On("Get", simId.String()).
+			Return(nil,
+				fmt.Errorf("GetSim failure: %w",
+					client.ErrorStatus{StatusCode: 404})).Once()
+
+		simInfo, err := c.GetSim(simId.String())
+
+		assert.Error(t, err)
+		assert.IsType(t, err, rest.HttpError{})
+		assert.Contains(t, err.Error(), "404")
+
+		assert.Nil(t, simInfo)
+	})
+
+	t.Run("SimGetError", func(t *testing.T) {
+		simClient.On("Get", simId.String()).
+			Return(nil,
+				fmt.Errorf("Some unexpected error")).Once()
+
+		simInfo, err := c.GetSim(simId.String())
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "error")
+
+		assert.Nil(t, simInfo)
+	})
+}
+
+func TestCient_ConfigureSim(t *testing.T) {
+	simClient := &mocks.SimClient{}
+
+	simId := uuid.NewV4()
+	subscriberId := uuid.NewV4()
+	networkId := uuid.NewV4()
+	packageId := uuid.NewV4()
+	simType := "some-sim-type"
+	simToken := "some-sim-token"
+
+	c := client.NewClientsSet(nil, simClient)
+
+	t.Run("SimCreatedAndStatusUpdated", func(t *testing.T) {
+		simClient.On("Add", client.AddSimRequest{
+			SubscriberId: subscriberId.String(),
+			NetworkId:    networkId.String(),
+			PackageId:    packageId.String(),
+			SimType:      simType,
+			SimToken:     simToken,
+		}).Return(&client.SimInfo{
+			Id:           simId,
+			SubscriberId: subscriberId,
+			NetworkId:    networkId,
+			// PackageId:    packageId,
+			SimType: simType,
+			// simToken: simToken,
+			IsSynced: false,
+		}, nil).Once()
+
+		simInfo, err := c.ConfigureSim(subscriberId.String(),
+			networkId.String(), packageId.String(), simType, simToken)
+
+		assert.NoError(t, err)
+
+		assert.Equal(t, simInfo.Id, simId)
+		assert.Equal(t, simInfo.SubscriberId, subscriberId)
+	})
+
+	t.Run("NetworkNotCreated", func(t *testing.T) {
+		simClient.On("Add", client.AddSimRequest{
+			SubscriberId: subscriberId.String(),
+			NetworkId:    networkId.String(),
+			PackageId:    packageId.String(),
+			SimType:      simType,
+			SimToken:     simToken,
+		}).Return(nil, errors.New("some error")).Once()
+
+		simInfo, err := c.ConfigureSim(subscriberId.String(),
+			networkId.String(), packageId.String(), simType, simToken)
+
+		assert.Contains(t, err.Error(), "error")
+		assert.Nil(t, simInfo)
 	})
 }
