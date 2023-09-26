f0a03c6b93ed9f834189563d70c8dd200cd560da
Elvis Attro <attroelvis@gmail.com>
2023-09-26 17:23:00 +0000

    data-plan/api-gateway: update handler for overdraft et al.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/data-plan/api-gateway/pkg/rest/api.go
+++ systems/data-plan/api-gateway/pkg/rest/api.go
@@ -1,25 +1,28 @@
 package rest
 
 type AddPackageRequest struct {
-	Name        string  `example:"Monthly-Data" json:"name" validation:"required"`
-	From        string  `example:"2023-04-01T00:00:00Z" json:"from" validation:"required"`
-	To          string  `example:"2023-05-01T00:00:00Z" json:"to" validation:"required"`
-	OrgId       string  `example:"{{OrgUUID}}" json:"org_id" validation:"required"`
-	OwnerId     string  `example:"{{OwnerUUID}}" json:"owner_id" validation:"required"`
-	SimType     string  `example:"test" json:"sim_type" validation:"required"`
-	SmsVolume   int64   `example:"0" json:"sms_volume" validation:"required"`
-	DataVolume  int64   `example:"1024" json:"data_volume" validation:"required"`
-	DataUnit    string  `example:"MegaBytes" json:"data_unit" validation:"required"`
-	VoiceUnit   string  `example:"seconds" json:"voice_unit" validation:"required"`
-	Duration    uint64  `example:"1" json:"duration" validation:"required"`
-	Type        string  `example:"postpaid" json:"type" validation:"required"`
-	Flatrate    bool    `example:"false" json:"flat_rate" default:"false"`
-	Amount      float64 `example:"0" json:"amount" default:"0.00"`
-	Markup      float64 `example:"0" json:"markup" default:"0.00"`
-	Apn         string  `example:"ukama.tel" json:"apn" default:"ukama.tel"`
-	Active      bool    `example:"true" json:"active" validation:"required"`
-	VoiceVolume int64   `example:"0" json:"voice_volume" default:"0"`
-	BaserateId  string  `example:"{{baserate}}" json:"baserate_id" validation:"required"`
+	Name          string   `example:"Monthly-Data" json:"name" validation:"required"`
+	From          string   `example:"2023-04-01T00:00:00Z" json:"from" validation:"required"`
+	To            string   `example:"2023-05-01T00:00:00Z" json:"to" validation:"required"`
+	OrgId         string   `example:"{{OrgUUID}}" json:"org_id" validation:"required"`
+	OwnerId       string   `example:"{{OwnerUUID}}" json:"owner_id" validation:"required"`
+	SimType       string   `example:"test" json:"sim_type" validation:"required"`
+	SmsVolume     int64    `example:"0" json:"sms_volume" validation:"required"`
+	DataVolume    int64    `example:"1024" json:"data_volume" validation:"required"`
+	DataUnit      string   `example:"MegaBytes" json:"data_unit" validation:"required"`
+	VoiceUnit     string   `example:"seconds" json:"voice_unit" validation:"required"`
+	Duration      uint64   `example:"1" json:"duration" validation:"required"`
+	Type          string   `example:"postpaid" json:"type" validation:"required"`
+	Flatrate      bool     `example:"false" json:"flat_rate" default:"false"`
+	Amount        float64  `example:"0" json:"amount" default:"0.00"`
+	Markup        float64  `example:"0" json:"markup" default:"0.00"`
+	Apn           string   `example:"ukama.tel" json:"apn" default:"ukama.tel"`
+	Active        bool     `example:"true" json:"active" validation:"required"`
+	VoiceVolume   int64    `example:"0" json:"voice_volume" default:"0"`
+	BaserateId    string   `example:"{{baserate}}" json:"baserate_id" validation:"required"`
+	Overdraft     float64  `json:"overdraft"`
+	TrafficPolicy uint32   `json:"traffic_policy"`
+	Networks      []string `json:"networks"`
 }
 
 type UpdatePackageRequest struct {
