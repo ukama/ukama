4b2155db6818d0ee6af29470e0c08ebe19cf8fdd
salman-codewar <salmannaseem131@gmail.com>
2023-10-11 12:42:50 +0500

    api structs update for docs

--- systems/notification/node-gateway/pkg/rest/api.go
+++ systems/notification/node-gateway/pkg/rest/api.go
@@ -1,6 +1,11 @@
 package rest
 
+import (
+	"github.com/ukama/ukama/systems/common/rest"
+)
+
 type AddNotificationReq struct {
+	rest.BaseRequest
 	NodeId      string `json:"node_id"`
 	Severity    string `json:"severity,omitempty" type:"string"`
 	Type        string `json:"notification_type,omitempty" validate:"eq=alert|eq=event"`
@@ -12,10 +17,12 @@ type AddNotificationReq struct {
 }
 
 type GetNotificationReq struct {
+	rest.BaseRequest
 	NotificationId string `json:"notification_id" path:"notification_id" validate:"required"`
 }
 
 type GetNotificationsReq struct {
+	rest.BaseRequest
 	NodeId      string `form:"node_id" json:"node_id" query:"node_id" binding:"required"`
 	ServiceName string `form:"service_name" json:"service_name" query:"service_name" binding:"required"`
 	Type        string `form:"notification_type" json:"notification_type" query:"notification_type" binding:"required"`
@@ -24,6 +31,7 @@ type GetNotificationsReq struct {
 }
 
 type DelNotificationsReq struct {
+	rest.BaseRequest
 	NodeId      string `form:"node_id" json:"node_id" query:"node_id" binding:"required"`
 	ServiceName string `form:"service_name" json:"service_name" query:"service_name" binding:"required"`
 	Type        string `form:"notification_type" json:"notification_type" query:"notification_type" binding:"required"`
