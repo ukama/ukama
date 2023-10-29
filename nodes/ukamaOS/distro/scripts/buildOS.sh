5fe587788394619f3ce098e06abb186799de5e48
Kashif Ali <kashif@ukama.com>
2023-10-29 14:25:01 -0700

    Update copyright header for UkamaOS shell scripts
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- nodes/ukamaOS/distro/scripts/buildOS.sh
+++ nodes/ukamaOS/distro/scripts/buildOS.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
 # Copyright (c) 2021-present, Ukama Inc.
-# All rights reserved.
 
 # Script to generate ukamaOS bootable image with minimal rootfs
 
