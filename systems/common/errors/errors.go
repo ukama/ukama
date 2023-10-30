003e697ac46f0c5a2c00efd04dfcde96f23d11f0
Kashif Ali <kashif@ukama.com>
2023-10-29 21:15:44 -0700

    Add copyright header for go files
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- systems/common/errors/errors.go
+++ systems/common/errors/errors.go
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+
 // This package is used to replace usage of github.com/pkg/errors package that used all over the code base
 // with more conventional wrapping
 package errors
--- systems/common/testing/kratos/kratos.go
+++ systems/common/testing/kratos/kratos.go
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+
 package kratos
 
 // those methods are copy-paste from kratos example repo https://github.com/ory/kratos/tree/master/examples/go/pkg
