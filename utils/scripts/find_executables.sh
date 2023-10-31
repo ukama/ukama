a6b8519193b0d7c13faa1cf9c8f8c67375af8cdb
Kashif Ali <kashif@ukama.com>
2023-10-30 17:48:17 -0700

    Add copyright header in missing Makefiles
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- utils/scripts/find_executables.sh
+++ utils/scripts/find_executables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present, Ukama Inc.
+
 # Check if a directory argument is provided
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <directory>"
