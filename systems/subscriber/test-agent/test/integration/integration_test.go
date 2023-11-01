973a0a53b326d103218a22f9d5db87cb55fbd05e
Kashif Ali <kashif@ukama.com>
2023-10-31 21:22:48 -0700

    Fix the copyright header location for go
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- systems/subscriber/test-agent/test/integration/integration_test.go
+++ systems/subscriber/test-agent/test/integration/integration_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+// +build integration
+
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -5,9 +8,6 @@
  *
  * Copyright (c) 2023-present, Ukama Inc.
  */
-
-//go:build integration
-// +build integration
 
 package integration_test
 
