bf524aa3f0f2acd28741d6b0137d57befe54e7ed
Kashif Ali <kashif@ukama.com>
2023-11-17 22:20:30 -0800

    Add missing header for platform

---
+++ nodes/ukamaOS/distro/platform/sys/inc/usys_services.h
@@ -0,0 +1,31 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+
+#ifndef USYS_SYS_SERVICES_H
+#define USYS_SYS_SERVICES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SERVICE_BOOTSTRAP     "bootstrap"
+#define SERVICE_NODE          "node"
+#define SERVICE_STARTER       "starter"
+#define SERVICE_MESH          "mesh"
+#define SERVICE_DEVICE        "device"
+#define SERVICE_DEVICE_CLIENT "device-client"
+#define SERVICE_LOOKOUT       "lookout"
+#define SERVICE_WIMC          "wimc"
+#define SERVICE_METRICS       "metrics"
+#define SERVICE_CONFIG        "config"
+#define SERVICE_NOTIFY        "notify"
+
+#ifdef __cplusplus
+}
+#endif
+#endif
