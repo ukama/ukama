bfd31341705cb982f12036d5bef5ef71e1e8420d
salman-codewar <salmannaseem131@gmail.com>
2023-12-06 00:40:37 +0500

    deploy script improvements

--- builder/scripts/deploy.sh
+++ builder/scripts/deploy.sh
@@ -17,7 +17,7 @@ OWNEREMAIL=$(jq -r '.setup.email' "$1")
 OWNERNAME=$(jq -r '.setup.name' "$1")
 ORGNAME=$(jq -r '.setup["org-name"]' "$1")
 ORGID=$(jq -r '.setup["org-id"]' "$1")
-SYSTEMS=$(jq -r '.systems' "$1")
+SYS=$(jq -r '.systems' "$1")
 KEY=$(jq -r '.key' "$1")
 METADATA=$(jq -c '.' ../metadata.json)
 MAILERHOST=$(jq -r '.mailer.host' "$1")
@@ -32,7 +32,7 @@ elif [[ "$(uname)" == "Linux" ]]; then
     # For Linux
     LOCAL_HOST_IP=$(ifconfig enp0s25 | grep inet | awk '$1=="inet" {print $2}')
 fi
-OWNERAUTHID=$(uuidgen)
+OWNERAUTHID=""
 OWNERID=$(uuidgen)
 
 function set_env() {
@@ -59,14 +59,13 @@ function run_docker_compose() {
     set_env
     echo  "$TAG Running $2 docker compose..."
     cd $1
-    # docker-compose down  > /dev/null 2>&1
-    # docker-compose build > /dev/null 2>&1
-    # docker compose up --build -d > /dev/null 2>&1
 
     CONTAINER_NAME=$3
     while true; do
-        # docker-compose up -d > /dev/null 2>&1
         docker compose up --build -d > /dev/null 2>&1
+        # docker-compose down  > /dev/null 2>&1
+        # docker-compose build > /dev/null 2>&1
+        # docker compose up --build -d > /dev/null 2>&1
         sleep 5
         if docker ps | grep -q $CONTAINER_NAME; then
             echo  "$TAG $2 docker container is up"
@@ -96,59 +95,62 @@ function register_user() {
             "firstVisit": true
         }
     }')
-
+    sleep 2
     identity=$(echo $response | jq -r '.session.identity')
+    sleep 2
     OWNERAUTHID=$(echo $identity | jq -r '.id')
     echo  "$TAG User register with ${GREEN}AUTH ID = $OWNERAUTHID${NC}"
     echo  "$TAG Please verify your email address by visiting ${GREEN}http://localhost:4436${NC}"
 }
 
-# if [ $# q 0 ]; then
-#     echo "No arguments provided. Please provide the path to the deploy_config JSON file."
-#     exit 1
-# fi
+sort_systems() {
+    IFS=', ' read -r -a SYSTEMS_ARRAY <<< "$SYS"
 
-IFS=', ' read -r -a SYSTEMS_ARRAY <<< "$SYSTEMS"
+    if [[ ! " ${SYSTEMS_ARRAY[@]} " =~ " services " ]] || [[ ! " ${SYSTEMS_ARRAY[@]} " =~ " auth-services " ]]; then
+        echo "Error: 'services' and 'auth-services' are required in the systems array in the deploy_config JSON file."
+        exit 1
+    fi
 
+    SYSTEMS=($(for key in "${SYSTEMS_ARRAY[@]}"; do
+      if [ "$key" == "services" ]; then
+          echo "1 $key"
+      elif [ "$key" == "auth-services" ]; then
+          echo "2 $key"
+      elif [ "$key" == "init" ]; then
+          echo "3 $key"
+      elif [ "$key" == "nucleus" ]; then
+          echo "4 $key"
+      elif [ "$key" == "registry" ]; then
+          echo "5 $key"
+      else
+          echo "6 $key"
+      fi
+    done | sort -n -k1,1 | cut -d' ' -f2-))
+}
 
-if [[ $SYSTEMS != *"auth-services"* ]]; then
-    echo "Please add auth-services in the systems array in the deploy_config JSON file."
-else
-    auth=$(echo "$METADATA" | jq -c --arg AUTHSYSKEY "$AUTHSYSKEY" '.[$AUTHSYSKEY]')
-    # Run docker compose for ukama-auth
-    export COMPOSE_PROJECT_NAME="$(echo "$auth" | jq -r '.key')"
-    run_docker_compose "$(echo "$auth" | jq -r '.path')" "$(echo "$auth" | jq -r '.name')" "$COMPOSE_PROJECT_NAME"
-    register_user
-    cd ../ukama/builder/scripts
-fi
+sort_systems
 
-# Navigate to Ukama repo
-cd ../../systems
-sleep 3
 # Loop through the SYSTEMS array
-for SYSTEM in "${SYSTEMS_ARRAY[@]}"; do
-    if [ "$SYSTEM" == $AUTHSYSKEY ]; then
-        continue
+for SYSTEM in "${SYSTEMS[@]}"; do
+    cd ~
+    cd $root_dir
+    if [ "$SYSTEM" != $AUTHSYSKEY ]; then
+        cd ../../systems
     fi
     SYSTEM_OBJECT=$(echo "$METADATA" | jq -c --arg SYSTEM "$SYSTEM" '.[$SYSTEM]')
     export COMPOSE_PROJECT_NAME=$(echo "$SYSTEM_OBJECT" | jq -r '.key')
     run_docker_compose "$(echo "$SYSTEM_OBJECT" | jq -r '.path')" "$(echo "$SYSTEM_OBJECT" | jq -r '.name')" "$(echo "$SYSTEM_OBJECT" | jq -r '.key')"
     case $SYSTEM in
+    "auth-services")
+        sleep 2
+        register_user
+        ;;
     "init")
         sleep 2
-        ## Connect to init-lookup db and add org in orgs table
         echo  "$TAG Add org in lookup..."
         DB_URI="postgresql://postgres:Pass2020!@127.0.0.1:5401/lookup"
         QUERY="INSERT INTO \"public\".\"orgs\" (\"created_at\", \"updated_at\", \"name\", \"org_id\", \"certificate\") VALUES (NOW(), NOW(), '$ORGNAME', '$ORGID', 'ukama-cert')"
         psql $DB_URI -c "$QUERY"
-        ;;
-    "nucleus")
-        sleep 5
-        # Get user
-        echo  "$TAG Get User..."
-        USER_RESPONSE=$(curl -X 'GET' "http://localhost:8060/v1/users/auth/$OWNERAUTHID" -H 'accept: application/json')
-        sleep 2
-        OWNERID="$(echo "$USER_RESPONSE" | jq -r '.user.id')"
         ;;
     esac
     cd ../
