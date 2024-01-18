ede6e2443d2ff19643b757db1ef89d32d8656ccc
Brackley cassinga <pcassinga@gmail.com>
2024-01-18 19:14:42 +0200

    site naming resolver updated

---
+++ systems/console-bff/network/resolvers/getAllSites.ts
@@ -0,0 +1,23 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+import { Arg, Ctx, Query, Resolver } from "type-graphql";
+
+import { Context } from "../context";
+import { SitesResDto } from "./types";
+
+@Resolver()
+export class GetAllSitesResolver {
+  @Query(() => SitesResDto)
+  async getAllSites(
+    @Arg("networkId") networkId: string,
+    @Ctx() ctx: Context
+  ): Promise<SitesResDto> {
+    const { dataSources } = ctx;
+    return dataSources.dataSource.getAllSites(networkId);
+  }
+}
---
+++ systems/console-bff/network/resolvers/getSingleSite.ts
@@ -0,0 +1,25 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+import { Arg, Ctx, Query, Resolver } from "type-graphql";
+
+import { Context } from "../context";
+import { SiteDto } from "./types";
+
+@Resolver()
+export class GetSingleSiteResolver {
+  @Query(() => SiteDto)
+  async getSingleSite(
+    @Arg("networkId") networkId: string,
+    @Arg("siteId") siteId: string,
+    @Ctx() ctx: Context
+  ): Promise<SiteDto> {
+    const { dataSources } = ctx;
+
+    return dataSources.dataSource.getSingleSite(networkId, siteId);
+  }
+}
