503e6244c55574fe0413fa4215214877f0074ec3
Elvis Attro <attroelvis@gmail.com>
2024-02-02 15:28:38 +0000

    subscriber/simmanger: update proto & gen files for GetUsages.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/subscriber/sim-manager/pb/gen/mocks/SimManagerServiceClient.go
+++ systems/subscriber/sim-manager/pb/gen/mocks/SimManagerServiceClient.go
@@ -275,6 +275,43 @@ func (_m *SimManagerServiceClient) GetSimsBySubscriber(ctx context.Context, in *
 	return r0, r1
 }
 
+// GetUsages provides a mock function with given fields: ctx, in, opts
+func (_m *SimManagerServiceClient) GetUsages(ctx context.Context, in *gen.UsageRequest, opts ...grpc.CallOption) (*gen.UsageResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetUsages")
+	}
+
+	var r0 *gen.UsageResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *gen.UsageRequest, ...grpc.CallOption) (*gen.UsageResponse, error)); ok {
+		return rf(ctx, in, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *gen.UsageRequest, ...grpc.CallOption) *gen.UsageResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gen.UsageResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *gen.UsageRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ListSims provides a mock function with given fields: ctx, in, opts
 func (_m *SimManagerServiceClient) ListSims(ctx context.Context, in *gen.ListSimsRequest, opts ...grpc.CallOption) (*gen.ListSimsResponse, error) {
 	_va := make([]interface{}, len(opts))
