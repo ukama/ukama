5e60fd1ffd3554c3d1cbbdc280793a7072e3a027
vthakur7f <vthakur7@hotmail.com>
2024-03-04 13:11:42 -0800

    Updated cdr service.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/ukama-agent/cdr/pkg/db/cdr_repo.go
+++ systems/ukama-agent/cdr/pkg/db/cdr_repo.go
@@ -2,6 +2,8 @@
 package db
 
 import (
+	"time"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/ukama/ukama/systems/common/sql"
 )
@@ -11,8 +13,9 @@ type CDRRepo interface {
 	Add(cdr *CDR) error
 	GetByImsi(imsi string) (*[]CDR, error)
 	GetBySession(imsi string, session uint64) (*[]CDR, error)
-	GetByFilters(imsi string, startTime uint64, endTime uint64) (*[]CDR, error)
+	GetByFilters(imsi string, session uint64, policy string, startTime uint64, endTime uint64) (*[]CDR, error)
 	GetByPolicy(imsi string, policy string) (*[]CDR, error)
+	GetByTime(imsi string, startTime uint64, endTime uint64) (*[]CDR, error)
 }
 
 type cdrRepo struct {
@@ -38,20 +41,65 @@ func (p *cdrRepo) Add(cdr *CDR) error {
 
 func (p *cdrRepo) GetByImsi(imsi string) (*[]CDR, error) {
 	var cdr []CDR
+	r := p.db.GetGormDb().Where("ismi = ?", imsi).Find(cdr)
+	if r.Error != nil {
+		log.Errorf("error getting cdr for imsi %s.Error: %+v", imsi, r.Error)
+		return nil, r.Error
+	}
 	return &cdr, nil
 }
 
 func (p *cdrRepo) GetBySession(imsi string, session uint64) (*[]CDR, error) {
 	var cdr []CDR
+	r := p.db.GetGormDb().Where("ismi = ? AND session = ?", imsi, session).Find(cdr)
+	if r.Error != nil {
+		log.Errorf("error getting cdr for imsi %s with session %d.Error: %+v", imsi, session, r.Error)
+		return nil, r.Error
+	}
 	return &cdr, nil
 }
 
-func (p *cdrRepo) GetByFilters(imsi string, startTime uint64, endTime uint64) (*[]CDR, error) {
+func (p *cdrRepo) GetByFilters(imsi string, session uint64, policy string, startTime uint64, endTime uint64) (*[]CDR, error) {
 	var cdr []CDR
+	var query string
+	if policy == "" {
+		policy = "policy"
+	}
+
+	if endTime == 0 {
+		endTime = uint64(time.Now().Unix())
+	}
+
+	if session == 0 {
+		query = "ismi = ? AND session <> ? AND policy = ? AND start_time >= ? AND end_time <= ?"
+	} else {
+		query = "ismi = ? AND session = ? AND policy = ? AND start_time >= ? AND end_time <= ?"
+	}
+
+	r := p.db.GetGormDb().Where(query, imsi, session, policy, startTime, endTime).Find(cdr)
+	if r.Error != nil {
+		log.Errorf("error getting cdr for imsi %s with start time %d and end time %d.Error: %+v", imsi, startTime, endTime, r.Error)
+		return nil, r.Error
+	}
+	return &cdr, nil
+}
+
+func (p *cdrRepo) GetByTime(imsi string, startTime uint64, endTime uint64) (*[]CDR, error) {
+	var cdr []CDR
+	r := p.db.GetGormDb().Where("ismi = ? AND start_time >= ? AND end_time <= ?", imsi, startTime, endTime).Find(cdr)
+	if r.Error != nil {
+		log.Errorf("error getting cdr for imsi %s with start time %d and end time %d.Error: %+v", imsi, startTime, endTime, r.Error)
+		return nil, r.Error
+	}
 	return &cdr, nil
 }
 
 func (p *cdrRepo) GetByPolicy(imsi string, policy string) (*[]CDR, error) {
 	var cdr []CDR
+	r := p.db.GetGormDb().Where("ismi = ? AND policy = ?", imsi, policy).Find(cdr)
+	if r.Error != nil {
+		log.Errorf("error getting cdr for imsi %s with policy %s.Error: %+v", imsi, policy, r.Error)
+		return nil, r.Error
+	}
 	return &cdr, nil
 }
--- systems/ukama-agent/cdr/pkg/server/cdr.go
+++ systems/ukama-agent/cdr/pkg/server/cdr.go
@@ -39,17 +39,74 @@ func NewCDRServer(cdrRepo db.CDRRepo, orgId, orgName string, msgBus mb.MsgBusSer
 }
 
 func (s *CDRServer) PostCDR(c context.Context, req *pb.CDR) (*pb.CDRResp, error) {
-
+	log.Debugf("Received CDR post request %+v", req)
+	err := s.cdrRepo.Add(pbCDRToDbCDR(req))
+	if err != nil {
+		return nil, err
+	}
 	return &pb.CDRResp{}, nil
 }
 
 func (s *CDRServer) GetCDR(c context.Context, req *pb.RecordReq) (*pb.RecordResp, error) {
-
-	return &pb.RecordResp{}, nil
+	log.Debugf("Received CDR get request %+v", req)
+	cdrs, err := s.cdrRepo.GetByFilters(req.Imsi, req.SessionId, req.Policy, req.StartTime, req.EndTime)
+	if err != nil {
+		return nil, err
+	}
+	return dbCDRToRecordResp(cdrs), nil
 }
 
 func (s *CDRServer) GetUsage(c context.Context, req *pb.UsageReq) (*pb.UsageResp, error) {
-
+	log.Debugf("Received get usage request %+v", req)
 	return &pb.UsageResp{}, nil
+}
 
+func dbCDRToRecordResp(cdrs *[]db.CDR) *pb.RecordResp {
+	if len(*cdrs) > 0 {
+		return &pb.RecordResp{}
+	}
+	pcdrs := make([]*pb.CDR, 0, len(*cdrs))
+
+	for i, cdr := range *cdrs {
+		pcdrs[i] = dbCDRTopbCDR(cdr)
+	}
+
+	return &pb.RecordResp{Cdr: pcdrs}
+}
+
+func dbCDRTopbCDR(req db.CDR) *pb.CDR {
+
+	pcdr := &pb.CDR{
+		Session: req.Session,
+		Imsi:    req.Imsi,
+		//NodeId:        req.NodeId, TBU
+		Policy:        req.Policy,
+		ApnName:       req.ApnName,
+		Ip:            req.Ip,
+		StartTime:     req.StartTime,
+		EndTime:       req.EndTime,
+		LastUpdatedAt: req.LastUpdatedAt,
+		TxBytes:       req.TotalBytes,
+		RxBytes:       req.RxBytes,
+		TotalBytes:    req.TotalBytes,
+	}
+
+	return pcdr
+}
+
+func pbCDRToDbCDR(req *pb.CDR) *db.CDR {
+	return &db.CDR{
+		Session:       req.Session,
+		Imsi:          req.Imsi,
+		NodeId:        "TBU",
+		Policy:        req.Policy,
+		ApnName:       req.ApnName,
+		Ip:            req.Ip,
+		StartTime:     req.StartTime,
+		EndTime:       req.EndTime,
+		LastUpdatedAt: req.LastUpdatedAt,
+		TxBytes:       req.TotalBytes,
+		RxBytes:       req.RxBytes,
+		TotalBytes:    req.TotalBytes,
+	}
 }
