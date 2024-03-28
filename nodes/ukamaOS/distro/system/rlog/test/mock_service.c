0d4e6213ea735b9f133c40f164f644a3ba5a50ed
Kashif Ali <kashif@ukama.com>
2024-03-28 16:00:42 -0700

    add mocking service for testing
    Signed-off-by: Kashif Ali <kashif@ukama.com>

---
+++ nodes/ukamaOS/distro/system/rlog/test/mock_service.c
@@ -0,0 +1,52 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2024-present, Ukama Inc.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "usys_log.h"
+#include "usys_services.h"
+
+#define SERVICE_NAME "testApp"
+#define MSG_LENGTH   64
+
+void generate_random_message(char* message) {
+    char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    int charsetSize = sizeof(charset) - 1;
+    
+    srand(time(NULL));
+    
+    for (int i = 0; i < MSG_LENGTH; ++i) {
+        int key = rand() % charsetSize;
+        message[i] = charset[key];
+    }
+    
+    message[MSG_LENGTH] = '\0';
+}
+
+int main(int argc, char **argv) {
+
+    char logMessage[MSG_LENGTH+1] = {0};
+
+    usys_log_set_service(SERVICE_NAME);
+    usys_log_remote_init(SERVICE_NAME);
+
+    while (1) {
+        
+        sleep (1);
+
+        memset(logMessage, 0, MSG_LENGTH+1);
+        generate_random_message(logMessage);
+
+        usys_log_error(logMessage);
+        usys_log_debug(logMessage);
+        usys_log_info(logMessage);
+    }
+    
+	return 0;
+}
