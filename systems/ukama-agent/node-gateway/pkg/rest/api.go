5f6f87b732a61ea0cb013873bfc8a5c1fb89ec29
vthakur7f <vthakur7@hotmail.com>
2024-05-14 11:33:26 -0700

    Added licesense header.
    Signed-off-by: vthakur7f <vthakur7@hotmail.com>

--- systems/ukama-agent/node-gateway/pkg/rest/api.go
+++ systems/ukama-agent/node-gateway/pkg/rest/api.go
@@ -1,5 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+ 
 package rest
-
 type Guti struct {
 	PlmnId string `json:"plmn_id" validate:"required"`
 	Mmegi  uint32 `json:"mmegi" validate:"required"`
