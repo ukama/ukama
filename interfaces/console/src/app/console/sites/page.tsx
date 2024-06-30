de453b294837d651476dbde166b71705894e8a94
Brackley cassinga <pcassinga@gmail.com>
2024-06-30 09:58:11 +0200

    licence added to new comp and sonarcloud smell fixed

--- interfaces/console/src/app/console/sites/page.tsx
+++ interfaces/console/src/app/console/sites/page.tsx
@@ -33,64 +33,62 @@ const Sites = () => {
     console.log('Form data submitted:', formData);
   };
   return (
-    <>
-      <LoadingWrapper
-        radius="small"
-        width={'100%'}
-        isLoading={false}
-        cstyle={{
-          backgroundColor: false ? colors.white : 'transparent',
-        }}
-      >
-        <Grid container spacing={0} sx={{ mt: 1 }}>
-          <Grid item xs={12}>
-            <Paper sx={{ p: 4 }}>
-              <Grid container spacing={0} sx={{ mb: 2 }}>
-                <Grid xs={6}>
-                  <Typography variant="h6" color="initial">
-                    My sites
-                  </Typography>
-                </Grid>
-                <Grid
-                  xs={6}
-                  container
-                  justifyItems={'center'}
-                  justifyContent={'flex-end'}
+    <LoadingWrapper
+      radius="small"
+      width={'100%'}
+      isLoading={false}
+      cstyle={{
+        backgroundColor: false ? colors.white : 'transparent',
+      }}
+    >
+      <Grid container spacing={0} sx={{ mt: 1 }}>
+        <Grid item xs={12}>
+          <Paper sx={{ p: 4 }}>
+            <Grid container spacing={0} sx={{ mb: 2 }}>
+              <Grid xs={6}>
+                <Typography variant="h6" color="initial">
+                  My sites
+                </Typography>
+              </Grid>
+              <Grid
+                xs={6}
+                container
+                justifyItems={'center'}
+                justifyContent={'flex-end'}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleOpen}
                 >
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleOpen}
-                  >
-                    ADD SITE
-                  </Button>
-                </Grid>
+                  ADD SITE
+                </Button>
               </Grid>
+            </Grid>
 
-              <Grid container spacing={2}>
-                {Dummysites.map((site, index) => (
-                  <Grid item xs={12} md={4} lg={4} key={index}>
-                    <SiteCard
-                      siteId={site.siteId}
-                      name={site.name}
-                      address={site.address}
-                      users={site.users}
-                      status={site.status}
-                      onClickMenu={handleMenuClick}
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-            </Paper>
-          </Grid>
-          <SiteConfigurationStepperDialog
-            open={open}
-            handleClose={handleClose}
-            handleFormDataSubmit={handleFormDataSubmit}
-          />
+            <Grid container spacing={2}>
+              {Dummysites.map((site, index) => (
+                <Grid item xs={12} md={4} lg={4} key={index}>
+                  <SiteCard
+                    siteId={site.siteId}
+                    name={site.name}
+                    address={site.address}
+                    users={site.users}
+                    status={site.status}
+                    onClickMenu={handleMenuClick}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </Paper>
         </Grid>
-      </LoadingWrapper>
-    </>
+        <SiteConfigurationStepperDialog
+          open={open}
+          handleClose={handleClose}
+          handleFormDataSubmit={handleFormDataSubmit}
+        />
+      </Grid>
+    </LoadingWrapper>
   );
 };
 export default Sites;
