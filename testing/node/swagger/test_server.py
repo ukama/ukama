c2e957642375daa59e1bc13fbb0ea3346a62d375
Kashif Ali <kashif@ukama.com>
2024-07-10 01:43:11 -0700

    Properly handle invalid method (405) and invalid resource (404)
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- testing/node/swagger/test_server.py
+++ testing/node/swagger/test_server.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 import json
 import yaml
 import argparse
 from openapi_spec_validator import validate_spec
 
 app = Flask(__name__)
-
 
 def load_spec(spec_path):
     with open(spec_path, 'r') as file:
@@ -18,7 +17,6 @@ def load_spec(spec_path):
     validate_spec(spec_dict)
     return spec_dict
 
-
 def create_mock_endpoints(spec):
     base_path = spec.get('basePath', '')
     for path, methods in spec['paths'].items():
@@ -28,21 +26,31 @@ def create_mock_endpoints(spec):
                 endpoint = endpoint.replace('{', '<').replace('}', '>')
             create_mock_endpoint(endpoint, method, details)
 
-
 def create_mock_endpoint(endpoint, method, details):
-    response_code, response_details = next(iter(details['responses'].items()))
+    responses = details.get('responses', {})
 
     def handler():
-        return jsonify({'message': response_details.get('description', 'Mock response')}), int(response_code)
+        response_code = 200
+        response_description = "Mock response"
+        if '200' in responses:
+            response_code = 200
+            response_description = responses['200'].get('description', 'OK')
+        elif '404' in responses:
+            response_code = 404
+            response_description = responses['404'].get('description', 'Not Found')
+
+        return jsonify({'message': response_description}), response_code
 
     app.add_url_rule(endpoint, endpoint + '_' + method,
                      handler, methods=[method.upper()])
 
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({'message': 'Method Not Allowed'}), 405
 
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def catch_all(path):
-    return jsonify({'message': 'This is a mock server'}), 200
-
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'message': 'Not Found'}), 404
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
