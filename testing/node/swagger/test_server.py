b0f71fcac58e4ac518bbb3ae256c5a4ae8a0bf0f
Kashif Ali <kashif@ukama.com>
2024-07-10 03:30:08 -0700

    add script to run all app spec json (server and client)
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- testing/node/swagger/test_server.py
+++ testing/node/swagger/test_server.py
@@ -1,3 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2024-present, Ukama Inc.
+
 from flask import Flask, jsonify, request, abort
 import json
 import yaml
