3fbe4e6f6aaa63eff6f4665120c0e10a8f87194d
Salman Naseem <salmannaseem131@gmail.com>
2024-07-22 23:21:51 +0500

    Bff configs updated

--- systems/console-bff/common/configs/index.ts
+++ systems/console-bff/common/configs/index.ts
@@ -44,26 +44,26 @@ export const SUBSCRIPTIONS_PORT = parseInt(
   process.env.SUBSCRIPTIONS_PORT ?? "8081"
 );
 export const PLANNING_SERVICE_PORT = parseInt(
-  process.env.PLANNING_SERVICE_PORT ?? "5042"
+  process.env.PLANNING_SERVICE_PORT ?? "5041"
 );
-const NODE_PORT = parseInt(process.env.NODE_PORT ?? "5043");
-const USER_PORT = parseInt(process.env.USER_PORT ?? "5044");
+const ORG_PORT = parseInt(process.env.ORG_PORT ?? "5042");
+const USER_PORT = parseInt(process.env.USER_PORT ?? "5043");
+const INIT_PORT = parseInt(process.env.INIT_PORT ?? "5044");
 const PACKAGE_PORT = parseInt(process.env.PACKAGE_PORT ?? "5045");
 const RATE_PORT = parseInt(process.env.RATE_PORT ?? "5046");
-const ORG_PORT = parseInt(process.env.ORG_PORT ?? "5047");
-const NETWORK_PORT = parseInt(process.env.NETWORK_PORT ?? "5048");
-export const BILLING_PORT = parseInt(process.env.BILLING_PORT ?? "5051");
-const SIM_PORT = parseInt(process.env.SIM_PORT ?? "5052");
-const INVITATION_PORT = parseInt(process.env.INVITATION_PORT ?? "5053");
-const MEMBER_PORT = parseInt(process.env.MEMBER_PORT ?? "5054");
-const INIT_PORT = parseInt(process.env.INIT_PORT ?? "5055");
-const SUBSCRIBER_PORT = parseInt(process.env.SUBSCRIBER_PORT ?? "5056");
-const NOTIFICATION_PORT = parseInt(process.env.NOTIFICATION_PORT ?? "5057");
-export const SITE_PORT = parseInt(process.env.SITE_PORT ?? "5058");
+const NETWORK_PORT = parseInt(process.env.NETWORK_PORT ?? "5047");
+export const SITE_PORT = parseInt(process.env.SITE_PORT ?? "5048");
+const INVITATION_PORT = parseInt(process.env.INVITATION_PORT ?? "5049");
+const MEMBER_PORT = parseInt(process.env.MEMBER_PORT ?? "5050");
+const NODE_PORT = parseInt(process.env.NODE_PORT ?? "5051");
+const SUBSCRIBER_PORT = parseInt(process.env.SUBSCRIBER_PORT ?? "5052");
+const SIM_PORT = parseInt(process.env.SIM_PORT ?? "5053");
+const NOTIFICATION_PORT = parseInt(process.env.NOTIFICATION_PORT ?? "5054");
+export const BILLING_PORT = parseInt(process.env.BILLING_PORT ?? "5055");
 export const COMPONENT_INVENTORY_PORT = parseInt(
-  process.env.COMPONENT_INVENTORY_PORT ?? "5059"
+  process.env.COMPONENT_INVENTORY_PORT ?? "5056"
 );
-export const METRIC_PORT = parseInt(process.env.METRIC_PORT ?? "5060");
+export const METRIC_PORT = parseInt(process.env.METRIC_PORT ?? "5057");
 export const SUB_GRAPHS = {
   metric: {
     name: "metric",
