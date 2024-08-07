0ab04fceb4d880577fd32a7e98469d07e0095e86
Brackley cassinga <pcassinga@gmail.com>
2024-08-07 18:32:31 +0200

    Addressed review comments: updated license

--- interfaces/console/src/components/SiteInfos/index.tsx
+++ interfaces/console/src/components/SiteInfos/index.tsx
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
 import React from 'react';
 import { Paper, Stack, Typography } from '@mui/material';
 import { SiteDto } from '@/client/graphql/generated';
