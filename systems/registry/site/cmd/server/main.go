99ffc70efc44e54dec59bfd97086f71b5ad78f1b
Brackley cassinga <pcassinga@gmail.com>
2024-08-07 16:43:27 +0200

    Reverted changes from commits 2ea57fc and 4ffd804 with conflict resolution

--- systems/registry/site/cmd/server/main.go
+++ systems/registry/site/cmd/server/main.go
@@ -31,6 +31,7 @@ import (
 	cinvent "github.com/ukama/ukama/systems/common/rest/client/inventory"
 	generated "github.com/ukama/ukama/systems/registry/site/pb/gen"
 )
+<<<<<<< HEAD
  
  var serviceConfig *pkg.Config
  
@@ -121,3 +122,95 @@ import (
 	 <-done
 	 log.Infof("exiting service %s", pkg.ServiceName)
  }
+=======
+
+var serviceConfig *pkg.Config
+
+func main() {
+	ccmd.ProcessVersionArgument(pkg.ServiceName, os.Args, version.Version)
+	initConfig()
+	siteDb := initDb()
+	runGrpcServer(siteDb)
+}
+func initConfig() {
+	serviceConfig = pkg.NewConfig(pkg.ServiceName)
+	err := config.NewConfReader(pkg.ServiceName).Read(serviceConfig)
+	if err != nil {
+		log.Fatal("Error reading config ", err)
+	} else if serviceConfig.DebugMode {
+		b, err := yaml.Marshal(serviceConfig)
+		if err != nil {
+			log.Infof("Config:\n%s", string(b))
+		}
+	}
+	pkg.IsDebugMode = serviceConfig.DebugMode
+}
+
+func initDb() sql.Db {
+	log.Infof("Initializing Database")
+	d := sql.NewDb(serviceConfig.DB, serviceConfig.DebugMode)
+	err := d.Init(&db.Site{})
+	if err != nil {
+		log.Fatalf("Database initialization failed. Error: %v", err)
+	}
+	return d
+}
+func runGrpcServer(gormdb sql.Db) {
+
+	instanceId := os.Getenv("POD_NAME")
+	if instanceId == "" {
+		/* used on local machines */
+		inst := uuid.NewV4()
+		instanceId = inst.String()
+	}
+
+	invClient := cinvent.NewComponentClient(serviceConfig.Http.InventoryClient)
+
+	mbClient := msgBusServiceClient.NewMsgBusClient(serviceConfig.MsgClient.Timeout,
+		serviceConfig.OrgName, pkg.SystemName, pkg.ServiceName, instanceId, serviceConfig.Queue.Uri,
+		serviceConfig.Service.Uri, serviceConfig.MsgClient.Host, serviceConfig.MsgClient.Exchange,
+		serviceConfig.MsgClient.ListenQueue, serviceConfig.MsgClient.PublishQueue,
+		serviceConfig.MsgClient.RetryCount, serviceConfig.MsgClient.ListenerRoutes)
+
+	siteServer := server.NewSiteServer(serviceConfig.OrgName, db.NewSiteRepo(gormdb),
+		mbClient, providers.NewNetworkClientProvider(serviceConfig.Network), serviceConfig.PushGateway, invClient)
+
+	log.Debugf("MessageBus Client is %+v", mbClient)
+
+	grpcServer := ugrpc.NewGrpcServer(*serviceConfig.Grpc, func(s *grpc.Server) {
+		generated.RegisterSiteServiceServer(s, siteServer)
+	})
+
+	go grpcServer.StartServer()
+
+	go msgBusListener(mbClient)
+
+	waitForExit()
+}
+
+func msgBusListener(m mb.MsgBusServiceClient) {
+
+	if err := m.Register(); err != nil {
+		log.Fatalf("Failed to register to Message Client Service. Error %s", err.Error())
+	}
+
+	if err := m.Start(); err != nil {
+		log.Fatalf("Failed to start to Message Client Service routine for service %s. Error %s", pkg.ServiceName, err.Error())
+	}
+}
+
+func waitForExit() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	go func() {
+
+		sig := <-sigs
+		log.Info(sig)
+		done <- true
+	}()
+
+	log.Debug("awaiting terminate/interrrupt signal")
+	<-done
+	log.Infof("exiting service %s", pkg.ServiceName)
+}
+>>>>>>> parent of 2ea57fc06 (invClient url updated)
--- systems/registry/site/pkg/config.go
+++ systems/registry/site/pkg/config.go
@@ -14,47 +14,47 @@ import (
 	uconf "github.com/ukama/ukama/systems/common/config"
 	metric "github.com/ukama/ukama/systems/common/metrics"
 )
- 
- const (
-	 NumberOfSites = "number_of_sites"
-	 GaugeType     = "gauge"
- )
- 
- type Config struct {
-	 uconf.BaseConfig `mapstructure:",squash"`
-	 DB               *uconf.Database  `default:"{}"`
-	 Grpc             *uconf.Grpc      `default:"{}"`
-	 Queue            *uconf.Queue     `default:"{}"`
-	 Timeout          time.Duration    `default:"3s"`
-	 MsgClient        *uconf.MsgClient `default:"{}"`
-	 PushGateway      string           `default:"http://localhost:9091"`
-	 Network          string           `default:"network:9090"`
-	 Service          *uconf.Service
-	 OrgName          string
-	 Http             HttpServices
- }
- 
- type HttpServices struct {
-	 InventoryClient string `defaut:"localhost:8080"`
- }
- 
- var SiteMetric = []metric.MetricConfig{
-	 {
-		 Name:   NumberOfSites,
-		 Type:   GaugeType,
-		 Labels: map[string]string{"network": "", "org": ""},
-		 Value:  0,
-	 },
- }
- 
- func NewConfig(name string) *Config {
-	 return &Config{
-		 DB: &uconf.Database{
-			 DbName: name,
-		 },
-		 Service: uconf.LoadServiceHostConfig(name),
-		 MsgClient: &uconf.MsgClient{
-			 Timeout: 5 * time.Second,
-		 },
-	 }
- }
+
+const (
+	NumberOfSites = "number_of_sites"
+	GaugeType     = "gauge"
+)
+
+type Config struct {
+	uconf.BaseConfig `mapstructure:",squash"`
+	DB               *uconf.Database  `default:"{}"`
+	Grpc             *uconf.Grpc      `default:"{}"`
+	Queue            *uconf.Queue     `default:"{}"`
+	Timeout          time.Duration    `default:"3s"`
+	MsgClient        *uconf.MsgClient `default:"{}"`
+	PushGateway      string           `default:"http://localhost:9091"`
+	Network          string           `default:"network:9090"`
+	Service          *uconf.Service
+	OrgName          string
+	Http             HttpServices
+}
+
+type HttpServices struct {
+	InventoryClient string `defaut:"localhost:8080"`
+}
+
+var SiteMetric = []metric.MetricConfig{
+	{
+		Name:   NumberOfSites,
+		Type:   GaugeType,
+		Labels: map[string]string{"network": "", "org": ""},
+		Value:  0,
+	},
+}
+
+func NewConfig(name string) *Config {
+	return &Config{
+		DB: &uconf.Database{
+			DbName: name,
+		},
+		Service: uconf.LoadServiceHostConfig(name),
+		MsgClient: &uconf.MsgClient{
+			Timeout: 5 * time.Second,
+		},
+	}
+}
