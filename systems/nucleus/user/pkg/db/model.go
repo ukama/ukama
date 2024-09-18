a46eb82cfcf9efd8ef346461abd1c1b9e9f15c74
Salman Naseem <salmannaseem131@gmail.com>
2024-09-18 19:50:49 +0500

    nucleus - user removed required check on phone number

--- systems/nucleus/user/pkg/db/model.go
+++ systems/nucleus/user/pkg/db/model.go
@@ -19,7 +19,7 @@ type User struct {
 	Id          uuid.UUID `gorm:"primaryKey;type:uuid"`
 	Name        string    `gorm:"not null;default:'unknown'"`
 	Email       string    `gorm:"not null;unique"`
-	Phone       string    `gorm:"unique"`
+	Phone       string
 	Deactivated bool
 	AuthId      uuid.UUID `gorm:"uniqueIndex:authid_unique,where:deleted_at is null;not null;type:uuid"`
 	CreatedAt   time.Time
