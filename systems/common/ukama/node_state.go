f81e1cd4e2d171724060372c55e2c0c7db24a2a2
Bracklaye Cassinga <pcassinga@gmail.com>
2024-10-10 16:33:51 +0200

    node states updated in common with new states

--- systems/common/ukama/node_state.go
+++ systems/common/ukama/node_state.go
@@ -20,8 +20,7 @@ const (
 	Undefined NodeState = iota
 	Onboarded
 	Configured
-	Active
-	Maintenance
+	operational
 	Faulty
 )
 
@@ -36,7 +35,7 @@ func (s NodeState) Value() (driver.Value, error) {
 }
 
 func (s NodeState) String() string {
-	t := map[NodeState]string{0: "undefined", 1: "onboarded", 2: "configured", 3: "active", 4: "maintenance", 5: "faulty"}
+	t := map[NodeState]string{0: "undefined", 1: "onboarded", 2: "configured", 3: "operational", 4: "faulty"}
 
 	v, ok := t[s]
 	if !ok {
@@ -52,7 +51,7 @@ func ParseNodeState(value string) NodeState {
 		return NodeState(i)
 	}
 
-	t := map[string]CdrType{"undefined": 0, "onboarded": 1, "configured": 2, "active": 3, "maintenance": 4, "faulty": 5}
+	t := map[string]CdrType{"undefined": 0, "onboarded": 1, "configured": 2, "operational": 3, "faulty": 4}
 
 	v, ok := t[strings.ToLower(value)]
 	if !ok {
