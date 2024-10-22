86847bd95f828aa552f54d8c5abfcf2f474c7f36
Kashif Ali <kashif@ukama.com>
2024-10-22 16:44:07 -0700

    refactor mkimage.sh to add functions and logging info
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- builder/scripts/mkimage.sh
+++ builder/scripts/mkimage.sh
@@ -9,158 +9,213 @@
 # this script create a bootable image which can then run with QEMU
 # and act as Ukama Node. Image is Ubuntu (20.04) with 5GB HD.
 
-get_bootstrap_port() {
-
-    local repo=$1
-    local service_name="node-gateway-init"
-
-    compose_file="${repo}/systems/init/docker-compose.yml"
-
-    # Extract the port
-    local port=$(grep -A 10 "${service_name}:" "${compose_file}" | \
-                     grep -A 2 ports | awk -F"'" '{print $2}' | cut -d ':' -f 1)
-
-    # Check if port is not empty
-    if [ -z "$port" ]; then
-        echo "Error: Port not found for service ${service_name}"
-        exit 1
-    else
-        # Write the port to the file
-        BOOTSTRAP_PORT=$port
-    fi
-}
-
 set -e  # Exit immediately if a command exits with a non-zero status.
-
-NODE_ID=$1
-UKAMA_REPO=$2
 
 UBUNTU_ISO_URL="https://releases.ubuntu.com/22.04/ubuntu-22.04.4-live-server-amd64.iso"
 ISO_FILE="ubuntu.iso"
-IMG_FILE="$NODE_ID.img"
 IMG_SIZE="5G"
 BOOTSTRAP_PORT=0
+NODE_ID=$1
+UKAMA_REPO=$2
+IMG_FILE="$NODE_ID.img"
 
-# Step 0: make sure have all the right packages
-apt-get update
-apt-get install -y qemu-system \
-        virt-manager \
-        virt-viewer \
-        libvirt-daemon-system \
-        libvirt-clients \
-        bridge-utils \
-        debootstrap \
-        extlinux \
-        kpartx
+log() {
+    local type="$1"
+    local message="$2"
+    local color
 
-# Step 1: Download Ubuntu ISO
-echo "Downloading Ubuntu 22.04 (jammy) ISO..."
-wget --no-check-certificate $UBUNTU_ISO_URL -O $ISO_FILE \
-    || { echo "Failed to download ISO"; exit 1; }
+    case "$type" in
+        "INFO") color="\033[1;34m";;
+        "SUCCESS") color="\033[1;32m";;
+        "ERROR") color="\033[1;31m";;
+        *) color="\033[1;37m";;
+    esac
+    echo -e "${color}${type}: ${message}\033[0m"
+}
 
-# Step 2: Create a Raw Disk Image, format, parition and mount 
-echo "Creating and partitioning disk image..."
-qemu-img create -f raw $IMG_FILE $IMG_SIZE 
-LOOP_DEVICE=$(losetup -fP --show $IMG_FILE)
-sleep 5
-echo -e "o\nn\np\n1\n\n\nw" | fdisk $LOOP_DEVICE
-partprobe $LOOP_DEVICE
-mkfs.ext4 ${LOOP_DEVICE}p1
+check_status() {
 
-# Mount the partition
-mkdir -p /mnt/image
-mount ${LOOP_DEVICE}p1 /mnt/image || { echo "Unable to mount the partition"; exit 1;}
+    if [ $1 -ne 0 ]; then
+        log "ERROR" "Script failed at stage: $3"
+        exit 1
+    fi
+    log "SUCCESS" "$2"
+}
 
-# Step 3: Install Ubuntu on the Disk Image
-echo "Installing Ubuntu on the disk..."
-debootstrap --arch amd64 jammy /mnt/image || { echo "Debootstrap failed"; exit 1; }
+# Function to check if the script is run as root
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        log "ERROR" "This script must be run as root."
+        exit 1
+    fi
+    log "INFO" "Running as root, proceeding..."
+}
 
-# Mounting necessary filesystems and setting up chroot
-mount --bind /dev  /mnt/image/dev
-mount --bind /proc /mnt/image/proc
-mount --bind /sys  /mnt/image/sys
-
-echo "Installing packages on the disk ..."
-
-#setup environment variables
-get_bootstrap_port $UKAMA_REPO
-export BOOTSTRAP_PORT
-export NODE_ID
-
-chroot /mnt/image /bin/bash <<'EOL'
-    set -e	
-    export DEBIAN_FRONTEND=noninteractive
-    locale-gen en_US.UTF-8
-    update-locale LANG=en_US.UTF-8
-    debconf-set-selections <<< "grub-pc grub-pc/install_devices_empty boolean true"
+# Function to install necessary packages
+install_prerequisites() {
+    log "INFO" "Installing necessary packages..."
     apt-get update
-    apt-get install -y -o Dpkg::Options::="--force-confnew" linux-image-generic
+    apt-get install -y qemu-system virt-manager virt-viewer libvirt-daemon-system \
+        libvirt-clients bridge-utils debootstrap extlinux kpartx
+    check_status $? "Packages installed successfully" "install_prerequisites"
+}
 
-    mkdir -p /ukama
-    mkdir -p /ukama/configs
-    mkdir -p /ukama/apps/pkgs
-    mkdir -p /ukama/apps/rootfs
-    mkdir -p /ukama/apps/registry
+# Function to download the Ubuntu ISO
+download_ubuntu_iso() {
+    log "INFO" "Downloading Ubuntu 22.04 (jammy) ISO..."
+    wget --no-check-certificate $UBUNTU_ISO_URL -O $ISO_FILE
+    check_status $? "Ubuntu ISO downloaded" "download_ubuntu_iso"
+}
 
-    echo $NODE_ID > /ukama/nodeid
-    echo "localhost" > /ukama/bootstrap
-    touch /ukama/apps.log
+# Function to create and partition the disk image
+create_disk_image() {
+    log "INFO" "Creating and partitioning disk image..."
+    qemu-img create -f raw $IMG_FILE $IMG_SIZE 
+    check_status $? "Raw disk image created" "create_disk_image"
+    
+    LOOP_DEVICE=$(losetup -fP --show $IMG_FILE)
+    sleep 5
+    echo -e "o\nn\np\n1\n\n\nw" | fdisk $LOOP_DEVICE
+    check_status $? "Disk partitioned" "create_disk_image"
+    
+    partprobe $LOOP_DEVICE
+    mkfs.ext4 ${LOOP_DEVICE}p1
+    check_status $? "Filesystem created" "create_disk_image"
+}
 
-    # create systemd service for the starter.d program
-    cat > /etc/systemd/system/starterd.service << EOF
-    [Unit]
-    Description=Ukama's capp starter.d
-    After=network.target
+# Function to mount the disk image
+mount_disk_image() {
+    log "INFO" "Mounting the disk image..."
+    mkdir -p /mnt/image
+    mount ${LOOP_DEVICE}p1 /mnt/image
+    check_status $? "Disk image mounted" "mount_disk_image"
+}
 
-    [Service]
-    ExecStart=/sbin/starter.d --manifest-file /manifest.json
-    Type=simple
+# Function to install Ubuntu on the image
+install_ubuntu() {
+    log "INFO" "Installing Ubuntu on the disk..."
+    debootstrap --arch amd64 jammy /mnt/image
+    check_status $? "Ubuntu installed" "install_ubuntu"
+}
 
-    [Install]
-    WantedBy=multi-user.target
-    EOF
+# Function to set up the chroot environment and install additional packages
+setup_chroot_environment() {
+    log "INFO" "Setting up chroot environment..."
+    mount --bind /dev  /mnt/image/dev
+    mount --bind /proc /mnt/image/proc
+    mount --bind /sys  /mnt/image/sys
 
-    # Enable the service
-    systemctl enable starterd.service
+    log "INFO" "Installing packages inside the chroot environment..."
+    get_bootstrap_port $UKAMA_REPO
+    export BOOTSTRAP_PORT
+    export NODE_ID
 
+    chroot /mnt/image /bin/bash <<'EOL'
+        set -e	
+        export DEBIAN_FRONTEND=noninteractive
+        locale-gen en_US.UTF-8
+        update-locale LANG=en_US.UTF-8
+        debconf-set-selections <<< "grub-pc grub-pc/install_devices_empty boolean true"
+        apt-get update
+        apt-get install -y -o Dpkg::Options::="--force-confnew" linux-image-generic
+
+        mkdir -p /ukama /ukama/configs /ukama/apps/pkgs /ukama/apps/rootfs /ukama/apps/registry
+        echo $NODE_ID > /ukama/nodeid
+        echo "localhost" > /ukama/bootstrap
+        touch /ukama/apps.log
+
+        cat > /etc/systemd/system/starterd.service << EOF
+        [Unit]
+        Description=Ukama's capp starter.d
+        After=network.target
+
+        [Service]
+        ExecStart=/sbin/starter.d --manifest-file /manifest.json
+        Type=simple
+
+        [Install]
+        WantedBy=multi-user.target
+        EOF
+
+        systemctl enable starterd.service
 EOL
 
-# Unmount filesystems
-umount /mnt/image/dev /mnt/image/proc /mnt/image/sys
+    check_status $? "Chroot environment set up" "setup_chroot_environment"
+}
 
-# Step 4: Set Up  EXTLINUX as Bootloader and create the cfg file
-echo "Setting up EXTLINUX..."
-extlinux --install /mnt/image/boot
+# Function to unmount the filesystems
+unmount_filesystems() {
+    log "INFO" "Unmounting filesystems..."
+    umount /mnt/image/dev /mnt/image/proc /mnt/image/sys
+    check_status $? "Filesystems unmounted" "unmount_filesystems"
+}
 
-cat <<EOF > /mnt/image/boot/extlinux.cfg
+# Function to set up EXTLINUX bootloader
+setup_bootloader() {
+    log "INFO" "Setting up EXTLINUX..."
+    extlinux --install /mnt/image/boot
+    check_status $? "EXTLINUX bootloader installed" "setup_bootloader"
+
+    cat <<EOF > /mnt/image/boot/extlinux.cfg
 DEFAULT linux
 LABEL linux
     KERNEL /boot/vmlinuz-$(ls /mnt/image/boot/ | grep vmlinuz | head -n 1)
     APPEND root=${LOOP_DEVICE}p1 ro quiet
 EOF
+    check_status $? "EXTLINUX bootloader configured" "setup_bootloader"
+}
 
-# Unmount and detach loop device
-umount /mnt/image
-losetup -d $LOOP_DEVICE
-rm -f $ISO_FILE
-sleep 5
+# Function to extract kernel and initRAMFS
+extract_kernel_and_initramfs() {
+    log "INFO" "Extracting kernel and initRAMFS from the OS image..."
+    mkdir -p /mnt/${NODE_ID}
+    mount -o loop,offset=$((512*2048)) ${IMG_FILE} /mnt/${NODE_ID}
+    check_status $? "OS image mounted for extraction" "extract_kernel_and_initramfs"
 
-# Step 5: mount the image and extract kenerl and initramFS. This will
-# be needed by QEMU to run the image.
-#
-# QEMU command would be:
-# sudo qemu-system-x86_64 -hda ${IMG_FILE} -m 1024 -kernel ./vmlinuz-5.4.0-26-generic \
-    # -initrd ./initrd.img-5.4.0-26-generic -append "root=/dev/sda1"
-echo "Extracting kernel and initRAMfs from the OS image"
-mkdir -p /mnt/${NODE_ID}
-mount -o loop,offset=$((512*2048)) ${IMG_FILE} /mnt/${NODE_ID}
+    cp /mnt/${NODE_ID}/boot/vmlinuz-* .
+    cp /mnt/${NODE_ID}/boot/initrd.img-* .
+    check_status $? "Kernel and initRAMFS extracted" "extract_kernel_and_initramfs"
 
-cp /mnt/${NODE_ID}/boot/vmlinuz-*    .
-cp /mnt/${NODE_ID}/boot/initrd.img-* .
+    umount /mnt/${NODE_ID}
+    rmdir /mnt/${NODE_ID}
+    check_status $? "Unmounted and cleaned up image extraction" "extract_kernel_and_initramfs"
+}
 
-echo "Cleanup and done!"
-# Unmount the image
-umount /mnt/${NODE_ID}
-rmdir /mnt/${NODE_ID}
+# Function to get the bootstrap port
+get_bootstrap_port() {
+    log "INFO" "Getting bootstrap port..."
+    local repo=$1
+    local service_name="node-gateway-init"
+    local compose_file="${repo}/systems/init/docker-compose.yml"
+
+    # Extract the port
+    local port=$(grep -A 10 "${service_name}:" "${compose_file}" | \
+                     grep -A 2 ports | awk -F"'" '{print $2}' | cut -d ':' -f 1)
+
+    if [ -z "$port" ]; then
+        log "ERROR" "Port not found for service ${service_name}"
+        exit 1
+    else
+        BOOTSTRAP_PORT=$port
+    fi
+    check_status $? "Bootstrap port retrieved" "get_bootstrap_port"
+}
+
+# Main
+check_root
+
+log "INFO" "Starting the script to create Ukama Node Image"
+
+install_prerequisites
+download_ubuntu_iso
+create_disk_image
+mount_disk_image
+install_ubuntu
+setup_chroot_environment
+unmount_filesystems
+setup_bootloader
+extract_kernel_and_initramfs
+
+log "SUCCESS" "Node image creation completed successfully!"
 
 exit 0
