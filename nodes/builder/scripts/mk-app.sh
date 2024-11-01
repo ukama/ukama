e301e5a04b96fbef738144bd76ac1febf0316d30
Kashif Ali <kashif@ukama.com>
2024-11-01 16:21:10 -0700

    script to make apps
    Signed-off-by: Kashif Ali <kashif@ukama.com>

---
+++ nodes/builder/scripts/mk-app.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present, Ukama Inc.
+
+# Script to build and package ukamaOS app
+
+# Base parameters
+UKAMA_OS=`realpath ../ukamaOS`
+SYS_ROOT=${UKAMA_OS}/distro/
+SCRIPTS_ROOT=${SYS_ROOT}/scripts/
+
+# Build the app at given src path and cmd
+build_app() {
+
+    CWD=`pwd`
+    SRC=$1
+    CMD=$2
+
+    cd ${SRC} && ${CMD} && cd ${CWD}
+}
+
+# copy all the required lib to rootfs
+copy_all_libs() {
+
+    BIN=$1
+
+    for lib in $(ldd ${BIN} | cut -d '>' -f2 | awk '{print $1}')
+    do
+        if [ -f "${lib}" ]; then
+            cp --parents "${lib}" ${ROOTFS}
+            cp "${lib}" ${ROOTFS}/lib
+        fi
+    done
+}
+
+# main
+ACTION=$1
+case "$ACTION" in
+    "init")
+        rm -rf $2
+        mkdir $2
+        ;;
+    "build")
+	    build_app $3 "$4"
+	    ;;
+    "cp")
+	    cp $2 $3
+	    ;;
+    "exec")
+	    $2
+	    ;;
+    "patchelf")
+	    patchelf --set-rpath /lib $2
+	    ;;
+    "mkdir")
+	    mkdir -p $2
+	    ;;
+    "libs")
+	    copy_all_libs $2
+	    ;;
+    "clean")
+	    rm -rf $2
+	    ;;
+    "pack")
+	    mkdir -p pkgs
+	    tar -czf pkgs/$2 $3
+	    if [ $4 -eq 1 ]
+	    then
+	        rm -rf $3 ${ROOTFS}
+	    fi
+	    ;;
+esac
+
+exit 0
