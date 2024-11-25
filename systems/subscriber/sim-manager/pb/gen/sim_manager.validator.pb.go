90d885473a24ea2f49f50fc9e75d9f74dcf191a8
Elvis Attro <attroelvis@gmail.com>
2024-11-25 12:09:21 +0000

    subscriber/sim-manager: add constraint on simId for ListPackagesForSim.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/subscriber/sim-manager/pb/gen/sim_manager.validator.pb.go
+++ systems/subscriber/sim-manager/pb/gen/sim_manager.validator.pb.go
@@ -164,7 +164,16 @@ func (this *AddPackageRequest) Validate() error {
 func (this *AddPackageResponse) Validate() error {
 	return nil
 }
+
+var _regex_ListPackagesForSimRequest_SimId = regexp.MustCompile(`^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[4][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$`)
+
 func (this *ListPackagesForSimRequest) Validate() error {
+	if !_regex_ListPackagesForSimRequest_SimId.MatchString(this.SimId) {
+		return github_com_mwitkow_go_proto_validators.FieldError("SimId", fmt.Errorf(`value '%v' must be a string conforming to regex "^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[4][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})?$"`, this.SimId))
+	}
+	if this.SimId == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("SimId", fmt.Errorf(`value '%v' must not be an empty string`, this.SimId))
+	}
 	return nil
 }
 func (this *ListPackagesForSimResponse) Validate() error {
