5c9bdd7520ee8fecf5cf98a095100c914e122a2b
Kashif Ali <kashif@ukama.com>
2024-12-03 16:03:06 -0800

    script to setup container and build apps within
    Signed-off-by: Kashif Ali <kashif@ukama.com>

---
+++ builder/docker/apps_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2024-present, Ukama Inc.
+
+# To run within specific container
+
+set -e
+set -x
+
+UKAMA_ROOT=/workspace/
+
+#default branch is main
+APPS=$1
+
+cd "${UKAMA_ROOT}/builder" && make clean && make ALPINE_BUILD=1 all
+
+IFS=',' read -r -a array <<< "${APPS}"
+for app in "${array[@]}"; do
+    "${UKAMA_ROOT}/builder/app_builder" \
+        --create \
+        --config "${UKAMA_ROOT}/builder/configs/${app}.toml"
+done
+
+cd "${UKAMA_ROOT}/builder" && make clean
+
+exit 0
---
+++ builder/docker/apps_setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2024-present, Ukama Inc.
+
+set -e
+set -x
+
+TARGET=$1
+UKAMA_ROOT=$2
+APPS=$3
+
+if [ "$TARGET" = "Darwin" ]; then
+    TARGETPLATFORM="amd64/ubuntu:latest"
+elif [ "$TARGET" = "alpine" ]; then
+    TARGETPLATFORM="alpine:latest"
+else
+    TARGETPLATFORM="ubuntu:latest"
+fi
+
+# Check if the Docker image already exists
+IMAGE_EXISTS=$(docker images -q apps-builder-${TARGETPLATFORM})
+
+if [ -z "$IMAGE_EXISTS" ]; then
+    # Build docker image using local Dockerfile
+    docker build --build-arg TARGETPLATFORM=${TARGETPLATFORM} \
+           -t apps-builder-${TARGETPLATFORM} .
+else
+    echo "Docker image 'apps-builder:latest' already exists. Skipping build."
+fi
+
+# Run the docker to build the apps 
+docker run --privileged \
+       -v ${UKAMA_ROOT}:/workspace \
+       apps-builder-${TARGETPLATFORM}:latest \
+       /bin/bash -c "/workspace/builder/docker/apps_build.sh ${APPS} > /workspace/apps_build.log 2>&1"
+
+# clean up
+docker image rm --force apps-builder-${TARGETPLATFORM}
+
+exit 0
