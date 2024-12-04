768b75ddf331d442cc4133bde6543ac6675b6e69
Salman Naseem <salmannaseem131@gmail.com>
2024-12-04 21:58:45 +0500

    minor script updates

--- builder/scripts/make-sys-for-mac.sh
+++ builder/scripts/make-sys-for-mac.sh
@@ -33,7 +33,7 @@ filter_make_sys() {
                 PATHS+=("notification/distributor" "notification/event-notify" "notification/mailer" "notification/api-gateway")
                 ;;
             "node")
-                PATHS+=("node/configurator" "node/controller" "node/health" "node/node-gateway" "node/software" "node/api-gateway" "node/notify")
+                PATHS+=("node/configurator" "node/controller" "node/health" "node/node-gateway" "node/software" "node/api-gateway" "node/notify" "node/state")
                 ;;
             "init")
                 PATHS+=("init/lookup" "init/api-gateway" "init/node-gateway")
