ddac3a6ede72bd33e4dedba4d15b0bbbc92c274f
Salman Naseem <salmannaseem131@gmail.com>
2025-01-28 15:06:51 +0500

    cleanup

--- testing/services/dummy-node/dnode/pkg/utils/pushEvents.go
+++ testing/services/dummy-node/dnode/pkg/utils/pushEvents.go
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+
 package utils
 
 import (
@@ -20,7 +28,7 @@ const (
 )
 
 const (
-	defaultClusterURL  = "http://localhost:15672"
+	defaultClusterURL  = "http://rabbitmq:15672"
 	defaultVhost       = "%2F"
 	defaultExchange    = "amq.topic"
 	defaultClusterUsr  = "guest"
