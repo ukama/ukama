91dae5aafe72af889e4069e82bf894a8583f6fc8
Salman Naseem <83802574+salman-codewar@users.noreply.github.com>
2025-02-03 18:44:30 +0500

    Dummy node add basic behaviours (#854)
    * dummy node - added node service and api gateway
    
    * dummy node, node service integrated with api gw
    
    * dummy node service names rename
    
    * dummy node publish events added
    
    * minor changes
    
    * added rest push event struct to dummy node
    
    * cleanup
    
    * dummy node workflows added
    
    * sonar scan step removed for dummy node workflow
    
    * made dummy node genaric
    
    * workflows cleanup
    
    * feedback addressed
    
    * feedback addressed
    
    * fixes
    
    * logs improved
    
    * minor chnages
    
    * Update docker-compose.yml
    
    * dummy node updated related to queue config
    
    * cleanup
    
    * REST methods feedback addressed
    
    * registry node make reverted

---
+++ testing/services/dummy-node/api-gateway/pkg/config.go
@@ -0,0 +1,60 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+
+package pkg
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/ukama/ukama/systems/common/config"
+	"github.com/ukama/ukama/systems/common/rest"
+)
+
+type Config struct {
+	config.BaseConfig `mapstructure:",squash"`
+	Server            rest.HttpConfig
+	Services          GrpcEndpoints `mapstructure:"services"`
+	HttpServices      HttpEndpoints `mapstructure:"httpServices"`
+}
+
+type GrpcEndpoints struct {
+	Timeout time.Duration
+	Node    string
+}
+
+type HttpEndpoints struct {
+	Timeout     time.Duration
+	NodeMetrics string
+}
+
+func NewConfig() *Config {
+	defaultCors := cors.DefaultConfig()
+	defaultCors.AllowWildcard = true
+	defaultCors.AllowOrigins = []string{"http://localhost", "https://localhost"}
+
+	return &Config{
+		BaseConfig: config.BaseConfig{
+			DebugMode: false,
+		},
+
+		Services: GrpcEndpoints{
+			Timeout: 10 * time.Second,
+			Node:    "dnode:9090",
+		},
+		HttpServices: HttpEndpoints{
+			Timeout:     3 * time.Second,
+			NodeMetrics: "http://localhost",
+		},
+
+		Server: rest.HttpConfig{
+			Port: 8080,
+			Cors: defaultCors,
+		},
+	}
+}
