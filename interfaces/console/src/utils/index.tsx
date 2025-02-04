be77d37cdf4cc00aabfa25af410420c6c686be8f
Bracklaye Cassinga <pcassinga@gmail.com>
2025-02-04 12:16:48 +0200

    Billing history updated to include PDF invoice downloadable link

--- interfaces/console/src/utils/index.tsx
+++ interfaces/console/src/utils/index.tsx
@@ -452,10 +452,28 @@ const NodeEnumToString = (type: NodeTypeEnum): string => {
   }
 };
 
+const base64ToBlob = (base64: string, contentType = ''): Blob => {
+  const byteCharacters = atob(base64.split(',')[1] || base64);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+    const slice = byteCharacters.slice(offset, offset + 512);
+    const byteNumbers = new Array(slice.length);
+
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    byteArrays.push(new Uint8Array(byteNumbers));
+  }
+
+  return new Blob(byteArrays, { type: contentType });
+};
 export {
   ConfigureStep,
   fileToBase64,
   formatBytes,
+  base64ToBlob,
   formatBytesToMB,
   formatTime,
   getDataPlanUsage,
