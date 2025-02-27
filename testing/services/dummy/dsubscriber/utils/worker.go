5f66b0dac4aee78d8e1ae296fc76fe61f08b7a2c
Salman Naseem <salmannaseem131@gmail.com>
2025-02-27 22:08:12 +0500

    dsubscriber coroutine code improvements

--- testing/services/dummy/dsubscriber/utils/worker.go
+++ testing/services/dummy/dsubscriber/utils/worker.go
@@ -5,7 +5,6 @@
  *
  * Copyright (c) 2023-present, Ukama Inc.
  */
-
 package utils
 
 import (
@@ -23,10 +22,11 @@ const (
 	MIN    = 10
 	NORMAL = 20
 	MAX    = 40
+	STEP   = 1
 )
 
 func Worker(iccid string, updateChan chan pkg.WMessage, initial pkg.WMessage) {
-	mint := 1
+	mint := STEP
 	count := 1
 	nodeId := initial.NodeId
 	cdrClient := initial.CDRClient
@@ -39,12 +39,17 @@ func Worker(iccid string, updateChan chan pkg.WMessage, initial pkg.WMessage) {
 
 	fmt.Printf("Coroutine %s started with: %d, %s, %s\n", iccid, profile, expiry, pkgId)
 
+	runLogic(iccid, nodeId, profile, cdrClient, count, mint)
+
+	ticker := time.NewTicker(STEP * time.Minute)
+	defer ticker.Stop()
+
 	for {
-		usage := 0.0
 		if mint > totalMints {
 			fmt.Printf("Coroutine %s stopping\n", iccid)
 			return
 		}
+
 		select {
 		case msg, ok := <-updateChan:
 			if !ok {
@@ -58,44 +63,48 @@ func Worker(iccid string, updateChan chan pkg.WMessage, initial pkg.WMessage) {
 			diff := time.Until(expiryDate)
 			totalMints = int(math.Round(diff.Minutes()))
 			fmt.Printf("Coroutine %s updated args: %d, %s\n", iccid, profile, expiry)
-		default:
+		case <-ticker.C:
+			runLogic(iccid, nodeId, profile, cdrClient, count, mint)
+			count += 1
+			mint += STEP
 		}
+	}
+}
 
-		if profile == cenums.PROFILE_MIN {
-			usage = MIN + rand.Float64()*(NORMAL-MIN)*0.1
-		} else if profile == cenums.PROFILE_NORMAL {
-			usage = NORMAL + rand.Float64()*(MAX-NORMAL)*0.1
-		} else if profile == cenums.PROFILE_MAX {
-			usage = MAX + rand.Float64()*(MAX-NORMAL)*0.1
+func runLogic(iccid, nodeId string, profile cenums.Profile, cdrClient clients.CDRClient, count, mint int) {
+	usage := 0.0
+	if profile == cenums.PROFILE_MIN {
+		usage = MIN + rand.Float64()*(NORMAL-MIN)*0.1
+	} else if profile == cenums.PROFILE_NORMAL {
+		usage = NORMAL + rand.Float64()*(MAX-NORMAL)*0.1
+	} else if profile == cenums.PROFILE_MAX {
+		usage = MAX + rand.Float64()*(MAX-NORMAL)*0.1
+	}
+
+	if len(iccid) > 4 {
+		iccidInImsi := iccid[4:] //TODO: TEMP logic
+		start := time.Now()
+		end := start.Add(time.Duration(STEP*60) * time.Second)
+		fmt.Printf("Coroutine PostCDR for %s: %d, %d\n", iccid, start.Unix(), end.Unix())
+		err := cdrClient.AddCDR(clients.AddCDRRequest{
+			Session:       uint64(count),
+			Imsi:          iccidInImsi,
+			NodeId:        nodeId,
+			Policy:        "policy",
+			ApnName:       "apn",
+			Ip:            "ip",
+			StartTime:     uint64(start.Unix()),
+			EndTime:       uint64(end.Unix()),
+			LastUpdatedAt: uint64(start.Unix()),
+			TxBytes:       uint64(usage * 1024 * 1024),
+			RxBytes:       uint64(usage * 1024 * 1024),
+			TotalBytes:    uint64(usage * 1024 * 1024),
+		})
+
+		if err != nil {
+			fmt.Printf("Coroutine PostCDR for %s error: %v\n", iccid, err)
 		}
-
-		if len(iccid) > 4 {
-			iccidInImsi := iccid[4:] //TODO: TEMP logic
-
-			err := cdrClient.AddCDR(clients.AddCDRRequest{
-				Session:       uint64(count),
-				Imsi:          iccidInImsi,
-				NodeId:        nodeId,
-				Policy:        "policy",
-				ApnName:       "apn",
-				Ip:            "ip",
-				StartTime:     uint64(time.Now().Unix()),
-				EndTime:       uint64(time.Now().Unix() + 300),
-				LastUpdatedAt: uint64(time.Now().Unix()),
-				TxBytes:       uint64(usage * 1024 * 1024),
-				RxBytes:       uint64(usage * 1024 * 1024),
-				TotalBytes:    uint64(usage * 1024 * 1024),
-			})
-
-			if err != nil {
-				fmt.Printf("Coroutine PostCDR for %s error: %v\n", iccid, err)
-			}
-		} else {
-			fmt.Println("String is too short to remove 4 characters")
-		}
-
-		count += 1
-		mint += 10
-		time.Sleep(10 * time.Minute)
+	} else {
+		fmt.Println("String is too short to remove 4 characters")
 	}
 }
