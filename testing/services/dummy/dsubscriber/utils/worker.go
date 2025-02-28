6ad7b2509a0834e497fa6fae80b84908446bee58
Salman Naseem <salmannaseem131@gmail.com>
2025-02-28 10:59:53 +0500

    Update worker.go

--- testing/services/dummy/dsubscriber/utils/worker.go
+++ testing/services/dummy/dsubscriber/utils/worker.go
@@ -26,16 +26,13 @@ const (
 )
 
 func Worker(iccid string, updateChan chan pkg.WMessage, initial pkg.WMessage) {
-	mint := STEP
+	mint := uint64(STEP)
 	count := 1
 	nodeId := initial.NodeId
 	cdrClient := initial.CDRClient
 	profile := initial.Profile
 	expiry := initial.Expiry
 	pkgId := initial.PackageId
-	expiryDate, _ := time.Parse(time.RFC3339, expiry)
-	diff := time.Until(expiryDate)
-	totalMints := int(math.Round(diff.Minutes()))
 
 	fmt.Printf("Coroutine %s started with: %d, %s, %s\n", iccid, profile, expiry, pkgId)
 
@@ -45,33 +42,34 @@ func Worker(iccid string, updateChan chan pkg.WMessage, initial pkg.WMessage) {
 	defer ticker.Stop()
 
 	for {
-		if mint > totalMints {
-			fmt.Printf("Coroutine %s stopping\n", iccid)
-			return
-		}
-
 		select {
 		case msg, ok := <-updateChan:
 			if !ok {
-				fmt.Printf("Coroutine %s stopping\n", iccid)
+				fmt.Printf("Coroutine %s stopping for status inactive\n", iccid)
 				return
 			}
-			nodeId = msg.NodeId
+			if msg.NodeId != "" {
+				nodeId = msg.NodeId
+			}
 			profile = msg.Profile
 			expiry = msg.Expiry
-			expiryDate, _ := time.Parse(time.RFC3339, expiry)
-			diff := time.Until(expiryDate)
-			totalMints = int(math.Round(diff.Minutes()))
 			fmt.Printf("Coroutine %s updated args: %d, %s\n", iccid, profile, expiry)
 		case <-ticker.C:
 			runLogic(iccid, nodeId, profile, cdrClient, count, mint)
 			count += 1
 			mint += STEP
 		}
+		expiryDate, _ := time.Parse(time.RFC3339, expiry)
+		diff := time.Until(expiryDate)
+		totalMints := uint64(math.Round(diff.Minutes()))
+		if mint > totalMints {
+			fmt.Printf("Coroutine %s stopping for limit reach\n", iccid)
+			return
+		}
 	}
 }
 
-func runLogic(iccid, nodeId string, profile cenums.Profile, cdrClient clients.CDRClient, count, mint int) {
+func runLogic(iccid, nodeId string, profile cenums.Profile, cdrClient clients.CDRClient, count int, mint uint64) {
 	usage := 0.0
 	if profile == cenums.PROFILE_MIN {
 		usage = MIN + rand.Float64()*(NORMAL-MIN)*0.1
