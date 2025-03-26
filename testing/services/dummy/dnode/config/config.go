1378065427103a64e6c027b3b23875db4ace3e10
Salman Naseem <salmannaseem131@gmail.com>
2025-03-26 13:09:57 +0500

    dnode configs added

--- testing/services/dummy/dnode/config/config.go
+++ testing/services/dummy/dnode/config/config.go
@@ -8,11 +8,42 @@
 package config
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 	cenums "github.com/ukama/ukama/testing/common/enums"
+
+	uconf "github.com/ukama/ukama/systems/common/config"
 )
 
 const PORT = 8085
+
+type Config struct {
+	uconf.BaseConfig `mapstructure:",squash"`
+	Port             int `default:"8085"`
+	KpiConfig        NodeKPIs
+	KpiRanges        KPIRanges
+}
+
+type WMessage struct {
+	Kpis     NodeKPIs         `json:"kpis"`
+	NodeId   string           `json:"nodeId"`
+	Profile  cenums.Profile   `json:"profile"`
+	Scenario cenums.SCENARIOS `json:"scenario"`
+}
+
+type NodeKPI struct {
+	Key    string
+	Min    float64
+	Normal float64
+	Max    float64
+	KPI    *prometheus.GaugeVec
+}
+
+type NodeKPIs struct {
+	KPIs []NodeKPI
+}
 
 type Ranges struct {
 	Min    float64 `json:"min"`
@@ -20,7 +51,7 @@ type Ranges struct {
 	Max    float64 `json:"max"`
 }
 
-type Config struct {
+type KPIRanges struct {
 	UnitUptime         Ranges `json:"{min:0,normal:2678400,max:2678400}"`
 	UnitHealth         Ranges `json:"{min:10,normal:80,max:100}"`
 	TrxLteCoreActiveUE Ranges `json:"{min:80,normal:95,max:100}"`
@@ -34,184 +65,207 @@ type Config struct {
 	MemoryUsage        Ranges `json:"{min:10,normal:80,max:100}"`
 	CpuUsage           Ranges `json:"{min:10,normal:80,max:100}"`
 	DiskUsage          Ranges `json:"{min:10,normal:80,max:100}"`
-	TxPower            Ranges `json:"{min:25,normal:31,max:34}"`
+	TxPower            Ranges
 }
 
-type WMessage struct {
-	Kpis     NodeKPIs         `json:"kpis"`
-	NodeId   string           `json:"nodeId"`
-	Profile  cenums.Profile   `json:"profile"`
-	Scenario cenums.SCENARIOS `json:"scenario"`
+func NewConfig() *Config {
+	return &Config{
+		KpiConfig: NodeKPIs{
+			KPIs: []NodeKPI{
+				{
+					Key:    "unit_uptime",
+					Min:    getConfigValue("KPIRANGES_UNITUPTIME_MIN", 0),
+					Normal: getConfigValue("KPIRANGES_UNITUPTIME_NORMAL", 2678400),
+					Max:    getConfigValue("KPIRANGES_UNITUPTIME_MAX", 3678400),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "unit_uptime",
+							Help: "Node uptime",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "unit_health",
+					Min:    getConfigValue("KPIRANGES_UNITHEALTH_MIN", 10),
+					Normal: getConfigValue("KPIRANGES_UNITHEALTH_NORMAL", 80),
+					Max:    getConfigValue("KPIRANGES_UNITHEALTH_MAX", 100),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "unit_health",
+							Help: "Health status of the unit",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "trx_lte_core_active_ue",
+					Min:    getConfigValue("KPIRANGES_TRXLTECOREACTIVEUE_MIN", 80),
+					Normal: getConfigValue("KPIRANGES_TRXLTECOREACTIVEUE_NORMAL", 95),
+					Max:    getConfigValue("KPIRANGES_TRXLTECOREACTIVEUE_MAX", 100),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "trx_lte_core_active_ue",
+							Help: "Active subscriber within the network",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "node_load",
+					Min:    getConfigValue("KPIRANGES_NODELOAD_MIN", 10),
+					Normal: getConfigValue("KPIRANGES_NODELOAD_NORMAL", 80),
+					Max:    getConfigValue("KPIRANGES_NODELOAD_MAX", 100),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "node_load",
+							Help: "Load on the node",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "cellular_uplink",
+					Min:    getConfigValue("KPIRANGES_CELLULARUP_MIN", 2),
+					Normal: getConfigValue("KPIRANGES_CELLULARUP_NORMAL", 5),
+					Max:    getConfigValue("KPIRANGES_CELLULARUP_MAX", 30),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "cellular_uplink",
+							Help: "Cellular uplink",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "cellular_downlink",
+					Min:    getConfigValue("KPIRANGES_CELLULARDOWN_MIN", 8),
+					Normal: getConfigValue("KPIRANGES_CELLULARDOWN_NORMAL", 60),
+					Max:    getConfigValue("KPIRANGES_CELLULARDOWN_MAX", 160),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "cellular_downlink",
+							Help: "Cellular downlink",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "backhaul_uplink",
+					Min:    getConfigValue("KPIRANGES_BACKHAULUP_MIN", 2),
+					Normal: getConfigValue("KPIRANGES_BACKHAULUP_NORMAL", 10),
+					Max:    getConfigValue("KPIRANGES_BACKHAULUP_MAX", 200),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "backhaul_uplink",
+							Help: "Backhaul uplink",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "backhaul_downlink",
+					Min:    getConfigValue("KPIRANGES_BACKHAULDOWN_MIN", 2),
+					Normal: getConfigValue("KPIRANGES_BACKHAULDOWN_NORMAL", 10),
+					Max:    getConfigValue("KPIRANGES_BACKHAULDOWN_MAX", 200),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "backhaul_downlink",
+							Help: "Backhaul downlink",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "backhaul_latency",
+					Min:    getConfigValue("KPIRANGES_BACKHAULLATENCY_MIN", 10),
+					Normal: getConfigValue("KPIRANGES_BACKHAULLATENCY_NORMAL", 800),
+					Max:    getConfigValue("KPIRANGES_BACKHAULLATENCY_MAX", 1000),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "backhaul_latency",
+							Help: "Backhaul latency",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "hwd_load",
+					Min:    getConfigValue("KPIRANGES_HWLOAD_MIN", 10),
+					Normal: getConfigValue("KPIRANGES_HWLOAD_NORMAL", 80),
+					Max:    getConfigValue("KPIRANGES_HWLOAD_MAX", 100),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "hwd_load",
+							Help: "Hardware load",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "memory_usage",
+					Min:    getConfigValue("KPIRANGES_MEMORYUSAGE_MIN", 10),
+					Normal: getConfigValue("KPIRANGES_MEMORYUSAGE_NORMAL", 80),
+					Max:    getConfigValue("KPIRANGES_MEMORYUSAGE_MAX", 100),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "memory_usage",
+							Help: "Memory usage",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "cpu_usage",
+					Min:    getConfigValue("KPIRANGES_CPUUSAGE_MIN", 10),
+					Normal: getConfigValue("KPIRANGES_CPUUSAGE_NORMAL", 80),
+					Max:    getConfigValue("KPIRANGES_CPUUSAGE_MAX", 100),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "cpu_usage",
+							Help: "CPU usage",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "disk_usage",
+					Min:    getConfigValue("KPIRANGES_DISKUSAGE_MIN", 10),
+					Normal: getConfigValue("KPIRANGES_DISKUSAGE_NORMAL", 80),
+					Max:    getConfigValue("KPIRANGES_DISKUSAGE_MAX", 100),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "disk_usage",
+							Help: "Disk usage",
+						},
+						[]string{"nodeid"},
+					),
+				},
+				{
+					Key:    "txpower",
+					Min:    getConfigValue("KPIRANGES_TXPOWER_MIN", 25),
+					Normal: getConfigValue("KPIRANGES_TXPOWER_NORMAL", 31),
+					Max:    getConfigValue("KPIRANGES_TXPOWER_MAX", 34),
+					KPI: prometheus.NewGaugeVec(
+						prometheus.GaugeOpts{
+							Name: "txpower",
+							Help: "Transmit power",
+						},
+						[]string{"nodeid"},
+					),
+				},
+			},
+		},
+	}
 }
 
-type NodeKPI struct {
-	Id     string
-	Key    string
-	Min    float64
-	Normal float64
-	Max    float64
-	KPI    *prometheus.GaugeVec
-}
-
-type NodeKPIs struct {
-	KPIs []NodeKPI
-}
-
-var KPI_CONFIG = NodeKPIs{
-	KPIs: []NodeKPI{
-		{
-			Id:  "UnitUptime",
-			Key: "unit_uptime",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "unit_uptime",
-					Help: "Node uptime",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "UnitHealth",
-			Key: "unit_health",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "unit_health",
-					Help: "Health status of the unit",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "TrxLteCoreActiveUE",
-			Key: "trx_lte_core_active_ue",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "trx_lte_core_active_ue",
-					Help: "Active subscriber within the network",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "NodeLoad",
-			Key: "node_load",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "node_load",
-					Help: "Load on the node",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "CellularUplink",
-			Key: "cellular_uplink",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "cellular_uplink",
-					Help: "Cellular uplink",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "CellularDownlink",
-			Key: "cellular_downlink",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "cellular_downlink",
-					Help: "Cellular downlink",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "BackhaulUplink",
-			Key: "backhaul_uplink",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "backhaul_uplink",
-					Help: "Backhaul uplink",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "BackhaulDownlink",
-			Key: "backhaul_downlink",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "backhaul_downlink",
-					Help: "Backhaul downlink",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "BackhaulLatency",
-			Key: "backhaul_latency",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "backhaul_latency",
-					Help: "Backhaul latency",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "HwdLoad",
-			Key: "hwd_load",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "hwd_load",
-					Help: "Hardware load",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "MemoryUsage",
-			Key: "memory_usage",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "memory_usage",
-					Help: "Memory usage",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "CpuUsage",
-			Key: "cpu_usage",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "cpu_usage",
-					Help: "CPU usage",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "DiskUsage",
-			Key: "disk_usage",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "disk_usage",
-					Help: "Disk usage",
-				},
-				[]string{"nodeid"},
-			),
-		},
-		{
-			Id:  "TxPower",
-			Key: "txpower",
-			KPI: prometheus.NewGaugeVec(
-				prometheus.GaugeOpts{
-					Name: "txpower",
-					Help: "Transmit power",
-				},
-				[]string{"nodeid"},
-			),
-		},
-	},
+func getConfigValue(key string, d float64) float64 {
+	v := os.Getenv(key)
+	if v == "" {
+		return d
+	}
+	if val, err := strconv.ParseFloat(v, 64); err == nil {
+		return val
+	}
+	return d
 }
--- testing/services/dummy/dnode/go.mod
+++ testing/services/dummy/dnode/go.mod
@@ -27,6 +27,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
 	github.com/gin-contrib/cors v1.7.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -41,14 +42,28 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/iamolegga/enviper v1.4.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/loopfz/gadgeto v0.11.4 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -60,21 +75,35 @@ require (
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.19.0 // indirect
 	github.com/streadway/amqp v1.1.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/wI2L/fizz v0.22.0 // indirect
 	github.com/wagslane/go-rabbitmq v0.15.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/arch v0.12.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250204164813-702378808489 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250204164813-702378808489 // indirect
 	google.golang.org/grpc v1.70.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/postgres v1.5.9 // indirect
 	gorm.io/gorm v1.25.12 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
