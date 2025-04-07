5ebbd4c4d397e1657ff61bbddee0d6c9e96367ff
Salman Naseem <salmannaseem131@gmail.com>
2025-04-07 15:22:41 +0500

    notification datetime format

--- interfaces/console/src/components/Layout/Header/index.tsx
+++ interfaces/console/src/components/Layout/Header/index.tsx
@@ -130,7 +130,6 @@ const Header = ({
                 <ManageAccountsIcon />
               </IconButton>
             )}
-            {/* <NotificationSubscription /> */}
             <Alert notifications={notifications} handleAction={handleAction} />
             <AccountPopover />
           </Stack>
