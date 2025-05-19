afdc59baa1cf8e24fe4ae40290045b33dcabdbe6
Elvis Attro <attroelvis@gmail.com>
2025-05-19 22:21:39 +0000

    metrics/api-gateway: update sanitizer worker.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/metrics/api-gateway/pkg/sanitizer/worker.go
+++ systems/metrics/api-gateway/pkg/sanitizer/worker.go
@@ -11,17 +11,63 @@ package sanitizer
 import (
 	"time"
 
+	"github.com/ukama/ukama/systems/common/rest/client/registry"
+	"github.com/ukama/ukama/systems/metrics/api-gateway/pkg"
+
 	log "github.com/sirupsen/logrus"
+	pmetric "github.com/ukama/ukama/systems/common/metrics"
 )
 
+const (
+	NodeSubscribers = "node_subscribers"
+	CountType       = "gauge"
+)
+
+var nodeMetrics = []pmetric.MetricConfig{
+	{
+		Name:   NodeSubscribers,
+		Type:   CountType,
+		Labels: map[string]string{"node_id": "", "site": "", "network": ""},
+		Value:  0,
+	},
+}
+
+type job struct {
+	Id              int
+	metricId        string
+	value           float64
+	task            jobFunc
+	pushGatewayHost string
+	nodeClient      registry.NodeClient
+}
+
+type result struct {
+	Id        int
+	IsSuccess bool
+	Err       error
+}
+
 type metricSanitizer struct {
-	period time.Duration
-	stop   chan bool
+	nodeClient registry.NodeClient
+	period     time.Duration
+	stop       chan bool
+}
+
+func NewMetricSanitizer(registryHost string, period time.Duration) *metricSanitizer {
+	m := &metricSanitizer{
+		nodeClient: registry.NewNodeClient(registryHost),
+		period:     period,
+	}
+
+	m.stop = make(chan bool)
+
+	m.Start()
+
+	return m
 }
 
 func (m *metricSanitizer) Start() {
 	log.Infof("Starting metric sanitizer routine with period %s.", m.period)
-
 	m.monitor()
 }
 
@@ -35,18 +81,60 @@ func (m *metricSanitizer) sanitize() error {
 	return nil
 }
 
-func (p *metricSanitizer) monitor() {
-	t := time.NewTicker(p.period)
+func (m *metricSanitizer) monitor() {
+	t := time.NewTicker(m.period)
 
 	go func() {
 		for {
 			select {
 			case <-t.C:
-				_ = p.sanitize()
-			case <-p.stop:
+				_ = m.sanitize()
+			case <-m.stop:
 				t.Stop()
 				return
 			}
 		}
 	}()
+}
+
+func worker(id int, jobs <-chan job, results chan<- result) {
+	for j := range jobs {
+		log.Info("worker", id, "started  job", j)
+
+		err := j.task(j)
+
+		res := result{
+			Id:        j.Id,
+			Err:       err,
+			IsSuccess: err == nil,
+		}
+
+		log.Info("worker", id, "finished job", j)
+
+		results <- res
+	}
+}
+
+func pushNodeSubscriberMetrics(value float64, labels map[string]string, pushGatewayHost string) error {
+	log.Infof("Collecting and pushing metric to push gateway host: %s", pushGatewayHost)
+
+	return pmetric.CollectAndPushSimMetrics(pushGatewayHost, nodeMetrics,
+		NodeSubscribers, float64(value), labels, pkg.SystemName)
+}
+
+type jobFunc func(job) error
+
+func nodeSubscriberTask(j job) error {
+	nodeDetails, err := j.nodeClient.Get(j.metricId)
+	if err == nil {
+		labels := map[string]string{
+			"node_id": j.metricId,
+			"network": nodeDetails.Site.NetworkId,
+			"site":    nodeDetails.Site.SiteId,
+		}
+
+		err = pushNodeSubscriberMetrics(float64(j.value), labels, j.pushGatewayHost)
+	}
+
+	return err
 }
