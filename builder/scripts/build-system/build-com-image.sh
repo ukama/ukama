6d4ebc31143486765184db813c6a0d73495088c2
Kashif Ali <kashif@ukama.com>
2025-05-28 18:18:27 -0700

    update com image script
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- builder/scripts/build-system/build-com-image.sh
+++ builder/scripts/build-system/build-com-image.sh
@@ -5,12 +5,31 @@
 #
 # Copyright (c) 2025-present, Ukama Inc.
 
+# FIX ME:
+# a. board specific
+# b. parition
+# c. label (fstab)
+#
+
 set -e
+
+PKG_UTILS="$(dirname "$0")/pkg-utils.sh"
+if [ ! -f "$PKG_UTILS" ]; then
+    echo "ERROR: Missing $PKG_UTILS"
+    exit 1
+fi
+source "$PKG_UTILS"
 
 STAGE="init"
 DIR="$(pwd)"
 UKAMA_OS=$(realpath ../../../nodes/ukamaOS)
 UKAMA_ROOT=$(realpath ../../../)
+UKAMA_REPO_APP_PKG="${UKAMA_ROOT}/build/pkgs"
+UKAMA_REPO_LIB_PKG="${UKAMA_ROOT}/build/libs"
+COMMON_CONFIG_FILE="${UKAMA_ROOT}/builder/boards/common.config"
+COM_CONFIG_FILE="${UKAMA_ROOT}/builder/boards/com.config"
+MANIFEST_FILE="manifest.json"
+
 BOOT_MOUNT="/media/boot"
 RECOVERY_MOUNT="/media/recovery"
 PRIMARY_MOUNT="/media/primary"
@@ -20,6 +39,11 @@ DATA_MOUNT="/media/data"
 RAW_IMG="ukama-com-image.img"
 
 ROOTFS_DIR=${UKAMA_ROOT}/builder/scripts/build-system/rootfs 
+UKAMA_APP_PKG="${ROOTFS_DIR}/ukama/apps/pkgs"
+APP_NAMES=()
+ALPINE_URL="http://dl-cdn.alpinelinux.org/alpine"
+ALPINE_VERSION="3.19"
+ALPINE_ARCH="x86_64"
 
 trap cleanup EXIT
 
@@ -58,26 +82,25 @@ cleanup() {
     log "INFO" "Cleanup completed."
 }
 
+check_label() {
+    local dev="$1"
+    local expected="$2"
+
+    actual=$(sudo e2label "$dev")
+    if [[ "$actual" != "$expected" ]]; then
+        log "ERROR" "$dev label mismatch: got '$actual', expected '$expected'"
+        exit 1
+    else
+        log "SUCCESS" "$dev label confirmed: $actual"
+    fi
+}
+
 create_disk_image() {
     STAGE="create_disk_image"
     log "INFO" "Creating a new raw image: ${RAW_IMG}"
     rm -f "${RAW_IMG}"
-    dd if=/dev/zero of="${RAW_IMG}" bs=512 count=0 seek=61120512
+    dd if=/dev/zero of="${RAW_IMG}" bs=512 count=0 seek=67108864
     check_status $? "Raw image created" ${STAGE}
-}
-
-build_firmware() {
-    STAGE="build_firmware"
-    local node=$1
-    local path="${UKAMA_ROOT}/nodes/ukamaOS/firmware"
-    cwd=$(pwd)
-    log "INFO" "Building firmware for Node: ${node}"
-
-    cd "${path}"
-    make clean TARGET="${node}" ROOTFSPATH="${path}/build"
-    make TARGET="${node}" ROOTFSPATH="${path}/build"
-    check_status $? "Firmware (coreboot) build successfull" ${STAGE}
-    cd "${cwd}"
 }
 
 setup_loop_device() {
@@ -190,34 +213,67 @@ set_permissions() {
     check_status $? "Permissions set for passive" ${STAGE}
 }
 
-# Update /etc/fstab based on partition type
+## Update /etc/fstab based on partition type
+#update_fstab() {
+#	PARTITION_TYPE=$1
+#    log "INFO" "Updating /etc/fstab for partition type: ${PARTITION_TYPE}"
+#
+#    if [[ "${PARTITION_TYPE}" == ${PRIMARY_MOUNT} ]]; then
+#        cat <<FSTAB > ${PRIMARY_MOUNT}/etc/fstab
+#proc            /proc        proc    defaults    0 0
+#sysfs           /sys         sysfs   defaults    0 0
+#devpts          /dev/pts     devpts  defaults    0 0
+#tmpfs           /tmp         tmpfs   defaults    0 0
+#/dev/mmcblk1p2  /recovery    auto    ro          0 2
+#/dev/mmcblk1p7  /data        auto    ro          0 2
+#/dev/mmcblk1p6  /passive     auto    ro          0 2
+#/dev/mmcblk1p5  /            auto    errors=remount-ro  0 1
+#/dev/mmcblk1p1  /boot/firmware auto  ro          0 2
+#FSTAB
+#    else
+#        cat <<FSTAB > ${PASSIVE_MOUNT}/etc/fstab
+#proc            /proc        proc    defaults    0 0
+#sysfs           /sys         sysfs   defaults    0 0
+#devpts          /dev/pts     devpts  defaults    0 0
+#tmpfs           /tmp         tmpfs   defaults    0 0
+#/dev/mmcblk1p2  /recovery    auto    ro          0 2
+#/dev/mmcblk1p7  /data        auto    ro          0 2
+#/dev/mmcblk1p5  /passive     auto    ro          0 2
+#/dev/mmcblk1p6  /            auto    errors=remount-ro  0 1
+#/dev/mmcblk1p1  /boot/firmware auto  ro          0 2
+#FSTAB
+#    fi
+#
+#    log "INFO" "${PARTITION_TYPE}/etc/fstab updated successfully."
+#}
+
 update_fstab() {
-	PARTITION_TYPE=$1
+    PARTITION_TYPE=$1
     log "INFO" "Updating /etc/fstab for partition type: ${PARTITION_TYPE}"
 
-    if [[ "${PARTITION_TYPE}" == ${PRIMARY_MOUNT} ]]; then
+    if [[ "${PARTITION_TYPE}" == "${PRIMARY_MOUNT}" ]]; then
         cat <<FSTAB > ${PRIMARY_MOUNT}/etc/fstab
-proc            /proc        proc    defaults    0 0
-sysfs           /sys         sysfs   defaults    0 0
-devpts          /dev/pts     devpts  defaults    0 0
-tmpfs           /tmp         tmpfs   defaults    0 0
-/dev/mmcblk1p2  /recovery    auto    ro          0 2
-/dev/mmcblk1p7  /data        auto    ro          0 2
-/dev/mmcblk1p6  /passive     auto    ro          0 2
-/dev/mmcblk1p5  /            auto    errors=remount-ro  0 1
-/dev/mmcblk1p1  /boot/firmware auto  ro          0 2
+proc              /proc           proc    defaults              0 0
+sysfs             /sys            sysfs   defaults              0 0
+devpts            /dev/pts        devpts  defaults              0 0
+tmpfs             /tmp            tmpfs   defaults              0 0
+LABEL=recovery    /recovery       ext4    ro                    0 2
+LABEL=data        /data           ext4    ro                    0 2
+LABEL=passive     /passive        ext4    ro                    0 2
+LABEL=primary     /               ext4    errors=remount-ro     0 1
+LABEL=boot        /boot/firmware  vfat    ro                    0 2
 FSTAB
     else
         cat <<FSTAB > ${PASSIVE_MOUNT}/etc/fstab
-proc            /proc        proc    defaults    0 0
-sysfs           /sys         sysfs   defaults    0 0
-devpts          /dev/pts     devpts  defaults    0 0
-tmpfs           /tmp         tmpfs   defaults    0 0
-/dev/mmcblk1p2  /recovery    auto    ro          0 2
-/dev/mmcblk1p7  /data        auto    ro          0 2
-/dev/mmcblk1p5  /passive     auto    ro          0 2
-/dev/mmcblk1p6  /            auto    errors=remount-ro  0 1
-/dev/mmcblk1p1  /boot/firmware auto  ro          0 2
+proc              /proc           proc    defaults              0 0
+sysfs             /sys            sysfs   defaults              0 0
+devpts            /dev/pts        devpts  defaults              0 0
+tmpfs             /tmp            tmpfs   defaults              0 0
+LABEL=recovery    /recovery       ext4    ro                    0 2
+LABEL=data        /data           ext4    ro                    0 2
+LABEL=primary     /passive        ext4    ro                    0 2
+LABEL=passive     /               ext4    errors=remount-ro     0 1
+LABEL=boot        /boot/firmware  vfat    ro                    0 2
 FSTAB
     fi
 
@@ -250,7 +306,34 @@ format_partitions
 mount_partition "${DISK}1" "${BOOT_MOUNT}"
 mount_partition "${DISK}5" "${PRIMARY_MOUNT}"
 mount_partition "${DISK}6" "${PASSIVE_MOUNT}"
+
+check_label "/dev/mapper/$(basename ${LOOPDISK})p5" "primary"
+check_label "/dev/mapper/$(basename ${LOOPDISK})p2" "recovery"
+check_label "/dev/mapper/$(basename ${LOOPDISK})p6" "passive"
+check_label "/dev/mapper/$(basename ${LOOPDISK})p7" "data"
+
+# create board specific manifest and cp its pkds/libs
+get_enabled_apps "$COMMON_CONFIG_FILE" "$COM_CONFIG_FILE"
+if [[ ${#APPS[@]} -gt 0 ]]; then
+    log "INFO" "APPS are: ${APPS[@]}"
+else
+    log "ERROR" "APPS not assigned. Exit"
+    unmount_partition "${BOOT_MOUNT}"
+    unmount_partition "${PRIMARY_MOUNT}"
+    unmount_partition "${PASSIVE_MOUNT}"
+    detach_loop_device
+
+    log "ERROR" "Disk image creation unsuccessful"
+    exit 1
+fi
+copy_all_apps "$UKAMA_REPO_APP_PKG" "$UKAMA_APP_PKG"
+copy_required_libs "$UKAMA_REPO_LIB_PKG" "$ROOTFS_DIR"
+create_manifest_file "$MANIFEST_FILE" "${APPS[@]}"
+
 copy_rootfs
+cp -rf "${MANIFEST_FILE}" "${PRIMARY_MOUNT}"
+cp -rf "${MANIFEST_FILE}" "${PASSIVE_MOUNT}"
+rm -rf "${MANIFEST_FILE}"
 set_permissions
 update_fstab "${PRIMARY_MOUNT}"
 update_fstab "${PASSIVE_MOUNT}"
@@ -261,4 +344,3 @@ unmount_partition "${PASSIVE_MOUNT}"
 detach_loop_device
 
 log "SUCCESS" "Disk image creation completed successfully!"
-
