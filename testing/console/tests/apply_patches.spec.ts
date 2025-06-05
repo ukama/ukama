13f930e5056b18b1abc25c2904b760b04a45b9bf
Salman Naseem <salmannaseem131@gmail.com>
2025-06-05 17:49:52 +0500

    trying run multiple test via CI

--- testing/console/tests/apply_patches.spec.ts
+++ testing/console/tests/apply_patches.spec.ts
@@ -40,38 +40,38 @@ test('v1.0.0 - Apply create network patch', async () => {
   await applyCreateNetworkPatch();
 });
 
-test('v1.0.0 - Apply claim sims patch', async () => {
-  await applyClaimSimsPatch();
-});
+// test('v1.0.0 - Apply claim sims patch', async () => {
+//   await applyClaimSimsPatch();
+// });
 
-test('v1.0.0 - Apply rename node patch', async () => {
-  await applyRenameNodePatch();
-});
+// test('v1.0.0 - Apply rename node patch', async () => {
+//   await applyRenameNodePatch();
+// });
 
-test('v1.0.0 - Apply rename site patch', async () => {
-  await applyRenameSitePatch();
-});
+// test('v1.0.0 - Apply rename site patch', async () => {
+//   await applyRenameSitePatch();
+// });
 
-test('v1.0.0 - Apply reset node patch', async () => {
-  await applyResetNodePatch();
-});
+// test('v1.0.0 - Apply reset node patch', async () => {
+//   await applyResetNodePatch();
+// });
 
-test('v1.0.0 - Apply node rf off patch', async () => {
-  await applyNodeRfOffPatch();
-});
+// test('v1.0.0 - Apply node rf off patch', async () => {
+//   await applyNodeRfOffPatch();
+// });
 
-test('v1.0.0 - Apply node rf on patch', async () => {
-  await applyNodeRfOnPatch();
-});
+// test('v1.0.0 - Apply node rf on patch', async () => {
+//   await applyNodeRfOnPatch();
+// });
 
-test('v1.0.0 - Apply create subscriber patch', async () => {
-  await applyCreateSubscriberPatch();
-});
+// test('v1.0.0 - Apply create subscriber patch', async () => {
+//   await applyCreateSubscriberPatch();
+// });
 
-test('v1.0.0 - Apply topup subscriber patch', async () => {
-  await applyTopupSubscriberPatch();
-});
+// test('v1.0.0 - Apply topup subscriber patch', async () => {
+//   await applyTopupSubscriberPatch();
+// });
 
-test('v1.0.0 - Apply onboarding patch', async () => {
-  await applyOnboardingPatch();
-});
+// test('v1.0.0 - Apply onboarding patch', async () => {
+//   await applyOnboardingPatch();
+// });
