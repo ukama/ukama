591e1977621d5b34ac7c504e2b5f13fb3361f502
Salman Naseem <salmannaseem131@gmail.com>
2025-06-05 20:03:51 +0500

    reverted apply patch

--- testing/console/tests/apply_patches.spec.ts
+++ testing/console/tests/apply_patches.spec.ts
@@ -9,58 +9,68 @@
 import { test } from '@playwright/test';
 
 import {
+  applyClaimSimsPatch,
   applyCreateNetworkPatch,
+  applyCreateSubscriberPatch,
+  applyDataPlanCreationPatch,
   applyLoginPatch,
+  applyLogoutPatch,
+  applyNodeRfOffPatch,
+  applyNodeRfOnPatch,
   applyOnboardingPatch,
+  applyRenameNodePatch,
+  applyRenameSitePatch,
+  applyResetNodePatch,
+  applyTopupSubscriberPatch,
 } from '../patches/v1.0.0';
 
 test('v1.0.0 - Apply login patch', async () => {
   await applyLoginPatch();
 });
 
-// test('v1.0.0 - Apply logout patch', async () => {
-//   await applyLogoutPatch();
-// });
+test('v1.0.0 - Apply logout patch', async () => {
+  await applyLogoutPatch();
+});
 
-// test('v1.0.0 - Apply data plan creation patch', async () => {
-//   await applyDataPlanCreationPatch();
-// });
+test('v1.0.0 - Apply data plan creation patch', async () => {
+  await applyDataPlanCreationPatch();
+});
 
 test('v1.0.0 - Apply create network patch', async () => {
   await applyCreateNetworkPatch();
 });
 
-// test('v1.0.0 - Apply claim sims patch', async () => {
-//   await applyClaimSimsPatch();
-// });
+test('v1.0.0 - Apply claim sims patch', async () => {
+  await applyClaimSimsPatch();
+});
 
-// test('v1.0.0 - Apply rename node patch', async () => {
-//   await applyRenameNodePatch();
-// });
+test('v1.0.0 - Apply rename node patch', async () => {
+  await applyRenameNodePatch();
+});
 
-// test('v1.0.0 - Apply rename site patch', async () => {
-//   await applyRenameSitePatch();
-// });
+test('v1.0.0 - Apply rename site patch', async () => {
+  await applyRenameSitePatch();
+});
 
-// test('v1.0.0 - Apply reset node patch', async () => {
-//   await applyResetNodePatch();
-// });
+test('v1.0.0 - Apply reset node patch', async () => {
+  await applyResetNodePatch();
+});
 
-// test('v1.0.0 - Apply node rf off patch', async () => {
-//   await applyNodeRfOffPatch();
-// });
+test('v1.0.0 - Apply node rf off patch', async () => {
+  await applyNodeRfOffPatch();
+});
 
-// test('v1.0.0 - Apply node rf on patch', async () => {
-//   await applyNodeRfOnPatch();
-// });
+test('v1.0.0 - Apply node rf on patch', async () => {
+  await applyNodeRfOnPatch();
+});
 
-// test('v1.0.0 - Apply create subscriber patch', async () => {
-//   await applyCreateSubscriberPatch();
-// });
+test('v1.0.0 - Apply create subscriber patch', async () => {
+  await applyCreateSubscriberPatch();
+});
 
-// test('v1.0.0 - Apply topup subscriber patch', async () => {
-//   await applyTopupSubscriberPatch();
-// });
+test('v1.0.0 - Apply topup subscriber patch', async () => {
+  await applyTopupSubscriberPatch();
+});
 
 test('v1.0.0 - Apply onboarding patch', async () => {
   await applyOnboardingPatch();
