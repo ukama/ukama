63eb0d8d2d885da1ad1da3edeb1b99079675e321
Elvis Attro <attroelvis@gmail.com>
2025-06-29 17:51:52 +0000

    connon/ukama: add units tests for DataUnitsType.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

---
+++ systems/common/ukama/dataunit_type_test.go
@@ -0,0 +1,54 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present, Ukama Inc.
+ */
+
+package ukama_test
+
+import (
+	"testing"
+
+	"github.com/tj/assert"
+	"github.com/ukama/ukama/systems/common/ukama"
+)
+
+func TestDataUnitType(t *testing.T) {
+	t.Run("DataUnitTypeValidString", func(tt *testing.T) {
+		dataUnitType := ukama.ParseDataUnitType("Bytes")
+
+		assert.NotNil(t, dataUnitType)
+		assert.Equal(t, dataUnitType.String(), "Bytes")
+		assert.Equal(t, uint8(dataUnitType), uint8(1))
+		assert.Equal(t, ukama.ReturnDataUnits(dataUnitType), int64(1))
+	})
+
+	t.Run("DataUnitTypeValidNumber", func(tt *testing.T) {
+		dataUnitType := ukama.ParseDataUnitType("2")
+
+		assert.NotNil(t, dataUnitType)
+		assert.Equal(t, uint8(dataUnitType), uint8(2))
+		assert.Equal(t, dataUnitType.String(), "KiloBytes")
+		assert.Equal(t, ukama.ReturnDataUnits(dataUnitType), int64(1))
+	})
+
+	t.Run("DataUnitTypeNonValidString", func(tt *testing.T) {
+		dataUnitType := ukama.ParseDataUnitType("failure")
+
+		assert.NotNil(t, dataUnitType)
+		assert.Equal(t, dataUnitType.String(), "unknown")
+		assert.Equal(t, uint8(dataUnitType), uint8(0))
+		assert.Equal(t, ukama.ReturnDataUnits(dataUnitType), int64(0))
+	})
+
+	t.Run("DataUnitTypeNonValidNumber", func(tt *testing.T) {
+		dataUnitType := ukama.DataUnitType(uint8(10))
+
+		assert.NotNil(t, dataUnitType)
+		assert.Equal(t, dataUnitType.String(), "unknown")
+		assert.Equal(t, uint8(dataUnitType), uint8(10))
+		assert.Equal(t, ukama.ReturnDataUnits(dataUnitType), int64(0))
+	})
+}
