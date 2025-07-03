2128e2072505666c5ef5a752e852feeddba3e6b4
Elvis Attro <attroelvis@gmail.com>
2025-07-03 01:48:18 +0000

    common/ukama: update unit tests for nodeId.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/common/ukama/nodeid_test.go
+++ systems/common/ukama/nodeid_test.go
@@ -15,73 +15,94 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNew(t *testing.T) {
-	ntypes := []string{"HomeNode", "homenode", "HOMENODE", "hnode", "HNODE",
-		"TowerNode", "towernode", "TNODE", "tnode",
-		"AmpNode", "ampnode", "AMPNODE", "ANODE", "anode"}
+func TestNodeId_GetNodeType(t *testing.T) {
+	t.Run("NodeTypeValid", func(tt *testing.T) {
+		ntypes := map[string]string{
+			"HNode":     NODE_ID_TYPE_HOMENODE,
+			"hnode":     NODE_ID_TYPE_HOMENODE,
+			"HOMENODE":  NODE_ID_TYPE_HOMENODE,
+			"Towernode": NODE_ID_TYPE_TOWERNODE,
+			"tnode":     NODE_ID_TYPE_TOWERNODE,
+			"TNODE":     NODE_ID_TYPE_TOWERNODE,
+			"ANode":     NODE_ID_TYPE_AMPNODE,
+			"anode":     NODE_ID_TYPE_AMPNODE,
+			"ANODE":     NODE_ID_TYPE_AMPNODE}
 
-	for _, n := range ntypes {
-		nodeid := NewVirtualNodeId(n)
-
-		_, err := ValidateNodeId(string(nodeid))
-		if err != nil {
-			t.Errorf("Expected Error nil; Got %s", err.Error())
+		for k, v := range ntypes {
+			nodeId := NewVirtualNodeId(k)
+			assert.Equal(tt, v, nodeId.GetNodeType())
 		}
-	}
+	})
+
+	t.Run("NodeTypeValid", func(tt *testing.T) {
+		nodeId := NewVirtualNodeId("some_weird_type")
+		assert.Equal(tt, NODE_ID_TYPE_UNDEFINED, nodeId.GetNodeType())
+	})
 }
 
-func TestValidate(t *testing.T) {
-	nodeid := "UK-SA2156-HNODE-A1-XXXX"
+func TestModuleId_StringLowercase(t *testing.T) {
+	t.Run("NodeTypeValid", func(tt *testing.T) {
+		ntypes := map[string]string{
+			"comv1": MODULE_ID_TYPE_COMP,
+			"trx":   MODULE_ID_TYPE_TRX,
+			"ctrl":  MODULE_ID_TYPE_CTRL,
+			"fe":    MODULE_ID_TYPE_FE,
+			"undef": MODULE_ID_TYPE_UNDEFINED}
 
-	uid, err := ValidateNodeId(string(nodeid))
-	if err != nil {
-		t.Errorf("Expected Error nil; Got %s", err.Error())
-	}
-
-	if !(strings.EqualFold(nodeid, string(uid))) {
-		t.Errorf("Expected  %s;Got %s", strings.ToLower(nodeid), string(uid))
-	}
-
-}
-
-func TestNegativeValidateCase1(t *testing.T) {
-	nodeid := "UK-SA2156"
-
-	_, err := ValidateNodeId(string(nodeid))
-	if err == nil {
-		t.Errorf("Expected Error; Got nil")
-	}
-
-}
-
-func TestNegativeValidateCase2(t *testing.T) {
-	nodeid := "UK-SA2156-CNODE-A1-XXXX"
-
-	_, err := ValidateNodeId(string(nodeid))
-	if err == nil {
-		t.Errorf("Expected Error ; Got nil")
-	}
-}
-
-func TestNodeType(t *testing.T) {
-
-	ntypes := map[string]string{
-		"HNode": NODE_ID_TYPE_HOMENODE,
-		"hnode": NODE_ID_TYPE_HOMENODE,
-		"HOMENODE": NODE_ID_TYPE_HOMENODE,
-		"Towernode": NODE_ID_TYPE_TOWERNODE,
-		"tnode": NODE_ID_TYPE_TOWERNODE,
-		"TNODE": NODE_ID_TYPE_TOWERNODE,
-		"ANode":  NODE_ID_TYPE_AMPNODE,
-		"anode":  NODE_ID_TYPE_AMPNODE,
-		"ANODE":  NODE_ID_TYPE_AMPNODE}
-
-	for k, v := range ntypes {
-		nodeid := NewVirtualNodeId(k)
-
-		res, err := ValidateNodeId(string(nodeid))
-		if assert.NoError(t, err) {
-			assert.Equal(t, v, res.GetNodeType())
+		for k, v := range ntypes {
+			moduleId := NewVirtualModuleId(k)
+			assert.Contains(tt, moduleId.StringLowercase(), v)
 		}
-	}
+	})
+
+	t.Run("NodeTypeValid", func(tt *testing.T) {
+		nodeId := NewVirtualNodeId("some_weird_type")
+		assert.Equal(tt, NODE_ID_TYPE_UNDEFINED, nodeId.GetNodeType())
+	})
+}
+
+func TestGetNodeType(t *testing.T) {
+	t.Run("NodeTypeValid", func(tt *testing.T) {
+		ntypes := map[string]string{
+			"HNode":     NODE_ID_TYPE_HOMENODE,
+			"hnode":     NODE_ID_TYPE_HOMENODE,
+			"HOMENODE":  NODE_ID_TYPE_HOMENODE,
+			"Towernode": NODE_ID_TYPE_TOWERNODE,
+			"tnode":     NODE_ID_TYPE_TOWERNODE,
+			"TNODE":     NODE_ID_TYPE_TOWERNODE,
+			"ANode":     NODE_ID_TYPE_AMPNODE,
+			"anode":     NODE_ID_TYPE_AMPNODE,
+			"ANODE":     NODE_ID_TYPE_AMPNODE}
+
+		for k, v := range ntypes {
+			nodeId := NewVirtualNodeId(k)
+			nodeType := GetNodeType(nodeId.StringLowercase())
+			assert.Equal(tt, v, *nodeType)
+		}
+	})
+}
+
+func TestValidateNodeId(t *testing.T) {
+	t.Run("NodeIdIsValid", func(tt *testing.T) {
+		nodeId := "UK-SA2156-HNODE-A1-XXXX"
+
+		uid, err := ValidateNodeId(string(nodeId))
+
+		assert.NoError(tt, err)
+		assert.Equal(tt, strings.ToLower(nodeId), string(uid))
+	})
+
+	t.Run("NegativeValideCase1", func(tt *testing.T) {
+		nodeId := "UK-SA2156"
+
+		_, err := ValidateNodeId(string(nodeId))
+		assert.Error(tt, err)
+	})
+
+	t.Run("NegativeValideCase2", func(tt *testing.T) {
+		nodeId := "UK-SA2156-CNODE-A1-XXXX"
+
+		_, err := ValidateNodeId(string(nodeId))
+		assert.Error(tt, err)
+	})
 }
