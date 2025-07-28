e10973cdaa09948d498c2875f5a39d16ffa65f45
Elvis Attro <attroelvis@gmail.com>
2025-07-28 16:37:23 +0000

    common/rest: refactor authClient rest client.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/common/rest/client/auth/auth.go
+++ systems/common/rest/client/auth/auth.go
@@ -6,72 +6,71 @@
  * Copyright (c) 2023-present, Ukama Inc.
  */
 
-// TODO: this should be moved under common/rest/client/auth
-package providers
+package auth
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"github.com/ukama/ukama/systems/common/rest"
+
+	"github.com/ukama/ukama/systems/common/rest/client"
+
+	log "github.com/sirupsen/logrus"
 )
 
-const AuthenticateEndpoint = "/v1/auth"
+const AuthEndpoint = "/v1/auth"
 
-type AuthRestClient struct {
-	R   *rest.RestClient
-	Jar *cookiejar.Jar
+type AuthClient interface {
+	AuthenticateUser(c *gin.Context, u string) error
 }
 
-func NewAuthClient(u string, debug bool) (*AuthRestClient, error) {
-	f, err := rest.NewRestClient(u, debug)
-	if err != nil {
-		logrus.Errorf("Can't conncet to %s url. Error %s", u, err.Error())
+type authClient struct {
+	u   *url.URL
+	Jar *cookiejar.Jar
+	R   *client.Resty
+}
 
-		return nil, err
+func NewAuthClient(h string, options ...client.Option) *authClient {
+	u, err := url.Parse(h)
+	if err != nil {
+		log.Fatalf("Can't parse %s url. Error: %v", h, err)
 	}
+
 	jar, err := cookiejar.New(nil)
 	if err != nil {
-		logrus.Errorf("Got error while creating cookie jar %s", err.Error())
-		return nil, err
+		log.Fatalf("Got error while creating cookie jar %v", err)
 	}
-	urlObj, _ := url.Parse(u)
-	jar.SetCookies(urlObj, []*http.Cookie{})
 
-	N := &AuthRestClient{
-		R:   f,
+	jar.SetCookies(u, []*http.Cookie{})
+
+	return &authClient{
+		u:   u,
 		Jar: jar,
+		R:   client.NewResty(options...),
 	}
-
-	return N, nil
 }
 
-func (a *AuthRestClient) AuthenticateUser(c *gin.Context, u string) error {
-	errStatus := &rest.ErrorMessage{}
-	urlObj, _ := url.Parse(u)
-	a.Jar.SetCookies(urlObj, c.Request.Cookies())
+func (a *authClient) AuthenticateUser(c *gin.Context, u string) error {
+	log.Debug("Authenticating user reuest:")
+
+	a.Jar.SetCookies(a.u, c.Request.Cookies())
 
 	a.R.C.Header = c.Request.Header
-	res, err := a.R.C.SetCookieJar(a.Jar).R().
-		SetError(errStatus).
-		Get(u + AuthenticateEndpoint)
-	if err != nil {
-		logrus.Errorf("Got error while authenticating user %s", err.Error())
-		return err
-	}
+	a.R.C = a.R.C.SetCookieJar(a.Jar)
 
-	if res.StatusCode() != http.StatusOK {
-		logrus.Errorf("Got error while authenticating user %s", res.Status())
-		return errors.New(res.String())
+	_, err := a.R.Get(a.u.String() + AuthEndpoint)
+	if err != nil {
+		log.Errorf("AuthenticateUser failure. error: %s", err.Error())
+
+		return fmt.Errorf("authenticateUser failure: %w", err)
 	}
 
 	return nil
 }
 
-func (a *AuthRestClient) MockAuthenticateUser(c *gin.Context, u string) error {
+func (a *authClient) MockAuthenticateUser(c *gin.Context, u string) error {
 	return nil
 }
