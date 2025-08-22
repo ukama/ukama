aed6a56bad628ed24389717aa2d76734f5360d0d
Kashif Ali <kashif@ukama.com>
2025-08-22 00:28:42 -0600

    remove dynamically build header files (version.h / static.h) from gps.d - mistake
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- nodes/apps/gps/static.h
+++
@@ -1,7 +0,0 @@
-#ifndef STATIC
-#ifdef  UNIT_TEST
-#define STATIC
-#else
-#define STATIC static
-#endif
-#endif
--- nodes/apps/gps/version.h
+++
@@ -1,4 +0,0 @@
-#ifndef VERSION_H_
-#define VERSION_H_
-#define VERSION "old_arch-0d2432bf5"
-#endif /* VERSION_H_ */
