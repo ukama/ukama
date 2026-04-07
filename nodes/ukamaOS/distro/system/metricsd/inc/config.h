34ff4e99aa02d4ad1ca42c6115ea1f516abcdb93
Kashif Ali <kashif@ukama.com>
2026-04-08 02:14:38 +0400

    fixer
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- nodes/ukamaOS/distro/system/metricsd/inc/config.h
+++ nodes/ukamaOS/distro/system/metricsd/inc/config.h
@@ -6,42 +6,42 @@
  * Copyright (c) 2021-present, Ukama Inc.
  */
 
-#ifndef SRC_PARSER_CONFIG_H_
-#define SRC_PARSER_CONFIG_H_
+#ifndef CONFIG_H_
+#define CONFIG_H_
 
 #include "metrics.h"
 
-#define MAX_KPI_KEY_NAME_LENGTH         128
-#define TAG_SEP                            "_"
-#define TAG_VERSION                     "version"
-#define TAG_SCRAPING_TIME_PERIOD        "scraping_time_period"
-#define TAG_NODE                        "ukamnode"
-#define TAG_GENERIC                     "generic"
-#define TAG_SYSTEM                      "system"
-#define TAG_CAT_SOC                     "SoC"
-#define TAG_SOURCE_LIST                 "sources"
-#define TAG_SOURCE                      "source"
-#define TAG_AGENT                       "agent"
-#define TAG_URL                         "url"
-#define TAG_RANGE                       "range"
-#define TAG_NAME                        "name"
-#define TAG_EXT                         "ext"
-#define TAG_TABLE                       "stats"
-#define TAG_KPI                         "kpi"
-#define TAG_IOR_LTE_STACK               "IoR-lte-stack"
-#define TAG_IOR_CORE                    "Ior-core"
-#define TAG_SENSORS                     "sensors"
-#define TAG_CPU                         "cpu"
-#define TAG_MEMORY                      "memory"
-#define TAG_NETWORK                     "network"
-#define TAG_DESC                        "desc"
-#define TAG_UNIT                        "unit"
-#define TAG_NUM_LABELS                  "numLabels"
-#define TAG_METRIC_TYPE                 "type"
-#define TAG_LABELS                      "labels"
+#define MAX_KPI_KEY_NAME_LENGTH 128
+#define TAG_SEP                 "_"
+#define TAG_VERSION             "version"
+#define TAG_SCRAPING_TIME_PERIOD "scraping_time_period"
+#define TAG_NODE                "ukamnode"
+#define TAG_GENERIC             "generic"
+#define TAG_SYSTEM              "system"
+#define TAG_CAT_SOC             "SoC"
+#define TAG_SOURCE_LIST         "sources"
+#define TAG_SOURCE              "source"
+#define TAG_AGENT               "agent"
+#define TAG_URL                 "url"
+#define TAG_RANGE               "range"
+#define TAG_NAME                "name"
+#define TAG_EXT                 "ext"
+#define TAG_TABLE               "stats"
+#define TAG_KPI                 "kpi"
+#define TAG_IOR_LTE_STACK       "IoR-lte-stack"
+#define TAG_IOR_CORE            "Ior-core"
+#define TAG_SENSORS             "sensors"
+#define TAG_CPU                 "cpu"
+#define TAG_MEMORY              "memory"
+#define TAG_NETWORK             "network"
+#define TAG_DESC                "desc"
+#define TAG_UNIT                "unit"
+#define TAG_NUM_LABELS          "numLabels"
+#define TAG_METRIC_TYPE         "type"
+#define TAG_LABELS              "labels"
 
-int toml_parse_config(char *cfg, char **version, int *scraping_time_period,
-                      MetricsConfig **pstat_cfg, int *source_count);
-void free_stat_cfg(MetricsConfig *stat_cfg, int count);
+int toml_parse_config(char *cfg, char **version, int *scrapingTimePeriod,
+                      MetricsConfig **pstatCfg, int *sourceCount);
+void free_stat_cfg(MetricsConfig *statCfg, int count);
 
-#endif /* SRC_PARSER_CONFIG_H_ */
+#endif /* CONFIG_H_ */
--- nodes/ukamaOS/distro/system/metricsd/inc/file.h
+++ nodes/ukamaOS/distro/system/metricsd/inc/file.h
@@ -12,23 +12,20 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define MIN_UKDB_OFFSET 0
 #define MAX_UKDB_OFFSET 65536
 
 int file_exist(char *fname);
 int file_open(char *fname, int flags);
-int file_read(void *fname, void *buff, off_t offset, uint16_t size);
-int file_write(void *fname, void *buff, off_t offset, uint16_t size);
 int file_remove(void *fname);
-void file_close();
+void file_close(int fd);
 
-#endif /*FILE_H_*/
-
+#endif /* FILE_H_ */
