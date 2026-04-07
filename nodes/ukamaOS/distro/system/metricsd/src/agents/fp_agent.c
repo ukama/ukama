c3ddebc7a042f2b6e9d81da9c6c7a84a573699c7
Kashif Ali <kashif@ukama.com>
2026-04-08 02:37:21 +0400

    refactor agents
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- nodes/ukamaOS/distro/system/metricsd/src/agents/fp_agent.c
+++ nodes/ukamaOS/distro/system/metricsd/src/agents/fp_agent.c
@@ -7,6 +7,7 @@
  */
 
 #include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "usys_log.h"
@@ -16,107 +17,119 @@
 #include "file.h"
 #include "metrics.h"
 
-/* Check if file exist */
+/* check if file exists */
 int fp_check_for_kpi_source(char *source) {
-  int ret = RETURN_OK;
-  if (!file_exist(source)) {
-    ret = RETURN_NOTOK;
-  }
-  return ret;
+    int ret = RETURN_OK;
+
+    if (!file_exist(source)) {
+        ret = RETURN_NOTOK;
+    }
+
+    return ret;
 }
 
-/* Check if string s2 is prefix + space in string s1
- * on failure it return 0 else a 1.
+/*
+ * check if string s2 is a prefix followed by a space in string s1.
+ * on failure return 0, otherwise return 1.
  */
 int fp_is_prefix(char *s1, char *s2) {
-  int ret = 1;
-  size_t n1 = strlen(s1);
-  size_t n2 = strlen(s2);
-  if (n1 > n2) {
-    for (unsigned int i = 0; i < n2; i++) {
-      if (tolower(s1[i]) != tolower(s2[i])) {
+    int ret     = 1;
+    size_t n1   = strlen(s1);
+    size_t n2   = strlen(s2);
+    unsigned int idx = 0;
+
+    if (n1 > n2) {
+        for (idx = 0; idx < n2; idx++) {
+            if (tolower(s1[idx]) != tolower(s2[idx])) {
+                ret = 0;
+                break;
+            }
+        }
+
+        if ((ret == 1) && (s1[n2] != ' ')) {
+            ret = 0;
+        }
+    } else {
         ret = 0;
-        break;
-      }
     }
-    /* check for space */
-    if ((ret == 1) && (s1[n2] != ' ')) {
-      ret = 0;
-    }
-  } else {
-    ret = 0;
-  }
-  return ret;
+
+    return ret;
 }
 
-/* Parse KPI file */
-KPIData *fp_parse_kpi(KPIConfig *kpi, int count, char *kpi_data) {
-  KPIData *kdata = NULL;
-  for (unsigned int id = 0; id < count; id++) {
+/* parse kpi file line */
+KPIData *fp_parse_kpi(KPIConfig *kpi, int count, char *kpiData) {
+    KPIData *kdata      = NULL;
+    unsigned int idx    = 0;
 
-    /* Check which  KPI metric */
-    if (fp_is_prefix(kpi_data, kpi[id].name)) {
-      usys_log_trace("Match found with KPI %s", kpi[id].name);
-      kdata = calloc(1, sizeof(KPIData));
-      if (kdata) {
-        kdata->kpi = &kpi[id];
-        int dataoffset = strlen(kpi[id].name);
-        kdata->value = atof(&kpi_data[dataoffset]);
-      }
-      break;
+    for (idx = 0; idx < (unsigned int)count; idx++) {
+        if (fp_is_prefix(kpiData, kpi[idx].name)) {
+            int dataOffset = 0;
+
+            usys_log_trace("match found for kpi %s", kpi[idx].name);
+
+            kdata = calloc(1, sizeof(KPIData));
+            if (kdata == NULL) {
+                break;
+            }
+
+            kdata->kpi = &kpi[idx];
+            dataOffset = strlen(kpi[idx].name);
+            kdata->value = atof(&kpiData[dataOffset]);
+            break;
+        }
     }
-  }
-  return kdata;
+
+    return kdata;
 }
 
-/* Read KPI file */
+/* read kpi file */
 int fp_read_kpi_from_file(MetricsCatConfig *stat, metricAddFunc addFunc) {
-  int ret = RETURN_NOTOK;
-  FILE *fp;
-  char *line = NULL;
-  size_t len = 0;
-  size_t read;
+    int ret      = RETURN_NOTOK;
+    FILE *fp     = NULL;
+    char *line   = NULL;
+    size_t len   = 0;
+    ssize_t read = 0;
 
-  /* url is the path of the file */
-  if (fp_check_for_kpi_source(stat->url) != RETURN_OK) {
-    usys_log_error("File %s doesn't exist.", stat->url);
-    return RETURN_NOTOK;
-  }
-
-  /* Open KPI file */
-  fp = fopen(stat->url, "r");
-  if (fp == NULL) {
-    usys_log_error("File %s doesn't exist", stat->url);
-    return RETURN_NOTOK;
-  }
-
-  /* Read KPI entries */
-  while ((read = getline(&line, &len, fp)) != -1) {
-
-    /* Parse KPI data */
-    KPIData *kdata = fp_parse_kpi(stat->kpi, stat->kpiCount, line);
-    if (kdata) {
-
-      /* Add metric  data for prometheus to scrape */
-      ret = addFunc(kdata->kpi, &kdata->value);
-      if (ret) {
-        usys_log_error("Failed to add KPI for %s.", kdata->kpi->fqname);
-        ret = RETURN_NOTOK;
-      } else {
-        usys_log_trace("Added KPI For %s Value %lf.", kdata->kpi->fqname, kdata->value);
-        ret = RETURN_OK;
-      }
-      /* clean */
-      free(kdata);
+    if (fp_check_for_kpi_source(stat->url) != RETURN_OK) {
+        usys_log_error("file %s does not exist", stat->url);
+        return RETURN_NOTOK;
     }
-  }
-  if (fp) {
-    fclose(fp);
-  }
 
-  if (line) {
-    free(line);
-  }
+    fp = fopen(stat->url, "r");
+    if (fp == NULL) {
+        usys_log_error("file %s does not exist", stat->url);
+        return RETURN_NOTOK;
+    }
 
-  return ret;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        KPIData *kdata = NULL;
+
+        (void)read;
+        kdata = fp_parse_kpi(stat->kpi, stat->kpiCount, line);
+        if (kdata == NULL) {
+            continue;
+        }
+
+        ret = addFunc(kdata->kpi, &kdata->value);
+        if (ret != RETURN_OK) {
+            usys_log_error("failed to add kpi for %s", kdata->kpi->fqname);
+            ret = RETURN_NOTOK;
+        } else {
+            usys_log_trace("added kpi %s value %lf", kdata->kpi->fqname,
+                           kdata->value);
+            ret = RETURN_OK;
+        }
+
+        free(kdata);
+    }
+
+    if (fp != NULL) {
+        fclose(fp);
+    }
+
+    if (line != NULL) {
+        free(line);
+    }
+
+    return ret;
 }
--- nodes/ukamaOS/distro/system/metricsd/src/agents/sys_mem.c
+++ nodes/ukamaOS/distro/system/metricsd/src/agents/sys_mem.c
@@ -6,165 +6,165 @@
  * Copyright (c) 2021-present, Ukama Inc.
  */
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "usys_log.h"
 
 #include "metrics.h"
 #include "sys_stat.h"
 
-#define MEM_SOURCE_DDR "ddr"
+#define MEM_SOURCE_DDR  "ddr"
 #define MEM_SOURCE_SWAP "swap"
 
-/* Translate kB to MB */
+/* translate kB to MB */
 static double translate_mem_value(unsigned long long mem) {
-  return ((double)(mem)) / (1024);
+    return ((double)mem) / 1024;
 }
 
-/* Read mem stats */
+/* read mem stats */
 int sys_read_memStat(SysMemMetrics *statMem) {
-  int ret = RETURN_OK;
-  FILE *fp;
-  char line[128];
+    int ret      = RETURN_OK;
+    FILE *fp     = NULL;
+    char line[128];
 
-  if ((fp = fopen(PROC_MEM_STAT, "r")) == NULL) {
-      usys_log_error("Cannot open %s: %s", PROC_MEM_STAT, strerror(errno));
-    return RETURN_NOTOK;
-  }
-  
-  while (fgets(line, sizeof(line), fp) != NULL) {
-
-    /* Read the total amount of memory in kB */
-    if (!strncmp(line, "MemTotal:", 9)) {
-      sscanf(line + 9, "%llu", &statMem->ddr.memTotal);
-    } else if (!strncmp(line, "MemFree:", 8)) {
-      /* Read the amount of free memory in kB */
-      sscanf(line + 8, "%llu", &statMem->ddr.memFree);
-    } else if (!strncmp(line, "MemAvailable:", 13)) {
-      /* Read the amount of available memory in kB */
-      sscanf(line + 13, "%llu", &statMem->ddr.memAvail);
-    } else if (!strncmp(line, "Buffers:", 8)) {
-      /* Read the amount of buffered memory in kB */
-      sscanf(line + 8, "%llu", &statMem->ddr.memBuffer);
-    } else if (!strncmp(line, "Cached:", 7)) {
-      /* Read the amount of cached memory in kB */
-      sscanf(line + 7, "%llu", &statMem->ddr.memCached);
-    } else if (!strncmp(line, "SwapTotal:", 10)) {
-      /* Read the total amount of swap memory in kB */
-      sscanf(line + 10, "%llu", &statMem->swap.total);
-    } else if (!strncmp(line, "SwapFree:", 9)) {
-      /* Read the amount of free swap memory in kB */
-      sscanf(line + 9, "%llu", &statMem->swap.free);
+    if ((fp = fopen(PROC_MEM_STAT, "r")) == NULL) {
+        usys_log_error("cannot open %s: %s", PROC_MEM_STAT,
+                       strerror(errno));
+        return RETURN_NOTOK;
     }
-  }
 
-  /* calculate mem used */
-  statMem->ddr.memUsed = statMem->ddr.memTotal - statMem->ddr.memFree -
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (!strncmp(line, "MemTotal:", 9)) {
+            sscanf(line + 9, "%llu", &statMem->ddr.memTotal);
+        } else if (!strncmp(line, "MemFree:", 8)) {
+            sscanf(line + 8, "%llu", &statMem->ddr.memFree);
+        } else if (!strncmp(line, "MemAvailable:", 13)) {
+            sscanf(line + 13, "%llu", &statMem->ddr.memAvail);
+        } else if (!strncmp(line, "Buffers:", 8)) {
+            sscanf(line + 8, "%llu", &statMem->ddr.memBuffer);
+        } else if (!strncmp(line, "Cached:", 7)) {
+            sscanf(line + 7, "%llu", &statMem->ddr.memCached);
+        } else if (!strncmp(line, "SwapTotal:", 10)) {
+            sscanf(line + 10, "%llu", &statMem->swap.total);
+        } else if (!strncmp(line, "SwapFree:", 9)) {
+            sscanf(line + 9, "%llu", &statMem->swap.free);
+        }
+    }
+
+    statMem->ddr.memUsed = statMem->ddr.memTotal - statMem->ddr.memFree -
                            statMem->ddr.memBuffer - statMem->ddr.memCached;
+    statMem->swap.used = statMem->swap.total - statMem->swap.free;
 
-  /* calculate swap mem used */
-  statMem->swap.used = statMem->swap.total - statMem->swap.free;
+    fclose(fp);
 
-  fclose(fp);
-
-  return ret;
+    return ret;
 }
 
-/* Collect and add ddr mem stats */
+/* collect and add ddr mem stats */
 int sys_mem_ddr_push_stat_to_metric_server(MetricsCatConfig *cfgStat,
-                                           SysMemDDRMetrics *ddr_stat,
+                                           SysMemDDRMetrics *ddrStat,
                                            metricAddFunc addFunc) {
-  int ret = RETURN_OK;
-  unsigned long long val = 0;
+    int ret                     = RETURN_OK;
+    int idx                     = 0;
+    unsigned long long val      = 0;
 
-  /* Start Collecting KPI */
-  for (int idx = 0; idx < (cfgStat->kpiCount); idx++) {
-    KPIConfig *kpi = &(cfgStat->kpi[idx]);
-    if ((kpi) && (kpi->fqname)) {
+    for (idx = 0; idx < cfgStat->kpiCount; idx++) {
+        KPIConfig *kpi = &(cfgStat->kpi[idx]);
 
-      if (strstr(kpi->fqname, "total")) {
-        val = ddr_stat->memTotal;
-      } else if (strstr(kpi->fqname, "used")) {
-        val = ddr_stat->memUsed;
-      } else if (strstr(kpi->fqname, "free")) {
-        val = ddr_stat->memFree;
-      } else {
-        continue;
-      }
+        if ((kpi != NULL) && (kpi->fqname != NULL)) {
+            if (strstr(kpi->fqname, "total")) {
+                val = ddrStat->memTotal;
+            } else if (strstr(kpi->fqname, "used")) {
+                val = ddrStat->memUsed;
+            } else if (strstr(kpi->fqname, "free")) {
+                val = ddrStat->memFree;
+            } else {
+                continue;
+            }
 
-      /* Add KPI to server*/
-      double castVal = translate_mem_value(val);
-      addFunc(kpi, &castVal);
+            {
+                double castVal = translate_mem_value(val);
+                addFunc(kpi, &castVal);
+            }
+        }
     }
-  }
-  return ret;
+
+    return ret;
 }
 
-/* Collect and add swap mem stats */
+/* collect and add swap mem stats */
 int sys_mem_swap_push_stat_to_metric_server(MetricsCatConfig *cfgStat,
                                             SysMemSwapMetrics *swapStat,
                                             metricAddFunc addFunc) {
-  int ret = RETURN_OK;
-  unsigned long long val = 0;
+    int ret                     = RETURN_OK;
+    int idx                     = 0;
+    unsigned long long val      = 0;
 
-  /* Start Collecting KPI */
-  for (int idx = 0; idx < (cfgStat->kpiCount); idx++) {
-    KPIConfig *kpi = &(cfgStat->kpi[idx]);
-    if ((kpi) && (kpi->fqname)) {
+    for (idx = 0; idx < cfgStat->kpiCount; idx++) {
+        KPIConfig *kpi = &(cfgStat->kpi[idx]);
 
-      if (strstr(kpi->fqname, "total")) {
-        val = swapStat->total;
-      } else if (strstr(kpi->fqname, "used")) {
-        val = swapStat->used;
-      } else if (strstr(kpi->fqname, "free")) {
-        val = swapStat->free;
-      } else {
-        continue;
-      }
+        if ((kpi != NULL) && (kpi->fqname != NULL)) {
+            if (strstr(kpi->fqname, "total")) {
+                val = swapStat->total;
+            } else if (strstr(kpi->fqname, "used")) {
+                val = swapStat->used;
+            } else if (strstr(kpi->fqname, "free")) {
+                val = swapStat->free;
+            } else {
+                continue;
+            }
 
-      /* Add KPI to server*/
-      double cast_val = translate_mem_value(val);
-      addFunc(kpi, &cast_val);
+            {
+                double castVal = translate_mem_value(val);
+                addFunc(kpi, &castVal);
+            }
+        }
     }
-  }
-  return ret;
+
+    return ret;
 }
 
-/* Collect memory  stats */
+/* collect memory stats */
 int sys_mem_collect_stat(MetricsCatConfig *cfgStat, metricAddFunc addFunc) {
-  int ret = RETURN_OK;
+    int ret                   = RETURN_OK;
+    SysMemMetrics *memStat    = NULL;
 
-  SysMemMetrics *memStat = calloc(1, sizeof(SysMemMetrics));
-  if (!memStat) {
-    usys_log_error("Failed to allocate memory for mem stat.");
-    return RETURN_NOTOK;
-  }
+    memStat = calloc(1, sizeof(SysMemMetrics));
+    if (memStat == NULL) {
+        usys_log_error("failed to allocate memory for mem stat");
+        return RETURN_NOTOK;
+    }
 
-  if (sys_read_memStat(memStat) != RETURN_OK) {
-    usys_log_error("Failed to read memory stats.");
+    if (sys_read_memStat(memStat) != RETURN_OK) {
+        usys_log_error("failed to read memory stats");
+        free(memStat);
+        return RETURN_NOTOK;
+    }
+
+    if (!strcmp(cfgStat->source, MEM_SOURCE_DDR)) {
+        if (sys_mem_ddr_push_stat_to_metric_server(cfgStat, &memStat->ddr,
+                                                   addFunc) != RETURN_OK) {
+            usys_log_error("failed to add %s memory stats to metric server",
+                           MEM_SOURCE_DDR);
+            ret = RETURN_NOTOK;
+        }
+    } else if (!strcmp(cfgStat->source, MEM_SOURCE_SWAP)) {
+        if (sys_mem_swap_push_stat_to_metric_server(cfgStat, &memStat->swap,
+                                                    addFunc) != RETURN_OK) {
+            usys_log_error("failed to add memory stats %s to metric server",
+                           MEM_SOURCE_SWAP);
+            ret = RETURN_NOTOK;
+        } else {
+            usys_log_error("failed to find memory stats source %s",
+                           cfgStat->source);
+            ret = RETURN_NOTOK;
+        }
+    }
+
     free(memStat);
-    return RETURN_NOTOK;
-  }
 
-  if (!strcmp(cfgStat->source, MEM_SOURCE_DDR)) {
-    if (sys_mem_ddr_push_stat_to_metric_server(cfgStat, &memStat->ddr,
-                                               addFunc) != RETURN_OK) {
-      usys_log_error("Failed to add %s memory stats to metric server.",
-                     MEM_SOURCE_DDR);
-      ret = RETURN_NOTOK;
-    }
-  } else if (!strcmp(cfgStat->source, MEM_SOURCE_SWAP)) {
-    if (sys_mem_swap_push_stat_to_metric_server(cfgStat, &memStat->swap,
-                                                addFunc) != RETURN_OK) {
-      usys_log_error("Failed to add memory stats %s to metric server.",
-                     MEM_SOURCE_SWAP);
-      ret = RETURN_NOTOK;
-    } else {
-      usys_log_error("Failed to find memory stats source  %s.",
-                     cfgStat->source);
-      ret = RETURN_NOTOK;
-    }
-  }
-
-  free(memStat);
-
-  return ret;
+    return ret;
 }
