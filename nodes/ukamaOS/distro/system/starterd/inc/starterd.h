676beee5dfa9a34a7bc4a6b471178cacd8c94914
Kashif Ali <kashif@ukama.com>
2026-04-13 18:50:40 -0600

    change manifest.json default from / to /ukama
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- nodes/ukamaOS/distro/system/starterd/inc/starterd.h
+++ nodes/ukamaOS/distro/system/starterd/inc/starterd.h
@@ -12,7 +12,7 @@
 #include <stdint.h>
 
 #define STARTERD_SERVICE_NAME          "starter"
-#define STARTERD_DEFAULT_MANIFEST_FILE "/manifest.json"
+#define STARTERD_DEFAULT_MANIFEST_FILE "/ukama/manifest.json"
 #define STARTERD_DEFAULT_LOG_PATH      "/ukama/apps.log"
 #define STARTERD_DEFAULT_READY_FILE    "/ukama/init/starter/ready"
 
