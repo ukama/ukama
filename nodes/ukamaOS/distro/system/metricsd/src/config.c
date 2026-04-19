19380546d116b663a0b7a7ca145d948e8b02fbad
Kashif Ali <kashif@ukama.com>
2026-04-19 15:14:32 -0600

    Fix compile warning
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- nodes/ukamaOS/distro/system/metricsd/src/config.c
+++ nodes/ukamaOS/distro/system/metricsd/src/config.c
@@ -210,20 +210,30 @@ static MetricsConfig *alloc_stat_cfg(int count) {
     return calloc(count, sizeof(MetricsConfig));
 }
 
-static void lower_string(char *string) {
+static void copy_lower(char *dst, size_t size, char *src) {
 
-    int idx = 0;
+    size_t i = 0;
 
-    if (string == NULL) {
+    if ((dst == NULL) || (size == 0)) {
         return;
     }
 
-    while (string[idx] != '\0') {
-        if ((string[idx] >= 'A') && (string[idx] <= 'Z')) {
-            string[idx] = string[idx] + 32;
-        }
-        idx++;
+    dst[0] = '\0';
+
+    if (src == NULL) {
+        return;
     }
+
+    while ((src[i] != '\0') && (i < (size - 1))) {
+        if ((src[i] >= 'A') && (src[i] <= 'Z')) {
+            dst[i] = src[i] + 32;
+        } else {
+            dst[i] = src[i];
+        }
+        i++;
+    }
+
+    dst[i] = '\0';
 }
 
 static char *set_fqkname(char *node,
@@ -238,49 +248,59 @@ static char *set_fqkname(char *node,
     char *name                               = NULL;
     char fqkn[MAX_KPI_KEY_NAME_LENGTH]       = {'\0'};
     char extKpiName[MAX_KPI_KEY_NAME_LENGTH] = {'\0'};
+    char nodeName[MAX_KPI_KEY_NAME_LENGTH]   = {'\0'};
+    char catName[MAX_KPI_KEY_NAME_LENGTH]    = {'\0'};
+    char srcName[MAX_KPI_KEY_NAME_LENGTH]    = {'\0'};
+    char kpiName[MAX_KPI_KEY_NAME_LENGTH]    = {'\0'};
+    char unitName[MAX_KPI_KEY_NAME_LENGTH]   = {'\0'};
+    char extName[MAX_KPI_KEY_NAME_LENGTH]    = {'\0'};
+    char srcWithRange[MAX_KPI_KEY_NAME_LENGTH] = {'\0'};
 
-    lower_string(node);
-    lower_string(category);
-    lower_string(source);
-    lower_string(kpi);
-    lower_string(unit);
+    copy_lower(nodeName, sizeof(nodeName), node);
+    copy_lower(catName, sizeof(catName), category);
+    copy_lower(srcName, sizeof(srcName), source);
+    copy_lower(kpiName, sizeof(kpiName), kpi);
+    copy_lower(unitName, sizeof(unitName), unit);
+    copy_lower(extName, sizeof(extName), ext);
 
-    if ((ext != NULL) && (ext[0] != '\0')) {
-        lower_string(ext);
+    if (unitName[0] != '\0') {
+        len = snprintf(extKpiName, sizeof(extKpiName), "%s%s%s",
+                       kpiName, TAG_SEP, unitName);
     } else {
-        ext = NULL;
+        len = snprintf(extKpiName, sizeof(extKpiName), "%s", kpiName);
     }
 
-    if ((unit != NULL) && (unit[0] != '\0')) {
-        snprintf(extKpiName, sizeof(extKpiName), "%s%s%s",
-                 kpi, TAG_SEP, unit);
-    } else {
-        snprintf(extKpiName, sizeof(extKpiName), "%s", kpi);
+    if ((len < 0) || (len >= (int)sizeof(extKpiName))) {
+        return NULL;
     }
 
     if (range < 0) {
-        if (ext != NULL) {
-            len = snprintf(fqkn, sizeof(fqkn), "%s%s%s%s%s%s%s%s%s",
-                           node, TAG_SEP, category, TAG_SEP, source,
-                           TAG_SEP, ext, TAG_SEP, extKpiName);
-        } else {
-            len = snprintf(fqkn, sizeof(fqkn), "%s%s%s%s%s%s%s",
-                           node, TAG_SEP, category, TAG_SEP, source,
-                           TAG_SEP, extKpiName);
-        }
+        len = snprintf(srcWithRange, sizeof(srcWithRange), "%s", srcName);
     } else {
-        if (ext != NULL) {
-            len = snprintf(fqkn, sizeof(fqkn), "%s%s%s%s%s%d%s%s%s%s%s",
-                           node, TAG_SEP, category, TAG_SEP, source,
-                           range, TAG_SEP, ext, TAG_SEP, extKpiName);
-        } else {
-            len = snprintf(fqkn, sizeof(fqkn), "%s%s%s%s%s%d%s%s",
-                           node, TAG_SEP, category, TAG_SEP, source,
-                           range, TAG_SEP, extKpiName);
-        }
+        len = snprintf(srcWithRange, sizeof(srcWithRange), "%s%d",
+                       srcName, range);
     }
 
-    if (len < 0) {
+    if ((len < 0) || (len >= (int)sizeof(srcWithRange))) {
+        return NULL;
+    }
+
+    if (extName[0] != '\0') {
+        len = snprintf(fqkn, sizeof(fqkn), "%s%s%s%s%s%s%s%s%s",
+                       nodeName, TAG_SEP,
+                       catName, TAG_SEP,
+                       srcWithRange, TAG_SEP,
+                       extName, TAG_SEP,
+                       extKpiName);
+    } else {
+        len = snprintf(fqkn, sizeof(fqkn), "%s%s%s%s%s%s%s",
+                       nodeName, TAG_SEP,
+                       catName, TAG_SEP,
+                       srcWithRange, TAG_SEP,
+                       extKpiName);
+    }
+
+    if ((len < 0) || (len >= (int)sizeof(fqkn))) {
         return NULL;
     }
 
