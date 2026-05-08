e333eb5622b5cec0ac1f6cb175c7659a2c8f1a1a
Kashif Ali <kashif@ukama.com>
2026-05-08 13:23:02 -0600

    fix compilation/dependency and other issues
    Signed-off-by: Kashif Ali <kashif@ukama.com>

--- systems/node/site-controller/cmd/server/main.go
+++ systems/node/site-controller/cmd/server/main.go
@@ -5,11 +5,16 @@ import (
 
 	"github.com/num30/config"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"gopkg.in/yaml.v3"
+
 	ccmd "github.com/ukama/ukama/systems/common/cmd"
 	ugrpc "github.com/ukama/ukama/systems/common/grpc"
 	mb "github.com/ukama/ukama/systems/common/msgBusServiceClient"
+	epb "github.com/ukama/ukama/systems/common/pb/gen/events"
 	"github.com/ukama/ukama/systems/common/sql"
 	"github.com/ukama/ukama/systems/common/uuid"
+
 	"github.com/ukama/ukama/systems/node/site-controller/cmd/version"
 	pb "github.com/ukama/ukama/systems/node/site-controller/pb/gen"
 	"github.com/ukama/ukama/systems/node/site-controller/pkg"
@@ -17,39 +22,51 @@ import (
 	"github.com/ukama/ukama/systems/node/site-controller/pkg/db"
 	"github.com/ukama/ukama/systems/node/site-controller/pkg/reconciler"
 	"github.com/ukama/ukama/systems/node/site-controller/pkg/server"
-	"google.golang.org/grpc"
-	"gopkg.in/yaml.v3"
 )
 
 var svcConf *pkg.Config
 
 func main() {
 	ccmd.ProcessVersionArgument(pkg.ServiceName, os.Args, version.Version)
+
 	pkg.InstanceId = os.Getenv("POD_NAME")
+
 	initConfig()
 	siteDb := initDb()
 	runGrpcServer(siteDb)
+
 	log.Infof("Starting %s", pkg.ServiceName)
 }
 
 func initConfig() {
 	svcConf = pkg.NewConfig(pkg.ServiceName)
-	if err := config.NewConfReader(pkg.ServiceName).Read(svcConf); err != nil {
+
+	err := config.NewConfReader(pkg.ServiceName).Read(svcConf)
+	if err != nil {
 		log.Fatal("Error reading config ", err)
 	} else if svcConf.DebugMode {
-		if b, err := yaml.Marshal(svcConf); err == nil {
+		b, err := yaml.Marshal(svcConf)
+		if err == nil {
 			log.Infof("Config:\n%s", string(b))
 		}
 	}
+
 	pkg.IsDebugMode = svcConf.DebugMode
 }
 
 func initDb() sql.Db {
 	log.Infof("Initializing Database")
+
 	d := sql.NewDb(svcConf.DB, svcConf.DebugMode)
-	if err := d.Init(&db.SiteIntent{}, &db.SiteState{}, &db.SiteComponent{}, &db.SiteSwitchPolicy{}); err != nil {
+
+	err := d.Init(&db.SiteIntent{},
+		&db.SiteState{},
+		&db.SiteComponent{},
+		&db.SiteSwitchPolicy{})
+	if err != nil {
 		log.Fatalf("Database initialization failed. Error: %v", err)
 	}
+
 	return d
 }
 
@@ -59,8 +76,7 @@ func runGrpcServer(gormdb sql.Db) {
 		instanceId = uuid.NewV4().String()
 	}
 
-	mbClient := mb.NewMsgBusClient(
-		svcConf.MsgClient.Timeout,
+	mbClient := mb.NewMsgBusClient(svcConf.MsgClient.Timeout,
 		svcConf.OrgName,
 		pkg.SystemName,
 		pkg.ServiceName,
@@ -72,51 +88,64 @@ func runGrpcServer(gormdb sql.Db) {
 		svcConf.MsgClient.ListenQueue,
 		svcConf.MsgClient.PublishQueue,
 		svcConf.MsgClient.RetryCount,
-		svcConf.MsgClient.ListenerRoutes,
-	)
+		svcConf.MsgClient.ListenerRoutes)
 
-	cmdAdapter, err := adapters.NewControllerAdapter(svcConf.Services.Controller, svcConf.Services.Timeout)
+	log.Debugf("MessageBus Client is %+v", mbClient)
+
+	cmdAdapter, err := adapters.NewControllerAdapter(svcConf.Services.Controller,
+		svcConf.Services.Timeout)
 	if err != nil {
 		log.Fatalf("failed to connect controller: %v", err)
 	}
 
-	r := reconciler.New(
-		db.NewIntentRepo(gormdb),
+	r := reconciler.New(db.NewIntentRepo(gormdb),
 		db.NewStateRepo(gormdb),
 		db.NewSwitchPolicyRepo(gormdb),
 		adapters.NewTowerAdapter(cmdAdapter),
 		adapters.NewAmplifierAdapter(cmdAdapter),
-		adapters.NewCNodeAdapter(cmdAdapter),
-	)
+		adapters.NewCNodeAdapter(cmdAdapter))
 
-	srv := server.NewSiteControllerServer(r)
+	siteServer := server.NewSiteControllerServer(r)
+	eventServer := server.NewSiteControllerEventServer(svcConf.OrgName,
+		siteServer,
+		mbClient)
+
 	grpcServer := ugrpc.NewGrpcServer(*svcConf.Grpc, func(s *grpc.Server) {
-		pb.RegisterSiteControllerServiceServer(s, srv)
+		pb.RegisterSiteControllerServiceServer(s, siteServer)
+		epb.RegisterEventNotificationServiceServer(s, eventServer)
 	})
 
 	go grpcServer.StartServer()
 	go msgBusListener(mbClient)
+
 	waitForExit()
 }
 
 func msgBusListener(m mb.MsgBusServiceClient) {
 	if err := m.Register(); err != nil {
-		log.Fatalf("Failed to register to Message Client Service. Error %s", err.Error())
+		log.Fatalf("Failed to register to Message Client Service. Error %s",
+			err.Error())
 	}
+
 	if err := m.Start(); err != nil {
-		log.Fatalf("Failed to start Message Client Service routine for service %s. Error %s", pkg.ServiceName, err.Error())
+		log.Fatalf("Failed to start Message Client Service routine for service %s. Error %s",
+			pkg.ServiceName,
+			err.Error())
 	}
 }
 
 func waitForExit() {
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
+
 	go func() {
 		sig := <-sigs
 		log.Info(sig)
 		done <- true
 	}()
-	log.Debug("awaiting terminate/interrupt signal")
+
+	log.Debug("awaiting terminate/interrrupt signal")
 	<-done
+
 	log.Infof("exiting service %s", pkg.ServiceName)
 }
--- systems/node/site-controller/pkg/config.go
+++ systems/node/site-controller/pkg/config.go
@@ -1,37 +1,53 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2026-present, Ukama Inc.
+ */
+
 package pkg
 
 import (
 	"time"
 
-	"github.com/ukama/ukama/systems/common/config"
-	"github.com/ukama/ukama/systems/common/grpc"
-	"github.com/ukama/ukama/systems/common/msgBusServiceClient"
+	uconf "github.com/ukama/ukama/systems/common/config"
 )
 
 type Config struct {
-	config.BaseConfig `mapstructure:",squash"`
-	DB                *config.Database                    `mapstructure:"db"`
-	Grpc              *grpc.Grpc                          `mapstructure:"grpc"`
-	Queue             config.Queue                        `mapstructure:"queue"`
-	Service           *config.Service                     `mapstructure:"service"`
-	MsgClient         msgBusServiceClient.MsgClientConfig `mapstructure:"msgClient"`
-	Services          GrpcEndpoints                       `mapstructure:"services"`
-	OrgName           string                              `mapstructure:"orgName"`
+	DB                *uconf.Database `default:"{}"`
+	uconf.BaseConfig `mapstructure:",squash"`
+	Grpc             *uconf.Grpc      `default:"{}"`
+	Queue            *uconf.Queue     `default:"{}"`
+	Service          *uconf.Service
+	MsgClient        *uconf.MsgClient `default:"{}"`
+	OrgName          string           `default:"ukama"`
+	Services         GrpcEndpoints
 }
 
 type GrpcEndpoints struct {
-	Timeout    time.Duration
-	Controller string
+	Timeout    time.Duration `default:"3s"`
+	Controller string        `default:"controller:9090"`
 }
 
 func NewConfig(name string) *Config {
 	return &Config{
-		BaseConfig: config.BaseConfig{DebugMode: false},
-		DB:         config.DefaultDatabase(),
-		Grpc:       grpc.DefaultGrpc(),
-		Queue:      *config.DefaultQueue(),
-		Service:    config.LoadServiceHostConfig(name),
-		MsgClient:  *msgBusServiceClient.DefaultMsgClientConfig(),
-		Services:   GrpcEndpoints{Timeout: 3 * time.Second, Controller: "controller:9090"},
+		DB: &uconf.Database{
+			DbName: name,
+		},
+		Service: uconf.LoadServiceHostConfig(name),
+		MsgClient: &uconf.MsgClient{
+			Timeout: 5 * time.Second,
+			ListenerRoutes: []string{
+				"event.cloud.local.{{ .Org}}.node.health.health.create",
+				"event.cloud.local.{{ .Org}}.node.state.state.create",
+				"event.cloud.local.{{ .Org}}.registry.node.node.create",
+				"event.cloud.local.{{ .Org}}.registry.node.node.update",
+			},
+		},
+		Services: GrpcEndpoints{
+			Timeout:    3 * time.Second,
+			Controller: "controller:9090",
+		},
 	}
 }
