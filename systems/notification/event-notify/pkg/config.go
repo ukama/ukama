488cad8348ab61b78868124923d0fe2a1eeaa828
Bracklaye Cassinga <pcassinga@gmail.com>
2026-06-01 11:46:29 +0200

    event-notify: handle OperationCompleted/Failed and fix EventHealthReportStore typo

--- systems/notification/event-notify/pkg/config.go
+++ systems/notification/event-notify/pkg/config.go
@@ -81,12 +81,14 @@ func NewConfig(name string) *Config {
 				evt.EventRoutingKey[evt.EventAccountingSync],
 				evt.EventRoutingKey[evt.EventInvoiceGenerate],
 				evt.EventRoutingKey[evt.EventInvoiceDelete],
-				evt.EventRoutingKey[evt.EventHealthCappStore],
+				evt.EventRoutingKey[evt.EventHealthReportStore],
 				evt.EventRoutingKey[evt.EventNotificationDelete],
 				evt.EventRoutingKey[evt.EventNotificationStore],
 				evt.EventRoutingKey[evt.EventPaymentSuccess],
 				evt.EventRoutingKey[evt.EventPaymentFailed],
 				evt.EventRoutingKey[evt.EventNodeStateTransition],
+				evt.EventRoutingKey[evt.EventOperationCompleted],
+				evt.EventRoutingKey[evt.EventOperationFailed],
 			}},
 	}
 }
