4d8e4603c6b882344ce72daa44f05fc7ede13219
Bracklaye Cassinga <pcassinga@gmail.com>
2026-06-01 08:25:31 +0200

    Operation monitor local srv added to node sys

---
+++ systems/node/operation-monitor/test/integration/integration_test.go
@@ -0,0 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2026-present, Ukama Inc.
+ */
+
+package integration
+
+// TODO: integration tests for RegisterIntent / GetIntent / CancelIntent and
+// for the event-driven completion path (NodeStateChange → operation.completed).
