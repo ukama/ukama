8541bd668eae12ef9d2ec6092771ccb80ccf9a8e
Salman Naseem <salmannaseem131@gmail.com>
2026-06-05 02:16:49 +0500

    Update make-sys-for-mac.sh
    Signed-off-by: Salman Naseem <salmannaseem131@gmail.com>

--- builder/scripts/make-sys-for-mac.sh
+++ builder/scripts/make-sys-for-mac.sh
@@ -46,7 +46,7 @@ filter_make_sys() {
                 PATHS+=("node/configurator" "billing/report" "billing/api-gateway" "billing/collector" )
                 ;;
             "init")
-                PATHS+=("init/lookup" "init/api-gateway" "init/node-gateway" "init/bootstrap")
+                PATHS+=("init/lookup" "init/api-gateway" "init/node-gateway" "init/bootstrap", "init/reflector")
                 ;;
             "inventory")
                 PATHS+=("inventory/accounting" "inventory/component" "inventory/api-gateway")
