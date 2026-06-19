852d5f30ea430c2104ffcdbbdf9f80c5d6d60ff5
Elvis Attro <attroelvis@gmail.com>
2026-06-19 20:29:09 +0000

    messaging/broadcaster: improve error handling.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- systems/messaging/broadcaster/pkg/server/events.go
+++ systems/messaging/broadcaster/pkg/server/events.go
@@ -12,9 +12,10 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ukama/ukama/systems/common/msgbus"
+
 	log "github.com/sirupsen/logrus"
 	evt "github.com/ukama/ukama/systems/common/events"
-	"github.com/ukama/ukama/systems/common/msgbus"
 	epb "github.com/ukama/ukama/systems/common/pb/gen/events"
 )
 
@@ -36,28 +37,31 @@ func NewBroadcasterEventServer(orgName string, s *BroadcasterServer) *Broadcaste
 func (n *BroadcasterEventServer) EventNotification(ctx context.Context, e *epb.Event) (*epb.EventResponse, error) {
 	log.Infof(logMsgRoutingKey, e.RoutingKey, e.Msg)
 	switch e.RoutingKey {
-		case msgbus.PrepareRoute(n.orgName, "event.cloud.local.{{ .Org}}.ukamaagent.asr.policies.publish"):
-			c := evt.EventToEventConfig[evt.EventSiteCreate]
-			msg, err := epb.UnmarshalBroadcasterEvent(e.Msg, c.Name)
-			if err != nil {
-				log.Errorf("Failed to unmarshal broadcaster event: %+v", err)
-				return nil, err
-			}
-			
-			if msg.Type == epb.BroadcastType_NODE_BROADCAST {
-				err = n.s.NodeFeederBroadcast(ctx, msg)
-				if err != nil {
-					log.Errorf("Failed to handle broadcast node feeder event: %+v", err)
-					return nil, err
-				}
-			} else {
-				log.Errorf("No handler broadcast type %s", msg.Type)
-				return nil, fmt.Errorf("no handler broadcast type %s", msg.Type)
-			}
-			return &epb.EventResponse{}, nil
-		default:
-			log.Errorf("No handler routing key %s", e.RoutingKey)
-			return nil, fmt.Errorf("no handler routing key %s", e.RoutingKey)
+	case msgbus.PrepareRoute(n.orgName, "event.cloud.local.{{ .Org}}.ukamaagent.asr.policies.publish"):
+		c := evt.EventToEventConfig[evt.EventSiteCreate]
+		msg, err := epb.UnmarshalBroadcasterEvent(e.Msg, c.Name)
+		if err != nil {
+			log.Errorf("Failed to unmarshal broadcaster event: %+v", err)
+
+			return nil, fmt.Errorf("failed to unmarshal broadcaster event: %w", err)
 		}
 
+		if msg.Type == epb.BroadcastType_NODE_BROADCAST {
+			err = n.s.NodeFeederBroadcast(ctx, msg)
+			if err != nil {
+				log.Errorf("Failed to handle broadcast node feeder event: %+v", err)
+
+				return nil, fmt.Errorf("failed to handle broadcast node feeder event: %w", err)
+			}
+		} else {
+			log.Errorf("No handler for broadcast of type %s", msg.Type)
+
+			return nil, fmt.Errorf("no handler for broadcast of type %s", msg.Type)
+		}
+		return &epb.EventResponse{}, nil
+	default:
+		log.Errorf("No handler for routing key %s", e.RoutingKey)
+
+		return nil, fmt.Errorf("no handler for routing key %s", e.RoutingKey)
+	}
 }
