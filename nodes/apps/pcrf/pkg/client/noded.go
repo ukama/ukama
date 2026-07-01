715b7f127eebf4212d8c02345ccc15609ec4eeb0
Elvis Attro <attroelvis@gmail.com>
2026-07-01 11:18:55 +0000

    nodes/pcrf: fix various linting issues.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- nodes/apps/pcrf/pkg/client/noded.go
+++ nodes/apps/pcrf/pkg/client/noded.go
@@ -9,106 +9,107 @@
 package client
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/url"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"strings"
 
-    log "github.com/sirupsen/logrus"
-    "github.com/ukama/ukama/systems/common/rest"
+	"github.com/ukama/ukama/systems/common/rest"
+
+	log "github.com/sirupsen/logrus"
 )
 
 const NodeEndpoint = "/v1/nodeinfo"
 
 type Version struct {
-    Major int `json:"major"`
-    Minor int `json:"minor"`
+	Major int `json:"major"`
+	Minor int `json:"minor"`
 }
 
 type NodeInfo struct {
-    Id            string  `json:"UUID"`
-    Name          string  `json:"name"`
-    NodeType      int     `json:"type"`
-    PartNumber    string  `json:"partNumber"`
-    Skew          string  `json:"skew"`
-    Mac           string  `json:"mac"`
-    ProdSwVersion Version `json:"prodSwVersion"`
-    SwVersion     Version `json:"swVersion"`
-    AssemblyDate  string  `json:"assemblyDate"`
-    OemName       string  `json:"oemName"`
-    ModuleCount   int     `json:"moduleCount"`
+	Id            string  `json:"UUID"`
+	Name          string  `json:"name"`
+	NodeType      int     `json:"type"`
+	PartNumber    string  `json:"partNumber"`
+	Skew          string  `json:"skew"`
+	Mac           string  `json:"mac"`
+	ProdSwVersion Version `json:"prodSwVersion"`
+	SwVersion     Version `json:"swVersion"`
+	AssemblyDate  string  `json:"assemblyDate"`
+	OemName       string  `json:"oemName"`
+	ModuleCount   int     `json:"moduleCount"`
 }
 
 type Node struct {
-    NodeInfo NodeInfo `json:"nodeInfo"`
+	NodeInfo NodeInfo `json:"nodeInfo"`
 }
 
 type nodedClient struct {
-    u     *url.URL
-    R     *rest.RestClient
-    debug bool
+	u     *url.URL
+	R     *rest.RestClient
+	debug bool
 }
 
 func NewNodedClient(h string, debug bool) (*nodedClient, error) {
-    u, err := url.Parse(strings.TrimRight(h, "/"))
-    if err != nil {
-        log.Errorf("Can't parse %s url. Error: %s", h, err.Error())
-        return nil, err
-    }
+	u, err := url.Parse(strings.TrimRight(h, "/"))
+	if err != nil {
+		log.Errorf("Can't parse %s url. Error: %s", h, err.Error())
+		return nil, err
+	}
 
-    return &nodedClient{
-        u:     u,
-        R:     rest.NewRestyClient(u, debug),
-        debug: debug,
-    }, nil
+	return &nodedClient{
+		u:     u,
+		R:     rest.NewRestyClient(u, debug),
+		debug: debug,
+	}, nil
 }
 
 func (r *nodedClient) GetNodeId() (string, error) {
-    ni, err := r.GetNodeInfo()
-    if err != nil {
-        return "", err
-    }
+	ni, err := r.GetNodeInfo()
+	if err != nil {
+		return "", err
+	}
 
-    if ni.Id == "" {
-        return "", fmt.Errorf("node info missing UUID")
-    }
+	if ni.Id == "" {
+		return "", fmt.Errorf("node info missing UUID")
+	}
 
-    return ni.Id, nil
+	return ni.Id, nil
 }
 
 func (r *nodedClient) GetNodeInfo() (*NodeInfo, error) {
-    var err error
-    var respBody string
-    node := &Node{}
+	var err error
+	var respBody string
+	node := &Node{}
 
-    log.Debugf("Getting NodeInfo from Noded.")
+	log.Debugf("Getting NodeInfo from Noded.")
 
-    resp, err := r.R.C.R().Get(r.u.String() + NodeEndpoint)
-    if err != nil {
-        log.Errorf("Get NodeInfo failure. error: %s", err.Error())
-        return nil, fmt.Errorf("get NodeInfo failure: %w", err)
-    }
+	resp, err := r.R.C.R().Get(r.u.String() + NodeEndpoint)
+	if err != nil {
+		log.Errorf("Get NodeInfo failure. error: %s", err.Error())
+		return nil, fmt.Errorf("get NodeInfo failure: %w", err)
+	}
 
-    if resp.StatusCode() != 200 {
-        respBody = strings.TrimSpace(string(resp.Body()))
-        return nil, fmt.Errorf("node info returned http %d: %s",
-            resp.StatusCode(), respBody)
-    }
+	if resp.StatusCode() != 200 {
+		respBody = strings.TrimSpace(string(resp.Body()))
+		return nil, fmt.Errorf("node info returned http %d: %s",
+			resp.StatusCode(), respBody)
+	}
 
-    err = json.Unmarshal(resp.Body(), node)
-    if err != nil {
-        log.Errorf("Failed to deserialize node info. Error message is: %s",
-            err.Error())
-        return nil, fmt.Errorf("node info deserialization failure: %w", err)
-    }
+	err = json.Unmarshal(resp.Body(), node)
+	if err != nil {
+		log.Errorf("Failed to deserialize node info. Error message is: %s",
+			err.Error())
+		return nil, fmt.Errorf("node info deserialization failure: %w", err)
+	}
 
-    log.Infof("NodeInfo Info: %+v", node)
+	log.Infof("NodeInfo Info: %+v", node)
 
-    return &node.NodeInfo, nil
+	return &node.NodeInfo, nil
 }
 
 type NodedClientAlias nodedClient
 
 func (r *NodedClientAlias) GetNodeId() (string, error) {
-    return (*nodedClient)(r).GetNodeId()
+	return (*nodedClient)(r).GetNodeId()
 }
