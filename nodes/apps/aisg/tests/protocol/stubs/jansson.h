dc68cc0073653ade52e6a57c367a236c7013b69f
Kashif Ali <kashif@ukama.com>
2026-07-01 22:29:11 -0600

    aisg: add protocol test and RET emulator hardware ladders
    Signed-off-by: Kashif Ali <kashif@ukama.com>

---
+++ nodes/apps/aisg/tests/protocol/stubs/jansson.h
@@ -0,0 +1,6 @@
+#ifndef AISG_TEST_STUB_JANSSON_H_
+#define AISG_TEST_STUB_JANSSON_H_
+
+typedef struct json_t json_t;
+
+#endif /* AISG_TEST_STUB_JANSSON_H_ */
---
+++ nodes/apps/aisg/tests/protocol/stubs/usys_services.h
@@ -0,0 +1,8 @@
+#ifndef AISG_TEST_STUB_USYS_SERVICES_H_
+#define AISG_TEST_STUB_USYS_SERVICES_H_
+
+#define SERVICE_AISG_CTRL "aisg-ctrl"
+#define SERVICE_AISG_EMU  "aisg-emu"
+#define SERVICE_AISGD     "aisgd"
+
+#endif /* AISG_TEST_STUB_USYS_SERVICES_H_ */
