cfc2b0f55325ddf0276254067a8886138b7edbfa
Elvis Attro <attroelvis@gmail.com>
2026-08-28 18:01:20 +0000

    testing/lab: updatge test harness.
    Signed-off-by: Elvis Attro <attroelvis@gmail.com>

--- testing/lab/src/bff.c
+++ testing/lab/src/bff.c
@@ -5764,11 +5764,13 @@ static int cleanup_sim_packages_from_bff(bff_client_t *c,
     char vars[ULAB_MAX_QUERY];
     char query[ULAB_MAX_QUERY];
     char package_record_ids[32][ULAB_MAX_ID];
+    int package_in_use[32];
     json_t *root;
     json_t *obj;
     json_t *arr;
     json_t *it;
     json_t *pid;
+    json_t *inuse;
     ulab_error_t qerr;
     size_t i;
     size_t count;
@@ -5813,6 +5815,8 @@ static int cleanup_sim_packages_from_bff(bff_client_t *c,
                 ulab_copy(package_record_ids[count],
                           sizeof(package_record_ids[count]),
                           json_string_value(pid));
+                inuse = it ? json_object_get(it, "is_currently_in_use") : NULL;
+                package_in_use[count] = inuse != NULL && json_is_true(inuse);
                 count++;
             }
         }
@@ -5822,13 +5826,15 @@ static int cleanup_sim_packages_from_bff(bff_client_t *c,
 
     for (i = 0; i < count; i++) {
 
-        n = snprintf(query, sizeof(query),
-                     "mutation { unsetPackageInUseForSim(data: {"
-                     "packageId: \"%s\", simId: \"%s\"}) { packageId } }",
-                     package_record_ids[i], ue->bff_id);
-        if (n >= 0 && (size_t)n < sizeof(query) &&
-            bff_cleanup_call(c, "unsetPackageInUseForSim", query)) {
-            (*failures)++;
+        if (package_in_use[i]) {
+            n = snprintf(query, sizeof(query),
+                         "mutation { unsetPackageInUseForSim(data: {"
+                         "packageId: \"%s\", simId: \"%s\"}) { packageId } }",
+                         package_record_ids[i], ue->bff_id);
+            if (n >= 0 && (size_t)n < sizeof(query) &&
+                bff_cleanup_call(c, "unsetPackageInUseForSim", query)) {
+                (*failures)++;
+            }
         }
 
         n = snprintf(query, sizeof(query),
