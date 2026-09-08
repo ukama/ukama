e8210c78dda332c04610f372290b1ad79094abd4
Salman Naseem <salmannaseem131@gmail.com>
2026-09-08 10:32:50 +0500

    Update pushgatewayMetrics.go

--- systems/common/metrics/pushgatewayMetrics.go
+++ systems/common/metrics/pushgatewayMetrics.go
@@ -175,7 +175,7 @@ func PushMetrics(pushMetricHost string, metrics []MetricConfig, metricJobName st
 			pusher.Collector(m.collector)
 		}
 	}
-	if err := pusher.Add(); err != nil {
+	if err := pusher.Push(); err != nil {
 		log.Errorf("Could not push metrics to Pushgateway: %s", err.Error())
 	}
 }
